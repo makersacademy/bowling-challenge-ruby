94b3dc5f9be6a050cf66fa236dc4cfd922821017
Otis <otisvickersgraver@gmail.com>
2020-10-31 16:53:55 +0000

    implement spare logic

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,4 +1,4 @@
-class BowlingScore
+class Bowling
 
   FRAMES = 10
 
@@ -13,10 +13,13 @@ class BowlingScore
   def total_score
     total = 0
     roll_number = 0
-    FRAMES.times do
+    10.times do
       if strike?(roll_number)
         total += strike_score(roll_number)
         roll_number +=1
+      elsif spare?(roll_number)
+        total += spare_score(roll_number)
+        roll_number +=2
       else
         total += regular_score(roll_number)
         roll_number +=2
@@ -25,15 +28,24 @@ class BowlingScore
     total
   end
 
-  def regular_score(roll_number)
-    @rolls[roll_number] + @rolls[roll_number + 1]
-  end
-
   def strike?(roll_number)
     @rolls[roll_number] == 10
   end
 
   def strike_score(roll_number)
     10 + @rolls[roll_number + 1] + @rolls[roll_number + 2]
+  end
+
+  def spare?(roll_number)
+    @rolls[roll_number] + @rolls[roll_number + 1] == 10
+  end
+
+  def spare_score(roll_number)
+    p roll_number
+   10 + @rolls[roll_number + 2]
+  end
+
+  def regular_score(roll_number)
+    @rolls[roll_number] + @rolls[roll_number + 1]
   end
 end
