8dbd972d80b86f5636e2cd35c9a8dd07908cd71a
Dan Holmes <dh@danielholmes.com>
2020-10-31 11:04:54 +0000

    new feature: next_roll_1

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,11 +1,18 @@
 class Frame
-  def initialize
-    @rolls = []
-    @bonus_rolls = []
+  attr_reader :rolls
+
+  def initialize(rolls: [], bonus_rolls: [], number:)
+    @rolls = rolls
+    @bonus_rolls = bonus_rolls
+    @number = number
   end
 
   def roll(n)
-    @rolls.push(n)
+    if @rolls.length < 2
+      @rolls.push(n)
+    else
+      @bonus_rolls.push(n)
+    end
   end
 
   def pins
@@ -18,5 +25,13 @@ class Frame
 
   def spare?
     pins == 10 && !strike?
+  end
+
+  def next_roll_1(next_frame_1 = nil)
+    if @number == 10
+      @bonus_rolls[0]
+    else
+      next_frame_1.rolls[0]
+    end
   end
 end
--- spec/units/frame_spec.rb
+++ spec/units/frame_spec.rb
@@ -1,5 +1,5 @@
 describe Frame do
-  let(:frame) { Frame.new }
+  let(:frame) { Frame.new(number: 1) }
   describe ".pins" do
     it "Can store two rolls are return their total" do
       frame.roll(3)
@@ -37,6 +37,24 @@ describe Frame do
     it "Knows a strike isn't a spare" do
       frame.roll(10)
       expect(frame).not_to be_spare
+    end
+  end
+
+  describe ".next_roll_1" do
+    context "On standard frame" do
+      it "gets the first roll from the next frame" do
+        standard_frame = Frame.new(rolls: [7, 2], number: 7)
+        next_frame_1 = Frame.new(rolls: [6, 2], number: 8)
+        expect(standard_frame.next_roll_1(next_frame_1)).to eq 6
+      end
+    end
+
+    context "On 10th frame" do
+      it "gets the first bonus roll" do
+        last_frame = Frame.new(rolls: [7, 3], number: 10)
+        last_frame.roll(9)
+        expect(last_frame.next_roll_1).to eq 9
+      end
     end
   end
 end
