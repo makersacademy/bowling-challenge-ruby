d68c76cf78c195d153f71d6f59ba7bfc7ff1c2f6
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-31 18:14:34 +0000

    Refactored, updated test names.  Also added test for #total method to ensure it returns total up to frame given as argument

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -4,14 +4,14 @@ attr_reader :score
 
   def initialize(team)
     @team = team
-    @score = score_card
+    @score = scorecard
   end
 
-  def score_card
+  def scorecard
     {@team => {} }
   end
 
-  def reset_score_card
+  def reset_scorecard
     @score[@team].each do |k,v|
     @score[@team][k] = 0
     end
@@ -22,7 +22,15 @@ attr_reader :score
   end
 
   def total(frame)
-    @score[@team][frame+0.1] + @score[@team][frame+0.2]
+    score_excl_bonus = 0
+    while frame > 0 do
+      score_excl_bonus += (@score[@team][frame+0.1] + @score[@team][frame+0.2])
+        if @score[@team][frame+0.1] == 10 || @score[@team][frame+0.2] == 10
+          score_excl_bonus += @score[@team][frame+1.1] + @score[@team][frame+1.2]
+        end
+        frame -= 1
+    end
+    score_excl_bonus
   end
 
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,40 +1,70 @@
 require 'scorecard'
 
 describe Scorecard do
-let (:new_scorecard) {Scorecard.new("my team")}
-  it 'using the #reset_score_card method should return emptied nested hash scorecard with team name' do
-    new_scorecard.add_roll(1.1, 5)
-    new_scorecard.add_roll(1.2, 3)
-    new_scorecard.add_roll(2.1, 2)
-    new_scorecard.add_roll(2.2, 7)
-    new_scorecard.reset_score_card
-    expect(new_scorecard.score).to eq({"my team"=>{
-      1.1=>0, 1.2=>0, 2.1=>0, 2.2=>0, }})
+  describe '#score and #reset_scorecard' do
+    let (:new_scorecard) {Scorecard.new("my team")}
+      it 'using the #reset_score_card method should return emptied nested hash scorecard with team name' do
+        new_scorecard.add_roll(1.1, 5)
+        new_scorecard.add_roll(1.2, 3)
+        new_scorecard.add_roll(2.1, 2)
+        new_scorecard.add_roll(2.2, 7)
+        new_scorecard.reset_scorecard
+        expect(new_scorecard.score).to eq({"my team"=>{1.1=>0, 1.2=>0, 2.1=>0, 2.2=>0}})
+      end
+
+      it 'entering scores should push to hash' do
+        new_scorecard.add_roll(1.1, 5)
+        new_scorecard.add_roll(1.2, 3)
+        expect(new_scorecard.score).to eq({"my team"=>{1.1=>5, 1.2=>3 }})
+      end
+
+      it 'check that resetting scorecard then adding scores pushes to hash' do
+        new_scorecard.add_roll(1.1, 5)
+        new_scorecard.add_roll(1.2, 3)
+        new_scorecard.add_roll(2.1, 2)
+        new_scorecard.add_roll(2.2, 7)
+        new_scorecard.reset_scorecard
+        new_scorecard.add_roll(1.1, 4)
+        new_scorecard.add_roll(1.2, 1)
+        expect(new_scorecard.score).to eq({"my team"=>{1.1=>4, 1.2=>1, 2.1=>0, 2.2=>0, }})
+     end
   end
 
-  it 'entering scores should push to hash' do
-    new_scorecard.add_roll(1.1, 5)
-    new_scorecard.add_roll(1.2, 3)
-    expect(new_scorecard.score).to eq({"my team"=>{
-      1.1=>5, 1.2=>3 }})
-  end
+  describe '#total' do
+    let (:new_scorecard) {Scorecard.new("my team")}
+    it 'checks that total adds one frame together' do
+      new_scorecard.add_roll(1.1, 5)
+      new_scorecard.add_roll(1.2, 3)
+      expect(new_scorecard.total(1)).to eq(8)
+    end
 
-  it 'check that resetting scorecard then adding scores pushes to hash' do
-    new_scorecard.add_roll(1.1, 5)
-    new_scorecard.add_roll(1.2, 3)
-    new_scorecard.add_roll(2.1, 2)
-    new_scorecard.add_roll(2.2, 7)
-    new_scorecard.reset_score_card
-    new_scorecard.add_roll(1.1, 4)
-    new_scorecard.add_roll(1.2, 1)
-    expect(new_scorecard.score).to eq({"my team"=>{
-      1.1=>4, 1.2=>1, 2.1=>0, 2.2=>0, }})
-  end
+    it 'checks that total adds 4 frames together' do
+      new_scorecard.add_roll(1.1, 5)
+      new_scorecard.add_roll(1.2, 3)
+      new_scorecard.add_roll(2.1, 2)
+      new_scorecard.add_roll(2.2, 4)
+      new_scorecard.add_roll(3.1, 6)
+      new_scorecard.add_roll(3.2, 1)
+      new_scorecard.add_roll(4.1, 2)
+      new_scorecard.add_roll(4.2, 1)
+      expect(new_scorecard.total(4)).to eq(24)
+    end
 
-  it 'check that resetting scorecard then adding scores pushes to hash' do
-    new_scorecard.add_roll(1.1, 5)
-    new_scorecard.add_roll(1.2, 3)
-    expect(new_scorecard.total(1)).to eq(8)
-  end
+    it 'checks that a strike on roll 1 adds bonus points of next frame' do
+      new_scorecard.add_roll(1.1, 10)
+      new_scorecard.add_roll(1.2, 0)
+      new_scorecard.add_roll(2.1, 5)
+      new_scorecard.add_roll(2.2, 3)
+      expect(new_scorecard.total(1)).to eq(18)
+    end
 
-end
+    it 'checks that a strike on roll 2 adds bonus points of next frame' do
+      new_scorecard.add_roll(1.1, 0)
+      new_scorecard.add_roll(1.2, 10)
+      new_scorecard.add_roll(2.1, 8)
+      new_scorecard.add_roll(2.2, 1)
+      expect(new_scorecard.total(1)).to eq(19)
+    end
+
+    end
+  end
