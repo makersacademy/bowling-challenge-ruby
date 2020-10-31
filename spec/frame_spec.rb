9849d3d855c5c126f00f5ca6f11419ae9b153e33
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-31 13:46:24 +0000

    Refactoring: create Frame class that stores its own score in an array and knows if it is a strike or a spare

---
+++ spec/frame_spec.rb
@@ -0,0 +1,44 @@
+require '../lib/frame'
+
+describe Frame do
+
+  let(:frame) { described_class.new }
+
+ describe "#record_roll" do
+   it "stores the roll in an array" do
+    frame.record_roll(5)
+    expect(frame.contents).to eq([5])
+   end
+ end
+
+ describe "#raw_score" do
+   it "returns the score without bonuses" do
+     frame.record_roll(5)
+     frame.record_roll(5)
+     expect(frame.raw_score).to eq(10)
+   end
+ end
+
+ describe "#spare?" do
+   it "returns true if the player rolled a spare" do
+     frame.record_roll(5)
+     frame.record_roll(5)
+     expect(frame.spare?).to eq(true)
+   end
+ end
+
+ describe "#strike?" do
+   it "returns true if the player rolled a strike" do
+     frame.record_roll(10)
+     expect(frame.strike?).to eq(true)
+   end
+ end
+
+ describe "#rolls" do
+   it "returns the number of rolls the player took this framew" do
+     frame.record_roll(10)
+     expect(frame.rolls).to eq(1)
+   end
+ end
+
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,20 +1,21 @@
-require 'game'
+require '../lib/game'
 
 describe Game do
 
   let(:game) { described_class.new }
+  let(:frame) { double :frame}
 
  describe "#record_roll" do
-   it "adds the score of a single roll to the current frame" do
+   it "passes the score of a single roll to the current frame" do
      game.record_roll(5)
-     expect(game.current_frame.sum).to eq(5)
+     expect(frame).to receive(:record_roll)
    end
  end
 
  describe "#running_score" do
-   it "calculates and returns the current score (single frame)" do
-     game.record_roll(7)
-     game.record_roll(2)
+   it "retrieves the current score from the current frame" do
+     allow(game).to receive(:current_frame).and_return(frame)
+     expect(frame).to receive(:raw_score)
      expect(game.running_score).to eq(9)
    end
    it "calculates and returns the current score (multiple frames)" do
@@ -39,19 +40,29 @@ describe Game do
      game.record_roll(0)
      expect(game.running_score).to eq(29)
    end
+   it "correctly applies bonus after multiple strikes" do
+     game.record_roll(10)
+     game.record_roll(10)
+     game.record_roll(10)
+     expect(game.running_score).to eq(30)
+   end
 
  end
 
   describe "#check_current_frame" do
    it "empties @current_frame into @completed_frames if it contains two rolls" do
-     game.record_roll(7)
-     game.record_roll(2)
+     allow(Frame).to receive(:new).and_return(frame)
+     allow(frame).to receive(:rolls).and_return(2)
+     allow(frame).to receive(:contents).and_return([])
+
      expect(game.check_current_frame).to eq([])
-     expect(game.completed_frames).to eq([[7,2]])
+     expect(game.completed_frames).to eq([frame])
    end
    it "empties @current_frame into @completed_frames if it contains a strike" do
-     game.record_roll(10)
-     expect(game.check_current_frame).to eq([])
+    allow(game).to receive(:current_frame).and_return(frame)
+    allow(frame).to receive(:strike?).and_return(true)
+
+    expect(game.check_current_frame).to eq([])
    end
  end
 end
