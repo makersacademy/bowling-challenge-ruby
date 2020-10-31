8631f84cbdcd1c53131b91d4a45426b465e009e3
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-31 10:25:58 +0000

    Refactoring: Making tests clearer and more descriptive

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,9 +1,16 @@
 require 'game'
 
-describe 'Add score' do
-  it 'User can add the score of a single roll and see it displayed' do
-    game = Game.new
-    game.record_roll(5)
-    expect(game.running_score).to eq(5)
+describe 'Bowling scorecard features' do
+
+    it 'can store the score of a single roll and display it for the user' do
+      game = Game.new
+      game.record_roll(5)
+      expect(game.running_score).to eq(5)
+    end
+
+    it 'can store multiple rolls over several frames and display the correct score' do
+        game = Game.new
+        game.record_roll(5)
+        expect(game.running_score).to eq(5)
+    end
   end
-end
