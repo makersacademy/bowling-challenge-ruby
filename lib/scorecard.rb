e60d32f076927b54cd7405f611819c0a5d4cf381
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-31 20:45:43 +0000

    refactoring, all tests pass

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -12,25 +12,48 @@ attr_reader :score
   end
 
   def reset_scorecard
-    @score[@team].each do |k,v|
-    @score[@team][k] = 0
-    end
+    @score[@team].clear
   end
 
   def add_roll(turn, pins)
     @score[@team][turn] = pins
   end
 
+  def strike?(input1, input2)
+    input1 == 10 || input2 == 10
+  end
+
+  def spare?(input1, input2)
+    input1 + input2 == 10 && (input1 != 0 || input2 != 0)
+  end
+
+  def next_frame_nil?(input1, input2)
+    input1 == nil || input2 == nil
+  end
+
+  def sum_a_frame(next_roll_1, next_roll_2)
+    next_roll_1 + next_roll_2
+  end
+
   def total(frame)
-    score_excl_bonus = 0
+    score = 0
     while frame > 0 do
-      score_excl_bonus += (@score[@team][frame+0.1] + @score[@team][frame+0.2])
-        if @score[@team][frame+0.1] == 10 || @score[@team][frame+0.2] == 10
-          score_excl_bonus += @score[@team][frame+1.1] + @score[@team][frame+1.2]
+      this_frame_roll1, this_frame_roll2 = @score[@team][frame+0.1], @score[@team][frame+0.2]
+      next_frame_roll1, next_frame_roll2 = @score[@team][frame+1.1], @score[@team][frame+1.2]
+      score += sum_a_frame(this_frame_roll1, this_frame_roll2)
+        if strike?(this_frame_roll1, this_frame_roll2)
+          unless next_frame_nil?(next_frame_roll1, next_frame_roll2)
+            score += sum_a_frame(next_frame_roll1, next_frame_roll2)
+          end
+        elsif
+          spare?(this_frame_roll1, this_frame_roll2)
+          unless next_frame_roll1 == nil
+            score += next_frame_roll1
+          end
         end
         frame -= 1
     end
-    score_excl_bonus
+    score
   end
 
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -9,7 +9,7 @@ describe Scorecard do
         new_scorecard.add_roll(2.1, 2)
         new_scorecard.add_roll(2.2, 7)
         new_scorecard.reset_scorecard
-        expect(new_scorecard.score).to eq({"my team"=>{1.1=>0, 1.2=>0, 2.1=>0, 2.2=>0}})
+        expect(new_scorecard.score).to eq({"my team"=>{}})
       end
 
       it 'entering scores should push to hash' do
@@ -26,45 +26,102 @@ describe Scorecard do
         new_scorecard.reset_scorecard
         new_scorecard.add_roll(1.1, 4)
         new_scorecard.add_roll(1.2, 1)
-        expect(new_scorecard.score).to eq({"my team"=>{1.1=>4, 1.2=>1, 2.1=>0, 2.2=>0, }})
+        expect(new_scorecard.score).to eq({"my team"=>{1.1=>4, 1.2=>1 }})
      end
   end
 
   describe '#total' do
     let (:new_scorecard) {Scorecard.new("my team")}
-    it 'checks that total adds one frame together' do
-      new_scorecard.add_roll(1.1, 5)
-      new_scorecard.add_roll(1.2, 3)
-      expect(new_scorecard.total(1)).to eq(8)
-    end
+      context 'checking sum without bonuses' do
+        it 'checks that total adds one frame together' do
+          new_scorecard.add_roll(1.1, 5)
+          new_scorecard.add_roll(1.2, 3)
+          expect(new_scorecard.total(1)).to eq(8)
+        end
 
-    it 'checks that total adds 4 frames together' do
-      new_scorecard.add_roll(1.1, 5)
-      new_scorecard.add_roll(1.2, 3)
-      new_scorecard.add_roll(2.1, 2)
-      new_scorecard.add_roll(2.2, 4)
-      new_scorecard.add_roll(3.1, 6)
-      new_scorecard.add_roll(3.2, 1)
-      new_scorecard.add_roll(4.1, 2)
-      new_scorecard.add_roll(4.2, 1)
-      expect(new_scorecard.total(4)).to eq(24)
-    end
-
-    it 'checks that a strike on roll 1 adds bonus points of next frame' do
-      new_scorecard.add_roll(1.1, 10)
-      new_scorecard.add_roll(1.2, 0)
-      new_scorecard.add_roll(2.1, 5)
-      new_scorecard.add_roll(2.2, 3)
-      expect(new_scorecard.total(1)).to eq(18)
-    end
-
-    it 'checks that a strike on roll 2 adds bonus points of next frame' do
-      new_scorecard.add_roll(1.1, 0)
-      new_scorecard.add_roll(1.2, 10)
-      new_scorecard.add_roll(2.1, 8)
-      new_scorecard.add_roll(2.2, 1)
-      expect(new_scorecard.total(1)).to eq(19)
-    end
-
-    end
+        it 'checks that total adds 4 frames together' do
+          new_scorecard.add_roll(1.1, 5)
+          new_scorecard.add_roll(1.2, 3)
+          new_scorecard.add_roll(2.1, 2)
+          new_scorecard.add_roll(2.2, 4)
+          new_scorecard.add_roll(3.1, 6)
+          new_scorecard.add_roll(3.2, 1)
+          new_scorecard.add_roll(4.1, 2)
+          new_scorecard.add_roll(4.2, 1)
+          expect(new_scorecard.total(4)).to eq(24)
+        end
+      end
   end
+
+    context 'checks strike bonus' do
+      let (:new_scorecard) {Scorecard.new("my team")}
+        it 'checks that a strike on roll 1 on frame 1 adds bonus points of next frame' do
+          new_scorecard.add_roll(1.1, 10)
+          new_scorecard.add_roll(1.2, 0)
+          new_scorecard.add_roll(2.1, 5)
+          new_scorecard.add_roll(2.2, 3)
+          expect(new_scorecard.total(1)).to eq(18)
+        end
+
+        it 'checks that a strike on roll 2 of frame 1 adds bonus points of next frame' do
+          new_scorecard.add_roll(1.1, 0)
+          new_scorecard.add_roll(1.2, 10)
+          new_scorecard.add_roll(2.1, 8)
+          new_scorecard.add_roll(2.2, 1)
+          expect(new_scorecard.total(1)).to eq(19)
+        end
+
+        it 'checks that a strike on roll 2 of frame 5 adds bonus points of next frame' do
+          new_scorecard.add_roll(1.1, 0)
+          new_scorecard.add_roll(1.2, 2)
+          new_scorecard.add_roll(2.1, 8)
+          new_scorecard.add_roll(2.2, 1)
+          new_scorecard.add_roll(3.1, 0)
+          new_scorecard.add_roll(3.2, 1)
+          new_scorecard.add_roll(4.1, 8)
+          new_scorecard.add_roll(4.2, 1)
+          new_scorecard.add_roll(5.1, 0)
+          new_scorecard.add_roll(5.2, 10)
+          new_scorecard.add_roll(6.1, 6)
+          new_scorecard.add_roll(6.2, 3)
+          expect(new_scorecard.total(6)).to eq(49)
+        end
+
+        it 'checks that total calculates without subsequent rolls if a strike is rolled on roll 1 of frame 3' do
+          new_scorecard.add_roll(1.1, 0)
+          new_scorecard.add_roll(1.2, 2)
+          new_scorecard.add_roll(2.1, 8)
+          new_scorecard.add_roll(2.2, 1)
+          new_scorecard.add_roll(3.1, 0)
+          new_scorecard.add_roll(3.2, 10)
+          expect(new_scorecard.total(3)).to eq(21)
+        end
+    end
+
+    context 'checks spare bonus' do
+      let (:new_scorecard) {Scorecard.new("my team")}
+      it 'checks that a spare on frame 4 adds bonus points of next frame' do
+        new_scorecard.add_roll(1.1, 0)
+        new_scorecard.add_roll(1.2, 2)
+        new_scorecard.add_roll(2.1, 8)
+        new_scorecard.add_roll(2.2, 1)
+        new_scorecard.add_roll(3.1, 0)
+        new_scorecard.add_roll(3.2, 1)
+        new_scorecard.add_roll(4.1, 8)
+        new_scorecard.add_roll(4.2, 2)
+        new_scorecard.add_roll(5.1, 5)
+        new_scorecard.add_roll(5.2, 3)
+        new_scorecard.add_roll(6.1, 6)
+        new_scorecard.add_roll(6.2, 3)
+        expect(new_scorecard.total(6)).to eq(44)
+      end
+
+      it 'checks that total still calculates if spare at end of frame and next rolls not rolled yet' do
+        new_scorecard.add_roll(1.1, 0)
+        new_scorecard.add_roll(1.2, 2)
+        new_scorecard.add_roll(2.1, 8)
+        new_scorecard.add_roll(2.2, 2)
+        expect(new_scorecard.total(2)).to eq(12)
+      end
+    end
+end
