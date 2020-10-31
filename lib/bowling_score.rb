7d29118410c05e55c5f2f1e29c54b84ce8697e1c
Otis <otisvickersgraver@gmail.com>
2020-10-31 16:14:12 +0000

    strike logic

--- lib/bowling_score.rb
+++ lib/bowling_score.rb
@@ -1,5 +1,7 @@
 class BowlingScore
 
+  FRAMES = 10
+
   def initialize
     @rolls = []
   end
@@ -9,7 +11,29 @@ class BowlingScore
   end
 
   def total_score
-    @rolls.sum
+    total = 0
+    roll_number = 0
+    FRAMES.times do
+      if strike?(roll_number)
+        total += strike_score(roll_number)
+        roll_number +=1
+      else
+        total += regular_score(roll_number)
+        roll_number +=2
+      end
+    end
+    total
   end
 
+  def regular_score(roll_number)
+    @rolls[roll_number] + @rolls[roll_number + 1]
+  end
+
+  def strike?(roll_number)
+    @rolls[roll_number] == 10
+  end
+
+  def strike_score(roll_number)
+    10 + @rolls[roll_number + 1] + @rolls[roll_number + 2]
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -8,5 +8,17 @@ describe BowlingScore do
       20.times {subject.roll(4)}
       expect(subject.total_score).to eq 80
     end
+
+    it 'gives the total score of 0 if player never hits a pin' do
+      20.times {subject.roll(0)}
+      expect(subject.total_score).to eq 0
+    end
+  end
+  context 'calculates correct score with strikes' do
+    it 'gives the total score of all rolls' do
+      subject.roll(10)
+      18.times {subject.roll(5)}
+      expect(subject.total_score).to eq 110
+    end
   end
 end
