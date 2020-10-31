9fd21f6ac0972d71d39da6b1cebc3f12e1f7588a
Dan Holmes <dh@danielholmes.com>
2020-10-31 14:10:14 +0000

    add feature tests for score

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,10 +1,16 @@
 class Frame
-  attr_reader :rolls, :bonus_rolls
-
   def initialize(rolls: [], bonus_rolls: [], number:)
     @rolls = rolls
     @bonus_rolls = bonus_rolls
     @number = number
+  end
+
+  def rolls(n)
+    !!@rolls[n] ? @rolls[n] : 0
+  end
+
+  def bonus_rolls(n)
+    !!@bonus_rolls[n] ? @bonus_rolls[n] : 0
   end
 
   def roll(n)
@@ -27,11 +33,11 @@ class Frame
     pins == 10 && !strike?
   end
 
-  def score(next_frame_1, next_frame_2)
+  def score(next_frame_1 = nil, next_frame_2 = nil)
     pins + bonus(next_frame_1, next_frame_2)
   end
 
-  def bonus(next_frame_1, next_frame_2)
+  def bonus(next_frame_1 = nil, next_frame_2 = nil)
     if strike?
       next_roll_1(next_frame_1) + next_roll_2(next_frame_1, next_frame_2)
     elsif spare?
@@ -43,21 +49,21 @@ class Frame
 
   def next_roll_1(next_frame_1 = nil)
     if @number == 10
-      @bonus_rolls[0]
+      bonus_rolls(0)
     else
-      next_frame_1.rolls[0]
+      next_frame_1.rolls(0)
     end
   end
 
   def next_roll_2(next_frame_1 = nil, next_frame_2 = nil)
     if @number == 10
-      @bonus_rolls[1]
+      bonus_rolls(1)
     elsif @number == 9 && next_frame_1.strike?
-      next_frame_1.bonus_rolls[0]
+      next_frame_1.bonus_rolls(0)
     elsif next_frame_1.strike?
-      next_frame_2.rolls[0]
+      next_frame_2.rolls(0)
     else
-      next_frame_1.rolls[1]
+      next_frame_1.rolls(1)
     end
   end
 end
