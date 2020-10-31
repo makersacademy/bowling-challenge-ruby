6b916e621650b37f813f6cfe5790857eb3ddadb2
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-31 10:03:06 +0000

    First commit. Test environment set up. Add first feature test for a single roll.

---
+++ spec/features_spec.rb
@@ -0,0 +1,7 @@
+describe 'Add score' do
+  it 'User can add the score of a single roll and see it displayed' do
+    game = Game.new
+    game.record_roll(5)
+    expect(game.running_score).to eq(5)
+  end
+end
