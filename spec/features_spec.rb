f48ccc832829c4379e705baf5c496e427e10f52a
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-31 10:38:05 +0000

    Add feature test for scoring spares and unit test for #current_frame method

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -9,8 +9,23 @@ describe 'Bowling scorecard features' do
       expect(game.running_score).to eq(5)
     end
 
-    it 'can store multiple rolls over several frames and display the correct score' do
+    it 'can display the correct score after multiple frames (no strikes or spares)' do
       game.record_roll(5)
-      expect(game.running_score).to eq(5)
+      game.record_roll(2)
+      game.record_roll(4)
+      game.record_roll(1)
+      game.record_roll(3)
+      game.record_roll(6)
+      expect(game.running_score).to eq(21)
+    end
+
+    it 'can display the correct score over multiple frames (including spares)' do
+      game.record_roll(5)
+      game.record_roll(2)
+      game.record_roll(7)
+      game.record_roll(3)
+      game.record_roll(3)
+      game.record_roll(6)
+      expect(game.running_score).to eq(29)
     end
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,11 +11,20 @@ describe Game do
    end
  end
 
- describe "running_score" do
+ describe "#running_score" do
    it "calculates and returns the current score" do
      game.record_roll(7)
      game.record_roll(2)
      expect(game.running_score).to eq(9)
+   end
+ end
+
+ describe "#current_frame" do
+   it "passes its contents to @completed_frames after two rolls" do
+     game.record_roll(7)
+     game.record_roll(2)
+     expect(game.current_frame).to eq([])
+     expect(game.completed_frames).to eq([[7,2]])
    end
  end
 end
