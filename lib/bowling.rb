6b8e61f06d8504ff04dc32abf001633e42bab16f
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-10-31 16:50:03 +0000

    Spare tests done

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,6 +1,5 @@
 class Bowling
-  FRAMES = 10
-  
+
   def initialize
     @rolls = []
   end
@@ -10,10 +9,13 @@ class Bowling
   def total_score
     total = 0
     roll_number = 0
-    FRAMES.times do
+    10.times do 
       if strike?(roll_number)
         total += strike_score(roll_number)
         roll_number +=1
+      elsif spare?(roll_number)
+        total += spare_score(roll_number)
+        roll_number +=2
       else
         total += regular_score(roll_number)
         roll_number +=2
@@ -21,13 +23,28 @@ class Bowling
     end
     total
   end
-  def regular_score(roll_number)
-    @rolls[roll_number] + @rolls[roll_number + 1]
-  end
+
   def strike?(roll_number)
     @rolls[roll_number] == 10
   end
+
   def strike_score(roll_number)
     10 + @rolls[roll_number + 1] + @rolls[roll_number + 2]
   end
+  
+  def spare?(roll_number)
+    @rolls[roll_number] + @rolls[roll_number + 1] == 10
+  end 
+
+  def spare_score(roll_number)
+    p roll_number
+   10 + @rolls[roll_number + 2]
+  end 
+
+  def regular_score(roll_number)
+    @rolls[roll_number] + @rolls[roll_number + 1]
+  end
+
+
+
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,32 +1,58 @@
 require 'bowling'
 
 describe Bowling do
+
   let (:subject) { described_class.new }
+
   context 'without rolling any strikes or spares' do
     it 'gives the total score of all rolls' do
       20.times {subject.roll(4)}
       expect(subject.total_score).to eq 80
     end
+
     it 'gives the total score of 0 if player never hits a pin' do
       20.times {subject.roll(0)}
       expect(subject.total_score).to eq 0
     end
   end
+
+
   context 'calculates correct score with strikes' do
 
     it 'gives the total score of all rolls' do
-      subject.roll(10)
-      18.times {subject.roll(5)}
-      expect(subject.total_score).to eq 110
+        2.times {subject.roll(2)}
+        subject.roll(10)
+        16.times {subject.roll(2)}
+      expect(subject.total_score).to eq 50
     end 
 
-    it 'gives the total score of all rolls' do
+    it 'gives the total if there are two strikes in a row' do
       subject.roll(10)
       subject.roll(10)
-      17.times {subject.roll(5)}
-      expect(subject.total_score).to eq 125
+      16.times {subject.roll(2)}
+      expect(subject.total_score).to eq 68
     end 
-
   end
+
+  context 'calculates correct score with spares' do
+
+    it 'gives the total score of one spare frame' do
+      subject.roll(5)
+      subject.roll(5)
+      subject.roll(5)
+      17.times {subject.roll(4)}
+      expect(subject.total_score).to eq 88
+    end 
+  end 
+
+  it 'gives the total score of two spare frames' do
+    subject.roll(5)
+    subject.roll(5)
+    subject.roll(5)
+    subject.roll(5)
+    16.times {subject.roll(4)}
+    expect(subject.total_score).to eq 93
+  end 
+
 
 end
