b4b58e0d8d8451897f6192dc78c74d23a8a93f9a
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-31 16:18:31 +0000

    Returns the total score with two consecutive stikes

--- spec/bowling_score_spec.rb
+++ spec/bowling_score_spec.rb
@@ -4,22 +4,29 @@ describe BowlingScore do
   let (:subject) { described_class.new }
 
   context 'without rolling any strikes or spares' do
-    it 'gives the total score of all rolls' do
+    it 'returns the total score of all rolls' do
       20.times {subject.roll(4)}
       expect(subject.total_score).to eq 80
     end
 
-    it 'gives the total score for 10 gutter rolls' do
+    it 'returns the total score for 10 gutter rolls' do
       20.times {subject.roll(0)}
       expect(subject.total_score).to eq 0
     end
   end
 
   context 'calculates correct score with strikes' do
-    it 'gives the total score of all rolls with one strike' do
+    it 'returns the total score of all rolls with one strike' do
       subject.roll(10)
       18.times {subject.roll(5)}
       expect(subject.total_score).to eq 110
+    end
+
+    it 'returns the total score with two consecutive strikes' do
+      subject.roll(10)
+      subject.roll(10)
+      17.times {subject.roll(5)}
+      expect(subject.total_score).to eq 125
     end
   end
 end
