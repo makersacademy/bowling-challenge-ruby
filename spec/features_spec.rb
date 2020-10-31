bbe4fe5a7864caeacefd51a65c8851163249ec6d
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-31 10:27:35 +0000

    Refactoring: Clean up formatting of feature tests

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -2,15 +2,15 @@ require 'game'
 
 describe 'Bowling scorecard features' do
 
+  let(:game) { Game.new }
+
     it 'can store the score of a single roll and display it for the user' do
-      game = Game.new
       game.record_roll(5)
       expect(game.running_score).to eq(5)
     end
 
     it 'can store multiple rolls over several frames and display the correct score' do
-        game = Game.new
-        game.record_roll(5)
-        expect(game.running_score).to eq(5)
+      game.record_roll(5)
+      expect(game.running_score).to eq(5)
     end
   end
