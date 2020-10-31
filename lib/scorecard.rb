315beca441c7dfa79e9fd4ea3832d0e52d56f568
Dan Holmes <dh@danielholmes.com>
2020-10-31 15:27:16 +0000

    new feature: scorecard.roll

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -5,6 +5,19 @@ class Scorecard
     @frames = []
   end
 
+  def roll(n, frame_class = Frame)
+    if @frames.empty?
+      frame = frame_class.new
+      add(frame)
+    elsif @frames.last.complete?
+      frame = frame_class.new
+      add(frame)
+    else
+      frame = @frames.last
+    end
+    frame.roll(n)
+  end
+
   def add(frame)
     frame.number = @frames.length + 1
     frames.push(frame)
--- spec/units/scorecard_spec.rb
+++ spec/units/scorecard_spec.rb
@@ -25,4 +25,36 @@ describe Scorecard do
       scorecard.score
     end
   end
+  describe ".roll" do
+    let(:f1) { double(:frame, :number= => nil) }
+    let(:f2) { double(:frame, :number= => nil) }
+    let(:frame_class) { double(:frame_class, :new => f1) }
+    context "First roll" do
+      it "creates a frame, calls roll on it and adds it to the scorecard" do
+        expect(frame_class).to receive(:new)
+        expect(f1).to receive(:number=).with(1)
+        expect(f1).to receive(:roll).with(8)
+        scorecard.roll(8, frame_class)
+      end
+    end
+    context "Current frame incomplete" do
+      it "calls roll on that frame" do
+        allow(f1).to receive(:complete?) { false }
+        expect(f1).to receive(:roll).with(2)
+        scorecard.add(f1)
+        scorecard.roll(2)
+      end
+    end
+    context "Current frame is complete do" do
+      it "creates a new frame, calls roll on it and adds it to the scorecard" do
+        scorecard.add(f1)
+        allow(f1).to receive(:complete?) { true }
+        allow(frame_class).to receive(:new) { f2 }
+        expect(frame_class).to receive(:new)
+        expect(f2).to receive(:number=).with(2)
+        expect(f2).to receive(:roll).with(8)
+        scorecard.roll(8, frame_class)
+      end
+    end
+  end
 end
