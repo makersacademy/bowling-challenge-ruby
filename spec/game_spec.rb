b45e87fa44e093ae9ff8c5fa1494568d1849d6d3
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-31 10:14:43 +0000

    Add unit test for #running_score method(unwritten currently)

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,21 @@
 require 'game'
 
 describe Game do
+
   let(:game) { described_class.new }
+
  describe "#record_roll" do
-   it "records the score of a single roll" do
+   it "adds the score of a single roll to the current frame" do
      game.record_roll(5)
      expect(game.current_frame.sum).to eq(5)
+   end
+ end
+
+ describe "running_score" do
+   it "calculates and returns the current score" do
+     game.record_roll(7)
+     game.record_roll(2)
+     expect(game.running_score).to eq(9)
    end
  end
 end
