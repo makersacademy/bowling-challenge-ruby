48e30894961888d9fb5fa7e3b8dd7d4065111f2b
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-31 17:13:20 +0000

    Added tests for totalling the score including spares

--- lib/bowling_score.rb
+++ lib/bowling_score.rb
@@ -1,39 +1,50 @@
 class BowlingScore 
+  FRAMES = 10
 
-    FRAMES = 10
-
-    def initialize
-      @rolls = []
-    end
-
-    def roll(score)
-      @rolls << score
-    end
-
-    def total_score
-      total = 0
-      roll_number = 0
-      FRAMES.times do
-        if strike?(roll_number)
-          total += strike_score(roll_number)
-          roll_number +=1
-        else
-          total += regular_score(roll_number)
-          roll_number +=2
-        end
-      end
-      total
-    end
-
-    def regular_score(roll_number)
-      @rolls[roll_number] + @rolls[roll_number + 1]
-    end
-
-    def strike?(roll_number)
-      @rolls[roll_number] == 10
-    end
-
-    def strike_score(roll_number)
-      10 + @rolls[roll_number + 1] + @rolls[roll_number + 2]
-    end
+  def initialize
+    @rolls = []
   end
+
+  def roll(score)
+    @rolls << score
+  end
+
+  def total_score
+    total = 0
+    roll_number = 0
+    FRAMES.times do
+      if strike?(roll_number)
+        total += strike_score(roll_number)
+        roll_number += 1
+      elsif spare?(roll_number)
+        total += spare_score(roll_number)
+        roll_number += 2
+      else
+        total += regular_score(roll_number)
+        roll_number += 2
+      end
+    end
+    total
+  end
+
+  def regular_score(roll_number)
+    @rolls[roll_number] + @rolls[roll_number + 1]
+  end
+
+  def strike?(roll_number)
+    @rolls[roll_number] == 10
+  end
+
+  def strike_score(roll_number)
+    10 + @rolls[roll_number + 1] + @rolls[roll_number + 2]
+  end
+
+  def spare?(roll_number)
+    @rolls[roll_number] + @rolls[roll_number + 1] == 10
+  end
+
+  def spare_score(roll_number)
+    roll_number
+    10 + @rolls[roll_number + 2]
+  end
+end
--- spec/bowling_score_spec.rb
+++ spec/bowling_score_spec.rb
@@ -1,16 +1,16 @@
 require 'bowling_score'
 
 describe BowlingScore do
-  let (:subject) { described_class.new }
+  let(:subject) { described_class.new }
 
   context 'without rolling any strikes or spares' do
     it 'returns the total score of all rolls' do
-      20.times {subject.roll(4)}
+      20.times { subject.roll(4) }
       expect(subject.total_score).to eq 80
     end
 
     it 'returns the total score for 10 gutter rolls' do
-      20.times {subject.roll(0)}
+      20.times { subject.roll(0) }
       expect(subject.total_score).to eq 0
     end
   end
@@ -18,15 +18,33 @@ describe BowlingScore do
   context 'calculates correct score with strikes' do
     it 'returns the total score of all rolls with one strike' do
       subject.roll(10)
-      18.times {subject.roll(5)}
-      expect(subject.total_score).to eq 110
+      18.times { subject.roll(4) }
+      expect(subject.total_score).to eq 90
     end
 
     it 'returns the total score with two consecutive strikes' do
       subject.roll(10)
       subject.roll(10)
-      17.times {subject.roll(5)}
-      expect(subject.total_score).to eq 125
+      17.times { subject.roll(4) }
+      expect(subject.total_score).to eq 106
+    end
+  end
+
+  context 'calculates the score with spares' do
+    it 'returns the total score of all rolls including one space' do
+      subject.roll(5)
+      subject.roll(5)
+      18.times { subject.roll(4) }
+      expect(subject.total_score).to eq 86
+    end
+
+    it 'returns the total score of all rolls including two space' do
+      subject.roll(5)
+      subject.roll(5)
+      subject.roll(4)
+      subject.roll(6)
+      16.times { subject.roll(4) }
+      expect(subject.total_score).to eq 92
     end
   end
 end
