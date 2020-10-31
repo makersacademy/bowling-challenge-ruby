cf2dcb2e8f7c02b08cd7590b3814524597de0860
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-10-31 16:14:06 +0000

    Tests for strike are passing now

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,15 +1,33 @@
 class Bowling
+  FRAMES = 10
+  
   def initialize
     @rolls = []
   end
-  
-  def roll(points)
-    @rolls << points
-  end 
-
+  def roll(pins)
+    @rolls << pins
+  end
   def total_score
-    @rolls.sum
-  end 
-
-
+    total = 0
+    roll_number = 0
+    FRAMES.times do
+      if strike?(roll_number)
+        total += strike_score(roll_number)
+        roll_number +=1
+      else
+        total += regular_score(roll_number)
+        roll_number +=2
+      end
+    end
+    total
+  end
+  def regular_score(roll_number)
+    @rolls[roll_number] + @rolls[roll_number + 1]
+  end
+  def strike?(roll_number)
+    @rolls[roll_number] == 10
+  end
+  def strike_score(roll_number)
+    10 + @rolls[roll_number + 1] + @rolls[roll_number + 2]
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,11 +2,31 @@ require 'bowling'
 
 describe Bowling do
   let (:subject) { described_class.new }
-
   context 'without rolling any strikes or spares' do
     it 'gives the total score of all rolls' do
-      20.times { subject.roll(4) }
+      20.times {subject.roll(4)}
       expect(subject.total_score).to eq 80
     end
+    it 'gives the total score of 0 if player never hits a pin' do
+      20.times {subject.roll(0)}
+      expect(subject.total_score).to eq 0
+    end
   end
+  context 'calculates correct score with strikes' do
+
+    it 'gives the total score of all rolls' do
+      subject.roll(10)
+      18.times {subject.roll(5)}
+      expect(subject.total_score).to eq 110
+    end 
+
+    it 'gives the total score of all rolls' do
+      subject.roll(10)
+      subject.roll(10)
+      17.times {subject.roll(5)}
+      expect(subject.total_score).to eq 125
+    end 
+
+  end
+
 end
