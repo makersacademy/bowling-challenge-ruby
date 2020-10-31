878ed6136e24b8b5eb3199f355ff726463131e95
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-31 16:50:11 +0000

    Refactoring and added total method

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -8,31 +8,7 @@ attr_reader :score
   end
 
   def score_card
-    {
-      @team => {
-        1.1=>0,
-        1.2=>0,
-        2.1=>0,
-        2.2=>0,
-        3.1=>0,
-        3.2=>0,
-        4.1=>0,
-        4.2=>0,
-        5.1=>0,
-        5.2=>0,
-        6.1=>0,
-        6.2=>0,
-        7.1=>0,
-        7.2=>0,
-        8.1=>0,
-        8.2=>0,
-        9.1=>0,
-        9.2=>0,
-        10.1=>0,
-        10.2=>0,
-        10.3=>0
-      }
-    }
+    {@team => {} }
   end
 
   def reset_score_card
@@ -43,6 +19,10 @@ attr_reader :score
 
   def add_roll(turn, pins)
     @score[@team][turn] = pins
+  end
+
+  def total(frame)
+    @score[@team][frame+0.1] + @score[@team][frame+0.2]
   end
 
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -2,16 +2,39 @@ require 'scorecard'
 
 describe Scorecard do
 let (:new_scorecard) {Scorecard.new("my team")}
-  it 'giving a team name should return empty nested hash scorecard with team name' do
+  it 'using the #reset_score_card method should return emptied nested hash scorecard with team name' do
+    new_scorecard.add_roll(1.1, 5)
+    new_scorecard.add_roll(1.2, 3)
+    new_scorecard.add_roll(2.1, 2)
+    new_scorecard.add_roll(2.2, 7)
+    new_scorecard.reset_score_card
     expect(new_scorecard.score).to eq({"my team"=>{
-      1.1=>0, 1.2=>0, 2.1=>0, 2.2=>0, 3.1=>0, 3.2=>0, 4.1=>0, 4.2=>0, 5.1=>0, 5.2=>0, 6.1=>0, 6.2=>0, 7.1=>0, 7.2=>0, 8.1=>0, 8.2=>0, 9.1=>0, 9.2=>0, 10.1=>0, 10.2=>0, 10.3=>0}})
+      1.1=>0, 1.2=>0, 2.1=>0, 2.2=>0, }})
   end
 
-  it 'giving a team name should return empty nested hash scorecard with team name' do
+  it 'entering scores should push to hash' do
     new_scorecard.add_roll(1.1, 5)
     new_scorecard.add_roll(1.2, 3)
     expect(new_scorecard.score).to eq({"my team"=>{
-      1.1=>5, 1.2=>3, 2.1=>0, 2.2=>0, 3.1=>0, 3.2=>0, 4.1=>0, 4.2=>0, 5.1=>0, 5.2=>0, 6.1=>0, 6.2=>0, 7.1=>0, 7.2=>0, 8.1=>0, 8.2=>0, 9.1=>0, 9.2=>0, 10.1=>0, 10.2=>0, 10.3=>0}})
+      1.1=>5, 1.2=>3 }})
+  end
+
+  it 'check that resetting scorecard then adding scores pushes to hash' do
+    new_scorecard.add_roll(1.1, 5)
+    new_scorecard.add_roll(1.2, 3)
+    new_scorecard.add_roll(2.1, 2)
+    new_scorecard.add_roll(2.2, 7)
+    new_scorecard.reset_score_card
+    new_scorecard.add_roll(1.1, 4)
+    new_scorecard.add_roll(1.2, 1)
+    expect(new_scorecard.score).to eq({"my team"=>{
+      1.1=>4, 1.2=>1, 2.1=>0, 2.2=>0, }})
+  end
+
+  it 'check that resetting scorecard then adding scores pushes to hash' do
+    new_scorecard.add_roll(1.1, 5)
+    new_scorecard.add_roll(1.2, 3)
+    expect(new_scorecard.total(1)).to eq(8)
   end
 
 end
