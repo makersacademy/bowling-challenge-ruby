b31357c4a648fd67f0a1700d4cd7174860f43688
KC Thomas <thisiskcthomas@gmail.com>
2020-11-01 23:12:59 +0000

    ScoreCard initializes with a new ScoreTracker

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -2,4 +2,10 @@ require_relative 'scoretracker'
 
 class ScoreCard
 
+  attr_reader :scores
+
+  def initialize(scoretracker = ScoreTracker)
+    @scores = scoretracker.new
+  end
+
 end
--- spec/unit tests/scorecard_spec.rb
+++ spec/unit tests/scorecard_spec.rb
@@ -2,4 +2,13 @@ require_relative "../../lib/scorecard.rb"
 
 describe ScoreCard do
 
+  #it will get user input to send to scoretracker
+  describe '#initialize' do
+    it 'creates a new instance of ScoreTracker' do
+      card = ScoreCard.new
+      expect(card.scores).to be_a ScoreTracker
+    end
+  end
+  #it will present score after each roll
+  #it will say whether a game was perfect or a gutter game
 end
