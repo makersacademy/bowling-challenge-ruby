0c8b0bb514fea306f5145641fd2a16c431f96bbc
Meryl Rastall <merylrastall@gmail.com>
2020-11-01 16:50:41 +0000

    extraceted frame and game classes from the scorecard class. Updated scorecard_spec tests to reflect the extraction

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -3,56 +3,24 @@ class Scorecard
   attr_reader :current_roll, :game, :current_frame
 
   def initialize
-    @current_roll = 0
-    @game = Array.new(0)
-    @current_frame = Hash.new(0)
+   @game = Game.new
   end
   
   def add_score(score)
-    reset_frame?
-    strike_or_spare?(score)
-    @current_roll += 1
-    @current_frame['frame_roll'] += 1
-    @current_frame['score'] += score
+    @game.add_score(score)
   end
 
 
   def frame?
-    @game.length + 1
+    @game.frame_number
   end
 
-  def strike_or_spare?(score)
-    if score == 10 && @current_frame['frame_roll'] == 0
-      @current_frame['strike'] = true
-      p "you got a strike"
-    elsif @current_frame['score'] + score == 10
-      @current_frame['spare'] = true
-      p "you got a spare"
-     end
+  def current_roll
+    @game.roll_number
   end
 
-  def reset_frame?
-    if @current_frame['frame_roll'] == 2
-      reset
-      p "frame has been reset because you had two rolls"
-    elsif @current_frame['strike'] == true
-      reset
-      p "frame has been reset because you got a stike"
-    end
-  end
-
-  def reset
-    @game << @current_frame
-    @current_frame = Hash.new(0)
-  end
-
-  # calculates the score from completed frames
   def current_score
-    if game.empty?
-      @current_frame['score']
-    else
-      @current_frame['score'] + @game.map {|frame| frame['score']}.reduce(:+)
-    end
+    @game.score
   end
 
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -2,64 +2,18 @@ require 'scorecard'
 
 describe Scorecard do
   
-  context 'adding a score' do
-    it 'takes a score and adds it to total' do
-      subject.add_score(5)
+  it 'knows the current score' do
+    subject.add_score(5)
 
-      expect(subject.current_score).to eq 5
-    end
-
-    it 'can tell the current roll number' do
-      subject.add_score(5)
-      subject.add_score(5)
-
-      expect(subject.current_roll).to eq 2
-    end
+    expect(subject.current_score).to eq 5
   end
 
-  context 'understanding frames' do
-    it 'can tell me the current frame number' do
-      subject.add_score(5)
+  it 'can tell the current roll number' do
+    subject.add_score(5)
+    subject.add_score(5)
 
-      expect(subject.frame?).to eq 1
-
-    end
-    
-    it 'knows that there are a max two rolls per frame' do
-      subject.add_score(5)
-      subject.add_score(5)
-      subject.add_score(5)
-
-      expect(subject.frame?).to eq 2
-    end
+    expect(subject.current_roll).to eq 2
   end
-
-  context 'understanding strikes and spares' do
-    it 'understands when a stike has been rolled' do
-    
-      subject.add_score(10)
-      
-      expect(subject.current_frame['score']).to eq 10
-      expect(subject.current_frame['strike']).to eq true
-      
-    end
-
-    it 'understands when a spare has been rolled' do
-      subject.add_score(5)
-      subject.add_score(5)
-
-      expect(subject.current_frame['score']).to eq 10
-      expect(subject.current_frame['strike']).to be 0
-      expect(subject.current_frame['spare']).to eq true
-
-    end
-
-    xit 'understands how to add a bonus to a strike' do
-
-    end
-
-    xit 'understands how to add a bonus to a spare' do
-    end
-  end
+ 
 end
 
