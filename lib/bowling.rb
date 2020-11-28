bb6ff2dbe434ded0e580fb9f80388067fccf75ac
Melissa <mystic101@gmail.com>
2020-11-28 19:14:00 +0000

    super super basic

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,7 +1,31 @@
 class Bowling
 
-  def roll(number)
+attr_reader :scorecard, :roll
 
+  def initialize
+    @scorecard = []
   end
-  
+
+
+  def roll(number)
+    @scorecard << number
+  end
+
+
+  def frame1
+    if @scorecard[0] + @scorecard[1] == 10
+      return @scorecard[0] + @scorecard[1] + @scorecard[2]
+    elsif @scorecard[0] == 10
+      return @scorecard[0] + @scorecard[2] + @scorecard[3]
+    else
+       return @scorecard[0] + @scorecard[1]
+    end
+  end
+# Work in progress.
+# Havent accounted for Frame 2 and Frame 3 being a strike.
+# Am I really going to have a method for each frame???
+# Havent figured out Frame 10 yet.
+# Maybe a hash where each key is a roll, or a hash where each element is a array with the 2 rolls?
+
+
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -3,8 +3,32 @@ require 'bowling'
 describe Bowling do
 
   it "allows the user to enter their roll" do
-    game = Bowling.new
-    expect(game).to respond_to(:roll).with(1).argument
+    expect(subject).to respond_to(:roll).with(1).argument
   end
 
+  it "totals the value of the rolls" do
+    game = Bowling.new
+    game.roll(5)
+    game.roll(3)
+    game.roll(4)
+    expect(game.frame1).to eq 8
+  end
+
+  it "adds the bonus to a strike" do
+    game = Bowling.new
+    game.roll(10)
+    game.roll(3)
+    game.roll(5)
+    game.roll(5)
+    expect(game.frame1).to eq 20
+  end
+
+  it "adds the bonus to a spare" do
+    game = Bowling.new
+    game.roll(5)
+    game.roll(5)
+    game.roll(1)
+    game.roll(2)
+    expect(game.frame1).to eq 11
+  end
 end
