bfc8021496c033e67e091d48d65ed091babc9e67
Ara <hello.ara@pm.me>
2021-01-07 12:45:49 +0100

    implement run method

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,7 +1,18 @@
 class ScoreCard
-  attr_reader :knocked_down_pins
+  DEFAULT_FRAMES = 10
+
+  attr_reader :frames, :first_roll, :second_roll
+
+  def initialize(frames = DEFAULT_FRAMES)
+    @frames = frames
+  end
+
   def run
     puts "Enter the number of knocked down pins:"
-    @knocked_down_pins = gets
+    @first_roll = gets
+    unless @first_roll == 10
+      puts "Enter the number of knocked down pins:"
+      @second_roll = gets
+    end
   end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -7,8 +7,26 @@ describe 'ScoreCard' do
     it 'should update the score based on user input' do
       allow_any_instance_of(ScoreCard).to receive(:gets).and_return(9)
       expect(subject.run).to eq 9
-      expect(subject.knocked_down_pins).to eq 9
+      expect(subject.first_roll).to eq 9
     end
 
+    it 'should prompt the user for second roll result' do
+      allow_any_instance_of(ScoreCard).to receive(:gets).and_return(9)
+      expect_any_instance_of(ScoreCard).to receive(:gets).twice
+      subject.run
+    end
+
+    it 'should skip the second roll if first roll was a strike' do
+      allow_any_instance_of(ScoreCard).to receive(:gets).and_return(10)
+      expect(subject.second_roll).to be nil
+    end
+
+    it 'should run the rounds 10 times' do
+      
+    end
+  end
+
+  it 'should count the number of frames' do
+    expect(subject.frames).to eq 10
   end
 end
