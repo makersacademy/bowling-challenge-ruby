00797b8c2245ec673c4f43dd0d0c374239e00cd9
xavierloos <xavier.loos04@gmail.com>
2021-01-09 14:55:20 +0000

    Refactore RSpec

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,15 +1,16 @@
 require "bowling_game"
 RSpec.describe BowlingGame do
   describe "behavior" do
+    before do
+      @game = described_class.new
+    end
     it "can roll" do
-      game = BowlingGame.new
-      20.times { game.roll 0 }
-      expect(game.score).to eq 0
+      20.times { @game.roll 0 }
+      expect(@game.score).to eq 0
     end
     it "can roll all ones" do
-      game = BowlingGame.new
-      20.times { game.roll 1 }
-      expect(game.score).to eq 20
+      20.times { @game.roll 1 }
+      expect(@game.score).to eq 20
     end
   end
 end
