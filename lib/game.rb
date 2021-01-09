9039d98e8bdc09cd0e4d8b07d4335b310a8c48ad
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-01-09 17:17:37 +0000

    US2: Refactored

--- lib/game.rb
+++ lib/game.rb
@@ -21,24 +21,19 @@ class Game
 
     @scores.each do |score|
       @frames[frame_index] << score
-      if new_frame?(frame_index)
-        frame_index += 1
-      end
+      frame_index += 1 if new_frame?(frame_index)
     end
   end
 
   def add_bonuses_to_frames
     @frames.each_with_index do |frame, frame_index|
-      if frame.sum < 10 || frame_index == 9
+      if frame_index == 9
         frame
-      elsif frame.length == 2
-        frame << @frames[frame_index + 1][0]
-      elsif @frames[frame_index + 1].length >= 2
-        frame << @frames[frame_index + 1][0]
-        frame << @frames[frame_index + 1][1]
-      else
-        frame << @frames[frame_index + 1][0]
-        frame << @frames[frame_index + 2][0]
+      elsif spare?(frame)
+        frame << next_score(frame_index)
+      elsif strike?(frame)
+        frame << next_score(frame_index)
+        frame << second_next_score(frame_index)
       end
     end
   end
@@ -53,10 +48,26 @@ class Game
   end
 
   def strike?(frame)
-    true if frame[0] == 10
+    frame[0] == 10
+  end
+
+  def spare?(frame)
+    frame.length == 2 && frame.sum == 10
   end
 
   def new_frame?(frame_index)
     (strike?(@frames[frame_index]) || @frames[frame_index].length == 2) && frame_index < 9
+  end
+
+  def next_score(frame_index)
+    @frames[frame_index + 1][0]
+  end
+
+  def second_next_score(frame_index)
+    if @frames[frame_index + 1].length >= 2
+      @frames[frame_index + 1][1]
+    else
+      @frames[frame_index + 2][0]
+    end
   end
 end
