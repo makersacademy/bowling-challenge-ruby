0a220e1ded85a955f8122c358f2998afbb0d1544
xavierloos <xavier.loos04@gmail.com>
2021-01-09 15:26:43 +0000

    User can roll a spare

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -2,15 +2,25 @@ require "bowling_game"
 RSpec.describe BowlingGame do
   describe "behavior" do
     before do
-      @game = described_class.new
+      @gm = described_class.new
     end
+
     it "can roll" do
-      20.times { @game.roll 0 }
-      expect(@game.score).to eq 0
+      20.times { @gm.roll 0 }
+      expect(@gm.score).to eq 0
     end
+
     it "can roll all ones" do
-      20.times { @game.roll 1 }
-      expect(@game.score).to eq 20
+      20.times { @gm.roll 1 }
+      expect(@gm.score).to eq 20
+    end
+
+    it "can roll a spare" do
+      @gm.roll 5
+      @gm.roll 5
+      @gm.roll 3
+      17.times { @gm.roll 0 }
+      expect(@gm.score).to eq 16
     end
   end
 end
