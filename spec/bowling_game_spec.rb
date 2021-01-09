bac0db7886bad3461345f0160b7439395de26cc5
xavierloos <xavier.loos04@gmail.com>
2021-01-09 16:14:36 +0000

    Perfect game RSpec

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -22,12 +22,17 @@ RSpec.describe BowlingGame do
       17.times { @gm.roll 0 }
       expect(@gm.score).to eq 16
     end
+
     it "can roll a strike" do
       @gm.roll 10
       @gm.roll 4
       @gm.roll 3
       16.times { @gm.roll 0 }
       expect(@gm.score).to eq 24
+    end
+    it "can roll a perfect game" do
+      12.times { @gm.roll 10 }
+      expect(@gm.score).to eq 300
     end
   end
 end
