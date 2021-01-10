a474f23c50fd6d73c7e84e4e3e5c4014bdbc94c5
Aman Tank <amantank187@gmail.com>
2021-01-10 16:14:02 +0000

    Able to score a spare

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,5 +1,7 @@
 class Bowling
 
+  attr_accessor :rolls
+
   def initialize
     @rolls = []
   end
@@ -9,6 +11,18 @@ class Bowling
   end
 
   def score
-    @rolls.reduce(:+)
+    result = 0
+    roll_index = 0
+    10.times do
+      if @rolls[roll_index].to_i + @rolls[roll_index + 1].to_i == 10
+        result += @rolls[roll_index].to_i + @rolls[roll_index + 1].to_i + @rolls[roll_index + 2].to_i
+      else
+        result += @rolls[roll_index].to_i + @rolls[roll_index + 1].to_i 
+      end
+      roll_index += 2
+    end
+
+    result
   end
+
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,7 +1,9 @@
 require 'bowling'
 
 describe "Bowling" do
-  game = Bowling.new
+
+  let(:game) {Bowling.new}
+
   describe "#roll" do
     it 'Should be able to make a roll' do
       game.roll(5)
@@ -13,6 +15,19 @@ describe "Bowling" do
     it 'Should be able to calculate a full game of bowling' do
       20.times{game.roll(1)}
       expect(game.score).to eq(20)
+    end
+
+    it 'Should be able to roll a gutter game' do
+      20.times{game.roll(0)}
+      expect(game.score).to eq(0)
+    end
+
+    it 'You can roll a spare' do
+      game.roll(5)
+      game.roll(5)
+      game.roll(3)
+      17.times{game.roll(0)}
+      expect(game.score).to eq(16)
     end
   end
 end
