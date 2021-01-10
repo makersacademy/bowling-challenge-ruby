c20f1f522e51a63203a6f738448451a0125c142e
Lisa <bardelli.lisa@gmail.com>
2021-01-10 12:29:16 +0000

    Add game messages

--- lib/main.rb
+++ lib/main.rb
@@ -14,14 +14,35 @@ def get_rolls
 end
 
 game = Game.new
-(1...10).each do |_i|
+puts 'Bowling game'.center(50)
+puts 'Welcome'.center(50)
+(1...10).each do |i|
+  puts "Frame #{i}".center(50)
+  puts
   roll1, roll2 = get_rolls
   frame = Frame.new(roll1, roll2)
+  puts "Roll 1: #{roll1}".center(60)
+  puts "Roll 2: #{roll2}".center(60)
+  puts "That's a strike!".center(60) if roll1 == 10
+  puts "That's a spare!".center(60) if (roll1 + roll2) == 10 && roll1 < 10
+
   game.play(frame)
+  puts
+  puts "Score: #{game.score}".center(50)
+  puts
 end
 roll1, roll2 = get_rolls
 roll3 = rand(0..10)
+puts 'Frame 10'.center(50)
+puts
+puts "Roll 1: #{roll1}".center(60)
+puts "Roll 2: #{roll2}".center(60)
+puts "That's a strike!".center(60) if roll1 == 10
+puts "That's a spare!".center(60) if (roll1 + roll2) == 10 && roll1 < 10
+puts "Roll 3: #{roll3}".center(60) if (roll1 + roll2) >= 10
 frame_ten = FrameTen.new(roll1, roll2, roll3)
 game.play(frame_ten)
 
-puts game.score
+puts "Score: #{game.score}".center(50)
+puts 'Perfect game'.center(50) if game.score == 300
+puts 'Gutter game'.center(50) if game.score.zero?
