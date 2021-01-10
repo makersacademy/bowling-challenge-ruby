7a53c1b75034a1ebc3fa2a933fad510e7c7fea0e
Ara <hello.ara@pm.me>
2021-01-10 02:28:57 +0100

    refactoring

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,63 +1,96 @@
+class Score 
+  FRAMES = 10
+  attr_reader :score_board, :frames, :total_score
 
-@score_board = []
-@frames = (0..9).to_a
-@total_score = 0
+  def initialize
+    @score_board = []
+    @frames = (0..FRAMES-1).to_a
+    @total_score = 0
+  end
 
-@frames.each do |frame|
-  puts "Round ##{frame}"
-  puts "First roll. Enter the number of pins"
-  @first_roll = gets.chomp.to_i
-  @score_board[frame] = [@first_roll] #update the board
-  @total_score += @first_roll # score += 10
-  p @score_board
-  p "total score:"
-  p @total_score
-  if frame == 9
-    if @first_roll == 10
-      @second_roll = gets.chomp.to_i
-      @score_board[frame] << @second_roll
-      @total_score += @second_roll
-      if @second_roll + @first_roll == 10
-        @bonus_roll = gets.chomp.to_i
-        @score_board[frame] << @bonus_roll
+  def print_score
+    puts @score_board
+    puts "total score:"
+    puts @total_score
+  end
+
+  def update_board(frame, pins)
+    if @score_board[frame].nil?
+      @score_board[frame] = [pins]
+    else
+      @score_board[frame] << pins
+    end
+  end
+
+  def update_score(pins)
+    @total_score += pins
+  end
+
+
+def strike?
+  @first_roll == 10
+end
+
+def spare?
+  @first_roll + @second_roll == 10
+end
+
+  def run
+  @frames.each do |current_frame|
+    puts "Round ##{current_frame}"
+    puts "First roll. Enter the number of pins"
+    @first_roll = gets.chomp.to_i
+    update_board(current_frame, @first_roll)
+    update_score(@first_roll)
+    # @first_roll = gets.chomp.to_i
+    # @score_board[current_frame] = [@first_roll] #update the board
+    # @total_score += @first_roll # score += 10
+    print_score
+    if current_frame == @frames.last
+      if self.strike?
+        @second_roll = gets.chomp.to_i
+        @score_board[current_frame] << @second_roll
+        @total_score += @second_roll
+        if self.spare? #spare?
+          @bonus_roll = gets.chomp.to_i
+          @score_board[current_frame] << @bonus_roll
+        end
+      else
+        puts "Second roll. Enter the number of pins"
+        # @second_roll = gets.chomp.to_i
+        # @score_board[current_frame] << @second_roll
+        # @total_score += @second_roll
+        update_second_roll
+        print_score
+        if @score_board[current_frame].sum == 10
+          puts "Bonus roll. Enter the number of pins"
+          @bonus_roll = gets.chomp.to_i
+          @score_board[current_frame] << @bonus_roll
+          @total_score += @bonus_roll
+          print_score
+        end
       end
     else
+      if current_frame != 0 && @score_board[current_frame-1].first == 10
+        @total_score += @first_roll
+      elsif current_frame != 0 && @score_board[current_frame-1].sum == 10
+        @total_score += @first_roll
+      end
+      if @first_roll == 10
+        next # skip 
+      else
       puts "Second roll. Enter the number of pins"
-      @second_roll = gets.chomp.to_i
-      @score_board[frame] << @second_roll
-      @total_score += @second_roll
-      p @score_board
-    p "total score:"
-    p @total_score
-      if @score_board[frame].sum == 10
-        puts "Bonus roll. Enter the number of pins"
-        @bonus_roll = gets.chomp.to_i
-        @score_board[frame] << @bonus_roll
-        @total_score += @bonus_roll
-        p @score_board
-        p "total score:"
-        p @total_score
+      # @second_roll = gets.chomp.to_i
+      # @score_board[current_frame] << @second_roll
+      # @total_score += @second_roll
+      if current_frame != 0 && @score_board[current_frame-1].first == 10
+        @total_score += @second_roll
       end
+      print_score
     end
-  else
-    if frame != 0 && @score_board[frame-1].first == 10
-      @total_score += @first_roll
-    elsif frame != 0 && @score_board[frame-1].sum == 10
-      @total_score += @first_roll
-    end
-    if @first_roll == 10
-      next # skip 
-    else
-    puts "Second roll. Enter the number of pins"
-    @second_roll = gets.chomp.to_i
-    @score_board[frame] << @second_roll
-    @total_score += @second_roll
-    if frame != 0 && @score_board[frame-1].first == 10
-      @total_score += @second_roll
-    end
-    p @score_board
-    p "total score"
-    p @total_score
+  end
   end
 end
+
 end
+
