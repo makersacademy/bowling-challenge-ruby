78ea8c663d604e6438542c6f794f32af5a0ebbc4
WillDixon93 <dixon.will1@gmail.com>
2021-01-17 12:06:43 +0000

    spare working

--- spec/bowlSpec.js
+++ spec/bowlSpec.js
@@ -16,6 +16,26 @@ describe('Game', function(){
    expect(game.score).toEqual(20);
  });
 
+ it('should return a score for a spare', function(){
+   game.roll(5);
+   game.roll(5);
+   game.roll(3);
+   rollMany(0, 17)
+   expect(game.score).toEqual(16)
+ });
+
+ it('should return the score for a strike', function(){
+   game.roll(10);
+   game.roll(1);
+   game.roll(1);
+   expect(game.score).toEqual(14)
+ });
+
+//  it('should return 300 for a perfect game', function(){
+//    rollMany(12, 10)
+//    expect(game.score).toEqual(300);
+//  })
+
  function rollMany(pins, rolls) {
   for (let i = 0; i < 20; i++) {
     game.roll(pins);
