a5e8257ea3e898e14887321a08cd428abcdf84db
CodeRed30 <katrinaharradine@gmail.com>
2021-02-05 20:57:22 +0000

    Simple game

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,5 +1,12 @@
 class Scorecard
   def score(array)
-    0
+    total = []
+    score = 0
+    frames = array.each_slice(2)
+    frames.each do |x|
+    score += x.sum
+    total << score
+    end
+    total
   end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -2,7 +2,22 @@ require 'scorecard'
 
 describe Scorecard do
   it 'returns a score' do
-    expect(subject.score([0])).to eq 0
+    expect(subject.score([0])).to eq [0]
   end
 
+  it 'returns a score when points not 0' do
+    expect(subject.score([1])).to eq [1]
+  end
+
+  it 'returns a score for a frame' do
+    expect(subject.score([1,1])).to eq [2]
+  end
+
+  it 'returns a score for two frames' do
+    expect(subject.score([1,1,1])).to eq [2,3]
+  end
+
+  it 'returns a score from a full array' do
+    expect(subject.score([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])).to eq [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  end
 end
