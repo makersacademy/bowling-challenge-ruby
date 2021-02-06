1785626d255207ddcb9baf56293b8fd1113e9d36
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-06 18:05:16 +0000

    refactored added private methods for strike and spare

--- lib/game.rb
+++ lib/game.rb
@@ -21,19 +21,35 @@ class Game
 
     TOTAL_FRAMES.times do
       @frame +=1
-      if @score[index] == MAX_POINTS
-        result += @score[index] + @score[index + 1] + @score[index + 2]
-      elsif @score[index] + @score[index + 1] == MAX_POINTS
-        result += @score[index] + @score[index + 1] + @score[index + 2]
+      if strike?(index)
+        result += strike(index)
+        index +=1
+      elsif spare?(index)
+        result += spare(index)
+        index += 2
       else
         result += @score[index] + @score[index + 1]
-      end
-      if @score[index] == MAX_POINTS
-        index +=1
-      else
         index += 2
       end
     end
     result
+  end
+
+  private
+
+  def strike?(index)
+    @score[index] == MAX_POINTS
+  end
+
+  def strike(index)
+    MAX_POINTS + @score[index + 1] + @score[index + 2]
+  end
+
+  def spare?(index)
+    @score[index] + @score[index + 1] == MAX_POINTS
+  end
+
+  def spare(index)
+    @score[index] + @score[index + 1] + @score[index + 2]
   end
 end
