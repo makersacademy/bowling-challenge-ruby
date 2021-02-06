1609b0a564f763dea1c559848cd1fea47357fce1
Hugh Cavanagh <hacavanagh@gmail.com>
2021-02-06 15:53:00 +0000

    Rolling strike changes frame number

--- lib/game.rb
+++ lib/game.rb
@@ -4,11 +4,13 @@ class Game
   def initialize
     @total_score = 0
     @current_bowl = 1
+    @current_frame = 1
   end
 
   def enter_score(score)
     @total_score += score
     @current_bowl = 2
+    @current_frame += 1 if score == 10
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,10 +4,11 @@ describe Game do
   let(:game) { described_class.new }
 
   describe '#enter_score' do
+    subject { game.enter_score(score) }
+
     context 'when it is the first bowl of a frame' do
       context 'when you enter a score of 0-9' do
         let(:score) { 7 }
-        subject { game.enter_score(score) }
         it 'changes the total score by that amount' do
           expect { subject }.to change { game.total_score }.by score
         end
@@ -15,7 +16,14 @@ describe Game do
           expect { subject }.to change { game.current_bowl }.to 2
         end
         it 'maintains the current frame' do
-          expect { subject }.not_to change { game.current_frame }
+          expect { subject }.not_to(change { game.current_frame })
+        end
+      end
+
+      context 'when you get a strike (enter 10)' do
+        let(:score) { 10 }
+        it 'changes the frame' do
+          expect { subject }.to change { game.current_frame }.by 1
         end
       end
     end
