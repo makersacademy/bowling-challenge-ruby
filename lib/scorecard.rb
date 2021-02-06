756198e2a2231157db71fdc439414c5a32039013
CodeRed30 <katrinaharradine@gmail.com>
2021-02-06 13:34:46 +0000

    refactor

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,29 +1,50 @@
+# frozen_string_literal: true
+
 class Scorecard
   def score(array)
+    find_strikes(array)
+    total = bowling_calulation(array)
+    ten_frames(total)
+  end
+
+  private
+
+  def find_strikes(array)
+    strikes = [] # to store strikes in the array
+    array.each_with_index do |roll, idx|
+      strikes << idx if roll == 10
+    end
+    strikes.reverse.each do |str_idx|
+      array.insert((str_idx + 1), 0)
+    end
+  end
+
+  def ten_frames(total)
+    total.pop unless total.length <= 10
+    total
+  end
+
+  def bowling_calulation(array)
     total = [] # where the score will be returned from
     score = 0 # to calculate the running total
-    strikes = []
-    array.each_with_index do |roll, idx|
-        if roll == 10
-          strikes << idx
-        end
-      end
-    strikes.reverse.each do |str_idx|
-       array.insert((str_idx+1), 0)
-    end
     frames = array.each_slice(2).to_a # to split the array score into frames
     frames.each_with_index do |roll, idx| # seperate out each frame to calculate
-      if roll.sum == 10 && roll[1] == 0 # for a strike
-        score += (roll.sum + frames[idx+1].sum)
-      elsif roll.sum == 10 # for a spare
-        score += (roll.sum + frames[idx+1][0])
-      else
-        score += roll.sum # calculate the score and add it to the total
-      end
+      score += if roll.sum == 10 && (roll[1]).zero? # for a strike
+                 if frames[idx + 1].sum == 10 && (roll[1]).zero? # roll 2 is strike
+                   if frames[idx + 2].sum == 10 && (roll[1]).zero? # roll 3 is a strike
+                     (roll.sum + frames[idx + 1].sum + frames[idx + 2].sum)
+                   else
+                     (roll.sum + frames[idx + 1].sum + frames[idx + 2][0])
+                   end
+                 else # roll 2 is not a strike
+                   (roll.sum + frames[idx + 1].sum)
+                 end
+               elsif roll.sum == 10 # for a spare
+                 (roll.sum + frames[idx + 1][0])
+               else
+                 roll.sum # calculate the score and add it to the total
+               end
       total << score # add the score to the scorecard array
-    end
-    unless total.length <= 10
-      total.pop
     end
     total
   end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'scorecard'
 
 describe Scorecard do
@@ -10,47 +12,84 @@ describe Scorecard do
   end
 
   it 'returns a score for a frame' do
-    expect(subject.score([1,1])).to eq [2]
+    expect(subject.score([1, 1])).to eq [2]
   end
 
   it 'returns a score for two frames' do
-    expect(subject.score([1,1,1])).to eq [2,3]
+    expect(subject.score([1, 1, 1])).to eq [2, 3]
   end
 
   it 'returns a score from 10 frames' do
-    expect(subject.score([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])).to eq [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    expect(subject.score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                          0])).to eq [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   end
 
   it 'returns a calculated score from 10 frames' do
-    expect(subject.score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])).to eq [2,4,6,8,10,12,14,16,18,20]
+    expect(subject.score([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                          1])).to eq [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
   end
 
   it 'calculates a single spare with a bonus in frames 1-9' do
-    expect(subject.score([4,6,3,3])).to eq [13,19]
+    expect(subject.score([4, 6, 3, 3])).to eq [13, 19]
   end
 
   it 'calculates multiple spares with a bonus in frames 1-9' do
-    expect(subject.score([4,6,3,3,7,3,2,2,1,9,8,1,3,7,7,3,2,2,2,2])).to eq [13,19,31,35,53,62,79,91,95,99]
+    expect(subject.score([4, 6, 3, 3, 7, 3, 2, 2, 1, 9, 8, 1, 3, 7, 7, 3, 2, 2, 2,
+                          2])).to eq [13, 19, 31, 35, 53, 62, 79, 91, 95, 99]
   end
 
   it 'calculates multiple spares with a bonus in frames 10' do
-    expect(subject.score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,1])).to eq [2,4,6,8,10,12,14,16,18,29]
+    expect(subject.score([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9,
+                          1])).to eq [2, 4, 6, 8, 10, 12, 14, 16, 18, 29]
   end
 
   it 'calculates multiple spares with a bonus in frames 10' do
-    expect(subject.score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,1])).to eq [2,4,6,8,10,12,14,16,18,29]
+    expect(subject.score([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9,
+                          1])).to eq [2, 4, 6, 8, 10, 12, 14, 16, 18, 29]
   end
 
   it 'calculates multiple spares with a bonus in all frames' do
-    expect(subject.score([4,6,8,2,2,9,1,9,9,1,6,4,9,1,5,5,5,5,6,4,7])).to eq [18,30,41,60,76,95,110,125,141,158]
+    expect(subject.score([4, 6, 8, 2, 2, 9, 1, 9, 9, 1, 6, 4, 9, 1, 5, 5, 5, 5, 6, 4,
+                          7])).to eq [18, 30, 41, 60, 76, 95, 110, 125, 141, 158]
   end
 
   it 'calculates a single strike in a frame' do
-    expect(subject.score([10,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])).to eq [12,14,16,18,20,22,24,26,28,30]
+    expect(subject.score([10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                          1])).to eq [12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
   end
 
   it 'calculates multiple non consecutive strikes in a game(no spares)' do
-    expect(subject.score([10,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])).to eq [12,14,16,18,20,22,24,26,28,30]
+    expect(subject.score([10, 3, 4, 10, 2, 2, 10, 0, 2, 10, 3, 4, 10, 3,
+                          3])).to eq [17, 24, 38, 42, 54, 56, 73, 80, 96, 102]
   end
 
+  it 'calculates multiple non consecutive strikes in a game(with spares non consecutive)' do
+    expect(subject.score([10, 3, 4, 3, 7, 3, 3, 10, 3, 3, 9, 1, 7, 1, 10, 1,
+                          1])).to eq [17, 24, 37, 43, 59, 65, 82, 90, 102, 104]
+  end
+
+  it 'calculates 2 consecutive strikes in a game' do
+    expect(subject.score([10, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                          1])).to eq [21, 33, 35, 37, 39, 41, 43, 45, 47, 49]
+  end
+
+  it 'calculates 3 consecutive strikes in a game' do
+    expect(subject.score([10, 10, 10, 1, 2, 3, 4, 5, 4, 6, 3, 2, 1, 0, 2, 9,
+                          0])).to eq [30, 51, 64, 67, 74, 83, 92, 95, 97, 106]
+  end
+
+  it 'calculates more than 3 consecutive strikes in a game' do
+    expect(subject.score([10, 10, 10, 10, 10, 9, 0, 6, 2, 8, 2, 3, 4, 2,
+                          3])).to eq [30, 60, 90, 119, 138, 147, 155, 168, 175, 180]
+  end
+
+  it 'calculates 9 strikes' do
+    expect(subject.score([10, 10, 10, 10, 10, 10, 10, 10, 10, 9,
+                          0])).to eq [30, 60, 90, 120, 150, 180, 210, 239, 258, 267]
+  end
+
+  it 'calculates a 9 strikes and a spare' do
+    expect(subject.score([10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 1,
+                          1])).to eq [30, 60, 90, 120, 150, 180, 210, 239, 259, 270]
+  end
 end
