e81ec72d813f2df900feee499b8d564ad5c3e2a4
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-06 11:49:50 +0000

    add method to Frame to determine if it's completed or not

--- lib/frame.rb
+++ lib/frame.rb
@@ -17,4 +17,14 @@ class Frame
     rolls << pins
   end
 
+  def completed?
+    if number < 10
+      rolls.length == 2 || rolls[0] == 10
+    elsif rolls[0, 2].sum < 10
+      rolls.length == 2
+    else
+      rolls.length == 3
+    end
+  end
+
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -8,14 +8,54 @@ describe Frame do
     expect(Frame.new.number).to eq 3
   end
 
-  it "can have rolls added to it" do
-    frame = Frame.new
-    frame.add_roll(3)
-    expect(frame.rolls).to include 3
+  context "contains rolls, frames 1-9" do
+
+    let(:frame) { Frame.new }
+
+    it "can have rolls added to it" do
+      frame.add_roll(3)
+      expect(frame.rolls).to include 3
+    end
+
+
+    it "knows if frame is ended (except 10th frame)" do
+      frame.add_roll(3)
+      frame.add_roll(2)
+      expect(frame).to be_completed
+    end
+
+    it "knows if frame ended with a strike (except 10th frame)" do
+      frame.add_roll(10)
+      expect(frame).to be_completed
+    end
+
   end
 
+  context "10th frame" do
 
-  xit "stores if it's completed" do
+    before do
+      @frame = Frame.new
+      allow(@frame).to receive(:number).and_return 10
+    end
+
+    it "is completed with 2 rolls if no strike/spare" do
+      @frame.add_roll(3)
+      @frame.add_roll(2)
+      expect(@frame).to be_completed
+    end
+
+    it "is not completed with 2 rolls if a spare" do
+      @frame.add_roll(3)
+      @frame.add_roll(7)
+      expect(@frame).not_to be_completed
+    end
+
+    it "is completed with 3 rolls if a spare/strike" do
+      @frame.add_roll(10)
+      @frame.add_roll(7)
+      @frame.add_roll(8)
+      expect(@frame).to be_completed
+    end
 
   end
 
