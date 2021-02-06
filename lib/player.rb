b446abb6e1c4e685410e6999fa65ba3b69c90820
acavalla <anna.cavalla@gmail.com>
2021-02-06 23:08:42 +0000

    rspec initialized, player class added

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,6 @@
 class Player
   attr_reader :roll
+  
   def roll
     "What is your roll?"
     @roll = gets.chomp
--- spec/units/player_spec.rb
+++ spec/units/player_spec.rb
@@ -2,4 +2,11 @@ require 'player'
 
 describe Player do
   it { is_expected.to respond_to(:roll)}
+
+  describe "roll" do
+    it 'opens a frame instance' do
+      subject.roll
+      expect(subject.roll).to eq roll
+    end
+  end
 end
