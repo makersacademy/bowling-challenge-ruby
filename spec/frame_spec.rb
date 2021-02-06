de63d414958a0b16ba6990be07432f6805a25361
Hugh Cavanagh <hacavanagh@gmail.com>
2021-02-06 17:19:07 +0000

    More testing for adding bonus score

--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -3,6 +3,7 @@ require './lib/frame'
 describe Frame do
   let(:frame) { described_class.new }
   let(:score) { 5 }
+  let(:strike) { 10 }
 
   describe '#add_score' do
     it 'should change the total score by the amount added' do
@@ -14,11 +15,24 @@ describe Frame do
         expect { frame.add_score(score) }.to change { frame.need_bonus? }.from(false).to(true)
       end
     end
+    context 'when you get a strike' do
+      it 'should set bonus_needed to true' do
+        expect { frame.add_score(strike) }.to change { frame.need_bonus? }.from(false).to(true)
+      end
+    end
   end
 
   describe '#add_bonus_score' do
     it 'should change the total score by the amount added' do
       expect { frame.add_bonus_score(score) }.to change { frame.total_score }.by score
+    end
+    context 'when it is not the final frame' do
+      context 'when the main score was a spare' do
+        it 'should immediately set bonus_needed to false' do
+          2.times { frame.add_score(score) }
+          expect { frame.add_bonus_score(score) }.to change { frame.need_bonus? }.from(true).to(false)
+        end
+      end
     end
   end
 
