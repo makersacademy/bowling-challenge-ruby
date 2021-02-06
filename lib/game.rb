2ce212b3df630ddb7cbc565691963435d60a5ca7
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-06 16:06:22 +0000

    added tests for gutter game and spare non refctored code to pass these

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,9 @@
 class Game
   attr_reader :score, :frame
 
+  TOTAL_FRAMES = 10
+  MAX_POINTS = 10
+
   def initialize
     @frame = 0
     @score = []
@@ -10,5 +13,21 @@ class Game
 
   def roll(pins)
     @score << pins
+  end
+
+  def bowling_game
+    result = 0
+    index = 0
+
+    TOTAL_FRAMES.times do
+      @frame +=1
+      if @score[index] + @score[index + 1] == 10
+        result += @score[index] + @score[index + 1] + @score[index + 2]
+      else
+        result += @score[index] + @score[index + 1]
+      end
+      index += 2
+    end
+    result
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,8 +4,8 @@ describe Game do
   let(:game) { described_class.new }
 
   describe 'initialize' do
-    it 'has two attributes score and frame' do
-      expect(game).to respond_to(:frame, :score)
+    it 'has three public methods' do
+      expect(game).to respond_to(:frame, :score, :bowling_game)
     end
 
     it 'score is an empty array' do
@@ -20,12 +20,33 @@ describe Game do
   describe '#roll' do
     it 'takes pins and places them into the score' do
       expect{ game.roll(9) }.to change{ game.score.sum }.by(9)
-      game.roll(2)
-      game.roll(3)
-      game.roll(1)
+      expect{ game.roll(6) }.to change { game.score.count}.by(1)
+
+      allow(game).to receive(:score).and_return([9,2,3,1])
       expect(game.score).to eq([9,2,3,1])
       expect(game.score.sum).to eq(15)
       expect(game.score.count).to eq(4)
+    end
+  end
+
+  describe '#bowling_game?' do
+
+    it 'adds 10 to frames' do
+      20.times { game.roll(1) }
+      expect{ game.bowling_game }.to change { game.frame}.by(10)
+    end
+
+    it 'returns 0 when you play a gutter game' do
+      20.times { game.roll(0) }
+      expect(game.bowling_game).to eq(0)
+    end
+
+    it 'can work out a spare' do
+      2.times { game.roll(5) } #10 + 1
+      2.times { game.roll(1) } #11 + 2
+      2.times { game.roll(5) } #13 + 11
+      14.times { game.roll(1) } #24 + 14
+      expect(game.bowling_game).to eq(38)
     end
   end
 end
