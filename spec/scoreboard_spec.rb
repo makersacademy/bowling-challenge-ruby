27aa71e41a7fcd01ce77205142488f19f8311455
Glykeria Stravodimou <strglykeria@gmail.com>
2021-02-06 14:02:15 +0000

    created class Scoreboard

--- spec/scoreboard_spec.rb
+++ spec/scoreboard_spec.rb
@@ -1,5 +1,11 @@
 require 'scoreboard'
 
 describe Scoreboard do
-  
+  subject(:game) { described_class.new }
+
+  describe '#score' do
+    it 'starts with 0 score' do
+      expect(game.score).to eq 0
+    end
+  end
 end
