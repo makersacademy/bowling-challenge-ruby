427518500d7e69dd08e89a847368897bdb1f6c44
CodeRed30 <katrinaharradine@gmail.com>
2021-02-06 11:27:45 +0000

    Spares in frames 1-9

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,12 +1,16 @@
 class Scorecard
   def score(array)
-    total = []
-    score = 0
-    frames = array.each_slice(2)
-    frames.each do |x|
-    score += x.sum
-    total << score
+    total = [] # where the score will be returned from
+    score = 0 # to calculate the running total
+    frames = array.each_slice(2).to_a # to split the array score into frames
+    frames.each_with_index do |roll, idx| # seperate out each frame to calculate
+      unless roll.sum == 10
+        score += roll.sum # calculate the score and add it to the total
+      else
+        score += (roll.sum + frames[idx+1][0])
+      end
+      total << score # add the score to the scorecard array
     end
-    total
+    total #return the scorecard array
   end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -17,7 +17,15 @@ describe Scorecard do
     expect(subject.score([1,1,1])).to eq [2,3]
   end
 
-  it 'returns a score from a full array' do
+  it 'returns a score from 10 frames' do
     expect(subject.score([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])).to eq [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  end
+
+  it 'returns a calculated score from 10 frames' do
+    expect(subject.score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])).to eq [2,4,6,8,10,12,14,16,18,20]
+  end
+
+  it 'calculates a spare with a bonus in frames 1-9' do
+    expect(subject.score([4,6,3,3])).to eq [13,19]
   end
 end
