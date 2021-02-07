fc24eb2e56e83915d25ef67e5477945dcde8bcc5
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-02-07 22:13:36 +0000

    Added #add_roll and #finished? methods to Frame and updated unit test

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,7 +1,16 @@
 class Frame
-    attr_reader :number
+    attr_reader :number, :rolls
 
     def initialize(number)
         @number = number
+        @rolls = []
+    end
+
+    def add_roll(pins)
+        @rolls << pins
+    end
+
+    def finished?
+        rolls[0] == 10 || rolls.length > 1
     end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -7,4 +7,15 @@ describe Frame do
         expect(frame.number).to eq(1)
     end
 
+    it 'adds a roll' do
+        frame.add_roll(3)
+        expect(frame.rolls.last).to eq(3)
+    end
+
+    it 'knows when the frame is finished' do
+        frame.add_roll(3)
+        frame.add_roll(3)
+        expect(frame).to be_finished
+    end
+
 end
