41d037b6a316b8aa503387d5ef887ae8b1f7d7bd
Reeshul <reeshul@hotmail.com>
2021-02-07 16:42:01 +0000

    passed feature test

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,7 +1,60 @@
 class Scorecard
 
+  def initialize
+    @frames = [[], [], [], [], [], [], [], [], [], []]
+  end
+
   def self.create
     Scorecard.new
+  end
+
+  def score(roll_scores)
+    @roll_scores = roll_scores
+    roll_score
+    frame_score
+    running_cumulative_frame_scores
+  end
+
+  def roll_score
+    frame_index = 0
+
+    @roll_scores.each do | roll_score |
+      @frames[frame_index] << roll_score
+      frame_index += 1 if new_frame?(frame_index)
+    end
+  end
+
+  def frame_score
+    @frames.each_with_index do | frame, frame_index |
+      if frame.sum < 10 || frame_index == 9
+        frame
+      elsif frame.length == 2
+        frame << @frames[frame_index + 1][0]
+      elsif @frames[frame_index + 1].length >= 2
+        frame << @frames[frame_index + 1][0]
+        frame << @frames[frame_index + 1][1]
+
+      else
+        frame << @frames[frame_index + 1][0]
+        frame << @frames[frame_index + 2][0]
+      end
+    end
+  end
+
+  def running_cumulative_frame_scores
+    prior_frame = 0
+    @frames.map do | frame |
+      frame = frame.sum + prior_frame
+      prior_frame = frame
+    end
+  end
+
+  def strike?(frame)
+    true if frame[0] == 10
+  end
+
+  def new_frame?(frame_index)
+    (@frames[frame_index].length == 2 || strike?(@frames[frame_index])) && frame_index < 9
   end
 
 end
--- spec/unit/scorecard_spec.rb
+++ spec/unit/scorecard_spec.rb
@@ -1,15 +1,31 @@
+EXAMPLES = {
+  [0] => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10] => [30, 60, 90, 120, 150, 180, 210, 240, 270, 300],
+  [0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0] => [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+  [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6] => [5,14,29,49,60,61,77,97,117,133]
+}
+
 describe Scorecard do
+
+  let ( :scorecard ) { Scorecard.create }
   
   describe ".create" do
     it "should create a new scorecard" do
-      expect(Scorecard.create).to be_a Scorecard
+      expect(scorecard).to be_a Scorecard
     end
   end
 
   describe ".score" do
     it "should have one argument" do
-      expect(Scorecard).to respond_to(:score).with(1).argument
+      expect(scorecard).to respond_to(:score).with(1).argument
+    end
+    
+    EXAMPLES.each do |input, output|
+      it "returns #{output} when the input is #{input}" do
+        result = scorecard.score(input)
+        expect(result).to eq output
+      end
     end
   end
-
 end
