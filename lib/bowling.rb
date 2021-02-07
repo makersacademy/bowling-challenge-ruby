517091d62c2c384886f3f1f0f197915e16869e8b
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-02-07 19:14:38 +0000

    Spare and Strike tests passing and refactored

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -11,11 +11,31 @@ class Bowling
   def score
     result = 0
     rollIndex = 0
-    20.times do
-      result += @rolls[rollIndex]
-      rollIndex += 1
+    10.times do
+      if @rolls[rollIndex] == 10
+        result += @rolls[rollIndex] + @rolls[rollIndex + 1] + @rolls[rollIndex + 2]
+        rollIndex += 1
+      elsif spare?(rollIndex)
+        result += spareScore(rollIndex)
+        rollIndex += 2
+      else
+        result += frameScore(rollIndex)
+        rollIndex += 2
+      end
     end
-    result 
+    result
+  end
+
+  def spare?(rollIndex)
+    @rolls[rollIndex] + @rolls[rollIndex + 1] == 10
+  end
+
+  def spareScore(rollIndex)
+    @rolls[rollIndex] + @rolls[rollIndex + 1] + @rolls[rollIndex + 2]
+  end
+
+  def frameScore(rollIndex)
+    @rolls[rollIndex] + @rolls[rollIndex + 1]
   end
 
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -20,4 +20,21 @@ describe 'A bowling game' do
     expect(@game.score).to eq 20
   end
 
+  it 'can roll a spare' do
+    @game.roll(5)
+    @game.roll(5)
+    @game.roll(3)
+    17.times{@game.roll(0)}
+    expect(@game.score).to eq 16
+  end
+
+  it 'can roll a strike' do
+    @game.roll(10)
+    @game.roll(3)
+    @game.roll(4)
+    16.times{@game.roll(0)}
+    expect(@game.score).to eq 24
+  end 
+
+
 end
