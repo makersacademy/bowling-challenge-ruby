e50c22035edfff44deab9aa66a3cf9b561c9653f
HolsDuckett <hf.duckett@gmail.com>
2021-02-07 15:58:56 +0000

    changed approach - basic game play implemented but no tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,77 @@
 class Game
 
-  attr_reader :total_score
-
   def initialize
     @total_score = 0
+    @bowl_history = []
+    @strike = false
+    @spare = false
+    @current_frame = 1
   end
 
-  def self.play
-    Game.new
+
+  def play_frame
+    puts "Whats is the score of your first bowl?"
+    bowl_1 = gets.chomp.to_i
+      if bowl_1 == 10
+        bowl_2 = 0
+      else
+        puts "Whats is the score of your second bowl?"
+        bowl_2 = gets.chomp.to_i
+      end
+    frame_score = calculate_frame_score(bowl_1, bowl_2)
+    @bowl_history << [bowl_1, bowl_2]
+    @total_score += frame_score
+    p "Your current score is #{@total_score}, not including pending bonus points ;)"
+    # below evaluates whether the current frame is a strike/spare/neither, 
+    # so it changes the variable value for the next frame to adjust its scores accordingly
+    strike?(bowl_1)
+    spare?(bowl_1, bowl_2)
   end
 
-  def bowl(score)
-    @total_score += score
-    p @total_score
+  # def bonus_round
+  #   if current_frame
+  # end
+
+  def calculate_frame_score(bowl_1, bowl_2)
+    if @strike == true
+      #this adds the
+      score = (bowl_1 + bowl_2) * 2
+    elsif @spare == true
+      score = (bowl_1 * 2) + bowl_2
+    elsif
+      score = bowl_1 + bowl_2
+    end
+    return score
   end
 
+
+  def strike?(bowl_1)
+    if bowl_1 == 10
+      @strike = true
+    else
+      @strike = false
+    end
+  end
+
+  def spare?(bowl_1, bowl_2)
+    if bowl_1 + bowl_2 == 10
+      @spare = true
+    else
+      @spare = false
+    end
+  end
+
+
+  def play_game
+    while @current_frame < 11
+      puts "Frame number: #{@current_frame}"
+      play_frame
+      @current_frame += 1
+    end
+    puts "Fun has stoppped folks"
+    puts "Total score: #{@total_score}"
+  end
 
 end
+
+Game.new.play_game
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,34 +1,43 @@
-require 'game'
-
-describe Game do
-  subject(:game) { described_class.play }
-
-    describe '.play' do
-      it "should allow game to respond to play" do
-      expect(Game.play).to be_a(Game)
-    end
-  end
-
-    describe '.bowl' do
-      it "should allow the user to input the score" do
-      expect(game).to respond_to(:bowl).with(1).argument
-    end
-
-    it "should return the total score" do
-      game = Game.play
-      game.bowl(7)
-      game.bowl(2)
-
-      expect(game.total_score).to eq(9)
-    end
-  end
-
-
-end
-
-
-
-# game.roll(7)
-# game.roll(2)
-# game.Score
-# => 9
+# require 'game'
+#
+# describe Game do
+#   subject(:game) { described_class.play }
+#
+#   describe '.play' do
+#     it "should allow game to respond to play" do
+#     expect(Game.play).to be_a(Game)
+#     end
+#   end
+#
+#   describe '#bowl' do
+#     it "should allow the user to input the score" do
+#       expect(game).to respond_to(:bowl).with(2).argument
+#     end
+#
+#     it "should return the total score" do
+#       game.bowl(7, 2)
+#       expect(game.total_score).to eq(9)
+#     end
+#   end
+#
+#   describe "#completed?" do
+#     it "raises an error if player tries to input a bowl after 10th frame is completed" do
+#       allow(game.bowl_history).to receive(:length).and_return(10)
+#       expect{ game.bowl(1,2) }.to raise_error("STOP. No more fun.")
+#     end
+#   end
+#
+#   describe "#spare" do
+#     it "should double the next bowls score" do
+#       game.bowl(6,4)
+#       game.bowl(3,4)
+#       expect(game.total_score).to eq(24)
+#     end
+#
+#   end
+#
+#   # describe "#spare" do
+#   #
+#   # end
+#
+# end
