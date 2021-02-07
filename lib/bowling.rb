15c0ba41ae867a6ac4243a076c5ee61f27882f5b
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-02-07 14:16:22 +0000

    programme asums the pin count for each roll

---
+++ lib/bowling.rb
@@ -0,0 +1,12 @@
+class Bowling
+  attr_reader :score
+
+  def initialize
+    @score = 0
+  end
+
+  def hit(pin_count)
+    @score += pin_count
+  end
+
+end
---
+++ spec/bowling_spec.rb
@@ -0,0 +1,15 @@
+require 'bowling'
+
+describe Bowling, "#score" do
+
+  context "with no strikes or spares" do
+
+    it "sums the pin count for each roll" do
+      bowling = Bowling.new
+      20.times { bowling.hit(4) }
+      expect(bowling.score).to eq 80
+    end
+
+  end
+
+end
