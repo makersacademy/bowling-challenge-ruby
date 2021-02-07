ba81795aa15f8b694b179d9af2017be9a8978a15
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-02-07 22:31:18 +0000

    Added #finished? to Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,13 @@ require 'game'
 describe Game do
     subject(:game) { described_class.new }
     let(:frame) { double :frame}
-    
+
     it 'adds a frame' do
         expect { game.add_frame(frame) }.to change { game.frames.length }.by(1)
+    end
+
+    it 'knows when the game is finished' do
+        10.times { || game.add_frame(frame) }
+        expect(game).to be_finished
     end
 end
