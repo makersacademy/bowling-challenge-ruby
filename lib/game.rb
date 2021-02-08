03d9b51a65f343c1946ba757fac1a6a8e7dec63b
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-02-08 09:50:19 +0000

    Some refacotring done

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,4 @@
 class Game
-
   def initialize
     @rolls = []
   end
@@ -13,17 +12,29 @@ class Game
     index = 0
     10.times do
       if @rolls[index] == 10
-        result += @rolls[index].to_i + @rolls[index + 1].to_i + @rolls[index + 2].to_i
+        result += spare_result(index)
         index += 1
-      elsif @rolls[index].to_i + @rolls[index + 1].to_i == 10
-        result += @rolls[index].to_i + @rolls[index + 1].to_i + @rolls[index + 2].to_i
-        index = index + 2
-      elsif
-        result += @rolls[index].to_i + @rolls[index + 1].to_i
-        index = index + 2
+      elsif spare(index)
+        result += spare_result(index)
+        index += 2
+      elsif result += regular_result(index)
+        index += 2
       end
     end
     result
   end
 
+  private
+
+  def spare(index)
+    @rolls[index].to_i + @rolls[index + 1].to_i == 10
+  end
+
+  def spare_result(index)
+    @rolls[index].to_i + @rolls[index + 1].to_i + @rolls[index + 2]
+  end
+
+  def regular_result(index)
+    @rolls[index].to_i + @rolls[index + 1].to_i
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,18 +1,17 @@
 require 'game'
 
 describe Game do
-
   it 'can respond to score' do
     expect(subject).to respond_to(:score)
   end
 
   it 'can return score of O if all fail' do
-    20.times{ subject.roll(0) }
+    20.times { subject.roll(0) }
     expect(subject.score).to eq 0
   end
 
   it 'can return score of 20 without strike or spare' do
-    20.times{ subject.roll(1) }
+    20.times { subject.roll(1) }
     expect(subject.score).to eq 20
   end
 
@@ -30,4 +29,8 @@ describe Game do
     expect(subject.score).to eq 28
   end
 
+  it 'perfect game' do
+    12.times { subject.roll(10) }
+    expect(subject.score).to eq 300
+  end
 end
