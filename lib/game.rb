bdbfb526dc4de8e0e7af4e39fe8f1be1a69ffdd3
sandy <pounoussamy.sandy@gmail.com>
2021-03-06 17:38:42 +0000

    added Game with @totale_frames, @frame and #add_frame

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,13 @@
 class Game
+  attr_reader :total_frames, :frames
+  FRAMES = 10
 
+  def initialize
+    @total_frames = FRAMES
+    @frames = []
+  end
+
+  def played(frame_score)
+    @frames << frame_score
+  end 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,14 @@
 require 'game'
 
 describe Game do
+  it 'has a max of ten frames' do
+    expect(subject.total_frames).to eq 10
+  end
 
+  it 'knows the score of all played frames' do
+    frame_one = instance_double('Frame', :score => 8)
+    subject.played(frame_one.score)
+
+    expect(subject.frames).to include 8
+  end
 end
