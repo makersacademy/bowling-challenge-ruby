9caf67c5c1d899e31cdc88b2050b8357db8920a8
iainaitken <iain_aitken@yahoo.co.uk>
2021-03-06 19:07:44 +0000

    adding incomplete scorer class files and tests

--- lib/runfile.rb
+++ lib/runfile.rb
@@ -3,3 +3,4 @@
 require_relative 'bowl'
 require_relative 'frame'
 require_relative 'game'
+require_relative 'scorer'
---
+++ lib/scorer.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class Scorer
+  # def calculate(frames:)
+  #   total_score = 0
+  #   frames.each do |frame|
+  #     frame.bowls.each { |bowl| total_score += bowl.pins }
+  #   end
+  #   return total_score
+  # end
+
+  # def calculate(frames:)
+  #   points = convert_frames_to_array_of_points(frames)
+  #   points.each do |frame|
+  #     check_strike(frame)
+      # Check for spare
+      # Work out score for frame
+      # add that score to array
+    # end
+  # end
+
+  # total scores in array\
+
+  # send array back to game - new method in game that takes array and total as arguments
+
+  # def check_strike(frame)
+  #   if frame == 10
+  # end
+
+  # def check_spare
+    # if total of bowls.pins == 10
+  #end
+
+  private
+
+  # def convert_frames_to_array_of_points(frames)
+  #   point_array = []
+  #   frames.each do |frame|
+  #     point_array.push(frame.bowls.map(&:pins).sum)
+  #   end
+  #   point_array
+  # end
+end
