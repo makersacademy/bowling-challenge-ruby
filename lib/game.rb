2bcbdba1aa33d09342b2b13cd49ee737c74a4e41
charlierdm <charlie.davismiller@gmail.com>
2021-03-06 17:17:45 +0000

    added roll_1 and roll_2, developing Game class

--- lib/game.rb
+++ lib/game.rb
@@ -18,7 +18,27 @@ end
 
   def begin_frame
     @turn += 1
-    @current_frame = Frame.new
+    @current_frame = Frame.new(turn)
   end
+
+  def mark_score(points)
+    current_frame.add_roll(points)
+  end
+
+  def spare_or_strike?
+    return "spare!" if current_frame.spare?
+    return "strike!" if current_frame.strike?
+  end
+
+  def roll_1(points)
+    mark_score(points)
+    spare_or_strike?
+  end
+
+  def roll_2(points)
+    mark_score(points)
+    spare_or_strike?
+  end
+  
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,6 +12,22 @@ describe Game do
     end
   end
 
+  describe '#roll_1' do
+    before(:each) do
+      subject.begin_frame
+    end
+
+    it 'returns strike! if the user throws a strike' do
+      expect(subject.roll_1(10)).to eq("strike!")
+    end
+
+    it 'adds the users points to the current frame score' do
+      subject.roll_1(7)
+      expect(subject.current_frame.score[0]).to eq(7)
+    end
+  end
+
+  
 
 
 end
