01e7a9d8f16c713a042b6bca2e8a4428c29b88de
sandy <pounoussamy.sandy@gmail.com>
2021-03-06 20:09:13 +0000

    works for basic scoring without edge case

--- scorecard.rb
+++ scorecard.rb
@@ -5,14 +5,15 @@ game = Game.new
 while game.frames.count < game.total_frames
   frame = Frame.new
   while !frame.completed? do
-    puts "input number of pins knocked down"
+    puts "Input number of pins knocked down"
     throw = STDIN.gets.chomp
     frame.add(throw.to_i)
   end
   if frame.strike?
+    puts "Strike!\nNext frame"
     new_frame = Frame.new
     while !new_frame.completed? do
-      puts "input number of pins knocked down"
+      puts "Input number of pins knocked down"
       throw = STDIN.gets.chomp
       new_frame.add(throw.to_i)
       frame.add(throw.to_i)
@@ -21,7 +22,20 @@ while game.frames.count < game.total_frames
     game.add_frame(new_frame.score)
     puts "New frame score: #{new_frame.score}"
   elsif frame.spare?
-    # do something
+    puts "Spare!\nNext frame"
+    new_frame = Frame.new
+    puts "Input number of pins knocked down"
+    throw = STDIN.gets.chomp
+    new_frame.add(throw.to_i)
+    frame.add(throw.to_i)
+    game.add_frame(frame.score)
+    puts "Frame score: #{frame.score}"
+    
+    puts "Input number of pins knocked down"
+    throw = STDIN.gets.chomp
+    new_frame.add(throw.to_i)
+    game.add_frame(new_frame.score)
+    puts "New frame score: #{new_frame.score}"
   else
     game.add_frame(frame.score)
   end
