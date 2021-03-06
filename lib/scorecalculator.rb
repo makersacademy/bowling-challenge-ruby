39aac99c2096922fc9502a29a30667c460781d36
Christine Chee <christinechee@hotmail.com>
2021-03-06 16:35:39 +0000

    Adding minimum argument rule for when it's not a strike and statement for the last frame

--- lib/scorecalculator.rb
+++ lib/scorecalculator.rb
@@ -7,6 +7,8 @@ class ScoreCalculator
   end
 
   def input(*args)
+    raise "You must input score for two rolls unless you have a strike" if args != [10] && args.length == 1
+    status
     @sheet << args
     bonuspoints
     puts "You scored #{args.sum} for frame #{currentframe}. Your overall score so far is #{currentscore}"
@@ -26,10 +28,33 @@ class ScoreCalculator
   end
 
   def bonuspoints
-    if @sheet[currentframe-2].sum == 10 && @sheet[currentframe - 2].first != 10 && @sheet.length > 1
-      @sheet[currentframe-2] << @sheet[currentframe - 1].first
-    elsif @sheet[currentframe - 2].sum == 10 && @sheet[currentframe - 2].first == 10 && @sheet.length > 1
-      @sheet[currentframe-2] << @sheet[currentframe - 1].sum
+
+    #for continuous strikes
+    if @sheet.length >= 3 && @sheet[currentframe-3].first == 10 && @sheet[currentframe-2].first == 10
+      @sheet[currentframe-3] << @sheet[currentframe-1].first
     end
+    #for a spare
+    if @sheet[currentframe-2].sum == 10 && @sheet[currentframe - 2].first != 10 && @sheet.length > 1
+        @sheet[currentframe-2] << @sheet[currentframe - 1].first
+    #for a strike
+    elsif @sheet[currentframe - 2].sum == 10 && @sheet[currentframe - 2].first == 10 && @sheet.length > 1
+        @sheet[currentframe-2] << @sheet[currentframe - 1].sum
+    end
+  end
+
+  def frame_info(number)
+   puts "Frame #{number}: #{@sheet[number-1]} | #{@sheet[number-1].sum}"
+  end
+
+  def scoreboard
+    score = 0
+    @sheet.map.with_index do |x, i|
+      puts "Frame #{i+1}: #{x} | #{x.sum} | #{score += x.sum}"
+    end
+  end
+
+  def status
+    puts "This the last frame!" if currentframe == 9
+    raise "You can't input any more score as you've finished the game!" if currentframe > 9
   end
 end
--- spec/scorecalculator_spec.rb
+++ spec/scorecalculator_spec.rb
@@ -20,5 +20,53 @@ describe ScoreCalculator do
       subject.input(10)
       expect{subject.input(4, 5)}.to output("You scored 9 for frame 2. Your overall score so far is 28\n").to_stdout
     end
+
+    it 'adds bonus points to the overall score correctly if there was a strike after a strike' do
+      subject.input(10) #23
+      subject.input(10) #18
+      subject.input(3,5) #8
+      expect{subject.input(4,5)}.to output("You scored 9 for frame 4. Your overall score so far is 58\n").to_stdout #9
+    end
+
+    it 'includes bonus points in a different way allowing max of 3 rolls in the tenth frame where there is a strike' do
+      subject.input(3, 4)
+      subject.input(4, 5)
+      subject.input(2, 3)
+      subject.input(6, 2)
+      subject.input(2, 3)
+      subject.input(2, 6)
+      subject.input(2, 6)
+      subject.input(4, 5)
+      subject.input(5, 2)
+      expect{subject.input(10,10,9)}.to output("This the last frame!\nYou scored 29 for frame 10. Your overall score so far is 95\n").to_stdout
+    end
+  end
+
+  describe '#frame_info' do
+    it 'returns details about a specific frame' do
+      subject.input(1,5)
+      expect{subject.frame_info(1)}.to output("Frame 1: [1, 5] | 6\n").to_stdout
+    end
+  end
+
+  describe '#scoreboard' do
+    it 'shows the rolls for each frame so far' do
+      subject.input(1,5)
+      subject.input(4,5)
+      expect{subject.scoreboard}.to output("Frame 1: [1, 5] | 6 | 6\nFrame 2: [4, 5] | 9 | 15\n").to_stdout
+    end
+
+    it 'includes bonus points in the frame score when there is a strike' do
+      subject.input(10)
+      subject.input(4,5)
+      expect{subject.scoreboard}.to output("Frame 1: [10, 9] | 19 | 19\nFrame 2: [4, 5] | 9 | 28\n").to_stdout
+    end
+
+    it 'includes bonus points in the frame score when there is a spare' do
+      subject.input(5,5)
+      subject.input(3,3)
+      expect{subject.scoreboard}.to output("Frame 1: [5, 5, 3] | 13 | 13\nFrame 2: [3, 3] | 6 | 19\n").to_stdout
+    end
+
   end
 end
