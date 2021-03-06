38d2a212696e0a498054084c63c9325998ea518a
rhc07 <rorieclarke@gmail.com>
2021-03-06 17:43:41 +0000

    added an error message if wrong score input

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,12 +1,14 @@
 class BowlingGame
 
+TOTAL_PINS = 10
+
   def initialize
     @score_card = []
   end
 
   def roll(pins)
-   @total_pins = 10
-   @score_card << pins
+    fail "Invalid score, please try again." if pins > TOTAL_PINS
+    @score_card << pins
   end
 
   def score
--- spec/unit/bowling_game_spec.rb
+++ spec/unit/bowling_game_spec.rb
@@ -3,7 +3,7 @@ require 'bowling_game'
 describe BowlingGame do
 let(:game) {described_class.new}
 
-    describe '#.score' do
+    context 'adding to the scorecard' do
       it 'returns a score of zero' do
         game.roll(0)
         expect(game.score).to eq 0
@@ -16,6 +16,12 @@ let(:game) {described_class.new}
         game.roll(2)
         game.roll(3)
         expect(game.score).to eq 5
+      end
+    end
+
+    context 'user inputs too many pins' do
+      it 'throws an error if user inputs an invalid score' do
+        expect {game.roll(11)}.to raise_error "Invalid score, please try again."
       end
     end
 end
