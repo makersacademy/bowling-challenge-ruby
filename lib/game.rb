45b2c012ca534d34d6b649101799d71da2c01b54
olga <itsglimerry@gmail.com>
2021-03-07 17:39:31 +0000

    Finished logic for spares and strikes with tests.

--- lib/game.rb
+++ lib/game.rb
@@ -16,12 +16,16 @@ def score
   result = 0
   roll_index = 0
   10.times do
-    if spare?(roll_index)
+    if strike?(roll_index)
+      result += strikeScore(roll_index)
+      roll_index += 1
+    elsif spare?(roll_index)
       result += spareScore(roll_index)
+      roll_index += 2
     else
     result += frameScore(roll_index)
-    end
     roll_index += 2
+    end
   end
   result
 end
@@ -30,8 +34,16 @@ def spare?(roll_index)
   @rolls[roll_index] + @rolls[roll_index + 1 ] == 10
 end
 
+def strike?(roll_index)
+  @rolls[roll_index] == 10
+end
+
 def spareScore(roll_index)
-   @rolls[roll_index] + @rolls[roll_index + 1 ] + @rolls[roll_index + 2]
+   10 +  @rolls[roll_index + 2]
+end
+
+def strikeScore(roll_index)
+   10 +  @rolls[roll_index + 1 ] + @rolls[roll_index + 2]
 end
 
 def frameScore(roll_index)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -26,6 +26,24 @@ describe Game do
    expect(@game.score).to eq 16
  end
 
+ it 'can roll a strike' do
+   @game.roll(10)
+   @game.roll(3)
+   @game.roll(4)
+   16.times{@game.roll(0)}
+   expect(@game.score).to eq 24
+ end
+
+ it 'can roll a perfect game' do
+   12.times{@game.roll(10)}
+   expect(@game.score).to eq 300
+ end
+
+ it 'can roll all spares' do
+   21.times{@game.roll(5)}
+   expect(@game.score).to eq 150
+ end
+
 
 
 end
