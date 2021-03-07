c3da4abcb8d47c64fd1e2479dd5fa5d729f02a3a
AdamP2000 <Adam2000Phillips@gmail.com>
2021-03-07 05:43:01 +0000

    Passing test for strike score implementation

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -14,7 +14,10 @@ class Scorecard
     score = 0
     player_rolls_index = 0
     10.times do
-    if spare?(player_rolls_index)
+    if strike?(player_rolls_index)
+      score += strike_points(player_rolls_index)
+      player_rolls_index += 1
+    elsif spare?(player_rolls_index)
       score += spare_points(player_rolls_index)
       player_rolls_index += 2
     else
@@ -32,6 +35,14 @@ end
  def spare_points(player_rolls_index)
    @player_rolls[player_rolls_index] + @player_rolls[player_rolls_index + 1] + @player_rolls[player_rolls_index + 2]
  end
+
+ def strike?(player_rolls_index)
+    @player_rolls[player_rolls_index] == 10
+  end
+
+def strike_points(player_rolls_index)
+  @player_rolls[player_rolls_index] + @player_rolls[player_rolls_index + 1] + @player_rolls[player_rolls_index + 2]
+end
 
  def noSpare_noStrike_points(player_rolls_index)
    @player_rolls[player_rolls_index] + @player_rolls[player_rolls_index + 1]
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -14,4 +14,17 @@ describe Scorecard do
    17.times{scorecard.player_roll(0)}
    expect(scorecard.points).to eq 20
  end
+ it 'can implement the scoring system of a strike' do
+  scorecard = Scorecard.new
+  scorecard.player_roll(10)
+  scorecard.player_roll(3)
+  scorecard.player_roll(5)
+  16.times{scorecard.player_roll(0)}
+  expect(scorecard.points).to eq 26
+end
+it 'shows the score of a perfect game' do
+  scorecard = Scorecard.new
+  12.times{scorecard.player_roll(10)}
+  expect(scorecard.points).to eq 300
+end
 end
