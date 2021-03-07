6174a9577020cd1c6660bbb2a46ecce62439e1cf
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-03-07 13:04:43 +0100

    refactored to add private add_to_score method

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -9,21 +9,21 @@ class BowlingGame
 
     def new_frame(roll_1, roll_2 = 0)
         @frame += 1
-        @score << [roll_1, roll_2]
-
-        
+       
         if @frame == 11
             raise "Game Over, you have played all your frames" unless @score[9].include?(10)
         end
 
-    
         if @frame > 1 && @score[@frame -2].include?(10)
              @score[@frame -2] << [roll_1 ,roll_2]
+             @score << [roll_1, roll_2]
         elsif @frame > 1 && @score[@frame -2].inject(:+) == 10
             @score[@frame -2] << [roll_1]  
+            @score << [roll_1, roll_2]
+        else
+            @score << [roll_1, roll_2]
         end
-
-    end
+     end
 
   def total_score
     @score.flatten.inject(:+)
