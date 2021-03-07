d3de4c8526df1a4e46be29b422af94f1c70f683a
olga <itsglimerry@gmail.com>
2021-03-07 16:54:08 +0000

    Added logic and tests for rolling spares

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,7 @@
 class Game
 attr_reader :score
 
+
 def initialize
   @score = 0
   @rolls = []
@@ -12,7 +13,17 @@ def roll(pins)
 end
 
 def score
-  @rolls.sum
+  result = 0
+  roll_index = 0
+  10.times do
+    if @rolls[roll_index] + @rolls[roll_index + 1 ] == 10
+      result += @rolls[roll_index] + @rolls[roll_index + 1 ] + @rolls[roll_index + 2]
+    else
+    result += @rolls[roll_index] + @rolls[roll_index + 1 ]
+    end
+    roll_index += 2
+  end
+  result
 end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,18 +2,29 @@ require 'game'
 
 describe Game do
 
+  before do
+    @game = Game.new
+  end
+
  it 'can roll gutter game' do
-   game = Game.new
-   20.times{game.roll(0)}
-   expect(game.score).to eq 0
+
+   20.times{@game.roll(0)}
+   expect(@game.score).to eq 0
  end
 
  it 'can roll all ones' do
-   game = Game.new
-   20.times{game.roll(1)}
-   expect(game.score).to eq 20
+
+   20.times{@game.roll(1)}
+   expect(@game.score).to eq 20
  end
 
+ it 'can roll a spare' do
+   @game.roll(5)
+   @game.roll(5)
+   @game.roll(3)
+   17.times{@game.roll(0)}
+   expect(@game.score).to eq 16
+ end
 
 
 
