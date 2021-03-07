fb814a6a952cfc239554d98c999fbd8bbe75b681
charlierdm <charlie.davismiller@gmail.com>
2021-03-07 00:52:23 +0000

    vs code

--- lib/ui.rb
+++ lib/ui.rb
@@ -3,35 +3,35 @@ require_relative 'frame'
 
 class Ui
 
-  attr_reader :game, :scorecard
+attr_reader :game, :scorecard
 
-	def initialize (game = Game)
-	  @game = game.create
-		@scorecard = []
-	end
+  def initialize (game = Game)
+    @game = game.create
+    @scorecard = []
+  end
 
-	def throw_1(points = gets.chomp.to_i)	
-		game.roll(points)
-	end
+  def throw_1(points = gets.chomp.to_i)	
+    game.roll(points)
+  end
 
-	def throw_2(points = gets.chomp.to_i)	
-		game.roll(points)
-	end
+  def throw_2(points = gets.chomp.to_i)	
+   game.roll(points)
+  end
 
-	def add_to_scorecard
-		scorecard[-1] << game.get_score
-	end
+  def add_to_scorecard
+    scorecard[-1] << game.get_score
+  end
 
-	def begin_frame
-		game.begin_frame
-		scorecard << [] 
-	end
+  def begin_frame
+    game.begin_frame
+    scorecard << [] 
+  end
 
-	def bonus
-		strike_bonus = scorecard[-1]
-		return scorecard[-2] = scorecard[-2] + strike_bonus  if scorecard[-2] == ([10] || [0,10])
-		spare_bonus = scorecard[-2][0]
-		scorecard[-2] = scorecard[-2] + spare_bonus  if scorecard[-2].sum == 10
-	end
+  def bonus
+    strike_bonus = scorecard[-1]
+    return scorecard[-2] = scorecard[-2] + strike_bonus  if scorecard[-2] == ([10] || [0,10])
+    spare_bonus = scorecard[-2][0]
+    scorecard[-2] = scorecard[-2] + spare_bonus  if scorecard[-2].sum == 10
+  end
 
 end
