8932f819b9fd24124b7540a1dfc640281f186883
olga <itsglimerry@gmail.com>
2021-03-07 16:04:36 +0000

    Refactoring

--- lib/game.rb
+++ lib/game.rb
@@ -6,21 +6,39 @@ class Game
   def initialize(frame = Frame.new)
     @frames_finished = []
     @frame = frame
+    @bonus_points = 0
+    @points = 0
   end
 
   def take_new_frame
     @frames_finished << @frame.frame_result
+    @points += @frames_finished[0][-1][:roll_one]
+    @points += @frames_finished[0][-1][:roll_two]
+
+    @frames_finished[0][-1]
   end
 
-  def check_frame_for_strike
-    @frames_finished[0][-1][:roll_one].include?('10') ? true : false
+  def check_frame_for_strike(frame)
+    frame[:roll_one].include?('10') ? true : false
   end
 
-  def check_frame_for_spares
-    @frames_finished[0][-1][:roll_one].include?('5') && @frames_finished[0][-1][:roll_two].include?('5') ? true : false
+  def check_frame_for_spares(frame)
+    frame[:roll_one].to_i + frame[:roll_two].to_i == 10 ? true : false
+    
   end
 
-  
+
+  def bonus_points
+    if check_frame_for_spares(@frames_finished[0][-1]) == true
+      new_frame = take_new_frame
+      if check_frame_for_spares(new_frame)
+
+    check_frame_for_strike(@frames_finished[0][-1])
+
+    take_new_frame
+end
+
+
 
 
   private
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -16,8 +16,7 @@ describe Game do
       frame = double()
       allow(frame).to receive(:frame_result){[:roll_one => '10', :roll_two => nil]}
       game = Game.new(frame)
-      game.take_new_frame
-      expect(game.check_frame_for_strike).to eq true
+      expect(game.check_frame_for_strike(game.take_new_frame)).to eq true
     end
   end
   describe '#check_frame_for_spares' do
@@ -25,8 +24,7 @@ describe Game do
       frame = double()
       allow(frame).to receive(:frame_result){[:roll_one => '5', :roll_two => '5']}
       game = Game.new(frame)
-      game.take_new_frame
-      expect(game.check_frame_for_spares).to eq true
+      expect(game.check_frame_for_spares(game.take_new_frame)).to eq true
     end
   end
 
