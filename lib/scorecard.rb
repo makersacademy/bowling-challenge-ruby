cc2c7818fe8713c0e47bb6d61da7be7912c2d052
AdamP2000 <Adam2000Phillips@gmail.com>
2021-03-07 05:09:57 +0000

    Passing 2 unit tests for gutter game and spare scoring

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,4 +1,42 @@
 class Scorecard
 
+  attr_reader :player_rolls
 
-end 
+  def initialize
+    @player_rolls = []
+  end
+
+  def player_roll(pins_knocked)
+    @player_rolls << pins_knocked
+  end
+
+  def points
+    score = 0
+    player_rolls_index = 0
+    10.times do
+    if spare?(player_rolls_index)
+      score += spare_points(player_rolls_index)
+      player_rolls_index += 2
+    else
+      score += noSpare_noStrike_points(player_rolls_index)
+      player_rolls_index += 2
+    end
+  end
+  score
+end
+
+  def spare?(player_rolls_index)
+   @player_rolls[player_rolls_index] + @player_rolls[player_rolls_index + 1] == 10
+ end
+
+ def spare_points(player_rolls_index)
+   @player_rolls[player_rolls_index] + @player_rolls[player_rolls_index + 1] + @player_rolls[player_rolls_index + 2]
+ end
+
+ def noSpare_noStrike_points(player_rolls_index)
+   @player_rolls[player_rolls_index] + @player_rolls[player_rolls_index + 1]
+ end
+
+
+
+end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,5 +1,17 @@
 require 'scorecard'
 
 describe Scorecard do
-
+  it 'shows the score of a gutter game' do
+    scorecard = Scorecard.new
+    20.times{scorecard.player_roll(0)}
+    expect(scorecard.points).to eq 0
+  end
+  it 'can implement the scoring system of a spare' do
+   scorecard = Scorecard.new
+   scorecard.player_roll(7)
+   scorecard.player_roll(3)
+   scorecard.player_roll(5)
+   17.times{scorecard.player_roll(0)}
+   expect(scorecard.points).to eq 20
+ end
 end
