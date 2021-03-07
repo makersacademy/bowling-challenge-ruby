a7e7d479e77abc775d3b9b804fdff9e68632fd7b
olga <itsglimerry@gmail.com>
2021-03-07 16:42:57 +0000

    Added tests for gutter games and all ones.

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,18 @@
-require 'frame'
 
 class Game
+attr_reader :score
 
+def initialize
+  @score = 0
+  @rolls = []
+end
 
+def roll(pins)
+ @rolls.push(pins)
+end
 
-
+def score
+  @rolls.sum
+end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,17 @@ require 'game'
 
 describe Game do
 
+ it 'can roll gutter game' do
+   game = Game.new
+   20.times{game.roll(0)}
+   expect(game.score).to eq 0
+ end
 
-
+ it 'can roll all ones' do
+   game = Game.new
+   20.times{game.roll(1)}
+   expect(game.score).to eq 20
+ end
 
 
 
