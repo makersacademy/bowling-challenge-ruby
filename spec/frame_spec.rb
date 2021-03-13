78dfe92670f5369faddc19444cc618c4266f13db
sandy <pounoussamy.sandy@gmail.com>
2021-03-13 17:33:48 +0000

    specs

--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,93 +1,66 @@
-# require 'frame'
+require 'frame'
 
-# describe Frame do
+describe Frame do
 
-#     it 'knows how many pins are in one frame' do
-#         expect(subject.pins).to eq 10
-#     end
-#     it 'knows how many pins remain standing' do
-#         subject.add(6)
-#         expect(subject.remaining_pins).to eq 4
-#     end
-#     it 'knows what was rolled' do
-#         subject.add(3)
-#         subject.add(3)
-#         expect(subject.rolls.sum).to eq 6 
-#     end
+    it 'knows what was rolled' do
+        subject.roll(3)
+        subject.roll(3)
+        expect(subject.score).to eq 6 
+    end
 
-#     describe '#score' do
-#         it 'return the current frame score' do
-#             expect(subject.score).to eq 0
-#         end
-#     end 
+    describe '#score' do
+        it 'return the current frame score' do
+            expect(subject.score).to eq 0
+        end
+    end 
 
-#     describe '#add' do
-#         it 'adds throws to the frame - example 1' do
-#             subject.add(3)
-#             subject.add(5)
-#             expect(subject.score).to eq 8
-#         end
-#         it 'adds throws to the frame - example 2' do
-#             subject.add(5)
-#             subject.add(4)
-#             expect(subject.score).to eq 9
-#         end
-#     end
+    describe 'completed?' do
+        it 'knows when the frame has been fully played' do
+            subject.roll(3)
+            subject.roll(5)
+            expect(subject.completed?).to be true
+        end
+        it 'knows when the frame has been fully played' do
+            subject.roll(3)
+            expect(subject.completed?).to be false
+        end
+        it 'knows when the frame has been fully played' do
+            subject.roll(10)
+            expect(subject.completed?).to be true
+        end
+    end
 
-#     describe 'completed?' do
-#         it 'knows when the frame has been fully played' do
-#             subject.add(3)
-#             subject.add(5)
-#             expect(subject.completed?).to be true
-#         end
-#         it 'knows when the frame has been fully played' do
-#             subject.add(3)
-#             expect(subject.completed?).to be false
-#         end
-#         it 'knows when the frame has been fully played' do
-#             subject.add(10)
-#             expect(subject.completed?).to be true
-#         end
-#     end
+    describe '#strike' do
+        it 'knows when a roll is a strike' do
+            subject.roll(10)
+            expect(subject.strike?).to be true
+        end
+        it 'knows when a roll is a strike' do
+            subject.roll(6)
+            expect(subject.strike?).to be false
+        end
+        it 'knows when a roll is a strike' do
+            subject.roll(3)
+            subject.roll(6)
+            expect(subject.strike?).to be false
+        end
+    end
 
-#     describe '#strike' do
-#         it 'knows when a roll is a strike' do
-#             subject.add(10)
-#             expect(subject.strike?).to be true
-#         end
-#         it 'knows when a roll is a strike' do
-#             subject.add(6)
-#             expect(subject.strike?).to be false
-#         end
-#         it 'knows when a roll is a strike' do
-#             subject.add(3)
-#             subject.add(6)
-#             expect(subject.strike?).to be false
-#         end
-#     end
+    describe 'spare?' do
+        it 'knows whe a roll is a spare' do
+            subject.roll(10)
+            expect(subject.spare?).to be false
+        end 
+        it 'knows whe a roll is a spare' do
+            subject.roll(3)
+            subject.roll(3)
+            expect(subject.spare?).to be false
+        end 
+        it 'knows whe a roll is a spare' do
+            subject.roll(3)
+            subject.roll(7)
+            expect(subject.spare?).to be true
+        end 
+    end
 
-#     describe 'spare?' do
-#         it 'knows whe a roll is a spare' do
-#             subject.add(10)
-#             expect(subject.spare?).to be false
-#         end 
-#         it 'knows whe a roll is a spare' do
-#             subject.add(3)
-#             subject.add(3)
-#             expect(subject.spare?).to be false
-#         end 
-#         it 'knows whe a roll is a spare' do
-#             subject.add(3)
-#             subject.add(7)
-#             expect(subject.spare?).to be true
-#         end 
-#     end
-
-#     describe 'input_throw' do
-#         it 'ask for user input and add it to the frame' do
-#             expect(subject).to receive(:add).with(1)
-#             subject.input_throw
-#             # expect(subject.rolls).to_not be_empty
-#         end
-#     end
-# end 
+end 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,34 +1,6 @@
 require 'game'
 
 describe Game do
-  it 'has a max of ten frames' do
-    expect(subject.total_frames).to eq 10
-  end
-  it 'knows how many frames have been played' do
-    4.times{ subject.roll(1) }
-    expect(subject.count).to eq 2
-  end
 
-  describe 'get_score' do
-    it 'should be 0 for only gutter rolls' do
-      20.times{ subject.roll(0) }
-      expect(subject.get_score).to eq 0
-    end
-    it 'should return the game total score' do
-      subject.roll(4)
-      subject.roll(5)
-      subject.roll(7)
-      subject.roll(1)
-      16.times{ subject.roll(1) }
-      expect(subject.get_score).to eq 33
-    end
-
-    it 'should return the game total score' do
-      subject.roll(10)
-      subject.roll(2)
-      subject.roll(4)
-      16.times{ subject.roll(0) }
-      expect(subject.get_score).to eq 22
-    end
-  end
+ 
 end
