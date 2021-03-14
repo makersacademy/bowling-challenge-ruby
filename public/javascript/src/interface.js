4c4e25c2cfa2e508724f6a110bdfcb9f5a5556d6
Kasey P <kaseyp@ma370.home>
2021-03-14 14:56:19 +0000

    Beginning to implement scoring tables

--- public/javascript/src/interface.js
+++ public/javascript/src/interface.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
-    
+    $('#games_section').hide();
+
     $("#names_form").submit(function(e){
         e.preventDefault();
         $("#names_form").ajaxSubmit({url: '/names_post', type: 'post'});
-        // var names = $(this).ajaxSubmit();
-        console.log(names)
-      
+        
         $("#names_form").remove();
-        $('#games_section').show();
         update_names();
       });
 
@@ -17,6 +15,8 @@ $(document).ready(function() {
             var data = JSON.parse(res);
             if(data.status === 200) {
                 $('#names_intro').text(data.player_1_name + ' vs. ' + data.player_2_name);
+                $('#player_1_name').text(data.player_1_name);
+                $('#player_2_name').text(data.player_2_name);
             };
         });
     };
@@ -28,7 +28,8 @@ $(document).ready(function() {
             console.log(data);
             if(data.status === 200) {
                 $('#names_intro').text(data.player_1_name + ' vs. ' + data.player_2_name);
-
+                $('#player_1_pins').text(data.player_1_points);
+                $('#player_1_bonus').text(daya.player_1_bonus);
             };
         });
     };
