2ba19acd5c713ccd7db6fdb27da1ccd0910513cc
jackmcc08 <jackmcc08@gmail.com>
2021-04-02 19:19:45 +0100

    built basic current score method, which displays score of all entered rolls to date

--- lib/bowling_scorecard.rb
+++ lib/bowling_scorecard.rb
@@ -1,15 +1,23 @@
 class BowlingScorecard
-  def enter_roll(score)
-    score_validator(score)
+  attr_reader :current_score
+
+  def initialize
+    @current_score = 0
   end
 
-  private
+  def enter_roll(score)
+    return "Invalid score entered, entered_score must be between 0 and 10." unless valid_score?(score)
+    
+    @current_score += score
+  end
 
-  def score_validator(score)
+private
+
+  def valid_score?(score)
     if score > 10 || score < 0
-      "Invalid score entered, entered_score must be between 0 and 10."
+      false
     else
-      score
+      true
     end
   end
 end
--- spec/unit/bowling_scorecard_spec.rb
+++ spec/unit/bowling_scorecard_spec.rb
@@ -2,6 +2,7 @@ require 'bowling_scorecard'
 
 describe BowlingScorecard do
   let(:test_scorecard) { BowlingScorecard.new }
+
   describe '#enter_roll' do
     it 'receives the roll score and returns it' do
       expect(test_scorecard.enter_roll(5)).to eq 5
@@ -15,4 +16,14 @@ describe BowlingScorecard do
       expect(test_scorecard.enter_roll(-1)).to eq "Invalid score entered, entered_score must be between 0 and 10."
     end
   end
+
+  describe '#current_score' do
+    it 'displays the current score' do
+      test_scorecard.enter_roll(5)
+      test_scorecard.enter_roll(4)
+      test_scorecard.enter_roll(6)
+      expect(test_scorecard.current_score).to eq 15
+    end
+  end
+
 end
