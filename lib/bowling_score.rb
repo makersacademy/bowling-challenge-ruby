fa5c222ee4885dba6dc3bd58aade327dbe087667
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-04-05 15:29:47 +0100

    Created and passed tests for frame_number attribute only changing after second roll of the frame

--- lib/bowling_score.rb
+++ lib/bowling_score.rb
@@ -1,11 +1,15 @@
 class BowlingScore
-  attr_reader :total_score
+  attr_reader :total_score, :frame_number
 
   def initialize
     @total_score = 0
+    @frame_number = 1
+    @current_frame = []
   end
 
   def roll(pins)
     @total_score += pins
+    @current_frame << pins
+    @frame_number += 1 if @current_frame.length == 2
   end
 end
--- spec/bowling_score_spec.rb
+++ spec/bowling_score_spec.rb
@@ -4,8 +4,20 @@ describe BowlingScore do
   let(:player1) { BowlingScore.new }
 
   describe '#roll' do
+    context 'when first roll of the frame' do
+      it "doesn't affect frame number" do
+        expect { player1.roll(5) }.not_to change { player1.frame_number }
+      end
+    end
 
     context 'when second roll of the frame' do
+      it 'changes frame number' do
+        expect {
+          player1.roll(5)
+          player1.roll(2)
+        }.to change { player1.frame_number }.by(1)
+      end
+
       context 'when not spare' do
         it 'changes total score by number of pins knocked down' do
           expect {
