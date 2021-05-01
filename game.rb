74ce437b20c7a331f09c5bc02f4ff490b82900e4
Phil Smithies <philsmithies@gmail.com>
2021-05-01 18:57:38 +0200

    basic logic of the game

---
+++ game.rb
@@ -0,0 +1,43 @@
+class Game
+  def self.play 
+    i = 0
+    score = 0
+    round = 0
+    knocked_over_pins = 0
+    while round < 20 
+      if i == 0  
+        puts "Please enter your score..."
+        userscore = gets.chomp.to_i
+          if userscore == 10 
+            puts "Strike!"
+            score += 20
+            knocked_over_pins += userscore
+            i += 1
+          else 
+            score += userscore
+            knocked_over_pins += userscore
+          end
+        i += 1
+      elsif i == 1 
+        puts "Please enter your score..."
+        userscore = gets.chomp.to_i
+        if (knocked_over_pins + userscore) == 10
+          puts "Spare!"
+          score += 20
+          knocked_over_pins += userscore
+        else 
+          score += userscore
+          knocked_over_pins += userscore
+        end
+        i += 1
+      else 
+        i = 0
+        round += 1 
+        puts "Your total score is #{score}"
+        puts "You knocked over #{knocked_over_pins} pins!"
+        knocked_over_pins = 0
+      end 
+    end
+    puts "Game Over.... You Finished with a score of #{score}"
+  end
+end
