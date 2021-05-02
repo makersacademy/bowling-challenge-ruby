5fb3059fa5125f0841a7a9a5b4bfc9db4304a3d1
dominicmeddick <dominicmeddick@gmail.com>
2021-05-02 15:22:10 +0100

    Spare logic implemented and all tests passing. Could do with a little refactoring

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -11,9 +11,13 @@ class Bowling
   def score
     total_score = 0
     roll_number = 0
-    20.times do 
-      total_score += @rolls[roll_number]
-      roll_number += 1
+    10.times do 
+      if @rolls[roll_number].to_i + @rolls[roll_number + 1].to_i == 10
+        total_score += @rolls[roll_number].to_i + @rolls[roll_number + 1].to_i + @rolls[roll_number + 2].to_i
+      else
+        total_score += @rolls[roll_number].to_i + @rolls[roll_number + 1].to_i
+      end
+      roll_number += 2
     end
     total_score
   end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,16 +2,27 @@ require 'bowling'
 
 describe 'Bowling Game Rules' do
 
+  before do
+    @game = Bowling.new
+  end
+
   it 'can roll a gutter game' do
-    game = Bowling.new
-    20.times{game.roll 0}
-    expect(game.score).to eq 0
+    20.times{@game.roll 0}
+    expect(@game.score).to eq 0
   end
 
   it 'can roll all ones' do
-    game = Bowling.new
-    20.times{game.roll 1}
-    expect(game.score).to eq 20
+    20.times{@game.roll 1}
+    expect(@game.score).to eq 20
+  end
+
+  it 'can roll a spare' do
+    @game.roll(9)
+    @game.roll(1)
+    @game.roll(4)
+    17.times{@game.roll 0}
+    expect(@game.score).to eq 18
+  
   end
 
 end 
