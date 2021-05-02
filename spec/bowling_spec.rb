0e989ff3a7b340ff765d6d49356acbaa31c3615e
Kerri McMahon <kerrimcm25@gmail.com>
2021-05-02 14:39:26 +0100

    Refactored the spec file

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,14 +1,14 @@
 require 'bowling'
 
 describe Bowling do
+  let(:game) { Bowling.new }
+
   it 'can score a gutter game' do
-    game = Bowling.new
     20.times { game.roll(0) }
     expect(game.score).to eq 0
   end
 
   it 'can score a game of all 1s' do
-    game = Bowling.new
     20.times { game.roll(1) }
     expect(game.score).to eq 20
   end
