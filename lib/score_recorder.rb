c4a115aa96b2b5f51a67654f3f50ac3595929098
ian-1 <iandavidwalton@mail.com>
2021-05-03 00:05:00 +0100

    next_input_roll returns nil after full game (no spare/strikes in 10th frame)

--- lib/score_recorder.rb
+++ lib/score_recorder.rb
@@ -1,6 +1,8 @@
 class ScoreRecorder
   def next_input_roll(frames)
-    if new_game?(frames) || full_frame?(frames)
+    if game_end?(frames)
+      nil
+    elsif new_game?(frames) || completed_frame?(frames)
       1
     else
       2
@@ -29,7 +31,23 @@ class ScoreRecorder
     frames == []
   end
 
+  def completed_frame?(frames)
+    full_frame?(frames) || strike?(frames)
+  end
+
   def full_frame?(frames)
     frames[-1].length == 2
+  end
+
+  def tenth_frame?(frames)
+    frames.length == 10
+  end
+
+  def strike?(frames)
+    frames[-1] == [10]
+  end
+
+  def game_end?(frames)
+    tenth_frame?(frames) && full_frame?(frames)
   end
 end
--- spec/score_recorder_spec.rb
+++ spec/score_recorder_spec.rb
@@ -4,41 +4,73 @@ describe ScoreRecorder do
   subject(:scorerecorder) { described_class.new }
 
   describe '#next_input_row' do
-    it 'returns 1 for new game' do
-      frames = []
-      expect(scorerecorder.next_input_roll(frames)).to eq(1)
+    context 'when less than 10 frames played' do
+      it 'returns 1 for new game' do
+        frames = []
+        expect(scorerecorder.next_input_roll(frames)).to eq(1)
+      end
+
+      it 'returns 2 after first (non_strike) roll' do
+        frames = [[3]]
+        expect(scorerecorder.next_input_roll(frames)).to eq(2)
+      end
+
+      it 'returns 1 after two (non_strike) rolls' do
+        frames = [[6, 4]]
+        expect(scorerecorder.next_input_roll(frames)).to eq(1)
+      end
+
+      it 'returns 2 after three (non_strike) rolls' do
+        frames = [[2, 3], [9]]
+        expect(scorerecorder.next_input_roll(frames)).to eq(2)
+      end
+
+      it 'returns 1 after strike' do
+        frames = [[10]]
+        expect(scorerecorder.next_input_roll(frames)).to eq(1)
+      end
     end
 
-    it 'returns 2 after first (non_strike) roll' do
-      frames = [[3]]
-      expect(scorerecorder.next_input_roll(frames)).to eq(2)      
-    end
+    context 'when 10 frames played' do
+      let(:nine_frames) do
+        nine_frames = []
+        9.times { nine_frames << [0, 0] }
+        nine_frames
+      end
 
-    it 'returns 1 after two (non_strike) rolls' do
-      frames = [[6, 4]]
-      expect(scorerecorder.next_input_roll(frames)).to eq(1)
-    end
+      it 'returns 2 after after first (non_strike) roll in 10th frame)' do
+        frames = nine_frames << [8]
+        expect(scorerecorder.next_input_roll(frames)).to eq(2)
+      end
 
-    it 'returns 2 after three (non_strike) rolls' do
-      frames = [[2, 3], [9]]
-      expect(scorerecorder.next_input_roll(frames)).to eq(2)      
+      it 'returns nil after full game (no spare/strikes in 10th frame)' do
+        frames = nine_frames << [2, 2]
+        expect(scorerecorder.next_input_roll(frames)).to eq(nil)
+      end
     end
   end
 
   describe '#next_input_frame' do
-    it 'returns 1 for new game' do
-      frames = []
-      expect(scorerecorder.next_input_frame(frames)).to eq(1)
-    end
+    context 'when less than 10 frames played' do
+      it 'returns 1 for new game' do
+        frames = []
+        expect(scorerecorder.next_input_frame(frames)).to eq(1)
+      end
 
-    it 'returns 1 after one (non-strike) roll' do
-      frames = [[9]]
-      expect(scorerecorder.next_input_frame(frames)).to eq(1)
-    end
+      it 'returns 1 after one (non-strike) roll' do
+        frames = [[9]]
+        expect(scorerecorder.next_input_frame(frames)).to eq(1)
+      end
 
-    it 'returns 2 after two (non-strike) rolls' do
-      frames = [[3, 4]]
-      expect(scorerecorder.next_input_frame(frames)).to eq(2)
+      it 'returns 2 after two (non-strike) rolls' do
+        frames = [[3, 4]]
+        expect(scorerecorder.next_input_frame(frames)).to eq(2)
+      end
+
+      it 'returns 2 after 1 strike roll' do
+        frames = [[10]]
+        expect(scorerecorder.next_input_frame(frames)).to eq(2)
+      end
     end
   end
 end
