25ee35b1419bc443d6943ae7b00460e5fa3d6562
Kerri McMahon <kerrimcm25@gmail.com>
2021-05-02 15:31:43 +0100

    Refactored to new spare? method in Bowling class

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,27 +1,32 @@
+# frozen_string_literal: true
+
 class Bowling
   attr_reader :rolls
 
   def initialize
     @rolls = []
-  end 
+  end
 
   def roll(pins)
     @rolls << pins
   end
 
   def score
-    score = 0 
+    score = 0
     roll_index = 0
 
     @rolls.each_slice(2) do |frame|
-      if frame[0].to_i + frame[1].to_i == 10
+      if spare?(frame)
         score += 10 + @rolls[roll_index + 2]
       else
         score += frame[0].to_i + frame [1].to_i
-      end 
-
+      end
       roll_index += 2
-    end 
+    end
     score
+  end
+
+  def spare?(frame)
+    frame[0].to_i + frame[1].to_i == 10
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'bowling'
 
 describe Bowling do
