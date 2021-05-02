b08b6073dd20b0f740e62d8dac22288e40439a68
ian-1 <iandavidwalton@mail.com>
2021-05-02 09:55:13 +0100

    frame_scores returns score when spare achieved

--- lib/score_card.rb
+++ lib/score_card.rb
@@ -1,11 +1,23 @@
 class ScoreCard
   def frame_scores(frames)
     results = []
-    frames.each do |frame|
-      total_score = 0
-      frame.each { |score| total_score += score }
+    0.upto(frames.length - 1) do |index|
+      total_score = basic_frame_score(frames, index)
+      total_score += spare(frames, index) if total_score == 10
       results << total_score
     end
     results
+  end
+
+  private
+
+  def basic_frame_score(frames, index)
+    total_score = 0
+    frames[index].each { |score| total_score += score }
+    total_score
+  end
+
+  def spare(frames, index)
+    frames[index + 1].first
   end
 end
--- spec/score_card_spec.rb
+++ spec/score_card_spec.rb
@@ -4,18 +4,22 @@ describe ScoreCard do
   let(:scorecard) { described_class.new }
 
   describe '#frame_scores' do
-    context 'when one frame played' do
+    context 'when less than 10 frames played' do
       it 'returns score for one non-strike/spare frame' do
         frames = [[3, 4]]
         scores = [7]
         expect(scorecard.frame_scores(frames)).to eq(scores)
       end
-    end
 
-    context 'when two frames played' do
-      it 'returns score for non-strike/spare frames' do
+      it 'returns score for two non-strike/spare frames' do
         frames = [[2, 1], [5, 0]]
         scores = [3, 5]
+        expect(scorecard.frame_scores(frames)).to eq(scores)
+      end
+
+      it 'returns score when spare achieved' do
+        frames = [[5, 5], [2, 4]]
+        scores = [12, 6]
         expect(scorecard.frame_scores(frames)).to eq(scores)
       end
     end
