afcbbd56a9248cfe928ddb55e6c94e2bbf7b7e0f
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-05-03 20:32:57 +0100

    code refactored

--- lib/game.rb
+++ lib/game.rb
@@ -1,25 +1,37 @@
 class Game
 
-def initialize
-  @rolls = []
-end
-
-def roll(pins)
-  @rolls << pins
-end
-
-def score
-  score_total = 0
-  roll_count = 0
-  10.times do
-    if @rolls[roll_count] + @rolls[roll_count + 1] == 10
-    score_total += @rolls[roll_count] + @rolls[roll_count + 1] + @rolls[roll_count + 2]
-    else 
-    score_total += @rolls[roll_count] + @rolls[roll_count + 1]
-    end
-    roll_count += 2
+  def initialize
+    @rolls = []
   end
-  score_total
-end
+
+  def roll(pins)
+    @rolls << pins
+  end
+
+  def score
+    score_total = 0
+    roll_count = 0
+    10.times do
+      if spare?(roll_count)
+      score_total += spare_score(roll_count)
+      else 
+      score_total += frame_score(roll_count)
+      end
+      roll_count += 2
+    end
+    score_total
+  end
+
+  def spare?(roll_count)
+    @rolls[roll_count] + @rolls[roll_count + 1] == 10
+  end
+
+  def spare_score(roll_count)
+    @rolls[roll_count] + @rolls[roll_count + 1] + @rolls[roll_count + 2]
+  end
+
+  def frame_score(roll_count)
+    @rolls[roll_count] + @rolls[roll_count + 1]
+  end
 
 end
