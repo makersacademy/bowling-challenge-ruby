f0a2978ef3ef1aaaf6a37cfd3d1eb294e82318d0
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-05-03 23:27:24 +0100

    Spare test passing

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,8 +1,41 @@
+# frozen_string_literal: true
+
 class Bowling
+  attr_reader :rolls
+
+  def initialize
+    @rolls = []
+  end
+
   def roll(pins)
+    @rolls << pins
   end
 
   def score
-    0
+    score = 0
+    index = 0
+    10.times do
+      if spare?(index)
+        score += spare_score(index)
+      else
+        score += frame_score(index)
+      end
+      index += 2
+    end
+    score
+  end
+
+  private
+
+  def spare?(index)
+    @rolls[index] + @rolls[index + 1] == 10
+  end
+
+  def spare_score(index)
+    @rolls[index] + @rolls[index + 1] + @rolls[index + 2]
+  end
+
+  def frame_score(index)
+    @rolls[index] + @rolls[index + 1]
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,10 +1,20 @@
+# frozen_string_literal: true
+
 require 'bowling'
 
 describe Bowling do
   let(:game) { Bowling.new }
 
-  it 'can rol a gutter game' do
+  it 'can roll a gutter game' do
     20.times { game.roll(0) }
     expect(game.score).to eq 0
+  end
+
+  it 'can roll a spare' do
+    game.roll(4)
+    game.roll(6)
+    game.roll(4)
+    17.times { game.roll 0 }
+    expect(game.score).to eq 18
   end
 end
