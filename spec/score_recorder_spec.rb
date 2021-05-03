d0121bab071dcc05a07f9f5742bcaef199bce739
ian-1 <iandavidwalton@mail.com>
2021-05-03 17:00:36 +0100

    add test coverage for extra roll on tenth frame - already passing due to existing design

--- spec/score_recorder_spec.rb
+++ spec/score_recorder_spec.rb
@@ -2,6 +2,11 @@ require 'score_recorder'
 
 describe ScoreRecorder do
   subject(:score_recorder) { described_class.new }
+  let(:nine_frames) do
+    nine_frames = []
+    9.times { nine_frames << [0, 0] }
+    nine_frames
+  end
 
   describe '#add_roll' do
     it 'adds first roll' do
@@ -15,6 +20,30 @@ describe ScoreRecorder do
       score_recorder.test_setup(frames)
       score_recorder.add_roll(2)
       expected_frames = [[1, 2]]
+      expect(score_recorder.frames).to eq(expected_frames)
+    end
+
+    it 'adds second roll after strike' do
+      frames = [[10]]
+      score_recorder.test_setup(frames)
+      score_recorder.add_roll(3)
+      expected_frames = [[10], [3]]
+      expect(score_recorder.frames).to eq(expected_frames)
+    end
+
+    it 'adds extra roll after strike 1st roll on tenth frame and additional roll' do
+      frames = nine_frames << [10, 1]
+      score_recorder.test_setup(frames)
+      score_recorder.add_roll(4)
+      expected_frames = nine_frames << [[10, 1, 4]]
+      expect(score_recorder.frames).to eq(expected_frames)
+    end
+
+    it 'adds extra roll after spare on tenth frame' do
+      frames = nine_frames << [5, 5]
+      score_recorder.test_setup(frames)
+      score_recorder.add_roll(5)
+      expected_frames = nine_frames << [[5, 5, 5]]
       expect(score_recorder.frames).to eq(expected_frames)
     end
   end
@@ -53,12 +82,6 @@ describe ScoreRecorder do
     end
 
     context 'when 10 frames played' do
-      let(:nine_frames) do
-        nine_frames = []
-        9.times { nine_frames << [0, 0] }
-        nine_frames
-      end
-
       it 'returns 2 after after first (non_strike) roll' do
         frames = nine_frames << [8]
         score_recorder.test_setup(frames)
@@ -125,12 +148,6 @@ describe ScoreRecorder do
     end
 
     context 'when 10 frames played' do
-      let(:nine_frames) do
-        nine_frames = []
-        9.times { nine_frames << [0, 0] }
-        nine_frames
-      end
-
       it 'returns 10 after strike first roll and additional roll' do
         frames = nine_frames << [10, 0]
         score_recorder.test_setup(frames)
