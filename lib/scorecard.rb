f1936eed58c359aa7daa1921438f85c212d39316
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-05-03 23:12:40 +0100

    spare conditional written - needs refactoring

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -11,7 +11,17 @@ class Scorecard
   end
 
   def score
-    @rolls.inject(:+)
+    result = 0
+    roll_index = 0
+    10.times do
+      if @rolls[roll_index] + @rolls[roll_index + 1] == 10
+        result += @rolls[roll_index] + @rolls[roll_index + 1] + @rolls[roll_index + 2]
+      else
+        result += @rolls[roll_index] + @rolls[roll_index + 1]
+      end
+      roll_index +=2
+    end
+    result 
   end
 
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -2,18 +2,24 @@ require 'scorecard'
 
 describe Scorecard do
 
-  before do 
-    @game = Scorecard.new
-  end
+  let(:game) { Scorecard.new }
 
   it 'can score a gutter game' do 
-    20.times{ @game.roll(0) }
-    expect(@game.score).to eq 0
+    20.times{ game.roll(0) }
+    expect(game.score).to eq 0
   end
 
   it 'can score a game of all ones' do 
-    20.times{ @game.roll(1) }
-    expect(@game.score).to eq 20
+    20.times{ game.roll(1) }
+    expect(game.score).to eq 20
+  end
+
+  it 'can score a spare' do
+    game.roll(6)
+    game.roll(4)
+    game.roll(5)
+    17.times{ game.roll(0) }
+    expect(game.score).to eq 20
   end
 
 end
