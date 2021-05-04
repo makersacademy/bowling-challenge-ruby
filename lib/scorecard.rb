5d45193b471895671c2597afcad183dbe3b36a02
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-05-04 01:05:01 +0100

    add scorecard and roll numbers functionality

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,13 +1,33 @@
 class Scorecard
-  attr_reader :scorecard
+  attr_reader :scorecard, :roll
 
   def initialize
-    @scorecard = []
+    @roll = 1
+    @scorecard = Array.new(10, Hash.new(0))
+    @frame_number = 0
   end
 
   def add_score(score)
-    frame = Hash.new(0)
-    frame[:total_score] += 5
-    @scorecard << frame
+    if @roll == 1
+      @scorecard[@frame_number][:roll_1_score] += score
+      @scorecard[@frame_number][:total_score] += score
+      switch_rolls
+
+    elsif @roll == 2
+      @scorecard[@frame_number][:roll_2_score] += score
+      @scorecard[@frame_number][:total_score] += score
+    end
+
+    update_frame
+  end
+
+  private
+
+  def switch_rolls
+    @roll == 1 ? @roll += 1 : @roll -= 1
+  end
+
+  def update_frame
+    @frame_number += 1
   end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -2,10 +2,24 @@ require 'scorecard'
 
 describe Scorecard do
   describe '#add_score' do
-    it 'adds the score to the scorecard' do
+    it 'updates the total score' do
       subject.add_score(5)
 
+
       expect(subject.scorecard.first[:total_score]).to eq 5
+    end
+
+    it 'determines which roll is the current roll' do
+      subject.add_score(1)
+    
+      subject.add_score(2)
+
+      # p subject.current_frame
+      subject.roll
+
+
+      expect(subject.scorecard.first[:roll_1_score]).to eq 1
+      expect(subject.scorecard.first[:roll_2_score]).to eq 2
     end
   end
 end
