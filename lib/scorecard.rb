899015fd582b279f08292de3f0fc29ea3de8b513
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-05-04 09:23:47 +0100

    adding feature for strikes and spares

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -3,25 +3,42 @@ class Scorecard
 
   def initialize
     @roll = 1
-    @scorecard = []
-    @frame_number = 0
+    @scorecard = [{}]
+    @frame_number = 1
   end
 
   def add_score(score)
     if @roll == 1
+      if @scorecard[@frame_number - 1][:strike?] == true || @scorecard[@frame_number - 1][:spare?] == true
+        @scorecard[@frame_number - 1][:total_score] += score
+      end  
+        
+      if score == 10
+        @frame = Hash.new(0)
+        @frame[:strike?] = true
+        @frame[:roll_1_score] += score
+        @frame[:total_score] += score
+        @scorecard[@frame_number] = @frame
+        update_frame
+        return
+      end  
       @frame = Hash.new(0)
       @frame[:roll_1_score] += score
       @frame[:total_score] += score
       @scorecard[@frame_number] = @frame
-
       switch_rolls
 
     elsif @roll == 2
+      if @scorecard[@frame_number - 1][:strike?] == true
+        @scorecard[@frame_number - 1][:total_score] += score
+      end  
+      @scorecard[@frame_number][:spare?] = true if @scorecard[@frame_number][:roll_1_score] + score == 10
       @frame[:roll_2_score] += score
       @frame[:total_score] += score
       @scorecard[@frame_number] = @frame
       update_frame
     end
+    @scorecard.drop(1) if @frame_number == 1 
 
   end
 
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -6,7 +6,7 @@ describe Scorecard do
       subject.add_score(5)
 
 
-      expect(subject.scorecard.first[:total_score]).to eq 5
+      expect(subject.scorecard[1][:total_score]).to eq 5
     end
 
     it 'determines which roll is the current roll' do
@@ -14,8 +14,17 @@ describe Scorecard do
     
       subject.add_score(2)
 
-      expect(subject.scorecard.first[:roll_1_score]).to eq 1
-      expect(subject.scorecard.first[:roll_2_score]).to eq 2
+      expect(subject.scorecard[1][:roll_1_score]).to eq 1
+      expect(subject.scorecard[1][:roll_2_score]).to eq 2
+    end
+
+    it 'updates the total score of last frame if we strike' do
+      subject.add_score(10)
+      subject.add_score(5)
+      subject.add_score(4)
+      p subject.scorecard
+      
+      expect(subject.scorecard[1][:total_score]).to eq 19
     end
   end
 end
