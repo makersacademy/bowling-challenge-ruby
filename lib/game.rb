057ceebb6b2ad0a826757c0fb792ccbb769af5a7
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-29 13:53:34 +0100

    bonus added for next two rolls when strike scored

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,27 @@
 class Game
+  attr_reader :total_pinfall, :bonus_rolls
+
   def initialize
     @total_pinfall = 0
+    @bonus_rolls = 0
   end
 
   def bowl(pinfall)
-    @total_pinfall += pinfall
+    if bonus_rolls?
+      @bonus_rolls -= 1
+      @total_pinfall += 2 * pinfall
+    else
+      @total_pinfall += pinfall
+    end
+    @bonus_rolls += 2 if strike?(pinfall)
+    total_pinfall
+  end
+
+  def bonus_rolls?
+    bonus_rolls.positive?
+  end
+
+  def strike?(pinfall)
+    pinfall == 10
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,16 +2,50 @@ require 'game'
 
 describe Game do
   describe '#bowl' do
-    context 'when given 4' do
-      it 'returns 4' do
-        expect(subject.bowl(4)).to eq(4)
+    context 'when less than 10 pins are knocked down in a single frame' do
+      context 'when given 4' do
+        it 'returns 4' do
+          expect(subject.bowl(4)).to eq(4)
+        end
+      end
+
+      context 'when given 4 then 2' do
+        it 'returns 6' do
+          subject.bowl(4)
+          expect(subject.bowl(2)).to eq(6)
+        end
       end
     end
 
-    context 'when given 4 then 2' do
-      it 'returns 6' do
-        subject.bowl(4)
-        expect(subject.bowl(2)).to eq(6)
+    context 'when a strike occurs' do
+      context 'when given 10' do
+        it 'returns 10' do
+          expect(subject.bowl(10)).to eq(10)
+        end
+      end
+
+      context 'when given 10 then 3' do
+        it 'returns 16' do
+          subject.bowl(10)
+          expect(subject.bowl(3)).to eq(16)
+        end
+      end
+
+      context 'when given 10 then 3 then 6' do
+        it 'returns 28' do
+          subject.bowl(10)
+          subject.bowl(3)
+          expect(subject.bowl(6)).to eq(28)
+        end
+      end
+
+      context 'when given 10 then 3 then 6 then 4' do
+        it 'returns 32' do
+          subject.bowl(10)
+          subject.bowl(3)
+          subject.bowl(6)
+          expect(subject.bowl(4)).to eq(32)
+        end
       end
     end
   end
