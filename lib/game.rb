edd68f5f2c72950fdd1849db95e8e258e05202fc
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-31 22:07:42 +0100

    refactored Game to use Frame objects

--- lib/game.rb
+++ lib/game.rb
@@ -1,75 +1,65 @@
+require_relative './frame'
+
 class Game
   FRAME_PINS = 10
   FRAME_ROLLS = 2
   MAX_FRAMES = 10
 
-  attr_reader :total_score, :current_frame, :current_frame_pinfall, :current_frame_rolls
-
   def initialize
-    @total_score = 0
-    @current_frame = 1
-    @current_frame_pinfall = 0
-    @current_frame_rolls = 0
-    @roll_multipliers = [1, 1, 1]
+    @frames = [Frame.new]
+  end
+
+  def total_score
+    @frames.reduce(0) { |sum, frame| sum + frame.score }
   end
 
   def bowl(pinfall)
-    return total_score if current_frame > MAX_FRAMES
+    return total_score if @frames.length > MAX_FRAMES
 
-    add_pinfall_to_score(pinfall)
-    bowl_current_frame(pinfall)
-    add_strike_bonus if strike?(pinfall) && (!current_frame == 10 || current_frame_rolls == 1)
-    add_spare_bonus if current_frame_spare?(pinfall)
-    reset_frame if current_frame_over?
+    current_frame.roll(pinfall)
+    distribute_bonuses(pinfall)
+    new_frame if current_frame_over?
     total_score
   end
 
-  def add_pinfall_to_score(pinfall)
-    @total_score += if current_frame_pinfall < FRAME_PINS
-                      @roll_multipliers.shift * pinfall
-                    else
-                      @roll_multipliers.shift * pinfall - pinfall
-                    end
-    @roll_multipliers.push(1)
+  def distribute_bonuses(pinfall)
+    return unless previous_frame
+
+    previous_frame.add_bonus(pinfall) if previous_frame.strike? && current_frame.rolls < 3
+    previous_frame.add_bonus(pinfall) if previous_frame.spare? && current_frame.rolls < 2
+
+    return unless previous_previous_frame
+
+    if previous_previous_frame.strike? && current_frame.rolls + previous_frame.rolls < 3
+      previous_previous_frame.add_bonus(pinfall)
+    end
   end
 
-  def add_strike_bonus
-    @roll_multipliers[0] += 1
-    @roll_multipliers[1] += 1
+  def previous_frame
+    @frames[@frames.length - 2] unless @frames.length < 2
   end
 
-  def add_spare_bonus
-    @roll_multipliers[0] += 1
+  def previous_previous_frame
+    @frames[@frames.length - 3] unless @frames.length < 3
   end
 
-  def strike?(pinfall)
-    pinfall == FRAME_PINS
-  end
-
-  def bowl_current_frame(pinfall)
-    @current_frame_pinfall += pinfall
-    @current_frame_rolls += 1
-  end
-
-  def current_frame_spare?(pinfall)
-    current_frame_pinfall == FRAME_PINS && !strike?(pinfall)
+  def current_frame
+    @frames.last
   end
 
   def current_frame_over?
-    if current_frame == MAX_FRAMES
-      current_frame_rolls == if current_frame_pinfall < FRAME_PINS
+    if @frames.length == MAX_FRAMES
+      current_frame.rolls == if current_frame.pinfall < FRAME_PINS
                                FRAME_ROLLS
                              else
                                3
                              end
     else
-      current_frame_rolls == FRAME_ROLLS || current_frame_pinfall == FRAME_PINS
+      current_frame.rolls == FRAME_ROLLS || current_frame.pinfall == FRAME_PINS
     end
   end
 
-  def reset_frame
-    @current_frame += 1
-    @current_frame_pinfall = 0
-    @current_frame_rolls = 0
+  def new_frame
+    @frames << Frame.new
   end
 end
