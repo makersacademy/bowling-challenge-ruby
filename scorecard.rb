0520087f0f5cdbd03b65fec30e4c7c06fd841f20
Ashley Slaney <ashleyslaney96@gmail.com>
2021-07-03 14:25:46 +0100

    more readable score method

--- scorecard.rb
+++ scorecard.rb
@@ -11,21 +11,23 @@ class BowlingScore
 
   def score
     total = 0
-    roll_index = 0
+    current_roll = 0
     frame = 1
+
     until frame == 11 do
-      if @rolls[roll_index] == 10
-        total += (@rolls[roll_index] + @rolls[roll_index + 1] + @rolls[roll_index + 2])
+      if @rolls[current_roll] == 10
+        total += (@rolls[current_roll] + @rolls[current_roll + 1] + @rolls[current_roll + 2])
         @rolls.push(0)
-        roll_index -= 1
-      elsif @rolls[roll_index] + @rolls[roll_index + 1] == 10
-        total += (@rolls[roll_index] + @rolls[roll_index + 1] + @rolls[roll_index + 2])
+        current_roll -= 1
+      elsif @rolls[current_roll] + @rolls[current_roll + 1] == 10
+        total += (@rolls[current_roll] + @rolls[current_roll + 1] + @rolls[current_roll + 2])
       else
-        total += (@rolls[roll_index] + @rolls[roll_index + 1])
+        total += (@rolls[current_roll] + @rolls[current_roll + 1])
       end
-      roll_index += 2
+      current_roll += 2
       frame += 1
     end
+    
     total
   end
 
