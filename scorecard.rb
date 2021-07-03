5216b1e107073fe1d9f7dec74141b0d47ea98396
Ashley Slaney <ashleyslaney96@gmail.com>
2021-07-03 14:08:38 +0100

    basic tests pass

--- scorecard.rb
+++ scorecard.rb
@@ -1,3 +1,20 @@
 class BowlingScore
+  attr_reader :score
 
+  def initialize
+    @rolls = []
+  end
+
+  def roll(pins)
+    @rolls.push(pins)
+  end
+
+  def score
+    total = 0
+    roll_index = 0
+    @rolls.each do |roll|
+      total += roll
+    end
+    total
+  end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -4,4 +4,14 @@ describe BowlingScore do
   before do
     @game = BowlingScore.new
   end
+
+  it 'can have a gutter game' do
+    20.times { @game.roll(0) }
+    expect(@game.score).to eq(0)
+  end
+
+  it 'can roll all ones' do
+    20.times { @game.roll(1) }
+    expect(@game.score).to eq(20)
+  end
 end
