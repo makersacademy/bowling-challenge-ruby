1d747c07f2484ae63272e842676e46cd5c437744
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-07-03 16:37:38 +0100

    added tenth_frame method

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -6,11 +6,18 @@ class Bowling
   end
 
   def frame(roll_one, roll_two = 'strike')
-    if roll_one == 10
-      @rolls << {first: roll_one}
-    else
-      @rolls << {first: roll_one, second: roll_two}
-    end
+    @rolls << if roll_one == 10
+                { first: roll_one }
+              else
+                { first: roll_one, second: roll_two }
+              end
   end
-  
+
+  def tenth_frame(roll_one, roll_two, roll_three = 'no strike or spare')
+    @rolls << if roll_three == 'no strike or spare'
+                { first: roll_one, second: roll_two }
+              else
+                { first: roll_one, second: roll_two, third: roll_three }
+              end
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -10,13 +10,24 @@ describe Bowling do
   context '.frame' do
     it 'pushes first and second roll into the rolls array' do
       subject.frame(2, 3)
-      expect(subject.rolls[0]).to eq({:first=>2, :second=>3})
+      expect(subject.rolls[0]).to eq({ first: 2, second: 3 })
     end
 
     it 'records a strike' do
       subject.frame(10)
-      expect(subject.rolls[0]).to eq({:first=>10})
+      expect(subject.rolls[0]).to eq({ first: 10 })
     end
   end
 
+  context '.tenth_frame' do
+    it 'pushes only two rolls into the rolls array if there is no strike or spare' do
+      subject.tenth_frame(2, 3)
+      expect(subject.rolls[0]).to eq({ first: 2, second: 3 })
+    end
+
+    it 'pushes three rolls into the rolls array with a strike or spare' do
+      subject.tenth_frame(6, 4, 10)
+      expect(subject.rolls[0]).to eq({ first: 6, second: 4, third: 10 })
+    end
+  end
 end
