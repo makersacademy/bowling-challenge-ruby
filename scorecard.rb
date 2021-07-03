b9489e9144926cb35e7cb1d1618d24a4287e8c54
Ashley Slaney <ashleyslaney96@gmail.com>
2021-07-03 14:12:08 +0100

    logic for strikes/spares and 10th frame bonus shot

--- scorecard.rb
+++ scorecard.rb
@@ -12,9 +12,22 @@ class BowlingScore
   def score
     total = 0
     roll_index = 0
-    @rolls.each do |roll|
-      total += roll
+    frame = 1
+    until frame == 11 do
+      if @rolls[roll_index] == 10
+        total += (@rolls[roll_index] + @rolls[roll_index + 1] + @rolls[roll_index + 2])
+        @rolls.push(0)
+        p roll_index -= 1
+      elsif @rolls[roll_index] + @rolls[roll_index + 1] == 10
+        total += (@rolls[roll_index] + @rolls[roll_index + 1] + @rolls[roll_index + 2])
+      else
+        total += (@rolls[roll_index] + @rolls[roll_index + 1])
+      end
+      p "roll index"
+      p roll_index += 2
+      frame += 1
     end
     total
   end
+
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -14,4 +14,53 @@ describe BowlingScore do
     20.times { @game.roll(1) }
     expect(@game.score).to eq(20)
   end
+
+  it 'adds bonus points for a spare' do
+    @game.roll(5)
+    @game.roll(5)
+    @game.roll(8)
+    @game.roll(1)
+    16.times { @game.roll(0) }
+    expect(@game.score).to eq(27)
+  end
+
+  it 'adds bonus points for a strike' do
+    @game.roll(10)
+    @game.roll(6)
+    @game.roll(2)
+    17.times { @game.roll(0) }
+    expect(@game.score).to eq(26)
+  end
+
+  it 'can score a perfect game' do
+    12.times { @game.roll(10) }
+    expect(@game.score).to eq(300)
+  end
+
+  it 'has 10 frames' do
+    5.times { @game.roll(10) }
+    @game.roll(2)
+    @game.roll(3)
+    3.times { @game.roll(10) }
+    @game.roll(5)
+    @game.roll(2)
+    5.times { @game.roll(10) }
+    expect(@game.score).to eq(211)
+  end
+
+  it 'allows 3 total shots in the 10th frame when the first/second is a strike/spare' do
+    10.times { @game.roll(10) }
+    @game.roll(5)
+    @game.roll(3)
+    expect(@game.score).to eq(283)
+  end
+
+  it 'does not allow a 3rd shot in the 10th frame when there is no strike/spare' do
+    9.times { @game.roll(10) }
+    @game.roll(5)
+    @game.roll(3)
+    @game.roll(3)
+    @game.roll(3)
+    expect(@game.score).to eq(261)
+  end
 end
