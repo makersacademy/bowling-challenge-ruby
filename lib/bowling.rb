af802d7700a08f87a2cb97491c17f8e4ca77d64b
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-07-03 16:00:25 +0100

    created frame method which pushes individual frame into array

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,3 +1,11 @@
 class Bowling
-  
+  attr_reader :rolls
+
+  def initialize
+    @rolls = []
+  end
+
+  def frame(roll_one, roll_two)
+    @rolls << {first: roll_one, second: roll_two}
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,5 +1,17 @@
 require 'bowling'
 
 describe Bowling do
+  context '.initialize' do
+    it 'initializes with an empty rolls array' do
+      expect(subject.rolls).to eq []
+    end
+  end
+
+  context '.frame' do
+    it 'pushes first and second roll into the rolls array' do
+      subject.frame(2, 3)
+      expect(subject.rolls[0]).to eq({:first=>2, :second=>3})
+    end
+  end
   
 end
