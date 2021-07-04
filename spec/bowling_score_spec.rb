d1dba4ebcf13bdda1b431e18f43b70b582696d03
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-07-04 12:41:48 +0100

    Refactored and encapsulated current_score test

--- spec/bowling_score_spec.rb
+++ spec/bowling_score_spec.rb
@@ -6,8 +6,16 @@ describe BowlingScore do
     expect(bowling.hit(5)).to eq (5)
   end
 
-  it 'current score to equal 5' do
-    bowling.hit(5)
-    expect(bowling.current_score).to eq (5)
+  context '#current_score' do
+    it 'current score to equal 5' do
+      bowling.hit(5)
+      expect(bowling.current_score).to eq (5)
+    end
+
+    it 'current score to equal 9' do
+      bowling.hit(5)
+      bowling.hit(4)
+      expect(bowling.current_score).to eq(9)
+    end
   end
 end
