46922ced5f629c2676c1391567a0d2a4a511c4e3
Fabio <alessf13@gmail.com>
2021-07-04 12:09:04 +0200

    Testing for spares, strikes, rolls, and total points completed

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,2 +1,20 @@
 class Frame
+	attr_reader :roll_1, :roll_2
+
+	def initialize(roll_1 = 0, roll_2 = 0)
+		@roll_1 = roll_1
+		@roll_2 = roll_2
+	end
+
+	def points
+		@roll_1 + @roll_2
+	end
+
+	def spare
+		true if points == 10
+	end
+
+	def strike
+		true if @roll_1 == 10
+	end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,5 +1,29 @@
 require 'frame'
 
 RSpec.describe Frame do
+	it 'can store how many pins were knocked down in both rolls' do
+		frame = Frame.new(2, 3)
+
+		expect(frame.roll_1).to eq(2)
+		expect(frame.roll_2).to eq(3)
+	end
+
+	it 'can return the total points so far' do
+		frame = Frame.new(2, 3)
+
+		expect(frame.points).to eq(5)
+	end
+
+	it 'can tell if a spare was made' do
+		frame = Frame.new(2, 8)
+
+		expect(frame.spare).to be_truthy
+	end
+
+	it 'can tell if a strike was made' do
+		frame = Frame.new(10)
+
+		expect(frame.strike).to be_truthy
+	end
 	
 end
