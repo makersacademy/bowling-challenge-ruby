fc5a682917745205ef5d5e5fdd43d940b0cf3c0b
mikey-roberts <mikey_roberts@outlook.com>
2021-07-04 21:16:35 +0100

    rules logic and unit tests and scorecard added.

--- lib/rules.rb
+++ lib/rules.rb
@@ -2,24 +2,49 @@ module Rules
 
   MAXIMUM_FRAMES = 10
   MAXIMUM_PINS = 10
+  MAXIMUM_THROW = 10
   
-  attr_accessor :frames, :pins
+  # Throw Validation
+
+  def strike?
+    first_throw == MAXIMUM_PINS
+  end
   
-  def self.frames
+  def spare?
+    first_throw + second_throw = MAXIMUM_PINS
+  end
+  
+  def pins_hit?
+    both_throws != 0
+  end
+  
+  def gutter?
+    both_throws == 0
+  end
+  
+  def both_throws
+    first_throw + second_throw
+  end
+
+  # Max validation
+
+  def single_turn_valid(bowl)
+    bowl <= MAXIMUM_PINS
+  end
+
+  def both_turns_valid
+    both_throws <= MAXIMUM_PINS
+  end
+
+  def max_throw
+    MAXIMUM_THROW
+  end
+  
+  def max_frames
     MAXIMUM_FRAMES
   end
 
-  def self.pins
+  def max_pins
     MAXIMUM_PINS
-  end 
-
-  def self.strike(first_throw)
-    first_throw == 10
   end
-
-  def self.spare(first_throw, second_throw)
-    first_throw + second_throw == 10
-  end
-
-
 end
--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -2,21 +2,61 @@ require 'rules'
 
   describe Rules do
 
+    include Rules
 
-  let(:rules) { double :rules }
-    it 'default number of frames set to 10' do
-      expect(subject.frames).to eq Rules::MAXIMUM_FRAMES
-    end
+    let(:dummy_class) { Class.new { extend Rules } }
+    let(:strike) { double(:strike, strike?: 10) }
+    let(:spare) { double(:spare, spare?: 10) }
+    let(:pins_hit) { double(:pins_hit, pins_hit?: 5) }
+    let(:spare) { double(:spare, spare?: 10) }
+    let(:gutter) { double(:gutter, gutter?: 0) }
+    let(:both_throw) { double(:both_throw, both_throws: 7) }
 
     it 'defaults the number of pins to 10' do
-      expect(subject.pins).to eq Rules::MAXIMUM_PINS
+      expect(pins_hit.pins_hit?).to eq 5
     end
 
     it 'strike is set as 10 pins being hit' do
-      expect(subject.strike(10)).to eq true
+      expect(strike.strike?).to eq 10
     end
 
     it 'spare is set at 10 pins being hit in total across two turns' do
-      expect(subject.spare(5, 5)).to eq true
+      expect(spare.spare?).to eq 10
+    end
+
+    it 'moves to next frame if you roll a strike on' do
+      expect(pins_hit.pins_hit?).to eq 5
+    end
+
+    it 'moves to next frame if you roll a strike on' do
+      expect(gutter.gutter?).to eq 0
+    end
+
+    it 'moves to next frame if you roll a strike on' do
+      expect(both_throw.both_throws).to eq 7
+    end
+
+    # Max Validation
+
+    it 'checks if turn is invalid' do
+      dummy_class.single_turn_valid(11)
+      expect(dummy_class.single_turn_valid(11)).to eq false
+    end
+
+    it 'checks turn is valid' do
+      dummy_class.single_turn_valid(5)
+      expect(dummy_class.single_turn_valid(5)).to eq true
+    end
+
+    it 'checks max throw value' do
+      expect(dummy_class.max_throw).to eq Rules::MAXIMUM_THROW
+    end
+
+    it 'checks max throw value' do
+      expect(dummy_class.max_frames).to eq Rules::MAXIMUM_FRAMES
+    end
+
+    it 'checks max throw pins' do
+      expect(dummy_class.max_pins).to eq Rules::MAXIMUM_PINS
     end
   end
