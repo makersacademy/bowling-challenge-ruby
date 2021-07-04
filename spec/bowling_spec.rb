dca7e5f764c382e566ec400f9ad650cbd269d52e
Jenny Moon <jenny0moon@gmail.com>
2021-07-04 11:09:09 +0100

    Test: can calculate the score

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -8,4 +8,10 @@ describe Bowling do
     20.times { game.roll(0) }
     expect(game.score).to eq(0)
   end
+
+  it 'can calculate the score ' do
+    game = Bowling.new
+    20.times {game.roll(1)}
+    expect(game.score).to eq(20)
+  end
 end
