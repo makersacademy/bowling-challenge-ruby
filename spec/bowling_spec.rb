99cbf5cba81617a1c8acd75380fea26b1babfcfe
Jenny Moon <jenny0moon@gmail.com>
2021-07-04 11:18:21 +0100

    Test: can play a strike

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -14,4 +14,13 @@ describe Bowling do
     20.times {game.roll(1)}
     expect(game.score).to eq(20)
   end
+
+  it 'can play a strike' do
+    game = Bowling.new
+    game.roll(10)
+    game.roll(2)
+    game.roll(3)
+    17.times {game.roll(0)}
+    expect(game.score).to eq (15)
+  end
 end
