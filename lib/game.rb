6065123b226a165a598c91cb0612495f7cd6bfb7
Cynthia Fu <yinnycfu@gmail.com>
2021-07-04 03:35:05 +0100

    game class changed dependency inj to inject roll class not instance

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,13 @@
 class Game
   attr_reader :score
 
-  def initialize
+  def initialize(roll_class = Roll)
     @score = 0
+    @roll_class = roll_class
   end
 
-  def roll(pins, roll = Roll.new)
+  def roll(pins)
+    roll = @roll_class.new
     roll.pins = pins
     @score += roll.pins
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,21 +1,25 @@
 require 'game'
 
 describe Game do
-  let(:roll) { instance_double(Roll) }
+  let(:roll_double) { instance_double(Roll) }
+  let(:roll_class_double) { double(:roll_class) }
+  subject { described_class.new(roll_class_double) }
 
   it 'can score a gutter game' do
-    allow(roll).to receive(:pins=).with(0).and_return(0)
-    allow(roll).to receive(:pins).and_return(0)
+    allow(roll_class_double).to receive(:new).and_return(roll_double)
+    allow(roll_double).to receive(:pins=).with(0).and_return(0)
+    allow(roll_double).to receive(:pins).and_return(0)
 
-    20.times { subject.roll(0, roll) }
+    20.times { subject.roll(0) }
     expect(subject.score).to eq 0 
   end
 
   it 'can score a game of all 1s' do
-    allow(roll).to receive(:pins=).with(1).and_return(1)
-    allow(roll).to receive(:pins).and_return(1)
+    allow(roll_class_double).to receive(:new).and_return(roll_double)
+    allow(roll_double).to receive(:pins=).with(1).and_return(1)
+    allow(roll_double).to receive(:pins).and_return(1)
 
-    20.times { subject.roll(1, roll) }
+    20.times { subject.roll(1) }
     expect(subject.score).to eq 20
   end
 
