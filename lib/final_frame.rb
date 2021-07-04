9680036372ea2f49fb4966d2480409764b00e348
Cynthia Fu <yinnycfu@gmail.com>
2021-07-04 18:46:07 +0100

    copied tframe and tests to apply to fFinalFrame class

---
+++ lib/final_frame.rb
@@ -0,0 +1,46 @@
+class Frame
+  attr_reader :pins, :rolls
+
+  def initialize
+    @pins = 10
+    @rolls = []
+  end
+
+  def ended?
+    false
+  end
+
+  def add(roll = Roll.new(pins))
+    @rolls << roll
+    deduct_pins(roll)
+  end
+
+  def score
+    @rolls.map { |roll| roll.pins }.inject(:+)
+  end
+
+  def spare?
+    strike? ? false : count_pins_on_turn(1) + count_pins_on_turn(2) == 10
+  end
+
+  def strike?
+    no_moves_made? ? false : count_pins_on_turn(1) == 10
+  end
+
+  private
+  def deduct_pins(roll)
+    @pins -= roll.pins
+  end
+
+  def count_pins_on_turn(number)
+    @rolls[number - 1].pins
+  end
+
+  def no_moves_made?
+    @rolls.empty?
+  end
+
+  def no_more_moves?
+    @rolls.length == 2
+  end
+end
---
+++ spec/final_frame_spec.rb
@@ -0,0 +1,72 @@
+require 'frame'
+
+describe Frame do
+  let(:roll) { double(:roll) }
+
+  context 'the default frame' do
+    it { is_expected.to have_attributes(pins: 10) }
+
+    it { is_expected.to respond_to(:rolls) }
+  end
+
+  context '#ended?' do
+    it 'defaults to false' do
+      expect(subject).not_to be_ended
+    end
+
+    xit 'can end the frame' do
+    end
+  end
+
+  context '#add' do
+    it 'can store rolls' do
+      allow(roll).to receive(:pins).and_return(5)
+      subject.add(roll)
+      expect(subject.rolls).to eq [roll]
+    end
+
+    it 'can deduct pins by the amount rolled over' do
+      allow(roll).to receive(:pins).and_return(5)
+      subject.add(roll)
+      expect(subject.pins).to eq 5
+    end
+
+    it 'can store a second roll' do
+      allow(roll).to receive(:pins).and_return(1)
+      subject.add(roll)
+      subject.add(roll)
+
+      expect(subject.rolls).to eq [roll, roll]
+    end
+  end
+
+  context '#score' do
+    it 'can score a frame' do
+      allow(roll).to receive(:pins).and_return(1)
+      subject.add(roll)
+      subject.add(roll)
+
+      expect(subject.score).to eq 2
+    end
+  end
+
+  context '#spare?' do
+    it 'can identify a spare' do
+      allow(roll).to receive(:pins).and_return(5)
+      subject.add(roll)
+      subject.add(roll)
+
+      expect(subject).to be_spare
+    end
+  end
+
+  context '#strike?' do
+    it 'can identify a strike' do
+      allow(roll).to receive(:pins).and_return(10)
+      subject.add(roll)
+
+      expect(subject).to be_strike
+      expect(subject).not_to be_spare
+    end
+  end
+end
