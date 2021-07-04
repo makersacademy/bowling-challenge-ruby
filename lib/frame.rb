f5793d81b1fc27a147da69d8535b15a70e0aeafd
Cynthia Fu <yinnycfu@gmail.com>
2021-07-04 03:17:06 +0100

    dependency inj for Frame class, Roll double, default frame

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,7 +1,9 @@
 class Frame
-  attr_reader :pins
+  attr_reader :pins, :roll_1, :roll_2
 
-  def initialize
+  def initialize(roll_1 = Roll.new, roll_2 = Roll.new)
     @pins = 10
+    @roll_1 = roll_1
+    @roll_2 = roll_2
   end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,5 +1,23 @@
 require 'frame'
 
 describe Frame do
-  it { is_expected.to have_attributes(pins: 10) }
+  let(:roll_1) { instance_double(Roll) }
+  let(:roll_2) { instance_double(Roll) }
+  subject { described_class.new(roll_1, roll_2) }
+
+  context 'the default frame' do
+    before do
+      allow(roll_1).to receive(:pins=).with(0).and_return(0)
+      allow(roll_1).to receive(:pins).and_return(0)
+      allow(roll_2).to receive(:pins=).with(0).and_return(0)
+      allow(roll_2).to receive(:pins).and_return(0)
+    end
+
+    it { is_expected.to have_attributes(pins: 10) }
+
+    it { is_expected.to respond_to(:roll_1) }
+
+    it { is_expected.to respond_to(:roll_2) }
+  end
+
 end
