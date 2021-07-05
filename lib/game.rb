a3664d54da8ee991b37ae9c98819b57c18ab821f
OptimusWall-E <iemailedmichael@gmail.com>
2021-07-05 10:00:15 +0100

    added spec tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,49 +1,54 @@
 
 class Game
-  attr_reader :pins_knocked_down, :pins, :frame_count, :consec_rolls, :roll_count 
-  # attr_accessor :frame_count
+  attr_reader :game_score, :pins_knocked_down, :frame_count_up, :consec_rolls, :roll_count, :frame_score, :turn
 
 
   def initialize
+    @game_score = []
+    @frame_score = []
     @frame_count = 0
     @consec_rolls = 0
-    @roll_count = 1
+    @roll_count = 0
+    @roll_score = 0
   end
 
   def pins_knocked_down(number)
     if number == 10
       strike
-    else
-      number
+    elsif @frame_score.sum == 10
+      spare
     end
+    turn_score(number)
   end
 
   def strike
+    turn_score(number)
     'strike'
   end
 
-  def frame_count
-    @frame_count = @frame_count + 1
+  def spare
+    turn_score(number)
+    'spare'
   end
 
-  def rolls
-    @consec_rolls += 1
+  def turn_score(number)
+    @frame_score << number
+    @roll_score += number
   end
 
-  # def strike?
-  #   pins_knocked_down(10)
-  #   if @pins == 10
-  #     'strike'
-  #   end
-  # end
-
-
-  # def roll_count
-  #   return 1
-  # end
+  def frame_count_up
+    @frame_count += 1
+  end
 
   def roll
+    @roll_count += 1
     @consec_rolls += 1
+    if @roll_count > 2
+      frame_count_up
+      @roll_count = 0
+      @frame_score = 0
+      @roll_score = 0
+    end
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,29 +7,65 @@ describe Game do
     it 'allows player to enter the number of pins knocked down' do
       expect(subject.pins_knocked_down(5)).to eq 5
     end
-
+    
     it 'recognises a strike' do
-      # expect(subject.strike?).to eq 'strike'
       expect(subject.pins_knocked_down(10)).to eq 'strike'
     end
+
+    it 'recognises a spare' do
+      subject.pins_knocked_down(5)
+      expect(subject.pins_knocked_down(5)).to eq 'spare'
+    end
   end
+    
+  
+  context '#frame_count' do
+    it 'keeps count of the frame' do
+      expect(subject.frame_count_up).to eq 1
+    end
+  end
+    
+    
 
   context '#consec_rolls' do
     it 'increase the count by one with each roll (from 0 to 1)' do
-      # roll_count = 0
-      expect(subject.rolls).to eq 1
+      subject.roll
+      expect(subject.consec_rolls).to eq 1
     end
+    
 
     it 'increase the count by one with each roll (from 1 to 2)' do
-      subject.rolls
-      expect(subject.rolls).to eq 2
+      subject.roll
+      subject.roll
+      expect(subject.consec_rolls).to eq 2
+    end
+  end
+  
+
+  context '#roll_count' do
+    it 'increments when the player rolls' do
+      subject.roll
+      subject.roll
+      expect(subject.roll_count).to eq 2
+    end
+    
+    it 'resets to 0 when the player rolls reaches two rolls' do
+      subject.roll
+      subject.roll
+      subject.roll
+      expect(subject.roll_count).to eq 0
     end
   end
 
-  context '#frame_count' do
-    it 'keeps count of the frame' do
-      expect(subject.frame_count).to eq 1
+
+  context '#turn_score' do
+    it 'records the player\'s score on their roll' do
+      subject.pins_knocked_down(8)
+      expect(subject.frame_score[0]).to be (8)
     end
-  end  
-  
+  end
 end
+
+  
+
+  
