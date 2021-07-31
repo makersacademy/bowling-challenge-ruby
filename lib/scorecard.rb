5ff6ae39f547d0abfeeb9497ee11e8a3de2ca1be
Else Meijerink <p.e.meijerink@gmail.com>
2021-07-31 23:17:40 +0100

    Implemented frames

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,12 +1,20 @@
 
 class Scorecard
-  attr_reader :score
+  attr_reader :score, :frame, :roll_count
 
   def initialize 
     @score = 0
+    @roll_count = 0
   end
 
   def roll_input(roll_score)
+    if @roll_count == 0
+      @frame = {roll1: roll_score}
+      @roll_count += 1
+    elsif @roll_count == 1
+      @frame[:roll2] = roll_score
+      @roll_count = 0
+    end
     @score += roll_score
   end
 
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -16,3 +16,22 @@ describe Scorecard do
     subject.roll_input(2)
     expect(subject.score). to eq 14
   end
+
+  it 'player roll scores are stored in frames' do
+    subject.roll_input(5)
+    subject.roll_input(4)
+    expect(subject.frame). to eq({roll1: 5, roll2: 4})
+  end
+
+  it 'roll count is reset every 2 normal rolls' do
+    subject.roll_input(5)
+    subject.roll_input(4)
+    expect(subject.roll_count). to eq(0)
+  end
+
+  it 'frame count goes up every 2 normal rolls' do
+    subject.roll_input(5)
+    subject.roll_input(4)
+    expect(subject.frame_count). to eq(1)
+  end
+end
