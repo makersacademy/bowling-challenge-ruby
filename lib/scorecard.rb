1e81d5d4a117c0874e67c82aa3701c6ca0db099c
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-07-31 14:32:42 +0100

    Add roll ball basics

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -8,4 +8,15 @@ class Scorecard
     @score = 0
   end
 
+  def roll(number)
+    raise 'This score is invalid' if number > 10
+    @score += number
+    if @scorecard[@frame.floor].nil?
+      @scorecard[@frame.floor] = [number]   
+    else
+      @scorecard[@frame.floor] << number
+    end
+
+    @frame += 0.5
+  end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -12,4 +12,25 @@ describe Scorecard do
       expect(subject.score).to eq 0
     end
   end
+
+  describe '#roll' do
+    it 'increases the score with the roll' do
+      subject.roll(5)
+      subject.roll(4)
+      expect(subject.score).to eq 9
+    end
+    it 'increases the frame' do
+      subject.roll(3)
+      subject.roll(4)
+      expect(subject.frame).to eq 2
+    end
+    it 'raises an error if score above 10 is entered' do
+      expect { subject.roll(11) }.to raise_error 'This score is invalid'
+    end
+    it 'adds to the scorecard' do
+      subject.roll(3)
+      subject.roll(7)
+      expect(subject.scorecard).to eq({ 1 => [3, 7] })
+    end
+  end
 end
