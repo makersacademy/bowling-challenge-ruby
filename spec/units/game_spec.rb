52fed41c57ed4053baff6144bcf2e24a6608da3c
eballantine <lizzy.kayballantine@gmail.com>
2021-08-27 22:03:55 +0100

    test added to restrict game rolls to max 12

--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -4,4 +4,9 @@ describe Game do
   it 'tracks rolls' do
     expect(subject.rolls).to eq []
   end
+
+  it 'allows for up to 12 rolls' do
+    expect(subject.rolls.length).to be >= 0
+    expect(subject.rolls.length).to be <= 12
+  end
 end
