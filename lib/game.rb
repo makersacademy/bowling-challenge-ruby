a716da4c4f6558f25dddebc983697e6d0ca1d496
eballantine <lizzy.kayballantine@gmail.com>
2021-08-27 20:56:05 +0100

    game tracks current frame

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,8 @@
 class Game 
-  attr_reader :total_score
+  attr_reader :total_score, :current_frame
 
   def initialize
     @total_score = 0
+    @current_frame = 1
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,4 +5,9 @@ describe Game do
     expect(subject.total_score).to be >= 0
     expect(subject.total_score).to be <= 300
   end
+
+  it 'tracks the frames' do
+    expect(subject.current_frame).to be >= 1
+    expect(subject.current_frame).to be <= 10
+  end
 end
