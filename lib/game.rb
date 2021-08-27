9b02362202b62494e7dfcc865305bc8457b24d9a
eballantine <lizzy.kayballantine@gmail.com>
2021-08-27 22:37:02 +0100

    restart

--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,3 @@
 class Game 
-  attr_reader :finished, :rolls
-
-  def initialize
-    @finished = false
-    @rolls = []
-  end
-
-  def roll(pins)
-    @rolls << pins
-  end
-
-  def finished?
-    if @rolls.length == 12 || @rolls.length == 11 && @rolls[9] != 10
-      @finished = true
-    end
-    @finished
-  end
+  
 end
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -1,56 +1,5 @@
 require 'game' 
 
 describe Game do 
-  it 'tracks rolls' do
-    expect(subject.rolls).to eq []
-  end
 
-  it 'allows for up to 12 rolls' do
-    expect(subject.rolls.length).to be >= 0
-    expect(subject.rolls.length).to be <= 12
-  end
-
-  it 'knows if the game is finished' do
-    expect(subject.finished).to eq false
-  end
-
-  describe '#rolls' do
-    it 'adds the roll to game rolls' do
-      subject.roll(1)
-      expect(subject.rolls).to eq [1]
-      subject.roll(2)
-      expect(subject.rolls).to eq [1, 2]
-    end
-  end
-
-  describe '#finished?' do
-    it 'returns if the game is finished' do
-      expect(subject.finished?).to eq false
-    end
-
-    context 'strike on 10th frame' do
-      it 'returns false after 11 rolls' do
-        10.times { subject.roll(10) }
-        subject.roll(10)
-        expect(subject.finished?).to eq false
-      end
-      it 'returns true after 12 rolls' do
-        10.times { subject.roll(10) }
-        subject.roll(10)
-        subject.roll(1)
-        expect(subject.finished?).to eq true
-      end
-    end
-
-    context 'not a strike on 10th frame' do
-      it 'returns true after 11 rolls' do
-        9.times { subject.roll(10) }
-        subject.roll(9)
-        subject.roll(1)
-        p subject.rolls
-
-        expect(subject.finished?).to eq true
-      end
-    end
-  end
 end
