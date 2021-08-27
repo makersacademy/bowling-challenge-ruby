0e059bd8b2b3f0c40250f53316fba63d747c0a11
eballantine <lizzy.kayballantine@gmail.com>
2021-08-27 22:41:58 +0100

    game tracks frame up to 10

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,7 @@
 class Game 
-  
+  attr_reader :frame
+
+  def initialize 
+    @frame = 1
+  end
 end
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -1,5 +1,11 @@
 require 'game' 
 
 describe Game do 
+  it 'tracks the current frame' do
+    expect(subject.frame).to eq 1
+  end
 
+  it 'tracks frames up to 10' do
+    expect(subject.frame).to be <= 10
+  end
 end
