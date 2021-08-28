6b28aca2abd100c07a280c0c760c85acdec87a07
edpackard <edpackard81@gmail.com>
2021-08-29 00:15:57 +0100

    Adding frame logic for frames 1-9

--- lib/frame.rb
+++ lib/frame.rb
@@ -2,26 +2,65 @@ require_relative 'validity'
 
 class Frame
 
-  FRAME_ID_MIN = 1
-  FRAME_ID_MAX = 10
+  FIRST_FRAME = 1
+  LAST_FRAME = 10
+  TOTAL_PINS = 10
+  ROLL_MIN = 0
   ERROR_MESSAGE = 'Not a valid frame number'
-  DEFAULT_FRAME = {  
-    1 => nil,
-    spare: nil,
-    strike: nil
-  }
-
-  attr_reader :content
 
   def initialize(frame_id, check = Validity.new)
-    raise ERROR_MESSAGE unless check.valid?(frame_id, FRAME_ID_MIN, FRAME_ID_MAX)
-
-    @content = DEFAULT_FRAME
-    @content[:frame] = frame_id
+    raise ERROR_MESSAGE unless check.valid?(frame_id, FIRST_FRAME, LAST_FRAME)
+    
+    @content = { frame: frame_id, 1 => nil, spare: nil, strike: nil }
+    @remaining_pins = TOTAL_PINS
+    @check = check
   end
 
   def add(roll)
-    content[1] = roll
+    raise 'Frame complete' if full?
+    
+    roll_check(roll)
+    @content[1].nil? ? first_roll(roll) : second_roll(roll)
+  end
+  
+  def print_frame
+    "Frame: #{@content[:frame]} | 1st roll: #{@content[1]} |"\
+    " 2nd roll: #{@content[2]} | Bonus: #{check_bonus}"
+  end
+
+  private
+  
+  def roll_check(roll)
+    p @remaining_pins
+    valid = @check.valid?(roll, ROLL_MIN, @remaining_pins)
+    raise 'Invalid roll' unless valid
+  end
+
+  def first_roll(roll)
+    @content[:strike] = true && @content[2] = 0 if strike?(roll)
+    @content[1] = roll
+    @remaining_pins -= roll
+  end
+
+  def second_roll(roll)
+    @content[2] = roll
+    @content[:spare] = spare?
+  end
+
+  def spare?
+    @content[1] + @content[2] == TOTAL_PINS
+  end
+
+  def strike?(roll)
+    @content[1].nil? && roll == TOTAL_PINS
+  end
+
+  def check_bonus
+    @content[:strike] ? 'X' : @content[:spare] ? '/' : nil
+  end
+
+  def full?
+    !@content[1].nil? && !@content[2].nil?
   end
 
 end
--- spec/unit/frame_spec.rb
+++ spec/unit/frame_spec.rb
@@ -5,10 +5,9 @@ describe Frame do
   let(:check) { double :check }
 
   it 'initialises @frame as a frame hash' do
-    hash = { frame: 1, 1 => nil, spare: nil, strike: nil }
     allow(check).to receive(:valid?) { true }
     frame = Frame.new(1, check)
-    expect(frame.content).to eq hash
+    expect(frame.print_frame).to eq 'Frame: 1 | 1st roll:  | 2nd roll:  | Bonus: '
   end
 
   it 'checks frame number is valid' do
@@ -28,13 +27,82 @@ describe Frame do
     expect { Frame.new('a', check) }.to raise_error message
   end
 
-  describe '#add' do
-    it 'adds first frame roll to the 1 key of frame hash' do
-      allow(check).to receive(:valid?) { true }
-      frame = Frame.new(1, check)
-      frame.add(4)
-      expect(frame.content).to include(1 => 4)
+  context "frames 1-9" do
+
+    describe '#add' do
+
+      before(:each) do
+        allow(check).to receive(:valid?) { true }
+      end  
+
+      it 'raises an error for an invalid roll' do
+        frame = Frame.new(1, check)
+        allow(check).to receive(:valid?) { false }
+        expect { frame.add(11) }.to raise_error 'Invalid roll'
+
+      end
+
+      it 'adds first frame roll to the frame' do
+        frame = Frame.new(1, check)
+        frame.add(4)
+        expect(frame.print_frame).to include '1st roll: 4'
+      end
+
+      it 'adds second frame roll to the frame' do
+        frame = Frame.new(1, check)
+        frame.add(4)
+        frame.add(5)
+        expect(frame.print_frame).to include '1st roll: 4 | 2nd roll: 5'
+      end
+
+      it 'does not enter a roll if frame full' do
+        frame = Frame.new(1, check)
+        frame.add(4)
+        frame.add(5)
+        expect { frame.add(3) }.to raise_error 'Frame complete'
+      end
+
+      it 'raises error if attempting to add a roll to frame after strike' do
+        frame = Frame.new(1, check)
+        frame.add(10)
+        expect { frame.add(1) }.to raise_error 'Frame complete'
+      end
+
+      it 'sets strike flag if first roll is 10' do
+        frame = Frame.new(1, check)
+        frame.add(10)
+        expect(frame.print_frame).to include 'Bonus: X'
+      end
+
+      it 'does not set strike flag if 1st roll is less than 10' do
+        frame = Frame.new(1, check)
+        frame.add(9)
+        expect(frame.print_frame).not_to include 'X'
+      end
+
+      it 'sets spare flag if 1st & 2nd rolls total 10' do
+        frame = Frame.new(1, check)
+        frame.add(2)
+        frame.add(8)
+        expect(frame.print_frame).to include 'Bonus: /'
+      end
+
+      it 'does not set spare flag if 1st & 2nd rolls total less than 10' do
+        frame = Frame.new(1, check)
+        frame.add(2)
+        frame.add(7)
+        expect(frame.print_frame).not_to include '/'
+      end
+
+      it 'raises error if second roll higher than remaining pins' do
+        frame = Frame.new(1, check)
+        frame.add(2)
+        allow(check).to receive(:valid?) { false }
+        expect { frame.add(9) }.to raise_error 'Invalid roll'
+      end
+
     end
+
   end
 
 end
