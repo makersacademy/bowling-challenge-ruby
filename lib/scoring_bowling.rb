30fb582a34ca30a67d0523345e54378af1902efa
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-28 16:37:38 +0100

    Adding #calculate_score, not testing strike or spare

--- lib/scoring_bowling.rb
+++ lib/scoring_bowling.rb
@@ -1,24 +1,42 @@
 class Scoring_Bowling
 
   MAX_FRAMES = 10
-
-  attr_reader :bowls, :frame
+  MAX_PINS = 10
+  attr_reader :rounds, :frame, :score
 
   def initialize
-    @bowls = []
+    @rounds = []
     @frame = 0
+    @score = 0
   end
 
-  def add_bowl(first_bowl, second_bowl = nil)
-    raise 'Game Over, no more scores can be added' if bowls.count == MAX_FRAMES
+  def add_bowl(first_bowl = nil, second_bowl = nil)
+    raise input_error_messages[0] if frame == MAX_FRAMES
+    raise input_error_messages[1] if [first_bowl, second_bowl].compact.sum > MAX_PINS
 
-    @bowls << {frame: frame_count, bowls: [first_bowl, second_bowl]}
+    @rounds << {frame: frame_count, bowls: [first_bowl, second_bowl]}
+  end
+  
+  def calculate_score
+    arr = []
+    current_frame = 0
+    until current_frame == MAX_FRAMES
+      @score += @rounds[current_frame][:bowls].flatten.sum
+      current_frame += 1
+    end
   end
 
   private
 
   def frame_count
     @frame += 1
+  end
+
+  def input_error_messages
+    [
+      'Game Over, no more scores can be added' ,
+      'Total sum of inputs cannot exceed 10, please enter the correct values' 
+    ]
   end
 
 end
--- spec/scoring_bowling_spec.rb
+++ spec/scoring_bowling_spec.rb
@@ -4,8 +4,8 @@ describe Scoring_Bowling do
 
   describe 'initialize' do
 
-    it 'is expected to have 2 public attributes' do
-      expect(subject).to have_attributes(:bowls => [], :frame => 0)
+    it 'is expected to have 3 public attributes' do
+      expect(subject).to have_attributes(:rounds => [], :frame => 0, :score => 0)
     end
 
   end
@@ -14,17 +14,38 @@ describe Scoring_Bowling do
 
     it 'is expected to add the roll to rolls array' do
       subject.add_bowl(10)
-      expect(subject.bowls).to include(frame: 1, bowls: [10, nil])
+      expect(subject.rounds).to include(frame: 1, bowls: [10, nil])
     end
 
     it 'is expected to be able to take two bowls' do
-      subject.add_bowl(10, 10)
-      expect(subject.bowls).to include(frame: 1, bowls: [10, 10])
+      subject.add_bowl(9, 1)
+      expect(subject.rounds).to include(frame: 1, bowls: [9, 1])
+    end
+
+    it 'is expected to update the frame' do
+      expect{ subject.add_bowl(9, 1) }.to change{ subject.frame }.by (1)
     end
 
     it 'is expected to raise Game Over message when trying to add more bowls past ten frames' do
-      10.times { subject.add_bowl(10, 10) }
+      10.times { subject.add_bowl(9, 1) }
       expect{ subject.add_bowl(10) }.to raise_error{ 'Game Over, no more scores can be added' }
+    end
+
+    it 'is expected to raise Game Over message when trying to add more bowls past ten frames' do
+      expect{ subject.add_bowl(10, 1) }.to raise_error{ 'Total sum of inputs cannot exceed 10, please enter the correct values' }
+    end
+
+
+
+    describe '#calculate_score' do
+
+      it 'is expected to produce the sum of all bowls made with no strikes made' do
+        3.times { subject.add_bowl(6, 3) }
+        3.times { subject.add_bowl(0, 9) }
+        4.times { subject.add_bowl(6, 0) }
+        expect{ subject.calculate_score }.to change{ subject.score }.by (78)
+      end
+
     end
 
 
