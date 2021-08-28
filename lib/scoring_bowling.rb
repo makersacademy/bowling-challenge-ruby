103ba943b61e1e20b2dc2c80c91335a5e5ce761f
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-28 12:45:01 +0100

    Adding magic number and Game Over error message

--- lib/scoring_bowling.rb
+++ lib/scoring_bowling.rb
@@ -1,5 +1,7 @@
 class Scoring_Bowling
 
+  MAX_FRAMES = 10
+
   attr_reader :bowls, :frame
 
   def initialize
@@ -7,8 +9,16 @@ class Scoring_Bowling
     @frame = 0
   end
 
-  def add_bowl(first_bowl, second_bowl)
-    @bowls << {frame: 1, bowls: [first_bowl, second_bowl]}
+  def add_bowl(first_bowl, second_bowl = nil)
+    raise 'Game Over, no more scores can be added' if bowls.count == MAX_FRAMES
+
+    @bowls << {frame: frame_count, bowls: [first_bowl, second_bowl]}
+  end
+
+  private
+
+  def frame_count
+    @frame += 1
   end
 
 end
--- spec/scoring_bowling_spec.rb
+++ spec/scoring_bowling_spec.rb
@@ -10,16 +10,21 @@ describe Scoring_Bowling do
 
   end
 
-  describe '.add_roll' do
+  describe '#add_roll' do
 
     it 'is expected to add the roll to rolls array' do
-      subject.add_bowl(10, nil)
+      subject.add_bowl(10)
       expect(subject.bowls).to include(frame: 1, bowls: [10, nil])
     end
 
     it 'is expected to be able to take two bowls' do
       subject.add_bowl(10, 10)
       expect(subject.bowls).to include(frame: 1, bowls: [10, 10])
+    end
+
+    it 'is expected to raise Game Over message when trying to add more bowls past ten frames' do
+      10.times { subject.add_bowl(10, 10) }
+      expect{ subject.add_bowl(10) }.to raise_error{ 'Game Over, no more scores can be added' }
     end
 
 
