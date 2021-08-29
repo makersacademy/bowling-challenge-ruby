4d25bd4b48e80a78a7c7c0515be4d6115ab2bf9b
Fiona Apps <wikipanyd@gmail.com>
2021-08-29 09:31:48 +0100

    testing with new PAC

---
+++ test.rb
@@ -0,0 +1,17 @@
+def main_method
+    p 'this is the main method'
+    count = 0
+    while count <9
+        count +1
+        second_method
+    end
+end
+
+def second_method
+    p 'this is the second method'
+    third_method
+end
+
+def third_method
+    p 'this is the third method'
+end
