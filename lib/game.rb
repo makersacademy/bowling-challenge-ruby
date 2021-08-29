060dc848fb821bb010128c6d9b688cd39147ca40
Blessing Ubogu <60205509+BlessingUb@users.noreply.github.com>
2021-08-29 23:43:58 +0100

    game can roll a gutter game and an all one game

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,20 @@
 class BowlingGame 
-  
+
+  def initialize 
+    @rolls = []
+  end
+
+  def roll(pins)
+    @rolls<< pins
+  end 
+
+  def score
+    total_score = 0
+    count = 0
+    20.times do 
+      total_score += @rolls[count]
+      count += 1
+    end
+    total_score
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,20 @@
 require 'game'
 RSpec.describe 'BowlingGame' do
-  it 'allows to create a new game' do
-    game = BowlingGame.new
+
+  context '#gutter game' do
+    it 'can roll all zero game' do
+      game = BowlingGame.new
+      20.times{game.roll 0}
+      expect(game.score).to eq 0
+    end
   end
 
+  context 'all one game' do
+    it 'can roll an all one game' do
+      game = BowlingGame.new
+      20.times{game.roll 1}
+      expect(game.score).to eq 20
+    end
+  end
+  
 end
