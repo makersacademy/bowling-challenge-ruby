df9d4ce75edca189ed72fd0b2a33f5499135b9d1
Graeme Stirling <graemejstirling@gmail.com>
2021-08-30 13:43:46 +0100

    total method can calculate a score inluding spares

--- lib/player.rb
+++ lib/player.rb
@@ -1,23 +1,43 @@
 class Player 
-  # needs to keep track of their own score 
-  # needs to track any streaks of strikes in their score ????
+  # needs to keep track of their own frames 
 
-attr_reader :name, :score
+attr_reader :name, :frames, :current_score
 
-  def initialize(name, score = Array.new)
+  def initialize(name, frames = Array.new, current_score = 0)
     @name = name
-    @score = score
+    @frames = frames
+    @current_score = current_score
   end
 
   def frame
-    fail "Game is complete no frames remaining" if @score.length == 10
+    fail "Game is complete no frames remaining" if @frames.length == 10
     first_bowl = rand(0..10)
     # case strike
-    return @score << ["X", "-"] if first_bowl == 10
+    return @frames << ["X", "-"] if first_bowl == 10
     remaining_pins = 10 - first_bowl
     second_bowl = rand(0..remaining_pins)
-    return @score << [first_bowl, "/"] if (first_bowl + second_bowl) == 10 
-    @score << [first_bowl, second_bowl]
+    # case spare
+    return @frames << [first_bowl, "/"] if (first_bowl + second_bowl) == 10 
+    # case no spare or strike scored
+    @frames << [first_bowl, second_bowl]
+  end
+
+  def total
+    spare = false
+    tally = [] 
+    iteration = 0
+    @frames.each do |frame| 
+      tally[iteration - 1] += frame[0] if spare
+      if frame[1] == '/'
+        tally << 10
+        spare = true
+      else 
+        tally << frame.sum 
+        spare = false
+      end 
+    iteration += 1
+    end 
+  tally.sum
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,9 @@
 describe Player do 
 
-  let(:player) { described_class.new("Graeme") }
+  let(:player) { described_class.new("Graeme")}
+  let(:test_score) { described_class.new("Graeme", [[2,3], [4,5], [6,1]])} 
+  let(:spare_score) { described_class.new("Graeme", [[2,3], [4,"/"], [6,1]]) }
+  let(:spares_score) { described_class.new("Graeme", [[2,"/"], [4,"/"], [6,2]]) }
 
   it "creates and instance of itself" do 
     expect(player).to be_an_instance_of(Player)
@@ -11,6 +14,19 @@ describe Player do
     expect { player.frame }.to raise_error("Game is complete no frames remaining")
   end 
  
+  it "calculates a the score of a bunch of frames with no spares or strikes" do
+    expect(test_score.total).to eq(21)
+  end 
+
+  it "calculates a the score with a spare" do
+    expect(spare_score.total).to eq(28)
+  end 
+
+  it "calculates a the score with multiple spares" do
+    expect(spares_score.total).to eq(38)
+  end 
+
+
 end 
 
 
