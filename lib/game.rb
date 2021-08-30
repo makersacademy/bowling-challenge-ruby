eeca275d99d596a100ef7d8ebf2b52dd2027eb6e
eballantine <lizzy.kayballantine@gmail.com>
2021-08-30 20:25:44 +0100

    the final rejig, written my plan in README.md

--- lib/game.rb
+++ lib/game.rb
@@ -1,27 +1,15 @@
 class Game 
-  attr_reader :score, :frame, :frame_results, :frame_scores
+  attr_reader :score, :frame, :frame_scores
 
   def initialize 
     @score = 0
     @frame = 1
     @frame_score = 0
-    @frame_results = [] # Xstrike, \spare, 0-9.
     @frame_scores = []
   end
 
   def roll(pins)
-    if pins == 10 
-      @frame_results <<  'X' 
-    elsif @frame_score + pins == 10
-      @frame_results << [@frame_score, '/'] 
-    else
-      @frame_score += pins
-      if @frame_results.length == 0
-        @frame_results << [pins]
-      else
-        @frame_results.last << pins
-      end
-    end
+
   end
 
   def gameover?
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -13,10 +13,6 @@ describe Game do
     expect(subject.frame).to be <= 10
   end
 
-  it 'tracks frame results' do 
-    expect(subject.frame_results).to eq []
-  end
-
   it 'tracks frame scores' do
     expect(subject.frame_scores).to eq []
   end
@@ -24,21 +20,6 @@ describe Game do
   describe '#roll' do
     it 'takes the number of pins the user knocked down' do
       expect(subject).to respond_to(:roll).with(1).argument
-    end
-
-    context 'user knocks 10 pins' do
-      it 'should record a strike' do
-        subject.roll(10)
-        expect(subject.frame_results.last).to eq 'X'
-      end
-    end
-
-    context 'user rolls a split' do
-      it 'should record a split' do
-        subject.roll(7)
-        subject.roll(3)
-        expect(subject.frame_results.last).to eq [7, '/']
-      end
     end
   end
 
