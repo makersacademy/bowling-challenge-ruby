4530a24ee4a00bdebbdd4417d8f48fd5096d7afa
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-31 13:25:54 +0100

    Refactoring #calculate_score into 4 different methods

--- lib/scoring_bowling.rb
+++ lib/scoring_bowling.rb
@@ -18,7 +18,6 @@ class ScoringBowling
   def calculate_score
     current_frame = 0
     arr = []
-    # Needs to be refactored, unsure how.
     until current_frame == rounds.length
       calculate_all_scenarios(current_frame, arr)
       current_frame += 1
@@ -28,25 +27,36 @@ class ScoringBowling
 
   private
 
-  def calculate_all_scenarios(which_frame, arr)
+  def calculate_all_scenarios(which_frame, chosen_arr)
     if strike?(round(which_frame))
-      calculate_strike(which_frame, arr)
+      calculate_strike(which_frame, chosen_arr)
     elsif spare?(round(which_frame))
-      calculate_spare(which_frame, arr)
+      calculate_spare(which_frame, chosen_arr)
     else
-      arr << round(which_frame)
+      chosen_arr << round(which_frame)
     end
   end
 
-  def calculate_strike(which_frame, arr)
-    arr << round(which_frame) if which_frame == 9
-    arr << [round(which_frame), round(which_frame + 1)[0..1]] if which_frame == 8
-    arr << [round(which_frame), round(which_frame + 1)[0], round(which_frame + 2)[0]] if which_frame < 8
+  def calculate_strike(chosen_frame, chosen_arr)
+    chosen_arr << choose_scenario(1, chosen_frame ) if chosen_frame == 9
+    chosen_arr << choose_scenario(2, chosen_frame, 1) if chosen_frame == 8
+    chosen_arr << choose_scenario(3, chosen_frame) if chosen_frame < 8
   end
 
-  def calculate_spare(which_frame, arr)
-    arr << round(which_frame) if which_frame == 9
-    arr << [round(which_frame), round(which_frame + 1)[0]] if which_frame < 9
+  def calculate_spare(chosen_frame, chosen_arr)
+    chosen_arr << choose_scenario(1, chosen_frame) if chosen_frame == 9
+    chosen_arr << choose_scenario(2, chosen_frame, 0) if chosen_frame < 9
+  end
+
+  def choose_scenario(input, chosen_frame, *index)
+    case input
+      when 1
+        round(chosen_frame)
+      when 2
+        [round(chosen_frame), round(chosen_frame + 1)[0..index.pop]]
+      when 3
+        [round(chosen_frame), round(chosen_frame + 1)[0], round(chosen_frame + 2)[0]]
+    end
   end
 
   def round(which_frame)
