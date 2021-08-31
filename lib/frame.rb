22b220abfa02b90fc0c86c6c900c85a737639f19
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-08-31 09:29:31 +0100

    Adding initialize function to Frame class

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,3 +1,11 @@
 class Frame
+  attr_reader :first_roll, :number
+  attr_accessor :second_roll
 
+  def initialize(scorecard, number, first_roll)
+    @number = number
+    @first_roll = first_roll
+    @second_roll = nil
+    @scorecard = scorecard
+  end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -4,9 +4,11 @@ describe Frame do
   let(:boring)      { double(Frame, number: 1, first_roll: 1, second_roll: 1) }
   let(:spare)       { double(Frame, number: 3, first_roll: 5, second_roll: 5) }
   let(:strike)      { double(Frame, number: 2, first_roll: 10) }
-  let(:blank_sheet) { double('Scoresheet', scores: [boring, spare, strike]) }
+  let(:mixed_sheet) { double('Scorecard', scores: [boring, spare, strike]) }
 
   it 'has a first roll' do   
-    
+    frame = Frame.new(mixed_sheet, 1, 1)
+
+    expect(frame.first_roll).to eq 1
   end
 end
