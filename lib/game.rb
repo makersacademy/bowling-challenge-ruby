c3671f5df20cfb1c444b520cde69060267a6c75a
danroche10 <danielroche10@gmail.com>
2021-08-31 18:52:39 +0100

    new attempt with better structure

--- lib/game.rb
+++ lib/game.rb
@@ -2,45 +2,23 @@ require_relative 'frame'
 
 class Game
 
-  attr_reader :current_frame, :game
+  attr_reader :no_of_frames_completed, :current_frame, :game
 
   def initialize(frame = Frame.new)
     @no_of_frames_completed = 0
     @current_frame = frame
     @game = []
-    run_game
   end
 
-  def run_game
-    until @game.length == 10
-      print "Please bowl"
-      score = gets.chomp.to_i
-      process_move(score)
-    end
+
+
+  def check_roll
+    return @current_frame.no_of_rolls 
   end
 
-  def process_move(score, frame = Frame.new)
-    @current_frame.add_roll
-    if @current_frame.no_of_rolls == 1 #don't take 10th frame into account yet
-      @current_frame.roll_1_score = score
-    else
-      @current_frame.roll_2_score = score
-      @current_frame.frame_number = @game.length + 1
-      @current_frame.calc_frame_total
-      @current_frame.set_frame_total
-      @game << @current_frame
-      @current_frame = frame
-    end
+  def add_roll_1_score(score)
+    @current_frame.roll_1_score + score
   end
 
-  def current_score
-    # called after each frame
-  end
-
-  def score_game
-    # called at the end of the game
-  end
 end
-test_game = Game.new
-print test_game.game
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,18 +1,24 @@
 require 'game'
 describe Game do
-
-
-
-  describe "#initialize" do
-    it "initializes with a start date, end date, user_id and space_id" do
-      Game.stub(:new).and_return(true)
-
-      expect(Game.new).to have_attributes(no_of_frames_completed: 0)
+  
+  let(:frame) { double(no_of_rolls: 1, roll_1_score: 0) }
+  let(:new_game) { described_class.new(frame)}
+  describe '#initialise' do
+    it 'initializes with empty game array' do
+      expect(new_game.game).to eq []
     end
   end
 
+  describe '#add_roll_1_score' do
+    it 'returns correct roll number' do
+      new_game.add_roll_1_score(10)
+      expect(frame.roll_1_score).to eq(10)
+    end
+  end
 
-
-
-
+  describe '#check_roll' do
+    it 'returns correct roll number' do
+      expect(new_game.check_roll).to eq(1)
+    end
+  end
 end
