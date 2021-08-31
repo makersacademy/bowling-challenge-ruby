726d6e5f60e792659acb8f565f82691334fadb94
edpackard <edpackard81@gmail.com>
2021-08-31 01:12:29 +0100

    adding work on scoring class

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -8,10 +8,7 @@ class Scoring
   def calculation
     calculation = []
     (0...@scores.length).each do |index|
-      total = simple_add(index) if both_integers?(index)
-      total = strike(index) if @scores[index][0] == :X
-      total = spare(index) if @scores[index][1] == :/
-      calculation  << [total]
+      calculation << [frame_total(index)]
     end
     calculation
   end
@@ -27,6 +24,18 @@ class Scoring
     score_array
   end
 
+  def frame_total(index)
+    return final_frame(index) unless @scores[index][2].nil?
+
+    return simple_add(index) if both_integers?(index)
+
+    return strike(index) if @scores[index][0] == :X
+
+    return spare(index) if @scores[index][1] == :/
+
+    @scores[index][0] if single_roll?(index)
+  end
+
   def both_integers?(index)
     (@scores[index][0].is_a? Integer) && (@scores[index][1].is_a? Integer)
   end
@@ -37,15 +46,24 @@ class Scoring
 
   def strike(index)
     next_index = index + 1
+    return 10 if @scores[next_index].nil?
+
     return 10 + simple_add(next_index) if both_integers?(next_index)
+
     return 10 + 10 + two_ahead(next_index + 1) if double?(next_index)
+
     return 10 + 10 if @scores[next_index][1] == :/
+
+    10 + @scores[next_index][0]
   end
 
   def spare(index)
     next_index = index + 1
+    return 10 if @scores[next_index].nil?
+
     return 10 + 10 if @scores[next_index][0] == :X
-    return 10 + @scores[next_index][0] 
+    
+    10 + @scores[next_index][0] 
   end
 
   def double?(next_index)
@@ -55,6 +73,14 @@ class Scoring
   def two_ahead(two_indexes_ahead)
     value = @scores[two_indexes_ahead][0]
     value == :X ? 10 : value 
+  end
+
+  def single_roll?(index)
+    (@scores[index][0].is_a? Integer) && @scores[index][1].nil?
+  end
+
+  def final_frame(index)
+    # tbc
   end
 
 end
--- spec/unit/scoring_spec.rb
+++ spec/unit/scoring_spec.rb
@@ -26,14 +26,14 @@ describe Scoring do
     end
 
     it 'adds next roll to a spare' do
-      frames = [{ roll_1: 3, roll_2: :/}, { roll_1: 3 , roll_2: 4 }]
+      frames = [{ roll_1: 3, roll_2: :/ }, { roll_1: 3, roll_2: 4 }]
       scoring = Scoring.new(frames)
       expect(scoring.calculation.first).to contain_exactly 13
       expect(scoring.calculation.last).to contain_exactly 7
     end
 
     it 'adds strike to a spare' do
-      frames = [{ roll_1: 3, roll_2: :/}, { roll_1: :X }, { roll_1: 3, roll_2: 4 }]
+      frames = [{ roll_1: 3, roll_2: :/ }, { roll_1: :X }, { roll_1: 3, roll_2: 4 }]
       scoring = Scoring.new(frames)
       expect(scoring.calculation.first).to contain_exactly 20
       expect(scoring.calculation[1]).to contain_exactly 17
@@ -49,7 +49,7 @@ describe Scoring do
     end
 
     it 'check if logic works for turkey' do
-      frames = [{ roll_1: :X }, { roll_1: :X }, { roll_1: :X }, { roll_1: 3, roll_2: 4}]
+      frames = [{ roll_1: :X }, { roll_1: :X }, { roll_1: :X }, { roll_1: 3, roll_2: 4 }]
       scoring = Scoring.new(frames)
       expect(scoring.calculation.first).to contain_exactly 30
       expect(scoring.calculation[1]).to contain_exactly 23
@@ -67,21 +67,62 @@ describe Scoring do
       expect(scoring.calculation[2]).to contain_exactly 30
       expect(scoring.calculation.last).to contain_exactly 3
     end
+
+    it 'returns base value of strike if no subsequent rolls' do
+      frames = [{ roll_1: :X }]
+      scoring = Scoring.new(frames)
+      expect(scoring.calculation.first).to contain_exactly 10
+    end
+
+    it 'returns base value of spare if no subsequent rolls' do
+      frames = [{ roll_1: 2, roll_2: :/ }]
+      scoring = Scoring.new(frames)
+      expect(scoring.calculation.first).to contain_exactly 10
+    end
+    
+    it 'begins to calculate strike bonus after one further roll' do
+      frames = [{ roll_1: :X }, { roll_1: 2 }]
+      scoring = Scoring.new(frames)
+      expect(scoring.calculation.first).to contain_exactly 12
+      expect(scoring.calculation.last).to contain_exactly 2
+    end
+
   end
 
-  context 'final frame' do
+#   context 'final frame' do
 
-    it 'adds 2 strikes to initial strike in final frame' do
-    end
+#     it 'calculates 3 strikes' do
+#       frames = [{ roll_1: :X, roll_2: :X, roll_3: :X }]
+#       scoring = Scoring.new(frames)
+#       expect(scoring.calculation.first).to contain_exactly 30
+#       p scoring.calculation
+#     end
 
-    it 'adds a spare in third roll of final frame' do
-    end
+#     it 'calculates strike followed by two low rolls ' do
+#       frames = [{ roll_1: :X, roll_2: 2, roll_3: 3 }]
+#       scoring = Scoring.new(frames)
+#       expect(scoring.calculation.first).to contain_exactly 15
+#     end
 
-    it 'adds third roll (non-strike) to spare in final frame' do
-    end
+#     it 'calculates low roll, strike, strike' do
+#       frames = [{ roll_1: 3, roll_2: :X, roll_3: :X }]
+#     end
 
-    it 'adds a strike in third roll of final frame' do
-    end
-  end
+#     it 'calculates strike, low roll, strike' do
+#       frames = [{ roll_1: :X, roll_2: 3, roll_3: :X }]
+#     end
+
+#     it 'calculates strike, strike, low roll' do
+#       frames = [{ roll_1: :X, roll_2: :X, roll_3: :X }]
+#     end
+
+#     it 'calculates spare followed by low roll' do
+#       frames = [{ roll_1: 3, roll_2: :/, roll_3: 3 }]
+#     end
+
+#     it 'calculates spare followed by strike' do
+#       frames = [{ roll_1: 3, roll_2: :/, roll_3: :X }]
+#     end
+#   end
 
 end
