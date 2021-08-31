73781d45ca7726c8e3ed4f5303e4c8357dbcae3c
Graeme Stirling <graemejstirling@gmail.com>
2021-08-31 12:29:40 +0100

    Cleaned up code/tests for review

--- lib/player.rb
+++ lib/player.rb
@@ -1,36 +1,18 @@
 class Player 
   # needs to keep track of their own frames 
 
-attr_reader :name, :frames, :strike_streak, :spare
+  attr_reader :name, :frames
 
   def initialize(name, frames = Hash.new)
     @name = name
     @frames = frames
-    @strike_streak = 0
-    @spare = false
-  end
-
-  def frame
-    fail "Game is complete no frames remaining" if @frames.length == 10
-    first_bowl = rand(0..10)
-    # case strike 
-    index = ((@frames.length) + 1)
-    return @frames[index] = [10, 0] if first_bowl == 10
-    remaining_pins = 10 - first_bowl
-    second_bowl = rand(0..remaining_pins)
-    # case spare 
-    # case no spare or strike scored
-    @frames[index] = [first_bowl ,second_bowl]
   end
 
   def total
     tally = []
     @frames.map do |key, frame|
-      # When a turkey or more is scored 
-      if false
-        p "WE HAVE A TURKEY"
       # When we score a double 
-      elsif (@frames[key] == @frames[key+1]) && (frame == [10, 0])
+      if (@frames[key] == @frames[key+1]) && (frame == [10, 0])
         score = 20 + @frames[key+2].sum 
         tally << score
       # if we get a strike include bonus points 
@@ -50,3 +32,17 @@ attr_reader :name, :frames, :strike_streak, :spare
   tally.sum
   end
 end
+
+# This method wasn't needed but was fun to make
+# def frame
+#   fail "Game is complete no frames remaining" if @frames.length == 10
+#   first_bowl = rand(0..10)
+#   # case strike 
+#   index = ((@frames.length) + 1)
+#   return @frames[index] = [10, 0] if first_bowl == 10
+#   remaining_pins = 10 - first_bowl
+#   second_bowl = rand(0..remaining_pins)
+#   # case spare 
+#   # case no spare or strike scored
+#   @frames[index] = [first_bowl ,second_bowl]
+
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,28 +1,28 @@
 describe Player do 
 
+  # helper methods that contain scores for testing ########################################
   let(:player) { described_class.new("Graeme")}
   # score with no spares or strikes
   let(:test_score) { described_class.new("Graeme", {1 => [2, 3], 2 => [4, 5], 3 => [6, 1]})} 
   # score with single spare
   let(:spare_score) { described_class.new("Graeme", {1 => [2, 3], 2 => [4, 6], 3 => [6, 1]}) }
   # # score with spares an no strikes 
-  let(:spares_score) { described_class.new("Graeme", {1 => [2, 3], 2 => [4, 5], 3 => [6, 4], 4 =>[7, 0], 5 => [8, 2], 6 => [3, 7], 7 => [3, 4], 8 => [3, 5], 9 => [6, 2]}) }
+  let(:spares_score) { described_class.new("Graeme", {1 => [2, 3], 2 => [4, 5], 3 => [6, 4], 
+  4 =>[7, 0], 5 => [8, 2], 6 => [3, 7], 7 => [3, 4], 8 => [3, 5], 9 => [6, 2]}) }
   # score with two strikes and some spares
-  let(:strike_score) { described_class.new("Graeme", {1 => [2, 3], 2 => [10, 0], 3 => [6, 4], 4 => [10, 0], 5 => [8, 2], 6 => [3, 5]}) }
+  let(:strike_score) { described_class.new("Graeme", {1 => [2, 3], 2 => [10, 0], 3 => [6, 4], 
+  4 => [10, 0], 5 => [8, 2], 6 => [3, 5]}) }
   # score that includes a double 
-  let(:double_score) { described_class.new("Graeme", { 1 => [7, 0], 2 => [10, 0], 3 => [10, 0], 4 => [5, 0]}) }
+  let(:double_score) { described_class.new("Graeme", { 1 => [7, 0], 2 => [10, 0], 3 => [10, 0],
+  4 => [5, 0]}) }
   # score that includes a turkey 
   let(:turkey_score) { described_class.new("Graeme", [[7, 0], [10, 0], [10, 0], [10, 0], [3, 2]]) }
+  #####################################
   
   it "creates and instance of itself" do 
     expect(player).to be_an_instance_of(Player)
   end 
 
-  it "stop player trying to do more than ten frames" do 
-    10.times { player.frame }
-    expect { player.frame }.to raise_error("Game is complete no frames remaining")
-  end 
- 
   it "calculates a the score with no spares or strikes" do
     expect(test_score.total).to eq(21)
   end 
@@ -44,3 +44,9 @@ describe Player do
   #   expect(turkey_score.total).to eq(82)
   # end
 end 
+
+  # unused test
+  # it "stop player trying to do more than ten frames" do 
+  #   10.times { player.frame }
+  #   expect { player.frame }.to raise_error("Game is complete no frames remaining")
+  # end 
