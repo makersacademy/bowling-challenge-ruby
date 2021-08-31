e426026e5a362d454969a2bd481b25c7288f8384
Graeme Stirling <graemejstirling@gmail.com>
2021-08-31 09:07:19 +0100

    Doubles  now able to be calculated in scores

--- lib/player.rb
+++ lib/player.rb
@@ -14,7 +14,7 @@ attr_reader :name, :frames, :strike_streak, :spare
     fail "Game is complete no frames remaining" if @frames.length == 10
     first_bowl = rand(0..10)
     # case strike 
-    index = ((@frames.length) + 1).to_s
+    index = ((@frames.length) + 1)
     return @frames[index] = [10,0] if first_bowl == 10
     remaining_pins = 10 - first_bowl
     second_bowl = rand(0..remaining_pins)
@@ -24,22 +24,29 @@ attr_reader :name, :frames, :strike_streak, :spare
   end
 
   def total
-    tally = 0
+    tally = []
     @frames.map do |key, frame|
+      # When a turkey or more is scored 
+      if false
+        p "WE HAVE A TURKEY"
+      # When we score a double 
+      elsif (@frames[key] == @frames[key+1]) && (frame == [10,0])
+        score = 20 + @frames[key+2].sum 
+        tally << score
       # if we get a strike include bonus points 
-      if frame[0] == 10 
-        bonus = @frames[(key.to_i + 1).to_s]
-        tally += bonus.sum + 10 
+      elsif frame[0] == 10 
+        bonus = @frames[(key + 1)]
+        tally << (bonus.sum + 10) 
       # if we get a spare add first bowl to tally
       elsif frame.sum == 10
-        bonus = @frames[(key.to_i + 1).to_s]
-        tally += (bonus[0] + frame.sum)
+        bonus = @frames[(key + 1)]
+        tally << (bonus[0] + frame.sum)
       # if no spare procced to add frame score to tally  
       else
-        tally += frame.sum
+        tally << frame.sum
       end 
     end
-  tally
+  p tally
+  tally.sum
   end
-
 end
