bbd5d946eea9bbf73ed88b5db8d5a1f4f64200d9
Paul Rainsford <paulrainsford69@gmail.co>
2021-08-31 09:18:35 +0100

    Added methods and tests passing but not sure if working properly

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,37 +1,48 @@
 class Bowling
-
-
   def initialize
     @rolls = []
   end
   
   def roll(pins)
+    raise 'You must enter a score less than 10' if pins > 10
     @rolls << pins
   end
 
   def score
-    total = 0
     current_roll = 0
-    @rolls.each do |roll|
-      total += roll
+    total = 0
+    10.times do 
+      if strike?(current_roll)
+        total += strike_bonus(current_roll)
+        current_roll += 1
+      elsif spare?(current_roll)
+        total += spare_bonus(current_roll)
+        current_roll += 2
+      else
+        total += sum_of_frame_rolls(current_roll)
+        current_roll += 2
+      end
     end
     total
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
+  def spare_bonus(current_roll)
+    @rolls[current_roll + 2] + 10
+  end
+  
+  def strike_bonus(current_roll)
+    @rolls[current_roll + 1] + @rolls[current_roll + 2] + 10
+  end
+  
+  def spare?(current_roll)
+    @rolls[current_roll] + @rolls[current_roll + 1] == 10
+  end
+  
+  def strike?(current_roll)
+    @rolls[current_roll] == 10
+  end
+  
+  def sum_of_frame_rolls(current_roll)
+    @rolls[current_roll] + @rolls[current_roll + 1]
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,7 +1,7 @@
 require 'bowling'
 
 describe Bowling do
-  let(:game) {Bowling.new}
+  let(:game) { Bowling.new }
 
   it 'can score a gutter game' do
     20.times { game.roll(0) }
@@ -13,8 +13,24 @@ describe Bowling do
     expect(game.score).to eq 20
   end
 
+  it 'can score a spare' do
+    game.roll(7)
+    game.roll(3)
+    game.roll(4)
+    17.times { game.roll(0) }
+    expect(game.score).to eq 18
+  end
 
+  it 'can score a strike' do
+    game.roll(10)
+    game.roll(7)
+    game.roll(5)
+    16.times { game.roll(0) }
+    expect(game.score).to eq 34
+  end
 
-
-
+  it 'can play a perfect game' do
+    12.times { game.roll(10) }
+    expect(game.score).to eq 300
+  end
 end
