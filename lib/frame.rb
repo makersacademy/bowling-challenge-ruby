019d9cc51453555010b728ad0258b91e5e51de1f
danroche10 <danielroche10@gmail.com>
2021-09-01 19:43:49 +0100

    1 point off matching first 6 frames in example

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,11 +1,10 @@
 class Frame
 
-  attr_reader :no_of_rolls, :bonus_roll_score, :frame_total
-  attr_accessor :roll_1_score, :roll_2_score, :frame_number
+  attr_reader :no_of_rolls, :frame_total
+  attr_accessor :roll_1_score, :roll_2_score, :bonus_score
 
   def initialize
     @no_of_rolls = 0
-    @frame_number = 0
     @roll_1_score = 0
     @roll_2_score = 0
     @bonus_score = 0
@@ -24,12 +23,13 @@ class Frame
     @roll_2_score = score
   end
 
-  def add_bonus_score(roll_1_score, roll_2_score)
-    @bonus_score = score
+  def add_bonus_score(bonus)
+    @bonus_score = bonus
+    
   end
 
   def calc_frame_total
-    @roll_1_score + @roll_2_score
+    @roll_1_score + @roll_2_score + @bonus_score
   end
 
   def set_frame_total
--- lib/game.rb
+++ lib/game.rb
@@ -12,8 +12,7 @@ class Game
   def roll(score)
     update_score(score)
     @current_frame.add_roll
-    return complete_frame if frame_complete?
-    # add_bonus_score(score) if strike_bonus_score?
+    complete_frame if frame_complete?
   end
   
   def check_roll
@@ -33,21 +32,26 @@ class Game
     return true if  @current_frame.no_of_rolls == 2
   end
 
-  def strike_bonus_score?
-    return true if @game.last.roll_1_score == 10
+  def add_strike_bonus
+    strike_bonus = @current_frame.roll_1_score + @current_frame.roll_2_score
+    @game.last.add_bonus_score(strike_bonus)
   end
 
-  # def add_bonus_score(score)
-  #   bonus = roll_1_score + roll_2_score
-  #   @current_frame.add_bonus_score(bonus)
-  # end
-
-  def spare_bonus_score?
-    return true if @game.last.frame_total == 10
+  def add_spare_bonus
+    spare_bonus = @current_frame.roll_1_score
+    @game.last.add_bonus_score(spare_bonus)
   end
 
-  def is_first_frame?
-    @games.length < 1 ? true : false
+  def strike_bonus?
+    return true if is_not_first_frame? && @game.last.roll_1_score == 10
+  end
+
+  def spare_bonus?
+    return true if is_not_first_frame? && @game.last.frame_total == 10
+  end
+
+  def is_not_first_frame?
+    @game == [] ? false : true
   end
 
   def complete_frame(frame = Frame.new)
@@ -57,17 +61,50 @@ class Game
   end
 
   def update_frame_total
-    @current_frame.calc_frame_total
-    @current_frame.set_frame_total
+    add_strike_bonus if strike_bonus?
+    add_spare_bonus if spare_bonus?
+    @current_frame.calc_frame_total && @current_frame.set_frame_total
+    @game.last.calc_frame_total && @game.last.set_frame_total if is_not_first_frame?
+    
+  end
+
+  def score_total
+    score = 0
+    @game.each { |frame| score += frame.frame_total }
+    score
   end
 
 end
 
 my_game = Game.new
-# my_game.roll(9)
-# my_game.roll(9)
-my_game.roll(10)
-my_game.roll(9)
-my_game.roll(9)
-print my_game.game
+my_game.roll(1)
+my_game.roll(4)
 
+# puts my_game.score_total
+
+my_game.roll(4)
+my_game.roll(5)
+# puts my_game.score_total
+
+my_game.roll(6)
+my_game.roll(4)
+# puts my_game.score_total
+# puts my_game.spare_bonus?
+
+my_game.roll(5)
+my_game.roll(5)
+# puts my_game.score_total
+# puts my_game.spare_bonus?
+
+my_game.roll(10)
+
+my_game.roll(0)
+my_game.roll(1)
+
+# puts my_game.score_total
+
+
+# puts my_game.score_total
+# puts "----"
+# my_game.game.each { |frame| puts frame.bonus_score }
+print my_game.score_total
