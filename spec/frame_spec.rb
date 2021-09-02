1dd17fdda8d21960ca4f29cabd2524b850942204
danroche10 <danielroche10@gmail.com>
2021-09-02 11:47:34 +0100

    updated tests

--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -6,45 +6,49 @@ describe Frame do
 
   describe '#initialise' do
     it 'initializes with empty game array' do
-      expect(new_frame.no_of_rolls).to eq 0
+      expect(new_frame.rolls).to eq []
     end
   end
 
-  describe '#add_roll' do
-    it 'adds 1 to number of rolls' do
-      new_frame.add_roll
-      expect(new_frame.no_of_rolls).to eq 1
+  describe '#add_score' do
+    it 'adds 1 roll to rolls array' do
+      new_frame.add_score(9)
+      expect(new_frame.rolls.length).to eq 1
+    end
+
+    it 'returns current score for the frame' do
+      new_frame.add_score(5)
+      expect(new_frame.rolls.sum).to eq 5
+    end
+
+    it 'adds 2 rolls to rolls array' do
+      new_frame.add_score(3)
+      new_frame.add_score(4)
+      expect(new_frame.rolls.length).to eq 2
+    end
+
+    it 'returns current score for two rolls' do
+      new_frame.add_score(5)
+      new_frame.add_score(4)
+      expect(new_frame.rolls.sum).to eq 9
     end
   end
 
-  describe '#add_roll_2_score' do
-  it 'add score to roll_2_score' do
-    new_frame.add_roll_1_score(5)
-    expect(new_frame.roll_1_score).to eq 5
-  end
-end
-
-  describe '#add_roll_1_score' do
-    it 'add score to roll_1_score' do
-      new_frame.add_roll_1_score(10)
-      expect(new_frame.roll_1_score).to eq 10
-    end
-  end
 
   describe '#calc_frame_total' do
     it 'calculates score for the frame' do
-      new_frame.add_roll_1_score(5)
-      new_frame.add_roll_2_score(7)
-      expect(new_frame.calc_frame_total).to eq 12
+      new_frame.add_score(5)
+      new_frame.add_score(4)
+      expect(new_frame.calc_frame_total).to eq 9
     end
   end
 
   describe '#calc_frame_total' do
   it 'calculates score for the frame' do
-    new_frame.add_roll_1_score(5)
-    new_frame.add_roll_2_score(7)
+    new_frame.add_score(5)
+    new_frame.add_score(4)
     new_frame.set_frame_total
-    expect(new_frame.frame_total).to eq 12
+    expect(new_frame.frame_total).to eq 9
   end
 end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,7 @@ require 'frame'
 
 describe Game do
   
-  let(:frame) { double(no_of_rolls: 0, roll_1_score: 0) }
+  let(:frame) { double(:frame) }
   let(:new_game) { described_class.new(frame)}
 
   describe '#initialise' do
@@ -12,35 +12,18 @@ describe Game do
     end
   end
 
-  # work out how to do a test that correctly uses frame methods
   describe '#roll' do
     it 'updates score' do
-      allow(frame).to receive(:add_roll_1_score)
-      allow(frame).to receive(:add_roll)
+      allow(new_game).to receive(:frame_complete?) { false }
+      allow(frame).to receive(:add_score)
       expect(new_game.roll(6)).to eq nil
     end
   end
 
-  # work out how to do a test that correctly uses frame methods
-  describe '#update_score' do
-    it 'takes the score as an argument' do
-      expect(new_game).to respond_to(:update_score).with(1).argument
-    end
-    # test for score update tbc
-    # it 'adds score to current frame' do
-    #   new_game.roll(6)
-    # end
-  end
-
   describe '#frame_complete?' do
-    it 'does not return true if frame not complete' do
-      expect(new_game.frame_complete?).not_to eq true
-    end
-  end
-
-  describe '#check_roll' do
-    it 'returns correct roll number' do
-      expect(new_game.check_roll).to eq(0)
+    it 'does not return true when strike' do
+      allow(new_game).to receive(:strike?) { true }
+      expect(new_game.frame_complete?).to eq true
     end
   end
 end
