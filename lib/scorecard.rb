a20571d64366417bc9a5e652a51f79590e4a7470
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-09-24 19:03:21 +0100

    added current_frame instvar

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,8 +1,9 @@
 class Scorecard
-  attr_reader :player_name, :current_score
+  attr_reader :player_name, :current_score, :current_frame
 
   def initialize(name)
     @player_name = name
     @current_score = 0
+    @current_frame = 1
   end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -13,5 +13,9 @@ describe Scorecard do
     it 'has a starting score of 0' do
       expect(@current_game.current_score).to eq 0
     end
+
+    it 'starts at the first frame' do
+      expect(@current_game.current_frame).to eq 1
+    end
   end
 end
