1df4c3633060ef7202b3bcacdd18334a2eb8a78e
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-09-24 18:58:55 +0100

    added player name instvar

---
+++ lib/scorecard.rb
@@ -0,0 +1,7 @@
+class Scorecard
+  attr_reader :player_name
+
+  def initialize(name)
+    @player_name = name
+  end
+end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,1 +1,12 @@
 require 'scorecard'
+
+describe Scorecard do
+  context 'upon initialization' do
+    before(:each) do
+      @current_game = described_class.new("Mabon")
+    end
+    it 'has a player name' do
+      expect(@current_game.player_name).to eq "Mabon"
+    end
+  end
+end
