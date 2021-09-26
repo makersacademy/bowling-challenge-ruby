decd28b3695453d2f9b8e36d0819e85cd09c65f7
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-26 14:36:03 +0100

    Recognising when a spare has been rolled

--- lib/game.rb
+++ lib/game.rb
@@ -2,13 +2,20 @@ class Game
 
     attr_reader :roll_one, :roll_two
 
-def initialize(roll_one, roll_two)
-  @roll_one = roll_one 
-  @roll_two = roll_two 
-end 
+  def initialize(roll_one, roll_two)
+    @roll_one = roll_one 
+    @roll_two = roll_two 
+  end 
 
-def total_of_frame
+  def total_of_frame
     roll_one + roll_two 
-end
+  end
 
+  def spare?
+    if (roll_one) + (roll_two) == 10 
+      true 
+    else 
+      false
+    end
+  end 
 end 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,15 @@
 require 'game'
 
 describe Game do
-  it 'should provide the total score of a frame correctly' do 
-   game = Game.new(:roll_one, :roll_two)
-   allow(game).to receive(:roll_one) { 3 }
-   allow(game).to receive(:roll_two) { 6 }
-   expect(game.total_of_frame).to eq (9)
-  end 
-end 
+  it 'should provide the total score of a frame correctly' do
+    game = Game.new(:roll_one, :roll_two)
+    allow(game).to receive(:roll_one) { 3 }
+    allow(game).to receive(:roll_two) { 6 }
+    expect(game.total_of_frame).to eq(9)
+  end
+
+  it 'should determine when a spare is rolled' do
+    game = Game.new(6, 4)
+    expect(game.spare?).to be true
+  end
+end
