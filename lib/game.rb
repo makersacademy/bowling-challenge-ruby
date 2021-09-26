14c9a3bffbdfdfa028e7afd6fc529db591c5b715
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-26 14:06:29 +0100

    Creating a method and test for sthe sum of two rolls in one frame

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,14 @@
 class Game
 
+    attr_reader :roll_one, :roll_two
 
-    
+def initialize(roll_one, roll_two)
+  @roll_one = roll_one 
+  @roll_two = roll_two 
+end 
+
+def total_of_frame
+    roll_one + roll_two 
+end
 
 end 
