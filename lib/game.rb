11576ec49db9f427b4fecbdc802adbf7b658bd8c
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-26 21:50:40 +0100

    Scoring for spares

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,6 @@
-class Game
+# frozen_string_literal: true
 
+class Game
   attr_reader :rolls, :score
 
   def initialize
@@ -11,35 +12,50 @@ class Game
     @rolls << num
   end
 
-  def score 
-    @rolls.reduce(:+)
-  end 
-  
+  def score
+    total_score  = 0
+    current_roll = 0
 
-  #   attr_reader :roll_one, :roll_two
+    while current_roll < @rolls.size - 1
+      roll      = @rolls[current_roll]
+      next_roll = @rolls[current_roll + 1]
 
-  # def initialize(roll_one, roll_two)
-  #   @roll_one = roll_one
-  #   @roll_two = roll_two
-  # end
+      if roll + next_roll == 10
+        total_score += 10 + @rolls[current_roll + 2]
+      else
+        total_score += roll + next_roll
+      end
 
-  # def total_of_frame
-  #   roll_one + roll_two
-  # end
+      current_roll += 2
+    end
 
-  # def spare?
-  #   if roll_one + roll_two == 10 && roll_one != 10
-  #     true
-  #   else
-  #     false
-  #   end
-  # end
-
-  # def strike?
-  #   if roll_one == 10 && roll_two == 0
-  #     true
-  #   else
-  #     false
-  #   end
-  # end
+    return total_score
+  end
 end
+
+#   attr_reader :roll_one, :roll_two
+
+# def initialize(roll_one, roll_two)
+#   @roll_one = roll_one
+#   @roll_two = roll_two
+# end
+
+# def total_of_frame
+#   roll_one + roll_two
+# end
+
+# def spare?
+#   if roll_one + roll_two == 10 && roll_one != 10
+#     true
+#   else
+#     false
+#   end
+# end
+
+# def strike?
+#   if roll_one == 10 && roll_two == 0
+#     true
+#   else
+#     false
+#   end
+# end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'game'
 
 describe Game do
-  let(:game) { Game.new } 
+  let(:game) { Game.new }
 
   describe '.initialize' do
     it 'sets @rolls to an empty array' do
@@ -10,42 +12,56 @@ describe Game do
   end
 
   describe '#score' do
-    it 'scores the game as 0 when only gutter balls are thrown' do
-      20.times { game.roll(0) }
-      expect(game.score).to eq(0)
+    context 'when only gutter balls are thrown' do
+      it 'scores the game as 0' do
+        20.times { game.roll(0) }
+        expect(game.score).to eq(0)
+      end
     end
 
-    it 'scores the game as 20 when one pin is knocked down each roll' do
-      20.times { game.roll(1) }
+    context 'when one pin is knocked down each roll' do
+      it 'scores the game as 20' do
+        20.times { game.roll(1) }
+        expect(game.score).to eq(20)
+      end
     end
-  end 
 
-  # it 'should provide the total score of a frame correctly' do
-  #   game.roll(3)
-  #   game.roll(6)
-  #   expect(game.score).to eq(9)
-  # end
-
-  # it 'should provide the total score of a frame correctly' do
-  #   game = Game.new(:roll_one, :roll_two)
-  #   allow(game).to receive(:roll_one) { 3 }
-  #   allow(game).to receive(:roll_two) { 6 }
-  #   expect(game.total_of_frame).to eq(9)
-  # end
-
-  # it 'should determine when a spare is rolled' do
-  #   game = Game.new(6, 4)
-  #   expect(game.spare?).to be true
-  # end
-
-  # it 'should determine when a strike is rolled' do
-  #   game = Game.new(10, 0)
-  #   expect(game.strike?).to be true
-  # end
-
-  # it 'can calculate a gutter game' do
-  #   game = Game.new(:roll_one, :roll_two)
-  #   20.times(game.roll(0))
-  #   expect(game.score).to eq 0
-  # end
+    context 'when a spare is thrown' do
+      it 'records the frame score as 10 + the number from the next round (twice to include the bonus)' do
+        3.times  { game.roll(5) }
+        17.times { game.roll(0) }
+        expect(game.score).to eq(20)
+        # total is 5+5+5 (+5 for bonus) = 15
+      end
+    end
+  end
 end
+
+# it 'should provide the total score of a frame correctly' do
+#   game.roll(3)
+#   game.roll(6)
+#   expect(game.score).to eq(9)
+# end
+
+# it 'should provide the total score of a frame correctly' do
+#   game = Game.new(:roll_one, :roll_two)
+#   allow(game).to receive(:roll_one) { 3 }
+#   allow(game).to receive(:roll_two) { 6 }
+#   expect(game.total_of_frame).to eq(9)
+# end
+
+# it 'should determine when a spare is rolled' do
+#   game = Game.new(6, 4)
+#   expect(game.spare?).to be true
+# end
+
+# it 'should determine when a strike is rolled' do
+#   game = Game.new(10, 0)
+#   expect(game.strike?).to be true
+# end
+
+# it 'can calculate a gutter game' do
+#   game = Game.new(:roll_one, :roll_two)
+#   20.times(game.roll(0))
+#   expect(game.score).to eq 0
+# end
