fa4f5c59c6101d3d399f04b912743a70aa215375
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-10-01 11:59:43 +0100

    Refactoring to have methods for strike and spare

--- lib/game.rb
+++ lib/game.rb
@@ -18,19 +18,32 @@ class Game
     current_roll = 0
 
     while current_roll < @rolls.size - 1 #.size returns the number of elements in the set
-      roll      = @rolls[current_roll]
+      roll = @rolls[current_roll]
       next_roll = @rolls[current_roll + 1]
-      if roll == 10
+      if  strike?(current_roll) #roll == 10
         total_score += 10 + next_roll + @rolls[current_roll + 2]
         current_roll += 1
-      elsif roll + next_roll == 10
+      elsif spare?(current_roll) #roll + next_roll == 10 #spare
         total_score += 10 + @rolls[current_roll + 2]
         current_roll += 2
-      else
-        total_score += roll + next_roll
+      else #not a spare or strike 
+        total_score += roll + next_roll 
         current_roll += 2
       end
     end
     total_score
   end
+
+  def strike?(current_roll)
+    @rolls[current_roll] == 10
+  end
+
+  def spare?(current_roll)
+    @rolls[current_roll] + @rolls[current_roll + 1] == 10
+  end 
+
+  def regular_frame(current_roll)
+    @rolls[current_roll] + @rolls[current_roll + 1] 
+  end 
+
 end
