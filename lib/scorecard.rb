6cbd92b9b97bd8290d8d6830fa7603d39233d135
Lilly Daniell <daniell.lilly@gmail.com>
2021-10-01 08:19:30 +0100

    Score functionality implemented and tested

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,11 +1,35 @@
 class Scorecard
-  attr_reader :running_score, :player, :card, :index
+  attr_reader :player, :running_score, :bowl_total, :bowl_card, :score_card
 
   def initialize(running_score = 0, player = 'Lilly')
-    @running_score = running_score
     @player = player
-    @card = []
-    @index = 0
+    @running_score = running_score
+    @bowl_card = []
+    @score_card = []
+  end
+
+  def bowl(first_bowl, second_bowl = 0)
+    frame = [first_bowl, second_bowl]
+    @bowl_card << frame
+    score
+    bowl_total = first_bowl + second_bowl
+  end
+
+  def roll_total
+    @bowl_card[-1].sum
+  end
+
+  def spare?
+    roll_total == 10 ? true : false
+  end
+
+  def strike?
+    @bowl_card[-1][0] == 10 ? true : false
+  end
+  
+  def score
+    @score_card << roll_total unless spare? || strike?
+    @running_score += roll_total
   end
 
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,19 +1,45 @@
 require 'scorecard'
 
 describe Scorecard do
-  
-  context 'initialize' do
-    it 'is an instance of bowling class' do
-      expect(subject).to be_an_instance_of(Scorecard)
+
+  # before(:each) do
+  # end
+
+  let(:scorecard) { Scorecard.new }
+
+  context 'Initialize' do
+    it 'is an instance of scorecard' do
+      expect(scorecard).to be_an_instance_of(Scorecard)
     end
 
     it 'starts with a score of 0' do
-      expect(subject.running_score).to eq(0)
+      expect(scorecard.running_score).to eq(0)
     end
 
     it 'has a player associated with the scorecard' do
-      expect(subject.player).to eq('Lilly')
+      expect(scorecard.player).to eq('Lilly')
     end
   end
 
+  context 'Score' do
+    it 'adds two bowls together for a frame' do
+      expect(scorecard.bowl(3, 4)).to eq(7)
+    end
+
+    it 'determines if bowl is a spare' do
+      scorecard.bowl(5, 5)
+      expect(scorecard.spare?).to be(true)
+    end
+
+    it 'determines if bowl is a strike' do
+      scorecard.bowl(10)
+      expect(scorecard.strike?).to be(true)
+    end
+
+    it 'totals cumulative score after each roll' do
+      scorecard.bowl(3, 3)
+      scorecard.bowl(4, 2)
+      expect(scorecard.running_score).to eq(12)
+    end
+  end
 end
