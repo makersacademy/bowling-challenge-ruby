900585c4fa2e9a7188b36b0121381a6849fc2471
AJOsmaston <dajoxx@gmail.com>
2021-10-23 19:19:41 +0100

    bonus_round implemented

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -10,7 +10,49 @@ class Bowling
   end
 
   def score
-    @rolls.sum
+    @frame = 0
+    @score = 0
+    turn = 1
+    loop do
+      if strike?(@frame)
+        @score += 10 + @rolls[@frame + 1] + @rolls[@frame + 2]
+        @frame += 1
+      elsif spare?(@frame)
+        @score += 10 + @rolls[@frame + 2]
+        @frame += 2
+      else
+        @score += sum_of_frame(@frame)
+        @frame += 2
+      end
+      turn += 1
+      bonus_round(@frame) if turn == 10
+      break if @frame >= @rolls.length
+    end
+    @score
+  end
+
+  def bonus_round(frame)
+    if @rolls[frame] < 10
+      @score += @rolls[frame] + @rolls[frame + 1]
+      @frame += 2
+    else
+      @score += @rolls[frame] + @rolls[frame + 1] + @rolls[frame + 2]
+      @frame += 3
+    end
+  end
+  
+  private
+  
+  def strike?(frame)
+    @rolls[frame] == 10
+  end
+
+  def spare?(frame)
+    @rolls[frame] + @rolls[frame + 1] == 10
+  end
+
+  def sum_of_frame(frame)
+    @rolls[frame] + @rolls[frame + 1]
   end
 
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -16,4 +16,39 @@ describe Bowling do
     expect(game.score).to eq 7
   end
 
+  it 'scores a spare' do
+    game.roll(6)
+    game.roll(4)
+    game.roll(1)
+    game.roll(1)
+    expect(game.score).to eq 13
+  end
+
+  it 'scores a strike' do
+    game.roll(10)
+    game.roll(3)
+    game.roll(4)
+    expect(game.score).to eq 24
+  end
+
+  it 'scores multiple strikes' do
+    3.times { game.roll(10) }
+    game.roll(3)
+    game.roll(4)
+    expect(game.score).to eq 77
+  end
+
+  it 'scores a perfect game' do
+    12.times { game.roll(10) }
+
+    expect(game.score).to eq 300
+  end
+
+  it 'misses a perfect name' do
+    9.times { game.roll(10) }
+    game.roll(3)
+    game.roll(4)
+
+    expect(game.score).to eq 257
+  end
 end
