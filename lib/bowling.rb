70eba53d9a3924a53b3a767be912ee05ded0397e
AJOsmaston <dajoxx@gmail.com>
2021-10-23 21:28:03 +0100

    refactors

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,58 +1,92 @@
 class Bowling
+  MAX_PINS = 10
+  MAX_ROUNDS = 10
   attr_reader :rolls
 
   def initialize
     @rolls = []
+    @frame = 0
+    @score = 0
   end
 
   def roll(pins)
-    @rolls << pins
+    @rolls.push(pins)
   end
 
   def score
-    @frame = 0
-    @score = 0
     turn = 1
     loop do
-      if strike?(@frame)
-        @score += 10 + @rolls[@frame + 1] + @rolls[@frame + 2]
-        @frame += 1
-      elsif spare?(@frame)
-        @score += 10 + @rolls[@frame + 2]
-        @frame += 2
+      if spare?(@frame)
+        spare
+      elsif strike?(@frame)
+        strike
       else
-        @score += sum_of_frame(@frame)
-        @frame += 2
+        sum_points
       end
       turn += 1
-      bonus_round(@frame) if turn == 10
-      break if @frame >= @rolls.length
+      final_round(@frame) if turn == MAX_ROUNDS
+      break if gameover
     end
     @score
   end
 
-  def bonus_round(frame)
-    if @rolls[frame] < 10
-      @score += @rolls[frame] + @rolls[frame + 1]
-      @frame += 2
+  private
+
+  def final_round(frame)
+    if @rolls[frame] < MAX_PINS
+      no_bonus(frame)
     else
-      @score += @rolls[frame] + @rolls[frame + 1] + @rolls[frame + 2]
-      @frame += 3
+      bonus(frame)
     end
   end
   
-  private
-  
   def strike?(frame)
-    @rolls[frame] == 10
+    @rolls[frame] == MAX_PINS
+  end
+
+  def strike
+    @score += strike_points
+    @frame += 1
+  end
+  
+  def strike_points
+    MAX_PINS + @rolls[@frame + 1] + @rolls[@frame + 2]
+  end
+
+  def spare
+    @score += spare_points
+    @frame += 2
   end
 
   def spare?(frame)
-    @rolls[frame] + @rolls[frame + 1] == 10
+    @rolls[frame] + @rolls[frame + 1] == MAX_PINS
+  end
+
+  def spare_points
+    MAX_PINS + @rolls[@frame + 2]
+  end
+
+  def sum_points
+    @score += sum_of_frame(@frame)
+    @frame += 2
   end
 
   def sum_of_frame(frame)
     @rolls[frame] + @rolls[frame + 1]
+  end
+
+  def gameover
+    @frame >= @rolls.length
+  end
+
+  def no_bonus(frame)
+    @score += @rolls[frame] + @rolls[frame + 1]
+    @frame += 2
+  end
+
+  def bonus(frame)
+    @score += @rolls[frame] + @rolls[frame + 1] + @rolls[frame + 2]
+    @frame += 3
   end
 
 end
