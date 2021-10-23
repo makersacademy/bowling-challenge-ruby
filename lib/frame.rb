06b10d267a8f03cd1d2e0d6be078dea95cac153b
Bernard <bernardleanse@Helenas-MacBook-Pro.local>
2021-10-23 13:58:03 +0100

    frame records rolls, shows strike or spare

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,9 +1,24 @@
 class Frame
 
-  attr_reader :score
+  attr_reader :score, :rolls
 
   def initialize
     @score = nil
+    @rolls = []
   end
   
+  def record_roll(roll)
+
+    raise 'Invalid Roll' if !rolls.empty? && roll + rolls.first > 10
+
+    if rolls.empty? && roll == 10
+      @rolls << 'X' 
+    elsif rolls.first && rolls.first + roll == 10 
+      @rolls << '/' if rolls.first && rolls.first + roll == 10  
+    else
+      @rolls << roll
+    end
+
+  end
+
 end
