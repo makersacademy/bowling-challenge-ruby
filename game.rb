738fb18d72b9bd9538614d0bf8e457b227493681
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-10-24 16:36:04 +0100

    Works - No last fram adjustment yet

--- game.rb
+++ game.rb
@@ -1,43 +1,51 @@
 class Game
 
-  attr_accessor :frame, :frame_number
-
   def initialize
-    @frame = Frame.new
-    @frame_number = 0
+    @frames = @frames = [[0, 6, 4], [0, 3, 5], [0, 10], [0, 10], [0, 5, 3], [0], [0], [0], [0], [0]] #Array.new(10){ [0] }
+    @frame_number = 1
+    @roll_number = 1
   end
 
   def roll(pins)
-    if pins < 10
-      @frame_number +=1
-      @frame.roll_number +=1
-      @frame.roll.pins += pins
+    if pins < 10 && @roll_number == 1
+      @frames[@frame_number -1] << pins
+      @roll_number += 1
+    elsif pins < 10 && @roll_number == 2
+      @frames[@frame_number -1] << pins
+      @frame_number += 1
+      @roll_number -= 1
     else
-      'dunno'
+      @frames[@frame_number -1] << pins
+      @frame_number += 1
     end
   end
 
   def score
+    @frames.each_with_index do |frame, i|
+      if strike?(frame)
+        unless @frames[i + 1][2] == nil
+          frame << @frames[i + 1][1]
+          frame << @frames[i + 1][2]
+        else
+          frame << @frames[i + 1][1]
+          frame << @frames[i + 2][1]
+        end
+      end
+    end
+
+    @frames.each_with_index do |frame, i|
+      frame << @frames[i + 1][1]  if spare?(frame) 
+    end
+
+    @frames.flatten.inject(:+)
   end
-end
 
-class Frame
-
-  attr_accessor :roll, :roll_number
-
-  def initialize
-    @roll = Roll.new
-    @roll_number = 0
+  def spare?(frame)
+    !frame.include?(10) && frame.sum == 10
   end
 
-end
-
-class Roll
-
-  attr_accessor :pins
-
-def initialize
-  @pins = 0
-end
+  def strike?(frame)
+    frame.include?(10)
+  end
 
 end
