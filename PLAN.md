db2a9856878798f1fd5264a7a237bfb480c3e5ac
Sergei Palmer <zigpalmer@gmail.com>
2021-10-24 18:43:18 +0100

    State machine added for frame_status

--- PLAN.md
+++ PLAN.md
@@ -6,3 +6,38 @@
 
 Frames 1 - 9 can accept 2 rolls.
 Frame 10 can accept up to 3.
+
+# - user enters roll
+# - roll added to the array in scorecard equivalent to (frame number - 1) - frame 1 would access array at index 0
+# - when 2 entries (or 1 strike) are in the array, the frame is complete
+# - if the sum of the two entries in a frame array = 10 (strike or spare) score calculation waits until the next roll to calculate bonuses
+
+# initialize with array of 10 arrays to represent frames
+# initialize with frame_number of zero
+# initialize with score of 0
+
+# user enters first roll (r1)
+#   frame number updated to 1
+#   roll pushed into scorecard array with index of 0 (frame_number - 1)
+#   if r1 = 10, end of turn, completed frame with strike
+#   else loop for turn 2
+#     if r1 + r2 = 10, completed frame with spare
+
+
+Order of play
+  1) input pins
+  2) increment frame number +1 (start first frame)
+standard
+  3) add knocked down pins to scorecardarray[0]
+  4) add second roll
+  5) add knocked down pins to scorecardarray[0]
+  6) end turn
+  7) calculate score
+if strike
+  3) add strike to scorecardarray[0]
+  4) end turn
+  5) don't calculate score until r2 of the next frame
+if spare
+  3) add spare to scorecardarray[0]
+  4) end turn
+  5) don't calculate score until r1 of the next frame
--- lib/bowling_score.rb
+++ lib/bowling_score.rb
@@ -1,29 +1,75 @@
 class BowlingScore
-  attr_reader :scorecard, :score_counter, :frame_number
+  attr_reader :scorecard, :gamescore, :frame_number, :frame_status
 
   def initialize
     @scorecard = Array.new(10) { Array.new }
-    @frame_number = 0
-    @score_counter = 0
+    @frame_number = 1
+    @gamescore = 0
+    @frame_status = nil
+  end
+
+  def capture_pins
+    p "Enter pins knocked down"
+    pins = gets.chomp.to_i
+  end
+
+  # def pins_to_score  
+  #   score(pins)
+  #   add_pins_to_frame(pins)
+  # end
+
+  def add_pins_to_frame(pins)
+    @frame_index = @frame_number - 1
+    @scorecard[@frame_index] << pins
+    p @scorecard
   end
 
   def score(pins)
-    @scorecard << pins
-    @score_counter += pins
-    print_score
+    if @frame_status.nil?
+      @gamescore += pins
+      if @gamescore == 10
+        @frame_status = :strike
+      else
+        @frame_status = :incomplete
+      end
+      print_score
+    elsif @frame_status == :incomplete
+      @gamescore += pins
+      if @gamescore > 10
+        raise "Illegal roll."
+      end
+      if @gamescore == 10
+        @frame_status = :spare
+      else
+        @frame_status = :open
+      end
+    end
   end
 
   def increment_frame
     @frame_number += 1
-    p "Frame: #{@frames}"
+    p "Frame: #{@frame_number}"
+  end
+  
+  def frame_complete?
+    @scorecard[@frame_index].length == 2
+    #   frame_status = :complete
+    # else
+    #   frame_status = :incomplete
+    # end
   end
 
   def print_score
-    p "Your score is #{@score_counter}"
+    p "Your score is #{@score}"
   end
-end
 
-# - user enters roll
-# - roll added to the array in scorecard equivalent to (frame number - 1) - frame 1 would access array at index 0
-# - when 2 entries (or 1 strike) are in the array, the frame is complete
-# - if the sum of the two entries in a frame array = 10 (strike or spare) score calculation waits until the next roll to calculate bonuses
+  # def game
+  #   input_pins_to_score
+  #   if frame_complete?
+  #     increment_frame
+  #   else
+  #     input_pins_to_score
+  #   end
+  # end
+
+end
