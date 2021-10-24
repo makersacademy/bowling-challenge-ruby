59121f1c5b041f2d289604848dfa3a8c0771fffb
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-10-24 17:40:12 +0100

    Calculates the score correctly when given the rolls - no refactoring yet

--- game.rb
+++ game.rb
@@ -1,47 +1,55 @@
 class Game
 
   def initialize
-    @frames = @frames = [[0, 6, 4], [0, 3, 5], [0, 10], [0, 10], [0, 5, 3], [0], [0], [0], [0], [0]] #Array.new(10){ [0] }
+    @frames = @frames = Array.new(10){ [] }
     @frame_number = 1
     @roll_number = 1
   end
 
   def roll(pins)
-    if pins < 10 && @roll_number == 1
-      @frames[@frame_number -1] << pins
-      @roll_number += 1
-    elsif pins < 10 && @roll_number == 2
-      @frames[@frame_number -1] << pins
-      @frame_number += 1
-      @roll_number -= 1
+    if @frame_number < 10
+      if pins < 10 && @roll_number == 1
+        @frames[@frame_number -1] << pins
+        @roll_number += 1
+      elsif pins < 10 && @roll_number == 2
+        @frames[@frame_number -1] << pins
+        @frame_number += 1
+        @roll_number -= 1
+      else
+        @frames[@frame_number -1] << pins
+        @frame_number += 1
+      end
     else
-      @frames[@frame_number -1] << pins
-      @frame_number += 1
+      @frames[9] << pins
     end
   end
 
   def score
     @frames.each_with_index do |frame, i|
       if strike?(frame)
-        unless @frames[i + 1][2] == nil
+        unless @frames[i + 1][1] == nil
+          frame << @frames[i + 1][0]
           frame << @frames[i + 1][1]
-          frame << @frames[i + 1][2]
         else
-          frame << @frames[i + 1][1]
-          frame << @frames[i + 2][1]
+          frame << @frames[i + 1][0]
+          frame << @frames[i + 2][0]
         end
       end
     end
 
     @frames.each_with_index do |frame, i|
-      frame << @frames[i + 1][1]  if spare?(frame) 
+      frame << @frames[i + 1][0]  if spare?(frame) 
     end
-
-    @frames.flatten.inject(:+)
   end
 
+  def total
+    @frames.flatten.compact.sum
+  end
+
+  private
+
   def spare?(frame)
-    !frame.include?(10) && frame.sum == 10
+    !frame.include?(10) && frame.compact.sum == 10
   end
 
   def strike?(frame)
