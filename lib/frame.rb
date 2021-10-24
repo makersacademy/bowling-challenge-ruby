5388e14674cdb8d739b650fb88a50745a66af749
Jacob Cho <jacobcho@live.com>
2021-10-24 07:45:46 +0100

    refactor

--- lib/frame.rb
+++ lib/frame.rb
@@ -69,7 +69,6 @@ class Frame
           spare_checker
           open_frame_checker
       
-
         end
 
     end
@@ -115,30 +114,5 @@ class Frame
     end
   end
 
-
-
-  def extendable?
-    rolls.length < 2 || 
-    (rolls[0] == 10 && rolls.length < 3) || 
-    (rolls[0] + rolls[1] == 10 && rolls.length < 3)
-  end
-
-  def start_next_frame?
-    rolls[0] == 10 || rolls.length > 1
-  end
-
-  # def score
-  #   @rolls.inject(0, :+)
-  # end
-
-  private
-
-  def rolls
-    @rolls ||= []
-  end
-
-  # def check_valid_frame
-  #   raise BowlingError if rolls.length == 2 && rolls[0] < 10 && rolls[0] + rolls[1] > 10
-  # end
 
 end
