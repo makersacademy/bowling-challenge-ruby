ee67322c9955b1726ccca0d565040e6b3a839ad4
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-10-24 21:06:08 +0100

    Slight refactor of Score and added comments

--- score.rb
+++ score.rb
@@ -8,18 +8,17 @@ class Score
   def add_bonuses(frames)
     frames.each_with_index do |frame, i|
       if strike?(frame)
-        unless frames[i + 1][1] == nil
-          frame << frames[i + 1][0]
-          frame << frames[i + 1][1]
+        frame << frames[i + 1][0] # add pins from next roll
+        unless frames[i + 1][1] == nil # if second roll is in the next frame
+          frame << frames[i + 1][1] # add pins from 1st roll from next frame
         else
-          frame << frames[i + 1][0]
-          frame << frames[i + 2][0]
+          frame << frames[i + 2][0] # or add pins from the second roll
         end
       end
     end
 
     frames.each_with_index do |frame, i|
-      frame << frames[i + 1][0]  if spare?(frame) 
+      frame << frames[i + 1][0]  if spare?(frame) # add pins from next roll
     end
   end
 
