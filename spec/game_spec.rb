4c98fcfe3db04cf2b80932e90e22d938785fe5a3
Kostas Ntanos <kostas@ntanos.co.uk>
2021-10-25 11:58:27 +0100

    Notes on TDD in game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -17,17 +17,10 @@ describe Game do
       expect(game.frames[2]).to include 2
       expect(game.frames[2]).to include 0
     end
-
-      xit "should put the last 3 rolls in the tenth frame" do
-        game = Game.new
-        
-        game.roll(5)
-        game.roll(3)
-        game.roll(10)
-
-        expect(game.frames[9]).to include 5
-        expect(game.frames[9]).to include 3
-        expect(game.frames[9]).to include 10
-    end
   end
 end
+
+# DO NOT TEST STATE - The only behaviour of the code is that it caclulate the correct score. That's what needs teting.
+#  Write one test that test that different inputs caclulate the correct score.
+#  Following TDD would mean that the first test is taking one roll and the score is that roll's pins.
+#  Then add one roll and the score will be the sum of the two rolls' pins... etc
