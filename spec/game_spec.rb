59dda9036f57833dae80e39953c22e207034e4d1
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-11-01 10:14:19 +0000

    some comments

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,26 +1,39 @@
 require './game'
 
-describe Game do
-  describe ".roll" do
-    it "should put the number of pins from each roll in successive frames" do
-      game = Game.new
+# describe Game do
+#   describe ".roll" do
+#     it "should put the number of pins from each roll in successive frames" do
+#       game = Game.new
 
-      game.roll(5)
-      game.roll(3)
-      game.roll(10)
-      game.roll(2)
-      game.roll(0)
+#       game.roll(5)
+#       game.roll(3)
+#       game.roll(10)
+#       game.roll(2)
+#       game.roll(0)
 
-      expect(game.frames[0]).to include 5
-      expect(game.frames[0]).to include 3
-      expect(game.frames[1]).to include 10
-      expect(game.frames[2]).to include 2
-      expect(game.frames[2]).to include 0
-    end
-  end
-end
+#       expect(game.frames[0]).to include 5
+#       expect(game.frames[0]).to include 3
+#       expect(game.frames[1]).to include 10
+#       expect(game.frames[2]).to include 2
+#       expect(game.frames[2]).to include 0
+#     end
+#   end
+# end
 
 # DO NOT TEST STATE - The only behaviour of the code is that it caclulate the correct score. That's what needs teting.
 #  Write one test that test that different inputs caclulate the correct score.
 #  Following TDD would mean that the first test is taking one roll and the score is that roll's pins.
 #  Then add one roll and the score will be the sum of the two rolls' pins... etc
+
+
+describe Game do
+    describe ".score" do
+      it "returns the correct score" do
+        game = Game.new
+  
+        game.roll(5)
+  
+        expect(game.score).to eq(5)
+      end
+    end
+  end
