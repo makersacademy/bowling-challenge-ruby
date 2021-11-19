b0f936b1dd57896e42ef57df08a47e8cb0e13d5b
Matt Zimmer <zimmermja@gmail.com>
2021-11-19 22:54:30 +0000

    Initializing methods written with rspec tests passing; DEFAULT_BOWLS included

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
+# This class initializes with an array of pins knocked down with a series of bowls
 class Scorecard
+  DEFAULT_BOWLS = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6].freeze
+
+  attr_reader :bowls
+
+  def initialize(bowls = DEFAULT_BOWLS)
+    @bowls = bowls
+  end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,3 +1,23 @@
 # frozen_string_literal: true
 
 require 'scorecard'
+
+describe Scorecard do
+  let(:score133) { [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6] }
+  let(:score300) { [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10] }
+  let(:score0) { [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
+  let(:game133) { Scorecard.new(score133) }
+  let(:game300) { Scorecard.new(score300) }
+  let(:game0) { Scorecard.new(score0) }
+  let(:game_default) { Scorecard.new }
+
+  it 'sets @bowls to the passed argument value when initialized' do
+    expect(game133.bowls).to eq score133
+    expect(game300.bowls).to eq score300
+    expect(game0.bowls).to eq score0
+  end
+
+  it 'sets @bowls to the default value if initialized with no arguments passed ' do
+    expect(game_default.bowls).to eq Scorecard::DEFAULT_BOWLS
+  end
+end
