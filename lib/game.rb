6a764f7ddd0c20d365a23ea99a3dc4a74e431214
88Andybird <anbird88@gmail.com>
2021-11-20 19:10:17 +0000

    added extra lif line to score to account for pperfect game

--- lib/game.rb
+++ lib/game.rb
@@ -25,8 +25,11 @@ class Game
         @bonus << (@rolls[index + 1] + @rolls[index + 2])
       end
       @bonus << 20 if roll == 10 && @rolls[index + 1] == 10 && @rolls[index + 2] == 10
-      @bonus << 10 + @rolls[index + 2] if roll == 10 && @rolls[index + 1] == 10 && @rolls[index + 2] != 10
+      if roll == 10 && @rolls[index + 1] == 10 && @rolls[index + 2] != 10 && @rolls[index + 2] != nil
+        @bonus << (10 + @rolls[index + 2])
+      end
     end
+    @bonus << -20 if @rolls - [10] == []
 
     @rolls.sum + @bonus.sum
   end
