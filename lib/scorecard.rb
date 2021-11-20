e7649613607a9e0342715da80dc4e1009c04ed1e
Matt Zimmer <zimmermja@gmail.com>
2021-11-20 18:54:41 +0000

    refactored code to be clearer; added notes

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -11,7 +11,8 @@ class Scorecard
   end
 
   def frames
-    check_strikes(@bowls).each_slice(2).to_a
+    presliced_bowls = add_zeros_after_tens(@bowls)
+    presliced_bowls.each_slice(2).to_a
   end
 
   def final_score
@@ -25,33 +26,43 @@ class Scorecard
   private
 
   # Ensures each frame will have two values by adding a 0 after each 10
-  def check_strikes(bowls)
-    bowls.map { |v| v == 10 ? [10, 0] : v }.flatten
+  def add_zeros_after_tens(bowls)
+    bowls.map { |bowl| bowl == 10 ? [bowl, 0] : [bowl] }.flatten
   end
 
   # Maps the individual scores of frames 0 - 9
   def frame_scores
-    (0..9).to_a.map { |i| check_frame_score(i) if i < 10 }
+    (0..9).to_a.map { |i| check_frame_score(i) }
   end
 
-  def check_frame_score(ind, framesum = frames[ind].sum)
-    framesum < 10 ? framesum : resolve_spare_strike(ind)
+  def check_frame_score(ind, frame_sum = frames[ind].sum)
+    # returns this value if the frame is not a strike or a spare
+    return frame_sum if frame_sum < 10
+
+    resolve_spare_or_strike(ind)
   end
 
-  def resolve_spare_strike(ind)
-    spare?(frames[ind]) ? frames[ind].sum + frames[ind + 1].first : resolve_strike(ind)
+  def resolve_spare_or_strike(ind, frame = frames[ind], next_frame = frames[ind + 1])
+    # returns this value if the frame is a spare
+    return (frame.sum + next_frame.first) if spare?(frame)
+
+    resolve_strike(ind)
   end
 
-  def spare?(frame)
-    frame.sum == 10 && !strike?(frame)
-  end
+  def resolve_strike(ind, next_frame = frames[ind + 1], next_next_frame = frames[ind + 2])
+    # returns this value if the frame is a strike followed by another strike
+    return 20 + next_next_frame.first if strike?(next_frame)
 
-  def resolve_strike(ind, next_frame = frames[ind + 1])
-    10 + (strike?(next_frame) ? 10 + frames[ind + 2].first : next_frame.sum)
+    # returns this value for a strike not followed by another strike
+    10 + next_frame.sum
   end
 
   def add_up_to_index(frame_scores, index)
     frame_scores.first(index + 1).sum
+  end
+
+  def spare?(frame)
+    frame.sum == 10 && !strike?(frame)
   end
 
   def strike?(frame)
