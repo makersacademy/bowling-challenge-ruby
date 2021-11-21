a467437e618ec33fb79db9caaf09980a0128f7f4
Matt Zimmer <zimmermja@gmail.com>
2021-11-21 18:08:29 +0000

    extracted Frame class with score method

--- lib/frame.rb
+++ lib/frame.rb
@@ -2,14 +2,48 @@
 
 # This class stores information for a specific frame
 class Frame
-  attr_reader :first_bowl, :second_bowl
-  
-  def initialize(frame)
+  attr_reader :first_bowl, :second_bowl, :frame, :next_frame, :next_next_frame
+
+  def initialize(frame, next_frame = nil, next_next_frame = nil)
     @first_bowl = frame.first
     @second_bowl = frame.last
+    @frame = frame
+    @next_frame = next_frame
+    @next_next_frame = next_next_frame
   end
 
   def sum
     @first_bowl + @second_bowl
+  end
+
+  def bonus_type
+    return :no_bonus if @next_frame.nil? || @frame.sum < 10
+    return :spare unless @frame.include?(10)
+    
+    :strike
+  end
+
+  def score
+    case bonus_type
+    when :no_bonus then sum
+    when :spare then calculate_spare
+    when :strike then calculate_strike
+    end
+  end
+
+  private
+
+  def calculate_spare
+    10 + @next_frame.first
+  end
+
+  def calculate_strike
+    return 20 + @next_next_frame.first if next_frame_is_strike? && !@next_next_frame.nil?
+
+    10 + @next_frame.sum
+  end
+
+  def next_frame_is_strike?
+    @next_frame.include?(10)
   end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -3,16 +3,42 @@
 require 'frame'
 
 describe Frame do
-  let(:gutter_frame) { Frame.new([0, 0]) }
-  let(:normal_frame) { Frame.new([5, 3]) }
-  let(:spare_frame) { Frame.new([5, 5]) }
-  let(:strike_frame) { Frame.new([10, 0]) }
+  let(:gutter_frame) { Frame.new([0, 0], [3, 3], [3, 3]) }
+  let(:normal_frame) { Frame.new([5, 3], [3, 3], [3, 3]) }
+  let(:spare_frame) { Frame.new([5, 5], [3, 3], [3, 3]) }
+  let(:spare_no_next) { Frame.new([5, 5]) }
+  let(:strike_frame) { Frame.new([10, 0], [3, 3], [3, 3]) }
+  let(:strike_no_next) { Frame.new([10, 0]) }
+  let(:strike_then_strike) { Frame.new([10, 0], [10, 0], [3, 3]) }
 
   describe '#sum' do
     it 'returns the sum of the passed in frame array' do
       expect(gutter_frame.sum).to eq 0
       expect(normal_frame.sum).to eq 8
       expect(strike_frame.sum).to eq 10
+    end
+  end
+
+  describe '#bonus_type' do
+    it 'returns the type of bonus the frame should receive' do
+      expect(gutter_frame.bonus_type).to eq :no_bonus
+      expect(normal_frame.bonus_type).to eq :no_bonus
+      expect(spare_no_next.bonus_type).to eq :no_bonus
+      expect(strike_no_next.bonus_type).to eq :no_bonus
+      expect(spare_frame.bonus_type).to eq :spare
+      expect(strike_frame.bonus_type).to eq :strike
+    end
+  end
+
+  describe '#score' do
+    it 'returns the score value of the frame' do
+      expect(gutter_frame.score).to eq 0
+      expect(normal_frame.score).to eq 8
+      expect(spare_frame.score).to eq 13
+      expect(spare_no_next.score).to eq 10
+      expect(strike_frame.score).to eq 16
+      expect(strike_no_next.score).to eq 10
+      expect(strike_then_strike.score).to eq 23
     end
   end
 end
