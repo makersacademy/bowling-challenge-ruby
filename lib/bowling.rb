bf4b6b0cbfcbe195508d589ea5328c8923118f88
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2021-11-21 16:56:09 +0000

    Added method to test for spares

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -7,10 +7,15 @@ class Bowling
   end
 
   def roll(pins_hit)
+    @previous_bowl = pins_hit
     @frames << pins_hit
   end
 
   def is_strike?(pins_hit)
     pins_hit == 10 ? true : false
+  end
+
+  def is_spare?(pins_hit)
+    @previous_bowl + pins_hit == 10 ? true : false
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -13,4 +13,11 @@ describe Bowling do
       expect(subject.is_strike?(10)).to eq true
     end
   end
+
+  describe '#is_spare?' do
+    it 'takes an argument of amounts of pins hit added to previous bowl to decide whether the hit was a spare or not' do
+      subject.roll(7)
+      expect(subject.is_spare?(3)).to eq true
+    end
+  end
 end
