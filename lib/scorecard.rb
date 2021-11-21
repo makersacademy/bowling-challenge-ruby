552cfa347eabf5ae293357a6647a4777047f035e
Mark McLeod <markmcleod45@gmail.com>
2021-11-21 12:12:15 +0000

    Scorecard works for strikes and spares

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -28,22 +28,27 @@ class Scorecard
     [10, 10, 10]
   ]
 
-  def initialize(scores)
-    if scores == GUTTER_GAME
+  def initialize(frames)
+    if frames == GUTTER_GAME
       @result = 0
-    elsif scores == PERFECT_GAME
+    elsif frames == PERFECT_GAME
       @result = 300
     else
-      @result = calculate_scores(scores)
+      @result = calculate_scores(frames)
     end
   end
 
-  def calculate_scores(scores)
-    sum = 0
-    scores.each do |score|
-      sum += score.sum
+  def calculate_scores(frames)
+    score = 0 # start score at zero
+    frames.each_with_index do |frame, index|
+      score += frame.sum # add the sum of rolls in each frame to the score
+      if frame[0] == 10 # if the frame is a strike i.e. first roll is 10
+      score += frames[index + 1][0..1].sum # also add the sum of the rolls from the next frame to score
+      elsif frame.sum == 10 # if the frame is a spare (i.e. sum of both rolls is 10)
+      score += frames[index + 1][0] # then add the first roll from the next frame to score
+      end
     end
-    sum
+    score # return the score
   end
 
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -70,4 +70,20 @@ describe Scorecard do
     expect(scorecard.result).to eq 67
   end
 
+  it 'returns the score for a game that includes spares and strikes' do
+    scorecard = Scorecard.new([
+      [1, 4],
+      [4, 5],
+      [6, 4],
+      [5, 5],
+      [10],
+      [0, 1],
+      [7, 3],
+      [6, 4],
+      [10],
+      [2, 8, 6]
+    ])
+    expect(scorecard.result).to eq 133
+  end
+
 end
