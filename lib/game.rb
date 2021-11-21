072b483a91fcc6f8401b30c382b616ce5d7c887d
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-21 13:20:25 +0000

    calculates a spare and refactor of game points logic

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,19 @@ class Game
   end
 
   def points
-    points = @rolls.sum
+    # points = @rolls.sum
+    frame_points = 0
+    rollIndex = 0
+    # calculates points per game frames(ten)
+    10.times do
+      if @rolls[rollIndex] + @rolls[rollIndex + 1] == 10
+        frame_points += @rolls[rollIndex] + @rolls[rollIndex + 1] + @rolls[rollIndex + 2]
+      else
+        frame_points += @rolls[rollIndex] + @rolls[rollIndex + 1]
+      end
+      rollIndex += 2
+    end
+    frame_points
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,7 +10,17 @@ describe Game do
 
   it 'calculates all ones' do
     game = Game.new
-    20.times { game.roll (1) }
+    20.times { game.roll(1) }
     expect(game.points).to eq 20
   end  
+
+  it 'calculates a spare' do
+    game = Game.new
+    game.roll(5)
+    game.roll(5)
+    game.roll(3)
+    17.times { game.roll(0) }
+    expect(game.points).to eq 16
+  end
+
 end
