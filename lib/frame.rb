36f241e874c58ae024c84a9f26a12f38f46b2a93
ginnythomas <ginnynjon@gmail.com>
2021-11-21 14:27:13 +0000

    frame class written

--- lib/frame.rb
+++ lib/frame.rb
@@ -6,4 +6,9 @@ class Frame
     puts 'Please enter the number of pins you knocked down on turn 1: '
     @t1p = $stdin.gets.chomp.to_i
   end
+
+  def turn1
+    puts 'Please enter the number of pins you knocked down on turn 1: '
+    @t2p = $stdin.gets.chomp.to_i
+  end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -8,4 +8,13 @@ describe Frame do
         expect(@t1p).to eq(5)
     end
   end
+
+  describe 'turn2' do
+    it 'accepts user input for their first turn count of pins' do
+      allow($stdin).to receive(:gets).and_return('4')
+      @t2p = $stdin.gets.chomp.to_i
+      expect(@t2p).to eq(4)
+    end
+  end
+
 end
