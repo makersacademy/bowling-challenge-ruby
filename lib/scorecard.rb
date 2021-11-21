c7cb80aff94dd6785454ab2b1adca613b27fb9c6
Mark McLeod <markmcleod45@gmail.com>
2021-11-21 10:55:51 +0000

    Initialize results attribute for Scorecard

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,3 +1,9 @@
 class Scorecard
 
+  attr_reader :result
+
+  def initialize(scores)
+    @result = 0
+  end
+
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -4,8 +4,37 @@ describe Scorecard do
   
   describe 'initialisation' do
     it 'will intitalise an instance of Scorecard' do
-      scorecard = Scorecard.new
+      scorecard = Scorecard.new([
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+      ])
       expect(scorecard).to be_a Scorecard
+    end
+  end
+
+  describe '.result' do
+    it 'returns the result of the scorecard for a gutter game' do
+      scorecard = Scorecard.new([
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+        [0,0],
+      ])
+      expect(scorecard.result).to eq 0
     end
   end
 
