6be05a7a1657914ef0abcd5e693c0fd195abe6c4
Matt Zimmer <zimmermja@gmail.com>
2021-11-21 17:44:28 +0000

    sum method added to Frame class

--- lib/frame.rb
+++ lib/frame.rb
@@ -2,4 +2,14 @@
 
 # This class stores information for a specific frame
 class Frame
+  attr_reader :first_bowl, :second_bowl
+  
+  def initialize(frame)
+    @first_bowl = frame.first
+    @second_bowl = frame.last
+  end
+
+  def sum
+    @first_bowl + @second_bowl
+  end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,3 +1,18 @@
 # frozen_string_literal: true
 
 require 'frame'
+
+describe Frame do
+  let(:gutter_frame) { Frame.new([0, 0]) }
+  let(:normal_frame) { Frame.new([5, 3]) }
+  let(:spare_frame) { Frame.new([5, 5]) }
+  let(:strike_frame) { Frame.new([10, 0]) }
+
+  describe '#sum' do
+    it 'returns the sum of the passed in frame array' do
+      expect(gutter_frame.sum).to eq 0
+      expect(normal_frame.sum).to eq 8
+      expect(strike_frame.sum).to eq 10
+    end
+  end
+end
