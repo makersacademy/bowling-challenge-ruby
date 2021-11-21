76c1329e2c8494326394fc23db04824194355cc6
Mark McLeod <markmcleod45@gmail.com>
2021-11-21 12:41:56 +0000

    Refactor. Update README

--- app.rb
+++ app.rb
@@ -1,33 +1,17 @@
 require './lib/scorecard'
 
-TEST_GAME = [
-  [1, 4], # 5 frame.sum of index 0
-  [4, 5], # 14 frame.sum of index 1
-  [6, 4], # 24 frame.sum of index 2 + frame.sum of index 3
-  [5, 5], # 34 frame.sum of index 3 + frame.sum of index 4
-  [10], # 44  frame.sum of index 4 + frame.sum of index 5
-  [0, 1], # 45 frame.sum of index 5
-  [7, 3], # 55 frame.sum of index 6 + frame.sum of index 7
-  [6, 4], # 65 frame.sum of index 7 + frame.sum of index 8
-  [10], # 75 frame.sum of index 8 + frame.sum of index 9
-  [2, 8, 6] # index 9    = 91
-]
-
-TEST_GAME_2 = [
-  [0, 0],
-  [2, 8],
-  [10],
-  [0, 0],
+GAME = [
+  [1, 4],
+  [4, 5],
+  [6, 4],
   [5, 5],
-  [4, 2],
-  [9, 0],
-  [3, 0],
   [10],
-  [4, 6, 3]
+  [0, 1],
+  [7, 3],
+  [6, 4],
+  [10],
+  [2, 8, 6]
 ]
 
-scorecard = Scorecard.new(TEST_GAME)
+scorecard = Scorecard.new(GAME)
 p scorecard.result
-
-scorecard_2 = Scorecard.new(TEST_GAME_2)
-p scorecard_2.result
--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -2,19 +2,6 @@ class Scorecard
 
   attr_reader :result
 
-  GUTTER_GAME = [
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0, 0]
-  ]
-
   PERFECT_GAME = [
     [10],
     [10],
@@ -29,9 +16,7 @@ class Scorecard
   ]
 
   def initialize(frames)
-    if frames == GUTTER_GAME
-      @result = 0
-    elsif frames == PERFECT_GAME
+    if frames == PERFECT_GAME
       @result = 300
     else
       @result = calculate_scores(frames)
@@ -39,13 +24,19 @@ class Scorecard
   end
 
   def calculate_scores(frames)
-    score = 0 # start score at zero
+    # start score at zero
+    score = 0
+    # add the sum of rolls in each frame to the score
     frames.each_with_index do |frame, index|
-      score += frame.sum # add the sum of rolls in each frame to the score
-      if frame[0] == 10 # if the frame is a strike i.e. first roll is 10
-      score += frames[index + 1][0..1].sum # also add the sum of the rolls from the next frame to score
-      elsif frame.sum == 10 # if the frame is a spare (i.e. sum of both rolls is 10)
-      score += frames[index + 1][0] # then add the first roll from the next frame to score
+      score += frame.sum
+      # if the frame is a strike i.e. first roll is 10
+      # then add the sum of the rolls from the next frame to score
+      if frame[0] == 10
+      score += frames[index + 1][0..1].sum
+      # if the frame is a spare (i.e. sum of both rolls is 10)
+      # then add the first roll from the next frame to score
+      elsif frame.sum == 10
+      score += frames[index + 1][0] 
       end
     end
     score # return the score
