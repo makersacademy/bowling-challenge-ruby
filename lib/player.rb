f61e4cbfb5ff5fc1c167ddef02d522ed7d3bb798
Jessica Gordon <ketchesj@gmail.com>
2021-12-18 16:11:34 +0000

    player.pins_knocked_down sends message to an instance of the points class to #update_roll

--- lib/player.rb
+++ lib/player.rb
@@ -5,5 +5,10 @@ class Player
   def initialize(name)
     @name = name
     @current_frame = 1
+    @current_roll = 1
+  end
+
+  def pins_knocked_down(number, points)
+    points.update_roll(@current_frame, @current_roll, number)
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,24 @@
 require 'player'
 
 describe Player do
+  let(:points) { double('points', :update_roll => true) }
+
+  before :each do
+    @player = Player.new('John Smith')
+  end
+
   describe '#initialize' do
     it 'records players name and their current frame' do
-      player = Player.new('John Smith')
+      expect(@player.name).to eq 'John Smith'
+      expect(@player.current_frame).to eq 1
+    end
+  end
 
-      expect(player.name).to eq 'John Smith'
-      expect(player.current_frame).to eq 1
+  describe '#pins_knocked_down' do
+    it 'sends a message to an instance of the points class to #update_roll' do
+      expect(points).to receive(:update_roll)
+
+      @player.pins_knocked_down(7, points)
     end
   end
 end
