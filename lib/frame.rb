32776e29ccfd2cf6977b839f25c6bd4103a14351
Jessica Gordon <ketchesj@gmail.com>
2021-12-18 20:11:19 +0000

    Frame Class: #update_roll updates the rolls instance variable

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,10 +1,13 @@
 class Frame
-  attr_reader :first_roll, :second_roll, :strike, :spare
+  attr_reader :rolls, :strike, :spare
 
   def initialize
-    @first_roll = 0
-    @second_roll = 0
+    @rolls = []
     @strike = false
     @spare = false
+  end
+
+  def update_roll(current_roll, pins_knocked_down)
+    @rolls[current_roll - 1] = pins_knocked_down
   end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -5,10 +5,21 @@ describe Frame do
 
   describe '#initialize' do
     it 'sets up instance variables to hold scores per roll' do
-      expect(frame.first_roll).to eq 0
-      expect(frame.second_roll).to eq 0
+      expect(frame.rolls).to be_empty 
       expect(frame.strike).to eq false
       expect(frame.spare).to eq false     
+    end
+  end
+
+  describe '#update_roll(current_roll, pins_knocked_down)' do
+    it 'updates the roll score instance variable' do
+      expect(frame.rolls).to be_empty 
+
+      frame.update_roll(1, 7)
+      frame.update_roll(2, 1)
+
+      expect(frame.rolls[0]).to eq 7
+      expect(frame.rolls[1]).to eq 1
     end
   end
 end
