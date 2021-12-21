8cbc9085e30c5c307e8f5c752fd7b9304bce69a9
riky moroni <riky5.moroni@gmail.com>
2021-12-21 19:48:50 +0000

    add error check if sum of the frame is more than 10

--- lib/game.rb
+++ lib/game.rb
@@ -7,5 +7,6 @@ class Game
 
   def add_rolls(roll1, roll2)
     @frames << [roll1, roll2]
+    raise 'Sum of the rolls cannot exceede 10' if [roll1, roll2].reduce(0, :+) > 10
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,5 +8,9 @@ describe Game do
       game.add_rolls(1,4)
       expect(game.frames).to include([1,4])
     end
+
+    it 'raises an error if the frame exceedes 10' do
+      expect{ game.add_rolls(5,6) }.to raise_error 'Sum of the rolls cannot exceede 10'
+    end
   end
 end
