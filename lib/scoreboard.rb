8ea266b412abf5761f2b1c62cee4a2514e882418
Francesco <francescocollotto@gmail.com>
2021-12-21 11:57:15 +0000

    add strike method

--- lib/scoreboard.rb
+++ lib/scoreboard.rb
@@ -1,3 +1,10 @@
 class Scoreboard
-  
+  def initialize(rolls)
+    @rolls = rolls
+    @score = 0
+  end
+
+  def strike?(pin)
+    true if pin == 10
+  end
 end
--- spec/scoreboard_spec.rb
+++ spec/scoreboard_spec.rb
@@ -1,5 +1,18 @@
 require 'scoreboard'
 
 describe Scoreboard do
-  
+  before do
+    @rolls = [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6]
+    @scoreboard = Scoreboard.new(@rolls)
+  end
+
+  context '#strike' do
+    it 'return true when strike' do
+      expect(@scoreboard.strike?(10)).to be true
+    end
+    it 'return false when not strike' do
+      expect(@scoreboard.strike?(3)).to be_falsey
+    end
+  end
+
 end
