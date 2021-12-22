55650f63d76284821a793498d47424c27601df54
riky moroni <riky5.moroni@gmail.com>
2021-12-22 00:15:08 +0000

    add 2 methods to the Score class to check if a spare or strike has been scored

--- lib/score.rb
+++ lib/score.rb
@@ -4,8 +4,16 @@ class Score
     @total = 0
   end
 
-  def get_total(frame_arr)
-    result = frame_arr.reduce(0, :+)
+  def get_total(current_frame)
+    result = current_frame.reduce(0, :+)
     @total += result
+  end
+
+  def spare?(current_frame)
+    current_frame.reduce(0, :+) == 10
+  end
+
+  def strike?(current_frame)
+    current_frame[0] == 10 || current_frame[1] == 10
   end
 end
--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -3,7 +3,7 @@ require 'score'
 describe Score do
   let(:score) { Score.new }
 
-  describe '#get_each_frame' do
+  describe '#get_total' do
     context 'returns total score from each frame added'
     it 'returns score from first frame' do
       expect(score.get_total([1,4])).to eq 5
@@ -12,5 +12,28 @@ describe Score do
       score.get_total([1,4])
       expect(score.get_total([4,5])).to eq 14
     end
+  end
+
+  describe '#spare' do
+    context 'Checks if there is a SPARE'
+    it 'returns true' do
+      expect(score.spare?([6,4])).to eq true
+    end
+
+    it 'returns false' do
+      expect(score.spare?([4,5])).to eq false
+    end
+  end
+
+  describe '#strike' do
+    context 'Checks if there is a STRIKE'
+      it 'returns true' do
+        expect(score.strike?([10,0])).to eq true
+        expect(score.strike?([0,10])).to eq true
+      end
+
+      it 'returns false' do
+        expect(score.strike?([9,1])).to eq false
+      end
   end
 end
