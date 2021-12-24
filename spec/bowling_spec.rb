49aecc3bdfd4b72d1c8b93096d72aec9b601f07d
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2021-12-24 16:16:09 +0100

    Clean up for test using before statement

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,35 +1,44 @@
 require 'bowling'
 
 describe Bowling do
+  before do
+    @bowling = Bowling.new
+  end
   describe '#enter_score' do
     context 'Player enters a score outside of 0 to 10'
     it 'returns an error' do
-      bowling = Bowling.new
-      expect{bowling.enter_score(11)}.to raise_error 'Outside of Range'
+      expect{@bowling.enter_score(11)}.to raise_error 'Outside of Range'
     end
     context 'Player enters a valid score'
     it 'is stored as a score' do
-      bowling = Bowling.new
-      bowling.enter_score(8)
-      expect(bowling.score.first).to eq 8
+      @bowling.enter_score(8)
+      expect(@bowling.score.first).to eq 8
     end
   end
   describe '#game' do
     it 'Stops after 10 Frames' do
-      bowling = Bowling.new
       10.times do 
-        bowling.enter_score(6)
+        @bowling.enter_score(6)
       end
-      expect(bowling.score.length).to eq 10
+      expect(@bowling.score.length).to eq 10
     end
   end
   describe '#total_score' do
     it 'returns the total score as a string' do
-      bowling = Bowling.new
       5.times do
-        bowling.enter_score(7)
+        @bowling.enter_score(7)
       end
-      expect(bowling.total_score).to eq 'TOTAL: 35'
+      expect(@bowling.total_score).to eq 'TOTAL: 35'
     end
-  end 
+  end
+  describe '#valid_score?' do
+    context 'A value outside of 0 to 10 is entered'
+    it 'returns false' do
+      expect(@bowling.valid_score?(12)).to be_falsey
+    end
+    context 'A value inside of 0 to 10 is entered'
+    it 'returns true' do
+      expect(@bowling.valid_score?(3)).to be_truthy
+    end
+  end
 end
