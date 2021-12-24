39d49a71554dc43ca89b4ef953ddd931f1433534
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2021-12-24 15:32:06 +0100

    initialize created with score array

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,9 +1,16 @@
 class Bowling
+
+  attr_reader :score
+
+  def initialize
+    @score = []
+  end
+
   def enter_score(pins)
     unless (0..10).include? pins
       raise 'Outside of Range'
     else
-      'Nice'
+      score << pins
     end
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -7,5 +7,11 @@ describe Bowling do
       bowling = Bowling.new
       expect{bowling.enter_score(11)}.to raise_error 'Outside of Range'
     end
+    context 'Player enters a valid score'
+    it 'is stored as a score' do
+      bowling = Bowling.new
+      bowling.enter_score(8)
+      expect(bowling.score.first).to eq 8
+    end
   end
 end
