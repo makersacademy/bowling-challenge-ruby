71a73d64ff58b69a07bd15c921bd120103debfcc
Sarah <dousse.sarah@gmail.com>
2021-12-30 02:06:13 +0100

    refactoring: #strike? & #strike_pts & two_rolls_pts

--- lib/bowlingGame.rb
+++ lib/bowlingGame.rb
@@ -12,26 +12,37 @@ class BowlingGame
     sum = 0
     @index = 0
     10.times do
-      if roll_list[@index] == 10 
-        sum += roll_list[@index] + roll_list[@index+1] + roll_list[@index+2] # frame1_roll1 + frame2_roll1 + frame2_roll2
+      if strike?
+        sum += strike_pts
         @index += 1 # move to next frame (there is no frame1_roll2 for a stike)
       elsif spare?
-        sum += spare_score
-        @index += 2 # move to next frame
+        sum += spare_pts
+        @index += 2 # move to next frame (i+1 = frame1_roll2, i+2 = frame2_roll1)
       else
-        sum +=  roll_list[@index] + roll_list[@index+1]
+        sum += two_rolls_pts
         @index += 2 # move to next frame
       end
     end
     sum
   end
 
+  def strike?
+    roll_list[@index] == 10 
+  end
+
+  def strike_pts
+    roll_list[@index] + roll_list[@index+1] + roll_list[@index+2]
+  end
+
   def spare?
     roll_list[@index] + roll_list[@index+1] == 10
   end
 
-  def spare_score
+  def spare_pts
     roll_list[@index] + roll_list[@index+1] + roll_list[@index+2]
   end
-  
+
+  def two_rolls_pts
+    roll_list[@index] + roll_list[@index+1]
+  end
 end
