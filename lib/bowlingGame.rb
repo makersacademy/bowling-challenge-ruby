513c7b13766bcf83293c1e66453c3ee9448152bd
Sarah <dousse.sarah@gmail.com>
2021-12-30 01:43:59 +0100

    refactoring: #spare? & #spare_score

--- lib/bowlingGame.rb
+++ lib/bowlingGame.rb
@@ -10,16 +10,25 @@ class BowlingGame
 
   def outcome
     sum = 0
-    i = 0
+    @index = 0
     10.times do
-      if roll_list[i] + roll_list[i+1] == 10
-        sum += roll_list[i] + roll_list[i+1] + roll_list[i+2] # frame1_roll1 + frame1_roll2 + frame2_roll1(bonus)
-        i += 2 # move to next frame
+      if spare?
+        sum += spare_score
+        @index += 2 # move to next frame
       else
-        sum +=  roll_list[i] + roll_list[i+1]
-        i += 2 # move to next frame
+        sum +=  roll_list[@index] + roll_list[@index+1]
+        @index += 2 # move to next frame
       end
     end
     sum
   end
+
+  def spare?
+    roll_list[@index] + roll_list[@index+1] == 10
+  end
+
+  def spare_score
+    roll_list[@index] + roll_list[@index+1] + roll_list[@index+2]
+  end
+  
 end
