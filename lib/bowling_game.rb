e2c553ca645b372db9803a6feb0fa3dffd6bb0d9
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2021-12-31 11:57:05 +0100

    added 2d array and proper user input

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -3,18 +3,28 @@ class BowlingGame
 
   def initialize(scorecard = [])
     @scorecard = scorecard
+    @frame = []
+  end
+
+  def add_roll(roll)
+    @frame << roll
+    if @frame.length == 2 || @frame.any? { |n| n == 10 }
+      @scorecard << @frame
+      @frame = []
+    end  
   end
 
   def score
     score = 0
     @scorecard.each_with_index do |frame,index|
       if strike?(frame)
-       score += frame.sum + @scorecard[index + 1].sum
+       score += frame.sum + @scorecard[index + 1].sum 
       elsif spare?(frame)
        score += frame.sum + @scorecard[index + 1].first
       else
        score += frame.sum
       end
+    p score
     end
     score
   end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,35 +1,32 @@
 require 'bowling_game'
 
 describe BowlingGame do
-
-  it "stores a scorecard inside a 2d array" do
-    game = BowlingGame.new([[5,4],[4,4],[3,2],[2,2]])
-    expect(game.scorecard).to be_a Array
-    expect(game.scorecard.length).to eq 4
-    expect(game.scorecard[0]).to eq [5,4]
-    expect(game.scorecard[-1]).to eq [2,2]
+  before do
+    @game = BowlingGame.new
   end
 
-  context ".score" do
-    it "adds both values in frame if neither spare or strike" do
-      game = BowlingGame.new([[5,4]])
-      expect(game.score).to eq 9
+  context "storing results using 2d array" do
+    it "creates a scorecard array on creation" do
+      expect(@game.scorecard).to be_a Array
+      expect(@game.scorecard.length).to eq 0
     end
 
-    it "if spare adds first value of next frame to score" do
-      game = BowlingGame.new([[5,5],[5,3]])
-      expect(game.score).to eq 23 #(10 from first + bonus 5 from next frame first roll)
-    end  
-
-    it "if strike adds both values of next frame to score" do
-      game = BowlingGame.new([[10],[5,3]])
-      expect(game.score).to eq 26 #(10 from first + bonus 8 from next frame both rolls)
-    end  
+    it "adds rolls to scorecard as valid frames" do
+      @game.add_roll(5)
+      @game.add_roll(3)
+      @game.add_roll(10)
+      @game.add_roll(3)
+      @game.add_roll(2)
+      expect(@game.scorecard.length).to eq 3
+      expect(@game.scorecard[0]).to eq [5,3]
+      expect(@game.scorecard[1]).to eq [10]
+      expect(@game.scorecard[2]).to eq [3,2]
+    end
   end
 
   context ".strike?" do
     it "returns true when strike" do
-      expect(subject.strike?([10,0])).to eq true
+      expect(subject.strike?([10])).to eq true
     end
 
     it "returns false when not strike" do
@@ -44,6 +41,40 @@ describe BowlingGame do
 
     it "returns false when not spare" do
       expect(subject.spare?([5,3])).to eq false
+    end
+  end
+
+  context ".score" do
+    it "adds both values in frame if neither spare or strike" do
+      @game.add_roll(5)
+      @game.add_roll(4)
+      expect(@game.score).to eq 9
+    end
+
+    it "if spare adds first value of next frame to score" do
+      @game.add_roll(5)
+      @game.add_roll(5)
+      @game.add_roll(5)
+      @game.add_roll(2)
+      expect(@game.score).to eq 22
+    end  
+
+    it "if strike adds both values of next frame to score" do
+      @game.add_roll(10)
+      @game.add_roll(5)
+      @game.add_roll(3)
+      expect(@game.score).to eq 26 
+    end  
+
+    it "gutter game" do
+      20.times { @game.add_roll(0)}
+      expect(@game.score).to eq 0
+
+    end
+
+    it "perfect game" do
+      10.times { @game.add_roll(10)}
+      expect(@game.score).to eq 300
     end
   end
 end
