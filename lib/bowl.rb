ba19d64dee675a76ace96e01c79b8d151ec57691
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-01-03 18:13:23 +0000

    implemented strikes in bowling and all tests pass

--- lib/bowl.rb
+++ lib/bowl.rb
@@ -1,45 +1,63 @@
 class Bowl
 
-  attr_reader :rolls, :score, :frame 
+  attr_reader :rolls
 
-  def initialise
-    rolls = []
-    score = 0
-    frame = 1
+  def initialize
+    @rolls = [ 
+      {frame: 1, roll_one: nil, roll_two: nil, total_roll: nil},
+      {frame: 2, roll_one: nil, roll_two: nil, total_roll: nil}, 
+      {frame: 3, roll_one: nil, roll_two: nil, total_roll: nil},
+      {frame: 4, roll_one: nil, roll_two: nil, total_roll: nil},
+      {frame: 5, roll_one: nil, roll_two: nil, total_roll: nil},
+      {frame: 6, roll_one: nil, roll_two: nil, total_roll: nil},
+      {frame: 7, roll_one: nil, roll_two: nil, total_roll: nil},
+      {frame: 8, roll_one: nil, roll_two: nil, total_roll: nil},
+      {frame: 9, roll_one: nil, roll_two: nil, total_roll: nil},
+      {frame: 10, roll_one: nil, roll_two: nil, total_roll: nil}  ]
+      @frames = 1
+  end
+
+  def first_roll(pins_one)
+      @rolls[@frames -1][:roll_one] = pins_one
+      if strike?
+        @frames += 1
+      end
+  end
+
+  def second_roll(pins_two)
+    fail 'Strike in previous roll' if @rolls[@frames -1][:roll_one] == 10
+    @rolls[@frames -1][:roll_two] = pins_two
+    calculate_total_roll_strike if previous_strike?
+    calculate_total_roll
+    @frames += 1
   end
 
 
-  TOTAL_FRAMES = 10
-
-  def roll_one
-
-    roll_result = [0,1,2,3,4,5,6,7,8,9,10].sample
-    score += roll_result
-    frame += 1 if is_strike
-    strike = true if is_strike
-
-  end
-
-  def roll_two 
-    roll_result = [0,1,2,3,4,5,6,7,8,9,10].sample
-    score += roll_result
-    frame += 1 
-
+  def total_score
+    @rolls.sum{|hash| hash[:total_roll]}
   end
 
 
 
+  
+  private 
+  def calculate_total_roll
+    @rolls[@frames -1][:total_roll] = @rolls[@frames -1][:roll_one] + @rolls[@frames -1][:roll_two]
+  end
 
+  def strike?
+     @rolls[@frames -1][:roll_one] == 10
+    
+  end
 
+  def previous_strike?
+     @rolls[@frames -2][:roll_one] == 10
+    
+  end
 
+  def calculate_total_roll_strike
+    @rolls[@frames -2][:total_roll] = @rolls[@frames -2][:roll_one]  +  @rolls[@frames -1 ][:roll_one] + @rolls[@frames -1][:roll_two]
 
-
-
-
-  private
-
-  def is_strike
-    roll_result == 10
   end
 
 
