b38c0ba8a840064b637d0d85e5bde27dad4a0694
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-01-03 10:14:14 +0000

    Refactoring

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -64,39 +64,3 @@ class BowlingGame
 	end
 
 end
-
-game = BowlingGame.new
-# game.roll(1)
-# game.roll(4)
-
-# game.roll(4)
-# game.roll(5)
-
-# game.roll(6)
-# game.roll(4)
-
-# game.roll(5)
-# game.roll(5)
-
-# game.roll(10)
-
-# game.roll(0)
-# game.roll(1)
-
-# game.roll(7)
-# game.roll(3)
-
-# game.roll(6)
-# game.roll(4)
-
-# game.roll(10)
-
-# game.roll(2)
-# game.roll(8)
-# game.roll(6)
-
-# 10.times do
-# 	game.roll(10)
-# end
-
-# p game.calc_score
