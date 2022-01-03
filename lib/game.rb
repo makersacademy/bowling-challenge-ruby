c72293244362a7e2c0aff9408ba0e2f3723d1d84
zidehen <zoeidehen@gmail.com>
2022-01-03 17:57:20 +0000

    refactored spare score method

--- lib/game.rb
+++ lib/game.rb
@@ -13,17 +13,31 @@ class Game
 
   def score
     running_score = 0
-    index = 0
+    @index = 0
     10.times do
-      if @rolls[index] + @rolls[index + 1] == 10
-        running_score += 10 + @rolls[index + 2] # add bonus from next roll to score
-        index += 2 # move to next frame
+      if spare?
+        running_score += spare_score
+        @index += 2 # move to next frame
       else
-        running_score += @rolls[index] + @rolls[index + 1]
-        index += 2  
+        running_score += sum_of_frame
+        @index += 2  
       end
     end
     running_score
+  end
+
+  private
+
+  def spare?
+    @rolls[@index] + @rolls[@index + 1] == 10
+  end
+
+  def spare_score
+    @rolls[@index] + @rolls[@index + 1] + @rolls[@index + 2]
+  end
+
+  def sum_of_frame
+    @rolls[@index] + @rolls[@index + 1]
   end
 end
 
