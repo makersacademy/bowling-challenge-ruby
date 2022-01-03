6dbf1b57d72d82c83e16e74cbc02c684ff0037de
Mayo-Theodore <mayoelith@gmail.com>
2022-01-03 19:00:41 +0000

    add tests

--- lib/bowling_scores.rb
+++ lib/bowling_scores.rb
@@ -5,172 +5,30 @@ class BowlingScores
     @results = []
   end
 
-  def record(result)
-    @results << result
+  def record(*scores)
+    raise ArgumentError, "Too many arguments" if scores.length > 12
+    @results = scores
   end 
 
-  def calculate_score(@results)
-    array.each_with_index do |element, index|
-      if element == [10, 0] && array[index + 1] == [10, 0] && array[index + 2] == [10, 0] && index <= 8
+  def calculate_score()
+    @results.each_with_index do |element, index|
+      if element == [10, 0] && @results[index + 1] == [10, 0] && @results[index + 2] == [10, 0] && index <= 8
           @total_score << 30
-      elsif element == [10 ,0] && index == 9 && array[index + 1] == [10, 0] && array[index + 2] == [10, 0]
+      elsif element == [10 ,0] && index == 9 && @results[index + 1] == [10, 0] && @results[index + 2] == [10, 0]
           @total_score << 30
-      elsif element == [10, 0] && array[index + 1] == [10, 0] && array[index + 2] != [10, 0] && index <= 8
-          @total_score << 20 + array[index + 2][0]
-      elsif element == [10, 0] && array[index + 1] != [10, 0] && array[index + 2] != [10, 0] && index <= 8
-           @total_score << 10 + array[index + 1].inject(:+)
+      elsif element == [10, 0] && @results[index + 1] == [10, 0] && @results[index + 2] != [10, 0] && index <= 8
+          @total_score << 20 + @results[index + 2][0]
+      elsif element == [10, 0] && @results[index + 1] != [10, 0] && @results[index + 2] != [10, 0] && index <= 8
+          @total_score << 10 + @results[index + 1].inject(:+)
       elsif element.inject(:+) == 10 && element[1] != 0 && index <= 8
-           @total_score << 10 + array[index + 1][0]
+          @total_score << 10 + @results[index + 1][0]
       elsif element.inject(:+) == 10 && element[1] != 0 && index == 9
-          @total_score << 10 + + array[index + 1][0]
+          @total_score << 10 + @results[index + 1][0]
       elsif index <= 9 
-           @total_score << element.inject(:+)
+          @total_score << element.inject(:+)
       end
-      @total_score.inject(:+)
-      end
+    end
+  @total_score.inject(:+)
   end
 
 end
-
-
-# array.each_with_index do |element, index|
-    
-#     if element == [10, 0] && array[index + 1] == [10, 0] && array[index + 2] == [10, 0] && index <= 8
-#         total_score << 30
-#     elsif element == [10 ,0] && index == 9 && array[index + 1] == [10, 0] && array[index + 2] == [10, 0]
-#         total_score << 30
-#     elsif element == [10, 0] && array[index + 1] == [10, 0] && array[index + 2] != [10, 0] && index <= 8
-#         total_score << 20 + array[index + 2][0]
-#     elsif element == [10, 0] && array[index + 1] != [10, 0] && array[index + 2] != [10, 0] && index <= 8
-#          total_score << 10 + array[index + 1].inject(:+)
-#     elsif element.inject(:+) == 10 && element[1] != 0 && index <= 8
-#          total_score << 10 + array[index + 1][0]
-#     elsif element.inject(:+) == 10 && element[1] != 0 && index == 9
-#         total_score << 10 + + array[index + 1][0]
-#     elsif index <= 9 
-#          total_score << element.inject(:+)
-#     end
-
-# end
-
-# p total_score.inject(:+)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ======Strike logic=======
-
-# array = [
-#   [10, 0],
-#   [10, 0],
-#   [10, 0],
-#   [10, 0],
-#   [10, 0],
-#   [10, 0],
-#   [10, 0],
-#   [10, 0],
-#   [10, 0],
-#   [10, 0],
-#   [10, 0],
-#   [10, 0],
-# ]
-
-# total_score = []
-
-# array.each_with_index do |element, index|
-#     if element == [10, 0] && index < 10
-#         if array[index + 1] == [10, 0] && index < 10
-#             if array[index + 2] == [10, 0] && index < 10
-#                 total_score << 30
-#             end
-#         end
-#     end
-# end
-
-# p total_score.inject(:+)
-
-# ===============================
-
-
-
-# array = [
-#   [3,2],
-#   [5,2],
-#   [10, 0],
-#   [10, 0],
-#   [3,6],
-#   [5,5],
-#   [3,4],
-#   [4,1],
-#   [0,0],
-#   [1,1],
-# ]
-
-# total_score = []
-# array.each_with_index do |element, index|  
-
-#   if element[0] == 10
-#     total_score << 10 + array[index+1].inject(:+)
-#     puts "Round #{index + 1}: #{10 + array[index+1].inject(:+)}"
-#   elsif element.inject(:+) == 10
-#      total_score << 10 + array[index+1][0]
-#      puts "Round #{index + 1}: #{10 + array[index+1][0]}"
-#   else 
-#     total_score << element.inject(:+)
-#     puts "Round #{index + 1}: #{element.inject(:+)}"
-#   end
-# end 
-
-# p total_score.inject(:+)
-
-
-# array = [
-#   [3,2],
-#   [5,2],
-#   [10, 0],
-#   [10, 0],
-#   [3,6],
-#   [5,5],
-#   [3,4],
-#   [4,1],
-#   [0,0],
-#   [1,1],
-# ]
-
-# total_score = []
-
-# array.each_with_index do |element, index|
- 
-#     # 3 Strikes in a row / Perfect Game
-#     if element == [10, 0] && index < 10
-#         if array[index + 1] == [10, 0] && index < 10
-#            total_score << 10 + 10 + array[index + 2][0]
-#         elsif  array[index + 1] != [10, 0] && index < 10
-#                 total_score << 10 + array[index + 1].inject(:+)
-#             if array[index + 2] == [10, 0] && index < 10
-#                 total_score << 30
-#             end
-#         end
-
-#     #Spares
-#     elsif element.inject(:+) == 10 && element[1] != 0 && index < 10
-#       total_score << 10 + array[index + 1][0]
-
-#     #Normal
-#     else
-#         total_score << element.inject(:+) 
-#     end
-
-# end
-
--- spec/bowling_scores_spec.rb
+++ spec/bowling_scores_spec.rb
@@ -2,25 +2,39 @@ require 'bowling_scores'
 
 describe BowlingScores do 
 
-  # describe '#record_score' do
-  #   it 'allows player to record their result' do
-  #     player = BowlingScores.new()
-  #     expect(player.record(3)).to include 3
-  #   end
-  # end
-
-  describe '#strike' do
-    it 'takes into account bonus points when player records a strike' do
-      player = BowlingScores.new()
-      expect(player.record(1, "strike")).to eq "You got a strike!"
+  describe '#record' do
+    it 'allows user to input their scores' do
+      score_card = BowlingScores.new()
+      expect(score_card.record([3,4])).to include [3,4]
     end
+
+    it 'prevents users from entering too many arguments' do 
+      score_card = BowlingScores.new()
+      expect { score_card.record([3,4], [3,4], [3,4], [3,4], [3,4], [3,4], [3,4], [3,4], [3,4], [3,4], [3,4], [3,4], [3,4]) }.to raise_error("Too many arguments")
+    end
+
   end
 
-  describe '#spare' do
-    it 'takes into account bonus points when player records a spare' do
-      player = BowlingScores.new()
-      expect(player.record(1, "spare")).to eq "You got a spare!"
+    describe '#calculate_score' do
+    it 'allows users to see their total score' do
+      score_card = BowlingScores.new()
+      score_card.record([1,5], [10,0], [10,0], [10,0], [3,7], [5,5], [3,4], [4,1], [0,0], [9,1], [9,0])
+      expect(score_card.calculate_score()).to eq 138
     end
+
+    it 'returns perfect score, when user inputs 12 strikes' do
+      score_card = BowlingScores.new()
+      score_card.record([10,0], [10,0], [10,0], [10,0], [10,0], [10,0], [10,0], [10,0], [10,0], [10,0], [10,0], [10,0])
+      expect(score_card.calculate_score()).to eq 300
+    end
+
+    it 'returns gutter game, when user misses all of the pins' do
+      score_card = BowlingScores.new()
+      score_card.record([0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0])
+      expect(score_card.calculate_score()).to eq 0
+    end
+
   end
+
 
 end
