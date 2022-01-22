b1e62879d8d475095e7a3ea85346d60bd3f3a4b2
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-01-22 15:14:48 +0000

    before separating out the bonus_points method to calculate how many points per frame depending on strikes/spares

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,9 +1,10 @@
 class Scorecard
 
-attr_reader :total_score
+attr_reader :total_score, :bonus_points
 
   def initialize
     @score = [] #Array.new(10) {Array.new(2,0)}
+    @bonus_points = 0 #this will store the bonus points resulting each frame from stikes or spares through the roll_score method
   end
 
   # def frame_score
@@ -14,14 +15,24 @@ attr_reader :total_score
   #   # final frame code here?
   # end
 
-  def frame_scores
-    @score.map{ |frame| frame.sum }.sum #map returns a new array with the results
-
+  def frame_scores # 
+    total = @score.map{ |frame| frame.sum }.sum #map returns a new array with the results. sum 
+    total += @bonus_points
   end
 
   def roll_score(a = 0, b = 0) 
     frame = [a, b]
-    @score << frame
+    if @score.length > 1
+      if @score[-1][0] == 10 && @score[-2][0] == 10 #access the previous frame's first roll score to check if strike 
+        @bonus_points += (a+b+10)
+      elsif @score[-1][0] == 10
+        @bonus_points += (a+b)
+      elsif @score[-1].sum == 10
+        @bonus_points += a
+      end
+    end
+    p @score << frame
+    p @bonus_points
   end
 
   def outcome 
@@ -40,8 +51,47 @@ attr_reader :total_score
   end
 end
 
+# p s= Scorecard.new
+# p s.roll_score(10,0) #strike. #returns @score array
+# p s.bonus_points #should be 0
+# p s.frame_scores #returns a value
+# p s.roll_score(5,2) #returns @score array
+# p s.bonus_points #should be 7
+# p s.roll_score(4,6) #spare
+# p s.bonus_points
+# p s.roll_score(8,1)
+# p s.bonus_points #should be 7+8 = 15 
+
 
 # frames = [
 #   [0,1]
 #   [2,3] etc. i.e. array of 10 2-element arrays
 # ]
+
+
+# perfect game 
+# [10,0]  
+# [10,0]
+# [10,0]
+# [10,0]
+# [10,0]
+# [10,0]
+# [10,0]
+# [10,0]
+# [10,0]
+# [10,0]
+# [10,0]
+# [10,0]
+
+# [30,0]
+# [30,0]
+# [30,0]
+# [30,0]
+# [30,0]
+# [30,0]
+# [30,0]
+# [30,0]
+# [30,0]
+# [10,0]
+# [10,0]
+# [10,0]
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -7,19 +7,30 @@ describe Scorecard do
   #     expect(subject.frame_scores).to eq [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   #     #expect(subject.outcome).to eq 'gutter game'
   #   end
-    it 'returns gutter game when all rolls are 0' do 
+    it 'returns 0 when all rolls are 0' do 
       10.times {subject.roll_score(0,0)} # player inputs the roll score using roll_score method
       expect(subject.frame_scores).to eq 0
       #expect(subject.outcome).to eq 'gutter game'
     end
+
+    it 'returns 300 when all rolls are 10, meaning 2 extra rolls of 10' do 
+      12.times {subject.roll_score(10,0)} # player inputs the roll score using roll_score method
+      expect(subject.frame_scores).to eq 300
+      #expect(subject.outcome).to eq 'gutter game'
+    end
+
   end
     
   describe '#outcome' do 
     it 'returns gutter game when all rolls are 0' do 
       10.times {subject.roll_score(0,0)} # player inputs the roll score using roll_score method
       expect(subject.outcome).to eq 'gutter game'
-      #expect(subject.outcome).to eq 'gutter game'
     end
+
+    # it 'returns perfect game when all rolls are 0' do 
+    #   12.times {subject.roll_score(10,10)} # player inputs the roll score using roll_score method
+    #   expect(subject.outcome).to eq 'perfect game'
+    #  end
     
   end 
 end
