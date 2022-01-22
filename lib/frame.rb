0d70ac3f947052e21c6f2c5f3311481598d018cb
mikitaro-27 <matt.diak@gmail.com>
2022-01-22 22:01:25 +0000

    create Frame class

--- lib/frame.rb
+++ lib/frame.rb
@@ -36,4 +36,33 @@
 
 class Frame
 
+  attr_accessor :score, :bonus_throws
+
+  def initialize(throw_1, throw_2 = nil)
+    @throw_1 = throw_1
+    @throw_2 = throw_2
+    @score = determine_score
+    @bonus_throws = bonus_throws?
+  end
+  
+  def add_points(points)
+    @score += points
+  end
+  
+  private
+  def bonus_throws?
+    strike? ? 2 : spare? ? 1 : 0
+  end
+
+  def determine_score
+    strike? ? @throw_1 : @throw_1 + @throw_2
+  end
+
+  def strike?
+    @throw_2.nil?
+  end
+
+  def spare?
+    strike? ? false : @throw_1 + @throw_2 == 10
+  end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,13 +1,29 @@
 require 'frame'
 
 describe Frame do
-  describe '#strike?' do
-    it 'determines a strike' do
-      # arrange
-      Frame.new()
-      # act
   
-      # assert
+  let(:strike_frame) { Frame.new(10) }
+  let(:spare_frame) { Frame.new(0, 10) }
+  let(:open_frame) { Frame.new(0, 0) }
+
+  describe '#bonus_throws?' do
+    it 'returns 2' do
+      expect(strike_frame.bonus_throws).to eq 2
+    end
+
+    it 'returns 1' do
+      expect(spare_frame.bonus_throws).to eq 1
+    end
+
+    it 'returns 0' do
+      expect(open_frame.bonus_throws).to eq 0
+    end
+  end
+  
+  describe '#add_points' do
+    it 'adds 5 to score' do
+      strike_frame.add_points(5)
+      expect(strike_frame.score).to eq 15
     end
   end
 end
