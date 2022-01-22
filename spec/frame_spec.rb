ffabd4a722a98f8fa33b835ac2d253cd8ef0b3ce
icvotria <icvotria@gmail.com>
2022-01-22 22:53:49 +0000

    updated frame tests

--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -3,10 +3,14 @@
 require 'frame'
 
 describe Frame do
-
-  let(:frame) { Frame.create([1, 2, 3]) }
+  let(:frame) { Frame.create([2, 5, 4])}
+  let(:strike) { Frame.create([1, 10]) }
+  let(:last_frame) { Frame.create([10, 9, 1, 6])}
+  
   it '.create' do
     expect(frame).to be_a Hash
-    expect(frame).to have_key :frame_number
+    expect(strike).to have_key :frame_number
+    expect(strike).to have_value 10
+    expect(last_frame.size).to eq 4
   end
 end
