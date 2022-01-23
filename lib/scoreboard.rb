7a74331e9f303f07360138e7aa833cf3f8314f24
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-01-23 17:27:15 +0000

    refactoring

--- lib/scoreboard.rb
+++ lib/scoreboard.rb
@@ -14,7 +14,7 @@ class Scoreboard
     [0, 0],
     [0, 0],
     [0, 0]
-  ]
+  ].freeze
 
   HIGHEST_SCORE = [
     [10],
@@ -27,28 +27,28 @@ class Scoreboard
     [10],
     [10],
     [10, 10, 10]
-  ]
+  ].freeze
   def initialize(frames)
-    if frames == LOWEST_SCORE
-      @result = 0
-		elsif
-      frames == HIGHEST_SCORE
-      @result = 300
-		else
-			@result = calculate_score(frames)
-    end
+    @result = case frames
+              when LOWEST_SCORE
+                0
+              when HIGHEST_SCORE
+                300
+              else
+                calculate_score(frames)
+              end
   end
 
   def calculate_score(frames)
     score = 0 # score starts at 0
-    frames.each_with_index do |frame, index| 
-			score += frame.sum # this will add the sum of rolls in each frame to the score
-			if frame [0] == 10 # if frame is strike
-				score += frames[index + 1][0..1].sum # it will add next frame score to the strike score which is 10
-			elsif frame.sum == 10 # if frame is spare
-				score += frames[index + 1][0] # it will add next frame first rolle score to the spare frame score which is 10
-			end
-		end
-		score # return the score
+    frames.each_with_index do |frame, index|
+      score += frame.sum # this will add the sum of rolls in each frame to the score
+      if frame [0] == 10 # if frame is strike
+        score += frames[index + 1][0..1].sum # it will add next frame score to the strike score which is 10
+      elsif frame.sum == 10 # if frame is spare
+        score += frames[index + 1][0] # it will add next frame first rolle score to the spare frame score which is 10
+      end
+    end
+    score # return the score
   end
 end
--- spec/scoreboard_spec.rb
+++ spec/scoreboard_spec.rb
@@ -2,89 +2,58 @@
 
 require 'scoreboard'
 describe Scoreboard do
+  let(:scoreboard) do
+    Scoreboard.new([
+                     [0, 0],
+                     [0, 0],
+                     [0, 0],
+                     [0, 0],
+                     [0, 0],
+                     [0, 0],
+                     [0, 0],
+                     [0, 0],
+                     [0, 0],
+                     [0, 0]
+                   ])
+  end
+
+  let(:scoreboard_1) do
+    Scoreboard.new([
+                     [10],
+                     [10],
+                     [10],
+                     [10],
+                     [10],
+                     [10],
+                     [10],
+                     [10],
+                     [10],
+                     [10, 10, 10]
+                   ])
+  end
   describe '#initialize' do
     it 'initialize an instance of Scoreboard' do
-      scoreboard = Scoreboard.new([
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0]
-                                  ])
       expect(scoreboard).to be_a Scoreboard
     end
   end
 
   describe '.result' do
     it 'should have result attribute' do
-      scoreboard = Scoreboard.new([
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0]
-                                  ])
       expect(scoreboard).to have_attributes(result: 0)
     end
   end
 
   describe '.result' do
     it 'returns lowest score of game as 0' do
-      scoreboard = Scoreboard.new([
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0],
-                                    [0, 0]
-                                  ])
       expect(scoreboard.result).to eq 0
     end
     it 'returns maximum score of game as 300' do
-      scoreboard = Scoreboard.new([
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10, 10, 10]
-                                  ])
-      expect(scoreboard.result).to eq 300
+      expect(scoreboard_1.result).to eq 300
     end
   end
   describe '#calculate_score' do
     it 'should calculate_score' do
-      scoreboard = Scoreboard.new([
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10],
-                                    [10, 10, 10]
-                                  ])
-      expect(scoreboard).to respond_to(:calculate_score)
+      expect(scoreboard_1).to respond_to(:calculate_score)
     end
 
     it 'calculate the numbers of pin when there is not any strikes and spares' do
