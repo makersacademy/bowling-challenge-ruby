f520730f1fa1356033bd72ab8ff291f990d6fee9
helloecho12345 <92740628+helloecho12345@users.noreply.github.com>
2022-01-23 20:47:40 +0000

    added gutter game = 0

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,3 +1,15 @@
 class Bowling
- perfect game = 300
+  perfect_game = 300
+
+  gutter_game = 0
+  
+  # score = []
+  # bonus = []
+  # frame = 1
+
+  # if frame <= 10 && roll == 1
+  #   bowl.
+  #   score + bonus
+  # end
+  # frame + 1
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,5 +1,17 @@
 describe 'player bowls a perfect game' do
   it 'returns a perfect game score of 300' do
-    300
+    perfect_game = 300
   end
 end
+
+describe 'Gutter Game is when no pins are knocked down' do
+  it ' returns a gutter_game score of 0' do
+    gutter_game = 0
+  end
+end
+
+# describe 'player bowls a strike' do
+#   it 'returns a score of 10' do
+#     expect(page)
+#   end
+# end
