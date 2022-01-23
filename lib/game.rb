abb3aeaa8e4cb66c6fcf9e5d9d37245ee0348289
mikitaro-27 <matt.diak@gmail.com>
2022-01-23 12:47:00 +0000

    add tests for #add_frame changing other frame scores

--- lib/game.rb
+++ lib/game.rb
@@ -15,26 +15,47 @@ class Game
   def add_frame(frame)
     analyse_frame(frame)
     @frames << frame
-    p total_score
   end
 
   private
+
   def analyse_frame(analysed_frame)
     @frames.each do |frame| 
       if analysed_frame.strike? 
         if frame.bonus_rolls > 0
-          frame.score += analysed_frame.roll_1
+          frame.add_points(analysed_frame.roll_1)
           frame.bonus_rolls -= 1
         end
       else
         if frame.bonus_rolls == 2
-          frame.score += analysed_frame.roll_1 + analysed_frame.roll_2
+          frame.add_points(analysed_frame.roll_1 + analysed_frame.roll_2)
           frame.bonus_rolls -= 2
         elsif frame.bonus_rolls == 1
-          frame.score += analysed_frame.roll_1
+          frame.add_points(analysed_frame.roll_1)
           frame.bonus_rolls -= 1
         end
       end
     end
   end
 end
+
+# game = Game.new
+
+# game.add_frame(Frame.new(10, nil))
+# game.add_frame(Frame.new(5, 5))
+# game.add_frame(Frame.new(10, nil))
+# game.add_frame(Frame.new(5, 3))
+# game.add_frame(Frame.new(0, 0))
+# game.add_frame(Frame.new(10, nil))
+# game.add_frame(Frame.new(10, nil))
+# game.add_frame(Frame.new(9, 1))
+# game.add_frame(Frame.new(5, 5))
+# game.add_frame(Frame.new(9, 0))
+
+# p game.frames.length
+# p game.total_score
+
+# take a frame
+# check if it is a strike
+# if it is a strike
+# add the first
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -35,8 +35,62 @@ describe Game do
 
   describe '#add_frame' do
     it 'adds the frame' do
+      expect(game).to receive(:analyse_frame)
       game.add_frame(strike_frame)
       expect(game.frames).to eq [strike_frame]
+    end
+  end
+
+  describe '#add_frame' do
+    it 'receives analyse_frame(frame) ' do
+      expect(game).to receive(:analyse_frame).with(spare_frame)
+      game.add_frame(spare_frame)
+    end
+  end
+
+  # isn't this testing the state?
+  describe '#add_frame' do
+    it 'updates the previous frames' do
+      game.add_frame(Frame.new(5, 4))
+      expect(game.total_score).to eq 9
+      
+      game.add_frame(Frame.new(10, nil))
+      expect(game.frames[-2].score).to eq 9
+      expect(game.total_score).to eq 19
+      
+      game.add_frame(Frame.new(5, 5)) # score 5 + 5 + 10
+      expect(game.frames[-2].score).to eq 20
+      expect(game.total_score).to eq 39
+      
+      game.add_frame(Frame.new(10, nil)) # score 10 + 10 + 10
+      expect(game.frames[-2].score).to eq 20
+      expect(game.total_score).to eq 59
+      
+      game.add_frame(Frame.new(10, nil)) # score 10 + 10 + 5
+      expect(game.frames[-2].score).to eq 20
+      expect(game.total_score).to eq 79
+      
+      game.add_frame(Frame.new(10, nil)) # score 10 + 5 + 5
+      expect(game.frames[-3].score).to eq 30
+      expect(game.frames[-2].score).to eq 20
+      expect(game.total_score).to eq 109
+      
+      game.add_frame(Frame.new(5, 5)) # score 5 + 5 + 0
+      expect(game.frames[-3].score).to eq 25
+      expect(game.frames[-2].score).to eq 20
+      expect(game.total_score).to eq 134
+      
+      game.add_frame(Frame.new(0, 0)) # score 0 + 0
+      expect(game.frames[-2].score).to eq 10
+      expect(game.total_score).to eq 134
+      
+      game.add_frame(Frame.new(3, 3)) # score 3 + 3
+      expect(game.frames[-2].score).to eq 0
+      expect(game.total_score).to eq 140
+      
+      game.add_frame(Frame.new(5, 4)) # score 5 + 4
+      expect(game.frames[-2].score).to eq 6
+      expect(game.total_score).to eq 149
     end
   end
 
