ba84a819e77a1fd3250adb3cbe7d43c1b4352c3d
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-01-23 20:21:57 +0000

    major refactor

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,8 +1,4 @@
-# frozen_string_literal: true
-
-require_relative 'frame'
-
-### check iphone for method
+# frozen_string_literal: trues
 
 class Scorecard
   attr_reader :frames, :total_score, :bonus_score
@@ -17,49 +13,27 @@ class Scorecard
     @bonus_score = 0
   end
 
+  def turn(ball1, ball2=0, ball3=0)
+    
+    if @frames.size < MAX_FRAMES
+
+      raise InvalidScoreError if score_invalid?(ball1, ball2)
+
+      if ball1 == MAX_PINS
+        @frames << [ball1]
+      else
+        @frames << [ball1, ball2]
+      end
+
+    end
+  end
+
  
-  def add_frame(roll1, roll2, roll3 = 0)
-    if @frames.size < 9
-
-      raise InvalidScoreError if score_invalid?(roll1, roll2)
-      
-      frame = Frame.new(roll1: roll1, roll2: roll2)
-      @frames << {'roll1' => frame.roll1, 'roll2' => frame.roll2}
-
-      calculate_score(roll1, roll2)
-
-    else
-      unless final_frame?
-        raise InvalidScoreError if final_frame_score_invalid?(roll1, roll2, roll3)
-        @frames << { 'roll1' => roll1, 'roll2' => roll2, 'roll3' => roll3 }
-
-        calculate_score(roll1, roll2, roll3)
-      end
-      
-    end
-  end
-
-  def calculate_score(roll1, roll2, roll3=0)
-    if first_frame?
-      @total_score += (roll1 + roll2)
-    else 
-      check_for_strike_bonus
-      check_for_spare_bonus
-      
-      @total_score += (roll1 + roll2) + @bonus_score
-      p "bonus score #{bonus_score}" 
-      @bonus_score = 0 
-
-      if final_frame? && roll1 + roll2 == 10  
-        @total_score += roll3 
-      end
-    end
-  end
 
   private
 
-  def score_invalid?(roll1, roll2)
-    roll1 + roll2 > MAX_PINS || (roll1 + roll2).negative?
+  def score_invalid?(ball1, ball2)
+    ball1 + ball2 > MAX_PINS || (ball1 + ball2).negative?
   end
 
   def final_frame_score_invalid?(roll1, roll2, roll3)
@@ -98,42 +72,3 @@ end
 
 class InvalidScoreError < StandardError
 end
-
-x = Scorecard.new
-
-9.times do 
-  x.add_frame(10, 0)
-  puts x.total_score
-end
-
-x.add_frame(10, 10, 10)
-puts x.total_score
-
-
-# puts x.total_score
-
-# x.add_frame(1, 4)
-# print x.frames[-1]
-# x.add_frame(4, 5)
-# print x.frames[-1]
-# x.add_frame(6, 4)
-# print x.frames[-1]
-# x.add_frame(5, 5)
-# print x.frames[-1]
-# x.add_frame(10, 0)
-# print x.frames[-1]
-# x.add_frame(0, 1)
-# print x.frames[-1]
-# x.add_frame(7, 3)
-# print x.frames[-1]
-# x.add_frame(6, 4)
-# print x.frames[-1]
-# x.add_frame(10, 0)
-# print x.frames[-1]
-# x.add_frame(2, 8, 6)
-# print x.frames[-1]
-# puts x.total_score
-
-
-
-
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,86 +1,33 @@
-# frozen_string_literal: true
-
 require 'scorecard'
-require 'frame'
-
-# As a bowler
-# So I can see my results
-# I want to be able to count and sum the scores of my bowling game.
 
 describe Scorecard do
   subject(:scorecard) { described_class.new }
 
   describe '#frames' do
-    it 'returns the array of frames (which begins empty)' do
+    it 'returns the array of rolls (which begins empty)' do
       expect(scorecard.frames).to eq([])
     end
-
-    it 'has a max capacity of 10 (a full bowling game)' do
-      15.times do
-        scorecard.add_frame(0, 5)
-      end
-      expect(scorecard.frames.length).to eq(10)
-    end
   end
 
-  describe '#total_score' do
-    it 'returns a the total score (which starts at 0)' do
-      expect(scorecard.total_score).to eq(0)
-    end
-  end
-
-  describe '#bonus_score' do
-    it 'returns a the bonus score (which starts at 0)' do
-      expect(scorecard.bonus_score).to eq(0)
-    end
-  end
-
-  describe '#add_frame' do
-    it 'adds a frame hash to the frames array' do
-      scorecard.add_frame(3, 5)
-      expect(scorecard.frames.first).to eq({ 'roll1' => 3, 'roll2' => 5 })
+  describe '#turn' do
+    it 'adds a turn to frames ' do
+      scorecard.turn(2, 2)
+      expect(scorecard.frames).to eq([[2, 2]])
     end
 
-    it 'lets user add multiple frame hashes (frame key increments)' do
-      scorecard.add_frame(3, 5)
-      scorecard.add_frame(4, 2)
-      expect(scorecard.frames).to include({ 'roll1' => 3, 'roll2' => 5 },
-                                          { 'roll1' => 4, 'roll2' => 2 })
+    it 'enters a strike frame as only ball1' do
+      scorecard.turn(10)
+      expect(scorecard.frames).to eq([[10]])
+    end
+
+    it 'does not enter strikes as ball1 and ball2 even if a ball2 is provided' do
+      scorecard.turn(10, 0)
+      expect(scorecard.frames).not_to eq([[10, 0]])
     end
 
     it 'raises an InvalidScoreError if an ivalid score is entered' do
-      expect { scorecard.add_frame(9, 9) }.to raise_error(InvalidScoreError)
-    end
-
-    context 'when it is the final frame and a strike or spare is scored' do
-      before do
-        9.times do
-          scorecard.add_frame(3, 2)
-        end
-      end
-
-      it 'adds a final_frame hash to the frames array with 3 rolls' do
-        scorecard.add_frame(1, 9, 10)
-        expect(scorecard.frames[-1]).to eq({ 'roll1' => 1, 'roll2' => 9, 'roll3' => 10 })
-      end
-
-      context 'if the total of the final frame score is invalid' do
-        it 'raises an InvalidScoreError (2 valid strikes and invalid enntry)' do
-          expect { scorecard.add_frame(10, 10, 12) }.to raise_error(InvalidScoreError)
-        end
-
-        it 'raises an InvalidScoreError (no spair and bowler takes bonus roll)' do
-          expect { scorecard.add_frame(1, 1, 10) }.to raise_error(InvalidScoreError)
-        end
-      end
+      expect { scorecard.turn(9, 9) }.to raise_error(InvalidScoreError)
     end
   end
 
-  # private method ? but logic requires testing
-  describe '#calculate_score' do
-    it 'calculates the score of the frame' do
-      scorecard.add_frame(3, 4)
-      expect(scorecard.total_score).to eq(7)
-    end
-  end
 end
