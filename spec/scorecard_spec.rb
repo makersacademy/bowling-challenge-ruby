e484d0354a478ef7153a4e19f672c73fc7551945
icvotria <icvotria@gmail.com>
2022-01-23 11:25:05 +0000

    updated scorecard tests to refelct correct scores

--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -3,23 +3,49 @@
 require 'scorecard'
 
 describe Scorecard do
-  it 'returns scores' do
+  let(:perfect_game) { Scorecard.new([{ :frame_number=>1, :roll1=>10 },
+    { :frame_number=>2, :roll1=>10 },
+    { :frame_number=>3, :roll1=>10 },
+    { :frame_number=>4, :roll1=>10 },
+    { :frame_number=>5, :roll1=>10 },
+    { :frame_number=>6, :roll1=>10 },
+    { :frame_number=>7, :roll1=>10 },
+    { :frame_number=>8, :roll1=>10 },
+    { :frame_number=>9, :roll1=>10 },
+    { :frame_number=>10, :roll1=>10 },
+    { :frame_number=>:bonus_roll, :roll1=>10 }]) }
+
+    let(:gutter_game) { Scorecard.new([{ :frame_number=>1, :roll1=>0 },
+      { :frame_number=>2, :roll1=>0 },
+      { :frame_number=>3, :roll1=>0 },
+      { :frame_number=>4, :roll1=>0 },
+      { :frame_number=>5, :roll1=>0 },
+      { :frame_number=>6, :roll1=>0 },
+      { :frame_number=>7, :roll1=>0 },
+      { :frame_number=>8, :roll1=>0 },
+      { :frame_number=>9, :roll1=>0 },
+      { :frame_number=>10, :roll1=>0 }]) }
+
+    let(:mixed_game) { Scorecard.new([{ :frame_number=>1, :roll1=>6, :roll2=>3 },
+      { :frame_number=>2, :roll1=>3, :roll2=>7 },
+      { :frame_number=>3, :roll1=>5, :roll2=>4 },
+      { :frame_number=>4, :roll1=>6, :roll2=>0 },
+      { :frame_number=>5, :roll1=>10 },
+      { :frame_number=>6, :roll1=>6, :roll2=>3 },
+      { :frame_number=>7, :roll1=>4, :roll2=>3 },
+      { :frame_number=>8, :roll1=>2, :roll2=>1 },
+      { :frame_number=>9, :roll1=>4, :roll2=>4 },
+      { :frame_number=>10, :roll1=>5, :roll2=>5 },
+      { :frame_number=>:bonus_roll, :roll1=>3 }]) }    
+  
+    it 'returns scores' do
     # game = double(Game.new)
     # 11.times { allow(game).to receive(:gets).and_return('10') }
     # scorecard = game.play
-    scorecard = Scorecard.new([{:frame_number=>1, :roll1=>10},
-      {:frame_number=>2, :roll1=>10},
-      {:frame_number=>3, :roll1=>10},
-      {:frame_number=>4, :roll1=>10},
-      {:frame_number=>5, :roll1=>10},
-      {:frame_number=>6, :roll1=>10},
-      {:frame_number=>7, :roll1=>10},
-      {:frame_number=>8, :roll1=>10},
-      {:frame_number=>9, :roll1=>10},
-      {:frame_number=>10, :roll1=>10},
-      {:frame_number=>:bonus_roll, :roll1=>10}])
 
-    expect(scorecard.total_score).to eq 110
+    expect(perfect_game.total_score).to eq 300
+    expect(gutter_game.total_score).to eq 0
+    expect(mixed_game.total_score).to eq 98
   end
 
   xdescribe '.frame_interpreter' do
