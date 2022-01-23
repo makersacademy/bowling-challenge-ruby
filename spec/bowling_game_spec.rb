fbf8a3b626c2cad5dccc0100c89237a3082b5a81
Raven Sears <ravenrosereynolds@gmail.com>
2022-01-23 22:32:30 +0000

    Added RSpec Tests and Functionality to Score Card

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,25 +1,45 @@
 require 'bowling_game'
 
 describe BowlingGame do
+  let(:bowling) { BowlingGame.new }
+    
   it 'starts a new game' do
-    game = BowlingGame.new
+    bowling = BowlingGame.new
+  end
+
+  it 'encounters a gutter game' do
+    20.times { bowling.roll(0) }
+    expect(bowling.score_card.sum).to eq 0
+  end
+
+  it 'encounters a perfect game' do
+    15.times { bowling.roll(20) }
+    expect(bowling.score_card.sum).to eq 300
   end
 
   context 'initialize game' do
     it 'has an empty array' do
-      game = BowlingGame.new
-      game.score_card
-            
-      expect(game.score_card).to be_empty
+      bowling.score_card
+      expect(bowling.score_card).to be_empty
     end
 
     it 'takes the first score' do
-      game = BowlingGame.new
-      game.roll(6)
+      bowling.roll(6)
+      expect(bowling.score_card).to eq [6]
+    end    
+  end
 
-      expect(game.score_card).to eq [6]
+  context 'score card' do
+    it 'can record multiple rolls' do
+        bowling.roll(7)
+        bowling.roll(9)
+        bowling.roll(20)
+        bowling.roll(15)
+
+        expect(bowling.score_card[0]).to eq 7
+        expect(bowling.score_card[1]).to eq 9
+        expect(bowling.score_card[2]).to eq 20
+        expect(bowling.score_card[3]).to eq 15
     end
-
-    
   end
 end
