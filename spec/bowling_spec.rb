3efdd3330ed9eccaf3d9eae965d931ec3a72db7b
helloecho12345 <92740628+helloecho12345@users.noreply.github.com>
2022-01-24 08:11:24 +0000

    add roll, score and rollCounter

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,17 +1,28 @@
+require 'bowling'
+
+describe 'new BowlingGame' do
+  it 'starts a new BowlingGame' do
+    game = BowlingGame.new
+  end
+end
+
 describe 'player bowls a perfect game' do
   it 'returns a perfect game score of 300' do
     perfect_game = 300
   end
 end
 
-describe 'Gutter Game is when no pins are knocked down' do
+describe 'player bowls a Gutter Game which is when no pins are knocked down' do
   it ' returns a gutter_game score of 0' do
-    gutter_game = 0
+    game = BowlingGame.new
+    20.times{game.roll 0}
+    expect(game.score).to eq 0
   end
 end
 
-# describe 'player bowls a strike' do
-#   it 'returns a score of 10' do
-#     expect(page)
-#   end
-# end
+
+describe 'player bowls a strike' do
+  it 'returns a score of 10' do
+    score = 10
+  end
+end
