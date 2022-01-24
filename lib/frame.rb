e092196ddaa27d3b3251ca29ea5d7285ccbdd808
SophieGowen <sophie.j.gowen@gmail.com>
2022-01-24 04:59:19 +0000

    Frames alternate, making a new frame / adding a second roll

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,8 +1,27 @@
 class Frame
+  attr_accessor :roll_two
+
   @@log = []
 
+  def initialize(no_pins)
+    @roll_one = no_pins
+    @roll_two = nil
+    @@log << self
+  end
+
   def self.fallen_pins(no_pins)
-    @@log << no_pins
-    @@log.sum
+    if @@log.empty? || @@log.last.is_complete?
+      self.new(no_pins)
+    else
+      @@log.last.roll_two = no_pins
+    end
+  end 
+  
+  def is_complete?
+    if @frame_no == 10
+      (@roll_two && (@roll_one + @roll_two < 10)) || (@roll_two && @roll_three)
+    else
+      @roll_two || @roll_one == 10 
+    end
   end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -3,12 +3,23 @@ require 'frame'
 describe Frame do
   before(:each) { Frame.class_variable_set :@@log, [] }
 
-  it 'returns a score of 1 after entering one pin' do
-    expect(Frame.fallen_pins(1)).to eq 1
-  end
+  describe '.fallen_pins' do
+    it 'creates a new frame on the first fallen pins' do
+      expect(Frame).to receive(:new).with 1
+      Frame.fallen_pins(1)
+    end
 
-  it 'returns a score of 2 after entering one pin twice' do
-    Frame.fallen_pins(1)
-    expect(Frame.fallen_pins(1)).to eq 2
+    it 'adds to the first frame with the second fallen pins' do
+      Frame.fallen_pins(1)
+      expect(Frame).to_not receive(:new).with 1
+      Frame.fallen_pins(1)
+    end
+
+    it 'creates a new frame on the third fallen pins' do
+      Frame.fallen_pins(1)
+      Frame.fallen_pins(1)
+      expect(Frame).to receive(:new).with 1
+      Frame.fallen_pins(1)
+    end
   end
 end
