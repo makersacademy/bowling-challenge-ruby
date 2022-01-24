3111096afa429d1dc85e68d978c6f39086253cd8
SophieGowen <sophie.j.gowen@gmail.com>
2022-01-24 03:39:46 +0000

    Maximum 10 pins can be entered

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,8 +1,16 @@
 class ScoreCard
-  @@total_pins = []
+  def initialize
+    @total_pins = []
+  end
 
   def enter_pins(no_pins)
-    @@total_pins << no_pins
-    @@total_pins.sum
+    return 'Invalid Entry' unless valid_entry?(no_pins)
+
+    @total_pins << no_pins
+    @total_pins.sum
+  end
+
+  def valid_entry?(no_pins)
+    no_pins <= 10
   end
 end
--- spec/score_card_spec.rb
+++ spec/score_card_spec.rb
@@ -1,8 +1,20 @@
 require 'scorecard'
 
 describe ScoreCard do
-  it 'returns a score of 2 after entering one pin twice' do
-    subject.enter_pins(1)
-    expect(subject.enter_pins(1)).to eq 2
+  let(:scorecard) { described_class.new }
+
+  describe '#enter_pins' do
+    it 'returns a score of 1 after entering one pin' do
+      expect(scorecard.enter_pins(1)).to eq 1
+    end
+
+    it 'returns a score of 2 after entering one pin twice' do
+      scorecard.enter_pins(1)
+      expect(scorecard.enter_pins(1)).to eq 2
+    end
+
+    it 'returns Invalid Entry if first pin entry is > 10' do
+      expect(scorecard.enter_pins(11)).to eq 'Invalid Entry'
+    end
   end
 end
