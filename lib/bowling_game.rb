d29d5b3e7c76e58bf3f3e0489982790cf2760957
Charlotte Buchan <cebuc@talktalk.net>
2022-02-26 13:29:26 +0000

    got test passing for a gutter ball

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,5 +1,18 @@
 class Bowlinggame
 
+  def initialize 
+    @rolls = []
+  end
 
-  
+  def pins(pin_number)
+    @rolls << pin_number
+
+  end
+
+  def score
+
+    0 
+
+  end
+
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -2,4 +2,21 @@ require_relative '../lib/bowling_game'
 
 describe Bowlinggame do
 
+  describe 'bowling game behavior' do
+    it "can roll a gutter game" do 
+      game = Bowlinggame.new
+      game.pins 0
+      expect(subject.score).to eq 0
+    end
+
+  end
+
+
+  # And then one for a gutter game
+  # then next a score
+  # This will be a new instance of the class whichh will equal 0
+  # Then, if test uis correct think about getting the score set up etc.
+
+
+
 end
