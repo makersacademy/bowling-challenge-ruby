022c01d9a465c8baa5d3c37482d9654f0bf79962
Eddie Arnold <eddiearnoldz@me.com>
2022-02-26 19:07:04 +0000

    working and tested with examples but feel the 10th frame is slightly shoehorned in. Could do with more attention. Also probably seperating frame into seperate class??

--- lib/game_one.rb
+++ lib/game_one.rb
@@ -1,43 +1,46 @@
-class GameOne
+# frozen_string_literal: true
 
-  attr_reader :scorecard, :frames_total, :roll_counter, :frame_counter, :spare, :strike, :bonus, :double_strike
+class GameOne
+  
+  attr_reader :scorecard, :frames_total, :roll_counter, :frame_counter, :spare, :strike, :bonus, :double_strike,
+              :game_total
 
   def initialize
-    @scorecard = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    @frames_total = [0,0,0,0,0,0,0,0,0,0]
-    @bonus = [0,0,0,0,0,0,0,0,0,0]
+    @scorecard = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    @frames_total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    @bonus = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     @roll_counter = 1
     @frame_counter = 1
     @spare = false
     @strike = false
     @double_strike = false
+    @game_total = 0
   end
 
   def roll(score)
+    return spare_finale(score) if tenth_frame_is_spare
+    return tenth_frame_first_roll(score) if tenth_frame_is_strike_first_go
 
-    double_strike_bonus(score)
-    strike_bonus(score)
-    spare_bonus(score)
+    tenth_frame_second_roll(score) if tenth_frame_is_strike_second_go
+    return tenth_frame_third_roll(score) if tenth_frame_is_strike_third_go
 
-    if first_roll_of_frame
-      score == 10 ? its_a_strike(score) : normal_first_role(score)
-     else frame_total(score) == 10 ? its_a_spare(score) : normal_second_role(score)
-     end
-  p "roll count: #{@roll_counter}"
-    p "spare #{@spare}"
-    p "strike: #{@strike}"
-    p "double strike: #{@double_strike}"
-    p "bonus: #{@bonus}"
-    p "frames total: #{@frames_total}"
-
-  end
-
-  def grand_total
-    @frames_total.sum + @bonus.sum
+    bonus_points(score)
+    normal_frames(score)
+    total
   end
 
   private
 
+  # normal behaviour when frame is 1..9
+  def normal_frames(score)
+    if first_roll_of_frame
+      score == 10 ? its_a_strike(score) : normal_first_roll(score)
+    else
+      frame_total(score) == 10 ? its_a_spare(score) : normal_second_role(score)
+    end
+  end
+
+  # common methods that have been renamed and occur each roll
   def frame_total(score)
     @frames_total[@frame_counter - 1] + score
   end
@@ -45,12 +48,15 @@ class GameOne
   def mark_roll_score(score)
     @scorecard[@roll_counter - 1] += score
   end
+
   def add_score_to_frame(score)
     @frames_total[@frame_counter - 1] += score
   end
+
   def end_of_frame
     @frame_counter += 1
   end
+
   def next_roll
     @roll_counter += 1
   end
@@ -59,18 +65,24 @@ class GameOne
     @roll_counter.odd?
   end
 
+  def total
+    @game_total = @frames_total.sum + @bonus.sum
+  end
+
+  # methods grouped together by different potential role scenarios
+  # a strike
   def its_a_strike(score)
     mark_roll_score(score)
     add_score_to_frame(score)
-    next_roll
-    next_roll
+    2.times{ next_roll }
     end_of_frame
     @double_strike = true if @strike == true
     @spare = false
     @strike = true
   end
 
-  def normal_first_role(score)
+  # normal first_roll
+  def normal_first_roll(score)
     mark_roll_score(score)
     add_score_to_frame(score)
     next_roll
@@ -78,6 +90,7 @@ class GameOne
     @double_strike = false
   end
 
+  # spare on second roll
   def its_a_spare(score)
     mark_roll_score(score)
     add_score_to_frame(score)
@@ -87,12 +100,20 @@ class GameOne
     @strike = false
   end
 
+  # normal second roll
   def normal_second_role(score)
     mark_roll_score(score)
     add_score_to_frame(score)
     next_roll
     end_of_frame
     @strike = false
+  end
+
+  #  methods extracted that provide functionality for bonuses
+  def bonus_points(score)
+    double_strike_bonus(score)
+    strike_bonus(score)
+    spare_bonus(score)
   end
 
   def add_score_to_bonus(score)
@@ -104,14 +125,54 @@ class GameOne
   end
 
   def strike_bonus(score)
-    # add_score_to_bonus(score) if @strike == true && @scorecard[@roll_counter - 3] && @scorecard[@roll_counter - 5] == 10 && score == 10
-    add_score_to_bonus(score) if @strike == true 
+    add_score_to_bonus(score) if @strike == true
   end
 
   def double_strike_bonus(score)
-    add_score_to_bonus(score) if @double_strike == true 
+    add_score_to_bonus(score) if @double_strike == true
   end
 
+  # last frame is a spare
+  def tenth_frame_is_spare
+    (@scorecard[18] + @scorecard[19]) == 10 && @scorecard[19] != 0
+  end
 
+  def spare_finale(score)
+    mark_roll_score(score)
+    @frames_total[@frame_counter - 2] += score
+    total
+  end
 
+  # last frame is a strike
+  def tenth_frame_is_strike_first_go
+    @scorecard[18] == 10 && @roll_counter == 20
+  end
+
+  def tenth_frame_first_roll(score)
+    mark_roll_score(score)
+    @bonus[@frame_counter - 3] += score
+    add_score_to_frame(score)
+    next_roll
+  end
+
+  def tenth_frame_is_strike_second_go
+    @scorecard[18] == 10 && @roll_counter == 21
+  end
+
+  def tenth_frame_second_roll(score)
+    @scorecard[@roll_counter - 2] += score
+    @frames_total[@frame_counter - 2] += score
+    end_of_frame
+    next_roll
+  end
+
+  def tenth_frame_is_strike_third_go
+    @scorecard[18] == 10 && @roll_counter == 22
+  end
+
+  def tenth_frame_third_roll(score)
+    @scorecard[@roll_counter - 2] += score
+    @frames_total[@frame_counter - 3] += score
+    total
+  end
 end
--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 require 'game_one'
 
-describe "a bowling match" do
+describe 'a bowling match' do
   let(:game) { GameOne.new }
-  it "a player rolls 5 strikes in a row" do
+  it 'a player rolls 5 strikes in a row' do
     game.roll(10)
     game.roll(10)
     game.roll(10)
     game.roll(10)
     game.roll(10)
-    expect(game.grand_total).to eq 120
+    expect(game.game_total).to eq 120
   end
-  it "a player scores 5 gutter frames in a row" do
-    20.times{ game.roll(0) }
-    expect(game.grand_total).to eq 0
+  it 'a player scores 5 gutter frames in a row' do
+    20.times { game.roll(0) }
+    expect(game.game_total).to eq 0
   end
   it "a player scores 5 4's and a 2 and a 10" do
     game.roll(4)
@@ -22,7 +24,7 @@ describe "a bowling match" do
     game.roll(4)
     game.roll(2)
     game.roll(10)
-    expect(game.grand_total).to eq 32
+    expect(game.game_total).to eq 32
   end
   it "a player scores 5 4's, a 2, a strike, a 5 and a 2" do
     game.roll(4)
@@ -35,10 +37,10 @@ describe "a bowling match" do
     game.roll(5)
     game.roll(2)
     expect(game.roll_counter).to eq 11
-    expect(game.grand_total).to eq 46
+    expect(game.game_total).to eq 46
   end
 
-  it "a player scores 10,10,3,7,3,7,10,10" do
+  it 'a player scores 10,10,3,7,3,7,10,10' do
     game.roll(10)
     game.roll(10)
     game.roll(3)
@@ -47,7 +49,6 @@ describe "a bowling match" do
     game.roll(7)
     game.roll(10)
     game.roll(10)
-    expect(game.grand_total).to eq 106
+    expect(game.game_total).to eq 106
   end
-
 end
