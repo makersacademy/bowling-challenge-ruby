71948142f498158145791a777b635d427d7b939b
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-02-26 23:01:09 +0000

    Restructured tests

--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -71,7 +71,7 @@ describe Frame do
     end
   end
 
-  context 'invalid input' do
+  context 'input validation' do
     it 'does not allow rolls less than 0' do
       expect { frame.log_roll(-1) }.to raise_error('Pins downed must be between 0 and 10')
     end
@@ -97,33 +97,37 @@ describe Frame do
       expect { frame.log_roll(5) }.to raise_error('Frame complete. Cannot roll again')
     end
   end
-
-  context 'no rolls' do
-    it 'there is no roll' do
-      expect(frame.roll_score(1)).to be_nil
+  describe "#log_roll" do
+    context 'no rolls' do
+      it 'there is no roll' do
+        expect(frame.roll_score(1)).to be_nil
+      end
     end
+
+    context 'No bonuses' do
+      it 'logs the first roll' do
+        frame.log_roll(6)
+        expect(frame.roll_score(1)).to eq 6
+      end
+
+      it 'logs the second roll' do
+        frame.log_roll(6)
+        frame.log_roll(3)
+        expect(frame.roll_score(2)).to eq 3
+        expect(frame.roll_score(1)).to eq 6
+      end
+    end
+
+    context 'With bonuses' do
+      it 'scores a spare on the second roll if first roll zero' do
+        frame.log_roll(0)
+        frame.log_roll(10)
+        expect(frame.roll_score(2)).to eq 10
+        expect(frame.spare_frame?).to be true
+      end
+    end
+    
   end
 
-  context 'No bonuses' do
-    it 'logs the first roll' do
-      frame.log_roll(6)
-      expect(frame.roll_score(1)).to eq 6
-    end
 
-    it 'logs the second roll' do
-      frame.log_roll(6)
-      frame.log_roll(3)
-      expect(frame.roll_score(2)).to eq 3
-      expect(frame.roll_score(1)).to eq 6
-    end
-  end
-
-  context 'With bonuses' do
-    it 'scores a spare on the second roll if first roll zero' do
-      frame.log_roll(0)
-      frame.log_roll(10)
-      expect(frame.roll_score(2)).to eq 10
-      expect(frame.spare_frame?).to be true
-    end
-  end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -13,156 +13,173 @@ describe ScoreCard do
 
   end
 
-  it 'gives frame number at the start of the game' do
-    expect(scorecard.current_frame_number).to eq 0
-  end
-
-  it 'gives score at the start of the game' do
-    expect(scorecard.score).to eq 0
-  end
-
-  it 'gives score after first frame, first roll, no strike' do
-    scorecard.log_roll(7)
-    expect(scorecard.score).to eq 0
-    expect(scorecard.current_frame_number).to eq 1
-  end
-
-  it 'gives score after first frame, first roll, no strike' do
-    scorecard.log_roll(8)
-    expect(scorecard.score).to eq 0
-  end
-
-  it 'gives score after first frame, second roll, no spare' do
-    scorecard.log_roll(7)
-    scorecard.log_roll(2)
-    expect(scorecard.score).to eq 9
-  end
-
-  it 'first frame and a spare' do
-    scorecard.log_roll(7)
-    scorecard.log_roll(3)
-    expect(scorecard.score).to eq 0
-  end
-
-  it 'second frame first roll, no bonus' do
-    scorecard.log_roll(7)
-    scorecard.log_roll(2)
-    scorecard.log_roll(4)
-
-    expect(scorecard.score).to eq 9
-  end
-
-  it 'second frame second roll, no bonus' do
-    scorecard.log_roll(7)
-    scorecard.log_roll(2)
-    scorecard.log_roll(4)
-    scorecard.log_roll(2)
-    expect(scorecard.score).to eq 15
-  end
-
-  it 'second frame first roll, spare previous' do
-    scorecard.log_roll(7)
-    scorecard.log_roll(3)
-    scorecard.log_roll(4)
-
-    expect(scorecard.score).to eq 14
-  end
-
-  it 'second frame first roll, strike on previous frame' do
-    scorecard.log_roll(10)
-    scorecard.log_roll(4)
-
-    expect(scorecard.score).to eq 0
-  end
-
-  it 'second frame second roll, strike on previous frame' do
-    scorecard.log_roll(10)
-    scorecard.log_roll(4)
-    scorecard.log_roll(5)
-    expect(scorecard.score).to eq 28
-  end
-
-  it 'third frame 2 non bonus rolls, second frame strike, first frame strike' do
-    scorecard.log_roll(10)
-    scorecard.log_roll(10)
-    scorecard.log_roll(4)
-    scorecard.log_roll(5)
-    expect(scorecard.score).to eq 52
-  end
-
-  it 'second frame second roll is spare, strike on previous frame' do
-    scorecard.log_roll(10)
-    scorecard.log_roll(3)
-    scorecard.log_roll(7)
-    expect(scorecard.score).to eq 20
-  end
-
-
-  it 'is a full game no end bonus' do
-    scorecard.log_roll(3)
-    scorecard.log_roll(1)
-    scorecard.log_roll(6)
-    scorecard.log_roll(3)
-    scorecard.log_roll(2)
-    scorecard.log_roll(1)
-    scorecard.log_roll(0)
-    scorecard.log_roll(0)
-    scorecard.log_roll(8)
-    scorecard.log_roll(0)
-    scorecard.log_roll(0)
-    scorecard.log_roll(9)
-    scorecard.log_roll(10)
-    scorecard.log_roll(5)
-    scorecard.log_roll(5)
-    scorecard.log_roll(8)
-    scorecard.log_roll(1)
-    scorecard.log_roll(1)
-    scorecard.log_roll(3)
-
-    expect(scorecard.score).to eq 84
-  end
-
-  it "You're awesome, strikes in every frame" do
-    12.times do
-      scorecard.log_roll(10)
-    end
-    expect(scorecard.score).to eq 300
-  end
-
-  it 'a spare and strike on the last frame of the game' do
-    9.times do
-      scorecard.log_roll(10)
+  describe "#current_frame_number" do
+    it 'gives frame number at the start of the game' do
+      expect(scorecard.current_frame_number).to eq 0
     end
 
-    scorecard.log_roll(7)
-    scorecard.log_roll(3) 
-    scorecard.log_roll(10)
+    it 'gives frame number at the end of a game' do
+      12.times do
+        scorecard.log_roll(10)
+      end
+      expect(scorecard.current_frame_number).to eq 10
+    end
 
-    expect(scorecard.score).to eq 277
+    it 'gives frame number mid game' do
+      5.times do
+        scorecard.log_roll(10)
+      end
+      expect(scorecard.current_frame_number).to eq 5
+    end
   end
+  
+  describe "#score" do
+    it 'gives score at the start of the game' do
+      expect(scorecard.score).to eq 0
+    end
 
-  it 'having a bad day.. Gutter Game!' do
-    20.times do
+    it 'gives score after first frame, first roll, no strike' do
+      scorecard.log_roll(7)
+      expect(scorecard.score).to eq 0
+    end
+
+    it 'gives score after first frame, first roll, no strike' do
+      scorecard.log_roll(8)
+      expect(scorecard.score).to eq 0
+    end
+
+    it 'gives score after first frame, second roll, no spare' do
+      scorecard.log_roll(7)
+      scorecard.log_roll(2)
+      expect(scorecard.score).to eq 9
+    end
+
+    it 'first frame and a spare' do
+      scorecard.log_roll(7)
+      scorecard.log_roll(3)
+      expect(scorecard.score).to eq 0
+    end
+
+    it 'second frame first roll, no bonus' do
+      scorecard.log_roll(7)
+      scorecard.log_roll(2)
+      scorecard.log_roll(4)
+
+      expect(scorecard.score).to eq 9
+    end
+
+    it 'second frame second roll, no bonus' do
+      scorecard.log_roll(7)
+      scorecard.log_roll(2)
+      scorecard.log_roll(4)
+      scorecard.log_roll(2)
+      expect(scorecard.score).to eq 15
+    end
+
+    it 'second frame first roll, spare previous' do
+      scorecard.log_roll(7)
+      scorecard.log_roll(3)
+      scorecard.log_roll(4)
+
+      expect(scorecard.score).to eq 14
+    end
+
+    it 'second frame first roll, strike on previous frame' do
+      scorecard.log_roll(10)
+      scorecard.log_roll(4)
+
+      expect(scorecard.score).to eq 0
+    end
+
+    it 'second frame second roll, strike on previous frame' do
+      scorecard.log_roll(10)
+      scorecard.log_roll(4)
+      scorecard.log_roll(5)
+      expect(scorecard.score).to eq 28
+    end
+
+    it 'third frame 2 non bonus rolls, second frame strike, first frame strike' do
+      scorecard.log_roll(10)
+      scorecard.log_roll(10)
+      scorecard.log_roll(4)
+      scorecard.log_roll(5)
+      expect(scorecard.score).to eq 52
+    end
+
+    it 'second frame second roll is spare, strike on previous frame' do
+      scorecard.log_roll(10)
+      scorecard.log_roll(3)
+      scorecard.log_roll(7)
+      expect(scorecard.score).to eq 20
+    end
+
+
+    it 'is a full game no end bonus' do
+      scorecard.log_roll(3)
+      scorecard.log_roll(1)
+      scorecard.log_roll(6)
+      scorecard.log_roll(3)
+      scorecard.log_roll(2)
+      scorecard.log_roll(1)
       scorecard.log_roll(0)
-    end
-    expect(scorecard.score).to eq 0
-  end
-
-  it 'almost all strikes and spares, full game' do
-    5.times do
+      scorecard.log_roll(0)
+      scorecard.log_roll(8)
+      scorecard.log_roll(0)
+      scorecard.log_roll(0)
+      scorecard.log_roll(9)
       scorecard.log_roll(10)
-    end
-    scorecard.log_roll(7)
-    scorecard.log_roll(3)
-    scorecard.log_roll(10)
-    scorecard.log_roll(7)
-    scorecard.log_roll(1)
-    scorecard.log_roll(4)
-    scorecard.log_roll(6)
-    scorecard.log_roll(10)
-    scorecard.log_roll(10)
-    scorecard.log_roll(10)
+      scorecard.log_roll(5)
+      scorecard.log_roll(5)
+      scorecard.log_roll(8)
+      scorecard.log_roll(1)
+      scorecard.log_roll(1)
+      scorecard.log_roll(3)
 
-    expect(scorecard.score).to eq 233
+      expect(scorecard.score).to eq 84
+    end
+
+    it "You're awesome, strikes in every frame" do
+      12.times do
+        scorecard.log_roll(10)
+      end
+      expect(scorecard.score).to eq 300
+    end
+
+    it 'a spare and strike on the last frame of the game' do
+      9.times do
+        scorecard.log_roll(10)
+      end
+
+      scorecard.log_roll(7)
+      scorecard.log_roll(3) 
+      scorecard.log_roll(10)
+
+      expect(scorecard.score).to eq 277
+    end
+
+    it 'having a bad day.. Gutter Game!' do
+      20.times do
+        scorecard.log_roll(0)
+      end
+      expect(scorecard.score).to eq 0
+    end
+
+    it 'almost all strikes and spares, full game' do
+      5.times do
+        scorecard.log_roll(10)
+      end
+      scorecard.log_roll(7)
+      scorecard.log_roll(3)
+      scorecard.log_roll(10)
+      scorecard.log_roll(7)
+      scorecard.log_roll(1)
+      scorecard.log_roll(4)
+      scorecard.log_roll(6)
+      scorecard.log_roll(10)
+      scorecard.log_roll(10)
+      scorecard.log_roll(10)
+
+      expect(scorecard.score).to eq 233
+    end
   end
 end
