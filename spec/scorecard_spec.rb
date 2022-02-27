e6aa67a79f1b9f2ce377dd8f03b384471fee08e3
Sarah Moffat <sarah.moffat5@gmail.com>
2022-02-27 19:42:19 +0000

    Basic functionality passing

--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,59 +1,58 @@
 require 'scorecard'
 
 describe Scorecard do
-    let(:scorecard) { Scorecard.new }
+  let(:scorecard) { Scorecard.new }
 
-    describe '#initialize' do
-      it 'initializes a new instance of scorecard' do
-        expect(scorecard).to be_a Scorecard
-      end
+  describe '#initialize' do
+    it 'initializes a new instance of scorecard' do
+      expect(scorecard).to be_a Scorecard
+    end
+  end
+
+  describe '#score' do
+    it 'can score a single frame where both rolls equal 0' do
+      2.times { scorecard.roll(0) }
+      expect(scorecard.score).to eq 0
     end
 
-    describe '#score' do
-      it 'can score a single frame where both rolls equal 0' do
-        2.times { scorecard.roll(0) } 
-        expect(scorecard.score).to eq 0
-      end
-
-      it 'can roll all 1s' do
-        20.times { scorecard.roll(1) }
-        expect(scorecard.score).to eq 20
-      end
-
-      it 'can roll all 2s' do
-        20.times { scorecard.roll(2) }
-        expect(scorecard.score).to eq 40
-      end
-
-      it 'can roll a gutter game' do
-        20.times { scorecard.roll(0) }
-        expect(scorecard.score).to eq 0
-      end
-
-      it 'can score a perfect game of 300 points' do
-        12.times { scorecard.roll(10) }
-        expect(scorecard.perfect_game).to eq 300
-      end
-
+    it 'can roll all 1s' do
+      20.times { scorecard.roll(1) }
+      expect(scorecard.score).to eq 20
     end
 
-    describe '#strike' do
-      it 'returns true when strike' do
-        expect(scorecard.strike?([10])).to eq true
-      end
-
-      it 'returns false when not strike' do
-        expect(scorecard.strike?([5, 3])).to eq false
-      end
+    it 'can roll all 2s' do
+      20.times { scorecard.roll(2) }
+      expect(scorecard.score).to eq 40
     end
 
-    describe '#spare' do
-      it 'returns true when spare' do
-        expect(scorecard.spare?([5, 5])).to eq true
-      end
-
-      it 'returns false when not spare' do
-        expect(scorecard.spare?([5, 3])).to eq false
-      end
+    it 'can roll a gutter game' do
+      20.times { scorecard.roll(0) }
+      expect(scorecard.score).to eq 0
     end
+
+    it 'can score a perfect game of 300 points' do
+      12.times { scorecard.roll(10) }
+      expect(scorecard.perfect_game).to eq 300
+    end
+  end
+
+  describe '#strike' do
+    it 'returns true when strike' do
+      expect(scorecard.strike?([10])).to eq true
+    end
+
+    it 'returns false when not strike' do
+      expect(scorecard.strike?([5, 3])).to eq false
+    end
+  end
+
+  describe '#spare' do
+    it 'returns true when spare' do
+      expect(scorecard.spare?([5, 5])).to eq true
+    end
+
+    it 'returns false when not spare' do
+      expect(scorecard.spare?([5, 3])).to eq false
+    end
+  end
 end
