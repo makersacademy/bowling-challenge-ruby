e5855973fa63fc0f13f62d5be59a6a287222cebe
Jack <winterschladen.j@gmail.com>
2022-02-27 14:33:22 +0000

    Initial specs and code, correctly calculates non-perfect games

---
+++ lib/bowling.rb
@@ -0,0 +1,34 @@
+class Bowling
+
+  attr_reader :total_score, :frame
+
+  def initialize
+    @total_score = 0
+    @frame = []
+  end
+
+  def total_frames(frame_scores)
+    raise "Minimum of 10 frames must be entered" if frame_scores.count < 10
+    frame_scores.each.with_index do |frame, index|
+      #  @total_score += frame.sum if frame[0] != 10 && frame[1] != 10
+       if frame[0] == 10
+        frame_scores[index] = [frame[0],(frame_scores[index+1].sum)]
+       elsif frame[0] != 10 && frame.sum == 10
+         frame_scores[index] = [frame[0],(frame[1] + (frame_scores[index+1][0]))]
+       end
+    end
+    p frame_scores
+    frame_scores.each { |scores| @total_score += scores.sum }
+  end
+
+  # def frame_score(roll_1, roll_2)
+  #   @total_score += (roll_1 + roll_2) unless roll_1 + roll_2 == 10
+  #   if roll_1 == 10
+
+  #   end
+  #   @frame << 1
+  # end
+
+
+
+end
---
+++ spec/bowling_spec.rb
@@ -0,0 +1,73 @@
+require 'bowling'
+
+describe Bowling do
+
+  let(:game) { described_class.new }
+
+  describe '#total_frames' do
+      it 'User must enter scores for a minimum of 10 frames' do
+        expect { game.total_frames([[9,0],[9,0],[9,0],[9,0],[9,0],[9,0],[9,0],[9,0],[9,0]]) }.to raise_error "Minimum of 10 frames must be entered"
+      end
+  end
+
+  describe '#total_score' do
+    context 'when no strikes or spares' do
+      before do
+        @frame_scores = [[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0]]
+      end
+      it 'returns the correct sum of all frames' do
+        game.total_frames(@frame_scores)
+        expect(game.total_score).to eq 10
+      end
+    end
+    context 'when player gets a strike' do
+      before do
+        @frame_scores = [[10,0],[1,1],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0]]
+      end
+      it 'adds bonus scores onto that frame equal to both scores from the next frame' do
+        game.total_frames(@frame_scores)
+        expect(game.total_score).to eq 22
+      end
+    end
+    context 'when player gets a spare' do
+      before do
+        @frame_scores = [[1,9],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0]]
+      end
+      it 'adds bonus scores onto that frame equal to the first score from the next frame' do
+        game.total_frames(@frame_scores)
+        expect(game.total_score).to eq 20
+      end
+    end
+    context 'when player has a perfect game' do
+      before do
+        @frame_scores = [[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0]]
+      end
+      it 'returns the maximum score of 300' do
+        game.total_frames(@frame_scores)
+        expect(game.total_score).to eq 300
+      end
+    end
+  end
+  # describe '#frame_score' do
+  #   it 'saves the score from the two rolls in a frame to the total score' do
+  #     game.frame_score(3, 2)
+  #     expect(game.total_score).to eq 5
+  #   end
+  #   it 'current frame increases by 1 after each score is entered' do
+  #     expect(game.frame).to eq 1
+  #     game.frame_score(2, 4)
+  #     expect(game.frame).to eq 2
+  #   end
+  #   it 'if a player gets a strike or a spare, the total score is not immediately updated' do
+  #     game.frame_score(10, 0)
+  #     expect(game.total_score).to eq 0
+  #   end
+  #   it 'if a player gets a strike, the following frame_score will be doubled' do
+  #     game.frame_score(10, 0)
+  #     expect(game.frame).to eq 2
+  #     expect(game.total_score).to eq 0
+  #     game.frame_score(2, 3)
+  #     expect(game.total_score).to eq 20
+  #   end
+
+end
