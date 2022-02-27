9466bde6fedb719c30dc1229dbb77be31cbbf155
Ed <ed321@hotmail.co.uk>
2022-02-27 16:46:01 +0000

    Totals scores and adds bonus for a spare

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,22 @@ class Game
 
     def initialize
       @total = 0
+      @spare = false
     end
 
 
   def score(game_result)
+    game_result.each do |frame, pins|
+      if @spare == true
+        @total += pins[0]
+        @spare = false
+      end
+      if pins.sum == 10
+        @spare = true
+      end
+      @total += pins.sum
+      
+    end
     @total
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,4 +11,23 @@ describe Game do
     expect(game.score(game_result)).to eq 0
   end
 
+  it 'keeps track of a score with no bonuses' do
+    game_result = { frame1: [1,0], frame2: [1,0], frame3: [1,0],
+                    frame4: [1,0], frame5: [1,0], frame6: [1,0],
+                    frame7: [1,0], frame8: [1,0], frame9: [1,0],
+                    frame10: [1,0,0]
+                  }
+    expect(game.score(game_result)).to eq 10
+  end
+
+  it 'adds a bonus for a spare' do
+    game_result = { frame1: [0,0], frame2: [5,5], frame3: [3,0],
+                    frame4: [0,0], frame5: [0,0], frame6: [0,0],
+                    frame7: [0,0], frame8: [0,0], frame9: [0,0],
+                    frame10: [0,0,0]
+                  }
+    expect(game.score(game_result)).to eq 16
+  end
+
 end
+
