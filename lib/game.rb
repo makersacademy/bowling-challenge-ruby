ea5325d92dc48dc42933a7636b103c5e7497aa11
Ed <ed321@hotmail.co.uk>
2022-02-27 19:17:00 +0000

    Changed the bonus calculation whi ch was not working correctly when bonuses occurred consecutively

--- lib/game.rb
+++ lib/game.rb
@@ -2,23 +2,40 @@ class Game
 
     def initialize
       @total = 0
-      @spare = false
     end
 
 
   def score(game_result)
-    game_result.each do |frame, pins|
-      if @spare == true
-        @total += pins[0]
-        @spare = false
+    game_result.each_with_index do |frame, idx|
+      if strike?(frame)
+        add_strike_bonus(game_result,idx)
+      elsif spare?(frame)
+        add_spare_bonus(game_result, idx)
+      else
+        @total += frame[0] + frame[1]
       end
-      if pins.sum == 10
-        @spare = true
-      end
-      @total += pins.sum
-      
     end
     @total
+  end
+
+  private
+
+  def add_spare_bonus(game_result, idx)
+    next_frame = game_result[idx + 1]
+    @total += 10 + next_frame[0]
+  end
+
+  def add_strike_bonus(game_result, idx)
+    next_frame = game_result[idx + 1]
+    @total += 10 + next_frame[0] + next_frame[1]
+  end
+
+  def strike?(pins)
+    pins[0] == 10
+  end
+
+  def spare?(pins)
+    pins[0] + pins[1] == 10
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,30 +3,70 @@ require_relative '../lib/game.rb'
 describe Game do
   subject(:game) { described_class.new }
   it 'plays 10 gutter balls' do
-    game_result = { frame1: [0,0], frame2: [0,0], frame3: [0,0],
-                    frame4: [0,0], frame5: [0,0], frame6: [0,0],
-                    frame7: [0,0], frame8: [0,0], frame9: [0,0],
-                    frame10: [0,0,0]
-                  }
+    game_result = [ [0,0], [0,0], [0,0],
+                    [0,0], [0,0], [0,0],
+                    [0,0], [0,0], [0,0],
+                    [0,0,0]
+                  ]
     expect(game.score(game_result)).to eq 0
   end
 
   it 'keeps track of a score with no bonuses' do
-    game_result = { frame1: [1,0], frame2: [1,0], frame3: [1,0],
-                    frame4: [1,0], frame5: [1,0], frame6: [1,0],
-                    frame7: [1,0], frame8: [1,0], frame9: [1,0],
-                    frame10: [1,0,0]
-                  }
+    game_result = [ [1,0], [1,0], [1,0],
+                    [1,0], [1,0], [1,0],
+                    [1,0], [1,0], [1,0],
+                    [1,0,0]
+                  ]
     expect(game.score(game_result)).to eq 10
   end
 
   it 'adds a bonus for a spare' do
-    game_result = { frame1: [0,0], frame2: [5,5], frame3: [3,0],
-                    frame4: [0,0], frame5: [0,0], frame6: [0,0],
-                    frame7: [0,0], frame8: [0,0], frame9: [0,0],
-                    frame10: [0,0,0]
-                  }
-    expect(game.score(game_result)).to eq 16
+    game_result = [ [0,0], [5,5], [3,0],
+                    [0,0], [0,0], [0,0],
+                    [0,0], [0,0], [0,0],
+                    [0,0,0]
+  ]
+    expect(game.score(game_result)).to eq (5 + 5 + 3) + 3
+  end
+
+  it 'adds a bonus 2 consequetive spares' do
+    game_result = [ [0,0], [5,5], [3,7],
+                    [3,0], [0,0], [0,0],
+                    [0,0], [0,0], [0,0],
+                    [0,0,0]
+  ]
+    expect(game.score(game_result)).to eq (5 + 5 + 3) + (3 + 7 + 3) + 3
+  end
+
+  it 'adds a bonus for a strike' do
+    game_result = [ [0,0], [10,0], [3,5],
+                  [0,0], [0,0], [0,0],
+                  [0,0], [0,0], [0,0],
+                  [0,0,0]
+  ]
+    expect(game.score(game_result)).to eq (10 + 0 + 3 + 5) + 3 + 5
+
+  end
+
+  it 'checks 3 strikes in a row' do
+    game_result = [ [10,0], [10,0], [10,0],
+                  [0,0] ]
+    expect(game.score(game_result)).to eq (10 + 0 + 10) + (10 + 0 + 10) + (10 + 0 + 0)
+
+  end
+
+  xit 'totals a spare on the final frame' do
+    game_result = [ [5, 5, 3] ]
+    expect(game.score(game_result)).to eq (5 + 5 + 3) + 3
+  end
+
+  xit 'scores a perfect game' do
+    game_result = [ [10,0], [10,0], [10,0],
+                  [10,0], [10,0], [10,0],
+                  [10,0], [10,0], [10,0],
+                  [10,10,10]
+  ]
+    expect(game.score(game_result)).to eq 300
   end
 
 end
