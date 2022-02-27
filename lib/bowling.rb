177d098230bae737bef6c6b37b93d64c366656de
Jack <winterschladen.j@gmail.com>
2022-02-27 15:13:30 +0000

    Created methods to refactor code

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,5 +1,4 @@
 class Bowling
-
   attr_reader :total_score, :frame
 
   def initialize
@@ -8,30 +7,33 @@ class Bowling
   end
 
   def total_frames(frame_scores)
-    raise "Minimum of 10 frames must be entered" if frame_scores.count < 10
+    raise 'Minimum of 10 frames must be entered' if frame_scores.count < 10
+    # p frame_scores
     frame_scores.map.with_index do |frame, index|
-      unless index == 9 || index == 10 || index == 11
-        if frame[0] == 10 && frame_scores[index+1][0] == 10
-          frame_scores[index] = [frame[0],((frame_scores[index+1].sum)+(frame_scores[index+2].sum))]
-        elsif frame[0] == 10 
-          frame_scores[index] = [frame[0],(frame_scores[index+1].sum)]
-        elsif frame[0] != 10 && frame.sum == 10
-          frame_scores[index] = [frame[0],(frame[1] + (frame_scores[index+1][0]))]
+      unless [9, 10, 11].include?(index)
+        if back_to_back_strike?(frame, frame_scores, index)
+          frame_scores[index] = [frame[0], (frame_scores[index + 1].sum + frame_scores[index + 2].sum)]
+        elsif strike?(frame)
+          frame_scores[index] = [frame[0], frame_scores[index + 1].sum]
+        elsif spare?(frame)
+          frame_scores[index] = [frame[0], (frame[1] + (frame_scores[index + 1][0]))]
         end
       end
     end
-    p frame_scores
+    # p frame_scores
     frame_scores.each { |scores| @total_score += scores.sum }
   end
 
-  # def frame_score(roll_1, roll_2)
-  #   @total_score += (roll_1 + roll_2) unless roll_1 + roll_2 == 10
-  #   if roll_1 == 10
+  def strike?(frame)
+    frame[0] == 10
+  end
 
-  #   end
-  #   @frame << 1
-  # end
+  def back_to_back_strike?(frame, frame_scores, index)
+    frame[0] == 10 && frame_scores[index + 1][0] == 10
+  end
 
-
-
+  def spare?(frame)
+    frame[0] != 10 && frame.sum == 10
+  end
+  
 end
