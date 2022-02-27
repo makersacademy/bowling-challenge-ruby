cea20b04199cfcbf24820731275f10b3116aa9f0
Dashiell Boyles <dashiellboyles@Dashiells-MacBook-Air.local>
2022-02-27 18:12:03 +0000

    added strike and spare methods

--- lib/bowling_score.rb
+++ lib/bowling_score.rb
@@ -1,7 +1,49 @@
 class BowlingScore
 
-  def scorecard
-    
+  def initialize
+    @rolls = []
+  end
+
+  def play(pins)
+    @rolls << pins
+  end
+
+  def score
+    result = 0
+    number_of_goes = 0
+    10.times do
+      if spare?(number_of_goes)
+        result += spare_score(number_of_goes)
+        number_of_goes += 2
+      elsif strike?(number_of_goes)
+        result += strike_score(number_of_goes)
+        number_of_goes += 1
+      else
+        result += frame_score(number_of_goes)
+        number_of_goes += 2
+      end
+    end
+    result
+  end
+
+  def strike?(number_of_goes)
+    @rolls[number_of_goes] == 10
+  end
+
+  def strike_score(number_of_goes)
+    @rolls[number_of_goes] + @rolls[number_of_goes + 1] + @rolls[number_of_goes + 2]
+  end
+  
+  def spare?(number_of_goes)
+    @rolls[number_of_goes] + @rolls[number_of_goes + 1] == 10
+  end
+
+  def spare_score(number_of_goes)
+    @rolls[number_of_goes] + @rolls[number_of_goes + 1] + @rolls[number_of_goes + 2]
+  end
+
+  def frame_score(number_of_goes)
+    @rolls[number_of_goes] + @rolls[number_of_goes + 1]
   end
 
 end
--- spec/bowling_score_spec.rb
+++ spec/bowling_score_spec.rb
@@ -1,10 +1,31 @@
 require_relative '../lib/bowling_score'
 
 describe 'BowlingScore' do
-  it 'can create a scorecard' do
-    score = BowlingScore.new 
+
+  before do
+    @game = BowlingScore.new
   end
 
+  it 'can roll all ones' do
+    20.times{@game.play(1)}
+    expect(@game.score).to eq 20
+  end
+
+  it 'can roll a spare' do
+    @game.play 9
+    @game.play 1
+    @game.play 5
+    17.times{@game.play(0)}
+    expect(@game.score).to eq 20 
+  end
+
+  it 'can roll a strike' do
+    @game.play 10
+    @game.play 4
+    @game.play 3
+    16.times{@game.play 0}
+    expect(@game.score).to eq 24 
+  end
 
 end
 
