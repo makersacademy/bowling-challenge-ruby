44678acc1a4f5f364ceea4d8776c5f8601e6b2d4
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-02-28 01:45:54 +0000

    Created tests, variable, and methods for rolling a spare and a strike.

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -11,11 +11,19 @@ class BowlingGame
     def score
         result = 0
         rollIndex = 0
-        20.times do
-            result += @rolls[rollIndex]
-            rollIndex += 1
+        10.times do
+          if @rolls[rollIndex] + @rolls[rollIndex + 1] == 10
+            result += @rolls[rollIndex] + @rolls[rollIndex + 1] = @rolls[rollIndex + 2]
+          else
+            result += @rolls[rollIndex] + @rolls[rollIndex + 1]
+          end
+          rollIndex += 2
         end
         result
+    end
+
+    def spare? rollIndex
+
     end
 
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -5,17 +5,34 @@ describe 'the behaviour of the game' do
     game = BowlingGame.new
   end
 
-    it "can roll a gutter game" do
+    it "is able to roll a gutter game" do
         game = BowlingGame.new
         20.times{game.roll 0}
         expect(game.score).to eq 0
   end
     
-    it 'can roll all 1s' do
+    it 'is able to roll all 1s' do
         game = BowlingGame.new
         20.times{game.roll 1}
         expect(game.score).to eq 20
+    end
 
+    it "is able to roll a spare" do
+        game = BowlingGame.new
+        @game.roll 3
+        @game.roll 7
+        @game.roll 3
+        17.times{@game.roll 0}
+        expect(@game.score).to eq 16
+    end
+
+    it "is able to roll a strike" do
+         game = BowlingGame.new
+         @game.roll 10
+         @game.roll 4
+         @game.roll 3
+         16.times(@game.roll 0)
+         expect(@game.score).to eq 24
     end
     
 end
