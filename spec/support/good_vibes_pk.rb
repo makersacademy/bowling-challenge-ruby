3963c44c98988be8185abc2edc180503ab9abeb2
Paul <pkilgarriff@gmail.com>
2022-03-25 22:11:42 +0000

    Add GoodVibes class for testing
    Update spec_helper to use GoodVibes

---
+++ spec/support/good_vibes_pk.rb
@@ -0,0 +1,18 @@
+class GoodVibesPK
+  POSITIVE_AFFIRMATIONS = [
+    "I am worthy of what I desire.",
+    "I can. I will. End of story.",
+    "I am adventurous. I overcome fears by following my dreams.",
+    "I am the hero of my own life.",
+    "I will not compare myself to strangers on the Internet.",
+    "I am worthy. I am loved. I am enough.",
+    "I have the power to create change.",
+    "I believe in the good things coming.",
+    "I am resilient and can get through anything.",
+    "I will accomplish everything that needs to get done.",
+    "When you really want it, you are unstoppable.",
+    "You are loved",
+    "You are deserving of happiness",
+    "Everything is going to be alright"
+  ].freeze  
+end
