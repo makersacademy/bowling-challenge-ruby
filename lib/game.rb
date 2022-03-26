df250de222cf4fb0d4c0606d5e9c23bf8a0e162d
Harry <harrydawson90@hotmail.co.uk>
2022-03-26 23:23:45 +0000

    built out logic for every case excluding spare/spare and strike/strike

---
+++ lib/game.rb
@@ -0,0 +1,62 @@
+class Game
+  attr_accessor :scorecard
+  def initialize
+    @scorecard = [
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 }
+    ]
+  end
+
+  def bowl(pins_down)
+  # frame index required to determine which 
+  # frame each bowl should be attributed to:
+    frame_index = @scorecard.find_index do |frame|
+      frame[:pins].sum < 10 && frame[:pins].size < 2
+      end
+  
+  # now add the pins_down 
+  # to the appropriate frame:
+    @scorecard[frame_index][:pins] << pins_down
+
+  # now to determine points,
+  # if no spare scored previously:
+    if @scorecard[frame_index - 1][:pins].sum < 10
+  # no bonuses are scored:
+      @scorecard[frame_index][:total] = @scorecard[frame_index][:pins].sum
+  # elsif previous turn was a spare:
+    elsif @scorecard[frame_index - 1][:pins].sum == 10 && @scorecard[frame_index - 1][:pins].size == 2
+    # add the next score as normal
+      @scorecard[frame_index][:total] = @scorecard[frame_index][:pins].sum
+    # if this is the first bowl of the next turn...
+      if @scorecard[frame_index][:pins].size == 1
+    # then add the score as a bonus to the previous total
+        @scorecard[frame_index - 1][:total] += @scorecard[frame_index][:pins][0]
+    # if this is the 2nd bowl of the next turn then do nothing...
+      else
+      end
+  # elsif previous turn was a strike
+    elsif @scorecard[frame_index - 1][:pins][0] == 10
+    # add the next score as normal
+    @scorecard[frame_index][:total] = @scorecard[frame_index][:pins].sum
+    # if this is the first bowl of the next turn...
+      if @scorecard[frame_index][:pins].size == 1
+    # then add the score as a bonus to the previous total
+        @scorecard[frame_index - 1][:total] += @scorecard[frame_index][:pins][0]
+    # if this is the 2nd bowl of the next turn then do nothing...
+      else
+        @scorecard[frame_index - 1][:total] += @scorecard[frame_index][:pins][1]
+      end
+    end
+  # display scorecard:
+    @scorecard
+  end
+
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,171 @@
+require 'game'
+
+describe Game do
+  it 'can add the number of pins down to a frame' do
+    expect(subject.bowl(4)).to eq [
+      {pins: [4], total: 4 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 }
+    ]
+  end
+
+  it 'starts a new frame every 2 bowls' do
+    game = Game.new
+    bowl1 = game.bowl(4)
+    bowl2 = game.bowl(3)
+    bowl3 = game.bowl(8)
+    bowl4 = game.bowl(1)
+    expect(game.bowl(9)).to eq [
+      {pins: [4, 3], total: 7 },
+      {pins: [8, 1], total: 9 },
+      {pins: [9], total: 9 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 }
+    ]
+  end
+
+  it 'adds a bonus for a spare' do
+    game = Game.new
+    bowl1 = game.bowl(2)
+    bowl2 = game.bowl(8)
+    bowl3 = game.bowl(2)
+    bowl4 = game.bowl(7)
+    bowl5 = game.bowl(9) 
+    bowl6 = game.bowl(1) 
+    bowl7 = game.bowl(5)  
+    expect(game.bowl(0)).to eq [
+      {pins: [2, 8], total: 12 },
+      {pins: [2, 7], total: 9 },
+      {pins: [9, 1], total: 15 },
+      {pins: [5, 0], total: 5 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 }
+    ]
+  end
+
+  it 'adds correct bonus if spare follows a spare' do
+    game = Game.new
+    bowl1 = game.bowl(2)
+    bowl2 = game.bowl(8)
+    bowl3 = game.bowl(7)
+    bowl4 = game.bowl(3)
+    bowl5 = game.bowl(4) 
+    bowl6 = game.bowl(5)  
+    expect(game.bowl(0)).to eq [
+      {pins: [2, 8], total: 17 },
+      {pins: [7, 3], total: 14 },
+      {pins: [4, 5], total: 9 },
+      {pins: [0], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 }
+    ]
+  end
+
+  it 'adds a bonus for a strike' do
+    game = Game.new
+    bowl1 = game.bowl(2)
+    bowl2 = game.bowl(7)
+    bowl3 = game.bowl(10)
+    bowl4 = game.bowl(3)
+    bowl5 = game.bowl(4) 
+    bowl6 = game.bowl(5)  
+    expect(game.bowl(0)).to eq [
+      {pins: [2, 7], total: 9 },
+      {pins: [10], total: 17 },
+      {pins: [3, 4], total: 7 },
+      {pins: [5, 0], total: 5 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 }
+    ]
+  end
+
+  it 'adds correct bonus if strike follows a spare' do
+    game = Game.new
+    bowl1 = game.bowl(2)
+    bowl2 = game.bowl(8)
+    bowl3 = game.bowl(10)
+    bowl4 = game.bowl(3)
+    bowl5 = game.bowl(4) 
+    bowl6 = game.bowl(5)  
+    expect(game.bowl(0)).to eq [
+      {pins: [2, 8], total: 20 },
+      {pins: [10], total: 17 },
+      {pins: [3, 4], total: 7 },
+      {pins: [5, 0], total: 5 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 }
+    ]
+  end
+
+  it 'adds correct bonus if spare follows a strike' do
+    game = Game.new
+    bowl1 = game.bowl(2)
+    bowl2 = game.bowl(7)
+    bowl3 = game.bowl(10)
+    bowl4 = game.bowl(3)
+    bowl5 = game.bowl(7) 
+    bowl6 = game.bowl(5)  
+    expect(game.bowl(0)).to eq [
+      {pins: [2, 7], total: 9 },
+      {pins: [10], total: 20 },
+      {pins: [3, 7], total: 15 },
+      {pins: [5, 0], total: 5 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 }
+    ]
+  end
+
+  it 'adds correct bonus if strike follows a strike' do
+    game = Game.new
+    bowl1 = game.bowl(2)
+    bowl2 = game.bowl(7)
+    bowl3 = game.bowl(10)
+    bowl4 = game.bowl(10)
+    bowl5 = game.bowl(7) 
+    bowl6 = game.bowl(5)  
+    expect(game.bowl(0)).to eq [
+      {pins: [2, 7], total: 9 },
+      {pins: [10], total: 25 },
+      {pins: [10], total: 15 },
+      {pins: [5, 0], total: 5 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 },
+      {pins: [], total: 0 }
+    ]
+  end
+end
