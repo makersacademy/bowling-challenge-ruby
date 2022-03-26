e93bdfb494ae58e13e794e9086d10891fc70da31
Valentina <maggio.vale@gmail.com>
2022-03-26 15:48:19 +0000

    Create methods to add first and second roll to frame
    - initialize current_frame to keep track of turns
    - initialize frames to store the rolls for each frame
    - two methods to add first roll and second roll if applicable
    - private method to change frame to next one
    - complete method to check if current frame is complete

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,10 +1,34 @@
 class Frame
+  FIRST_FRAME = 1
 
-  def first_roll(pin)
-    2
+  attr_reader :frames, :current_frame
+
+  def initialize(current_frame = FIRST_FRAME)
+    @frames = (1..10).map { |x| [x, []] }.to_h
+    @current_frame = current_frame
   end
 
-  def second_roll(pin)
-    7
+  def add_first_roll(first_pin)
+    if  first_pin == 10
+      @frames[@current_frame] = [first_pin, 0]
+      next_frame
+    else
+      @frames[@current_frame] = [first_pin]
+    end
+  end
+
+  def add_second_roll(second_pin)
+    @frames[@current_frame] << second_pin
+    next_frame
+  end
+
+  def complete?(current_frame)
+    @frames[current_frame].count == 2 ? true : false
+  end
+
+  private
+
+  def next_frame
+    @current_frame += 1
   end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -4,14 +4,42 @@ describe Frame do
 
   subject(:frame) { described_class.new }
 
-  describe '#first_roll'
-  it 'returns 2 if knocks down 2 pins on first roll' do
-    expect(frame.first_roll(2)).to eq 2
+  describe '#add_first_roll'
+  it 'adds 2 pins to first roll in first frame' do
+    expect(frame.add_first_roll(2)).to eq [2]
   end
 
-  describe '#second_roll'
-  it 'returns 7 if knocks down 7 pins on second roll' do
-    expect(frame.second_roll(7)).to eq 7
+  describe '#add_second_roll' do
+    it 'adds 7 pins to second roll in first frame' do
+      frame.add_first_roll(2)
+      frame.add_second_roll(7)
+      expect(frame.frames[1]).to eq [2, 7]
+    end
+
+    it 'adds 10 pins (strike) and completes the second roll with zero bu default' do
+      frame.add_first_roll(10)
+      expect(frame.frames[1]).to eq [10, 0]
+    end
   end
 
+  describe '#next_frame' do
+    it 'it changes the frame (turn) to the next one' do
+      frame.add_first_roll(2)
+      frame.add_second_roll(7)
+      expect(frame.current_frame).to eq 2
+    end
+
+    it 'it changes the frame (turn) to the next one after the first roll if strike' do
+      frame.add_first_roll(10)
+      expect(frame.current_frame).to eq 2
+    end
+  end
+
+  describe '#frame_complete?' do
+    it 'returns true if frame is complete' do
+      frame.add_first_roll(2)
+      frame.add_second_roll(7)
+      expect(frame.complete?(1)).to eq true
+    end
+  end
 end
