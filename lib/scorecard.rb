49634af4883ad1d02572de145d0c2de5c490dfbb
ruiined <ruiined@gmail.com>
2022-03-27 12:35:30 +0100

    added strike method

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -2,10 +2,13 @@
 
 # ScoreCard class
 class ScoreCard
-  attr_reader :score, :rolls
+  attr_reader :pins, :frames
+
+  MAX_PINS = 10
 
   def initialize
-    @score = 0
+    @pins = []
+    @frames = []
     @rolls = 0
     @to_be_named = [
       # { frame: frame_count,
@@ -17,9 +20,41 @@ class ScoreCard
     ]
   end
 
+  def strike
+    @frames[-2] = MAX_PINS + @pins.last(2).sum
+  end
+
+  def spare
+    
+  end
+  
+  def total_score
+    @frames.sum
+  end
+
+  def frame_score
+    if @pins[-4] == MAX_PINS && @pins[-3] == 0
+      @frames << @pins.last(2).sum
+      strike
+    elsif @pins.length.odd?
+      @pins.last
+    else
+      @frames << @pins.last(2).sum
+    end
+  end
+
   def roll(pins)
-    @rolls += 1
-    @score += pins
+    raise "incorrect number of pins" if pins < 0 || pins > MAX_PINS
+      if pins == MAX_PINS
+        @rolls += 2
+        @pins << pins
+        @pins << 0
+      else 
+        @pins << pins
+        @rolls += 1
+      end
+    frame_score
+    pins
   end
 
   def frame_count
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -9,11 +9,6 @@ describe ScoreCard do
     expect(scorecard.roll(4)).to eq 4
   end
 
-  it 'calculates the score after 2 rolls' do
-    2.times { scorecard.roll(4) }
-    expect(scorecard.score).to eq 8
-  end
-
   it 'starts a new frame after 2 rolls' do
     3.times { scorecard.roll(4) }
     expect(scorecard.frame_count).to eq 2
@@ -24,9 +19,59 @@ describe ScoreCard do
     expect(scorecard.roll_count).to eq 1
   end
 
-  xit 'adds the rolls to an array' do
+  it 'adds the knocked pins to an array' do
     scorecard.roll(4)
     scorecard.roll(2)
-    expect(scorecard.rolls).to include 4 && 2
+    expect(scorecard.pins).to include 4 && 2
+  end
+
+  describe ".frame_score" do
+    it 'calculates the frame score after 1 roll' do
+      scorecard.roll(4)
+      expect(scorecard.frame_score).to eq 4
+    end
+
+    it 'calculates the frame score after 2 rolls' do
+      2.times { scorecard.roll(4) }
+      expect(scorecard.total_score).to eq 8
+    end
+
+    it 'resets and calculates the frame score after 3 rolls' do
+      3.times { scorecard.roll(4) }
+      expect(scorecard.frame_score).to eq 4
+    end
+
+    it 'calculates the frame score after 4 rolls' do
+      4.times { scorecard.roll(4) }
+      expect(scorecard.frame_score).to include 8
+    end
+  end
+
+  describe ".strike" do
+    it "skips the frame after the strike" do
+      scorecard.roll(ScoreCard::MAX_PINS)
+      expect(scorecard.frame_score).to include ScoreCard::MAX_PINS
+      2.times { scorecard.roll(4) }
+      expect(scorecard.frames).to include 8
+    end
+
+    it "calculates strike score correctly" do
+      scorecard.roll(ScoreCard::MAX_PINS)
+      2.times { scorecard.roll(4) }
+      expect(scorecard.total_score).to eq ScoreCard::MAX_PINS + 16
+    end
+  end
+
+  
+
+  it "calculates spare correctly" do
+    
+  end
+
+  describe "edge cases" do
+    it 'raises an error when rolling incorrect amount of pins' do
+      expect { scorecard.roll(-1) }.to raise_error("incorrect number of pins")
+      expect { scorecard.roll(ScoreCard::MAX_PINS + 1)}.to raise_error("incorrect number of pins")
+    end
   end
 end
