bc7f7ea6ced555d34126438a84602185a5eba06b
Jonny Abrams <jonnyabrams@hotmail.com>
2022-03-27 11:42:44 +0100

    Add tests for perfect game and all spares

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -33,4 +33,14 @@ describe Game do
     16.times { game.roll(0) }
     expect(game.score).to eq 32
   end
+
+  it 'can roll a perfect game' do
+    12.times { game.roll(10) }
+    expect(game.score).to eq 300
+  end
+
+  it 'can roll all spares' do
+    21.times { game.roll(5) }
+    expect(game.score).to eq 150
+  end
 end
