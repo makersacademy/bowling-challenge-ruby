bcc768aa1cb37246bd7a4f08f35a81db90897c93
Valentina <maggio.vale@gmail.com>
2022-03-27 14:36:05 +0100

    Add logic fro strike and spare scenarios
    - previous frame score gets updated after completing current turn
    - methods too long and complex will need to be cleaned

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,6 +1,8 @@
 class Frame
   FIRST_FRAME = 1
   SECOND_FRAME = 2
+  STRIKE = 10
+  STRIKE_BONUS = 20
 
   attr_reader :frames, :current_frame, :next_frame
 
@@ -10,21 +12,40 @@ class Frame
     @next_frame = next_frame
   end
 
-  def add_first_roll(first_pin)
-    if first_pin == 10
-      @frames[@current_frame] = [first_pin, 0]
-      new_current_frame
+  def add_first_roll(first_pin) 
+    if @current_frame > 1 && strike? 
+      if first_pin == STRIKE
+        consecutive_strike(first_pin)
+      else
+        @frames[@current_frame] = [first_pin]
+      end
+    elsif @current_frame > 1 && spare? 
+      if first_pin == STRIKE
+        spare_and_strike(first_pin)
+      else
+        @frames[@current_frame] = [first_pin]
+        @frames[@current_frame - 1][1] += first_pin
+      end
     else
-      @frames[@current_frame] = [first_pin]
+      if first_pin == STRIKE
+      single_strike(first_pin)
+      else
+        @frames[@current_frame] = [first_pin]
+      end
     end
   end
 
   def add_second_roll(second_pin)
     if @current_frame != @next_frame
-      @frames[@current_frame] << second_pin
-      new_current_frame
+      if @current_frame > 1 && strike? 
+        @frames[@current_frame] << second_pin
+        @frames[@current_frame - 1][1] += @frames[@current_frame].sum
+      else
+        @frames[@current_frame] << second_pin
+      end
     end
     new_next_frame
+    new_current_frame
   end
 
   def complete?(current_frame)
@@ -48,4 +69,25 @@ class Frame
   def new_next_frame
     @next_frame += 1
   end
+
+  def consecutive_strike(first_pin)
+    @frames[@current_frame] = [first_pin, 0]
+    @frames[@current_frame - 1][1] += STRIKE_BONUS
+    new_next_frame
+    new_current_frame
+  end
+
+  def spare_and_strike(first_pin)
+    @frames[@current_frame] = [first_pin, 0]
+    @frames[@current_frame - 1][1] += first_pin
+    new_next_frame
+    new_current_frame
+  end
+
+  def single_strike(first_pin)
+    @frames[@current_frame] = [first_pin, 0]
+    new_next_frame
+    new_current_frame
+  end
+
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -8,11 +8,6 @@ describe Frame do
     it 'adds 2 pins to first roll in first frame' do
       expect(frame.add_first_roll(2)).to eq [2]
     end
-
-    it 'scores strike at first roll' do
-      frame.add_first_roll(10)
-      expect(frame.strike?).to eq true
-    end
   end
 
   describe '#add_second_roll' do
@@ -22,20 +17,52 @@ describe Frame do
       expect(frame.frames[1]).to eq [2, 7]
     end
 
-    it 'adds 10 pins (strike) and completes the second roll with zero by default' do
-      frame.add_first_roll(10)
-      expect(frame.frames[1]).to eq [10, 0]
-    end
-
-    it 'scores spare at second roll' do
-      frame.add_first_roll(2)
-      frame.add_second_roll(8)
-      expect(frame.spare?).to eq true
-    end
-
     it 'returns next frame if score strike in previous roll' do
       frame.add_first_roll(10)
       expect(frame.add_second_roll(2)).to eq 3
+    end
+  end
+
+  context 'strike' do
+    describe '#add-first_roll' do
+      it 'scores strike at first roll' do
+        frame.add_first_roll(10)
+        expect(frame.strike?).to eq true
+      end
+
+      it 'adds 10 pins (strike) and completes the second roll with zero by default' do
+        frame.add_first_roll(10)
+        expect(frame.frames[1]).to eq [10, 0]
+      end
+
+      it 'adds the bonus scores for previous strike' do
+        frame.add_first_roll(5)
+        frame.add_second_roll(2)
+        frame.add_first_roll(10)
+        frame.add_first_roll(2)
+        frame.add_second_roll(3)
+        expect(frame.frames[2]).to eq [10, 5]
+      end
+    end
+  end
+
+  context 'spare' do
+    describe '#add-first_roll' do
+      it 'adds the bonus scores for previous spare' do
+        frame.add_first_roll(5)
+        frame.add_second_roll(5)
+        frame.add_first_roll(3)
+        frame.add_second_roll(4)
+        expect(frame.frames[1]).to eq [5, 8]
+      end
+    end
+
+    describe '#add_second_roll' do
+      it 'scores spare at second roll' do
+        frame.add_first_roll(2)
+        frame.add_second_roll(8)
+        expect(frame.spare?).to eq true
+      end
     end
   end
 
