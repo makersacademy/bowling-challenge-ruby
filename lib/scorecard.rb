c1217cbe48ba42476ba740b7677e7a864230e7e2
Valentina <maggio.vale@gmail.com>
2022-03-27 00:29:23 +0000

    Create scorecard instance variable to store frame details
    - scorecard gets update with frame details, rolls detail and total score
    - create instance variable to keep track of the current turn

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,18 +1,39 @@
 require_relative 'frame'
 
 class ScoreCard
-  attr_reader :total_score, :frame
+  attr_reader :total_score, :frame, :current_turn, :scorecard
 
   def initialize
-    @total_score = (1..10).map { |x| [x, ''] }.to_h
+    @total_score = (1..10).map { |x| [x, ""] }.to_h
+    @scorecard = (1..10).map { |x| [x, []] }.to_h
     @frame = Frame.new
+    @current_turn = @frame.current_frame
   end
 
   def frame_score
     if @frame.strike? || @frame.spare?
-      @total_score[@frame.current_frame] = 'pending'
+      @total_score[@frame.current_frame - 1] = "pending"
     else
-      @total_score[@frame.current_frame] = @frame.frames[@frame.current_frame - 1].sum
+      @total_score[@frame.current_frame - 1] = @frame.frames[@frame.current_frame - 1].sum
     end
+  end
+
+  def update_scorecard
+    save_current_frame
+    save_roll_details
+    save_total_score
+    @current_turn += 1
+  end
+
+  def save_current_frame
+    @scorecard[@current_turn] << "Frame: #{@current_turn}"
+  end
+
+  def save_roll_details
+    @scorecard[@current_turn] << "Roll 1: #{@frame.frames[@current_turn][0]}, Roll 2: #{@frame.frames[@current_turn][1]}" 
+  end
+
+  def save_total_score
+    @scorecard[@current_turn] << "Total score: #{@total_score[@current_turn]}"
   end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -6,22 +6,57 @@ describe ScoreCard do
   subject(:card) { described_class.new }
   let(:frame) { Frame.new }
 
-  describe '#frame_score' do
-    it 'returns the total score of 5 for a frame' do
-      card.frame.add_first_roll(2)
-      card.frame.add_second_roll(3)
-      expect(card.frame_score).to eq 5
-    end
+  context 'calculating score' do
+    describe '#frame_score' do
+      it 'returns the total score of 5 for a frame' do
+        card.frame.add_first_roll(2)
+        card.frame.add_second_roll(3)
+        expect { card.frame_score }.to change { card.total_score[1] }.to(5)
+      end
 
-    it 'returns "pending" in case of strike' do
-      card.frame.add_first_roll(10)
-      expect(card.frame_score).to eq 'pending'
-    end
+      it 'returns "pending" in case of strike' do
+        card.frame.add_first_roll(10)
+        expect { card.frame_score }.to change { card.total_score[1] }.to('pending')
+      end
 
-    it 'returns "pending" in case of  spare' do
+      it 'returns "pending" in case of  spare' do
+        card.frame.add_first_roll(3)
+        card.frame.add_second_roll(7)
+        expect { card.frame_score }.to change { card.total_score[1] }.to('pending')
+      end
+    end
+  end
+
+  context 'saving frame details with pins by rolls and total score' do
+
+    before do
       card.frame.add_first_roll(3)
-      card.frame.add_second_roll(7)
-      expect(card.frame_score).to eq 'pending'
+      card.frame.add_second_roll(5)
+      card.frame_score
+    end
+
+    describe '#save_current_frame' do
+      it 'prints the current frame and knocked down pins for each roll' do
+        expect(card.save_current_frame).to include 'Frame: 1'
+      end
+    end
+
+    describe '#save_roll_details' do
+      it 'prints the current frame and knocked down pins for each roll' do
+        expect(card.save_roll_details).to include 'Roll 1: 3, Roll 2: 5'
+      end
+    end
+
+    describe '#save_total_score' do
+      it 'prints the total score for the frame' do
+        expect(card.save_total_score).to include 'Total score: 8'
+      end
+    end
+
+    describe '#update_scorecard' do
+      it 'saves details of the frame in scorecard' do
+        expect { card.update_scorecard }.to change { card.scorecard[1] }.to(["Frame: 1", "Roll 1: 3, Roll 2: 5", "Total score: 8"])
+      end
     end
   end
 end
