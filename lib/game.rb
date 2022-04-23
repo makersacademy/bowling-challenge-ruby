7d9f1f0562e8e6021dce9cedfa35826d03bbdb04
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-23 11:24:03 +0100

    gets an error if the user input is not between 0 and 10

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,10 @@
 class Game 
-attr_reader :user_input
-  def user_input_scores
+  attr_reader :user_input
+  
+  def user_input_scores(user_input = gets.chomp.to_i)
+    raise "Score need to between 0 and 10" if user_input > 10
     puts 'Please enter your scores - need to between 0 and 10: '
-    @user_input = gets.chomp
+   p user_input
   end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,10 +2,19 @@ require 'game'
 
 describe Game do
   describe '#run_game' do
+    let(:user_input) { 5 }
     it 'let user to input their score' do
       game = Game.new
-      input = game.user_input_scores
-      expect(input.to_i).to be_between(0,10) 
+      input = game.user_input_scores(user_input)
+      expect(input).to eq 5
+    end
+  end  
+
+  describe '#run_game-error' do
+     let(:user_input) { 15 }  
+    it 'raise an error is user enters' do 
+      game = Game.new
+      expect{game.user_input_scores(user_input)}.to raise_error 'Score need to between 0 and 10'
     end
   end
 end
