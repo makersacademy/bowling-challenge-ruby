757c02f60a3fd72da0386aee1a467e0a58d7d263
Michael Su <mailmsuu@gmail.com>
2022-04-23 11:04:56 +0100

    Passed initial tests for scoresheet, added more tests to calculate scores and bonuses from strikes and spares

--- lib/scoresheet.rb
+++ lib/scoresheet.rb
@@ -1,5 +1,26 @@
 require_relative './frame'
 
 class Scoresheet
+
+  def initialize(frame = Frame.new)
+    @frames = []
+    @current_frame = frame
+  end
+
+  def new_frame(frame = Frame.new)
+    @current_frame = frame
+  end
   
+  def current_frame
+    if @current_frame.rolls == 2 || @current_frame.strike?
+      @frames << @current_frame
+      new_frame
+    end
+    @current_frame
+  end
+
+  def total_score
+    0
+  end
+
 end
--- spec/scoresheet_spec.rb
+++ spec/scoresheet_spec.rb
@@ -1,7 +1,8 @@
 require 'scoresheet'
 
 describe Scoresheet do
-  let(:scoresheet) { described_class.new }
+  let(:frame) { Frame.new }
+  let(:scoresheet) { described_class.new(frame) }
 
   it 'Should create a new frame once there are 2 rolls in a frame' do
     scoresheet.current_frame.add_roll(5)
@@ -16,12 +17,36 @@ describe Scoresheet do
     scoresheet.current_frame.add_roll(2)
   end
 
-  it 'Should end the game by returning total score after the tenth frame has ended' do
-    9.times do 
-      scoresheet.current_frame.add_roll(10)
+  it 'Should be able to get a total score after the tenth frame has ended' do
+    20.times do 
+      scoresheet.current_frame.add_roll(0)
     end
-    scoresheet.current_frame.add_roll(2)
-    expect(scoresheet).to receive(:total_score)
-    scoresheet.current_frame.add_roll(2)
+    expect(scoresheet.total_score).to eq(0)
+  end
+
+  describe '#frame_score' do
+    it 'Should calculate the score of a frame' do
+      scoresheet.current_frame.add_roll(6)
+      scoresheet.current_frame.add_roll(2)
+      expect(scoresheet.frame_score(frame)).to eq(8)
+    end
+  end
+
+  describe '#strike_bonus' do
+    it 'Should add the value of the next two rolls as a bonus after rolling a strike' do
+      scoresheet.current_frame.add_roll(10)
+      scoresheet.current_frame.add_roll(6)
+      scoresheet.current_frame.add_roll(2)
+      expect(scoresheet.frame_score(frame)).to eq(18)
+    end
+  end
+
+  describe '#spare_bonus' do
+    it 'Should add the value of the next roll as a bonus after rolling a spare' do
+      scoresheet.current_frame.add_roll(10)
+      scoresheet.current_frame.add_roll(6)
+      scoresheet.current_frame.add_roll(2)
+      expect(scoresheet.frame_score(frame)).to eq(16)
+    end
   end
 end
