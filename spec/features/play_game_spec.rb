fbef5a895e4f416cd531fb8d7f0e030c6dd6ba12
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-23 19:46:43 +0100

    Update Game.play spec mock-up with tally points

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,51 +1,72 @@
-# class Game
-#   def self.play
-#     game = Game.new 
-#     until game.next_frame == 10
-#       game.play_frame
-#       next_frame += 1
-#     end
-#     game.play_frame(:last)
-#   end
+class Game
+  def self.play
+    game = Game.new 
+    while game.next_frame < 10
+      game.play_frame
+      game.display_scoresheet
+      next_frame += 1
+    end
+      game.play_frame(:last)
+      game.display_scoresheet
+    end
+    
+    
+  end
 
-#   attr_reader :next_frame, :scoresheet 
+  attr_reader :next_frame, :scoresheet 
 
-#   def initialize(scoresheet = Scoresheet.new, frame = Frame)
-#     @starting_frame = 1
-#     @next_frame = @starting_frame + 1
-#     @scoresheet = scoresheet
-#     @frame = frame
+  def initialize(scoresheet = Scoresheet.new, frame = Frame)
+    @starting_frame = 1
+    @next_frame = @starting_frame + 1
+    @scoresheet = scoresheet
+    @frame = frame
 
-#   end
+  end
 
-#   private
+  private
 
-#   def play_frame(type = :standard)
-#     unless type == :last
-#       result = frame.standard_play
-#     else
-#       result = frame.play_last
-#     end
-#     game.scoresheet.update(frame_result)
-#     game.scoresheet.display
-#   end
-# end
+  def play_frame(type = :standard)
+    unless type == :last
+      result = frame.standard_play
+    else
+      result = frame.play_last
+    end
+    game.scoresheet.update_score(result)
+  end
 
-# class Scoresheet
-#   def update(result)
+  def display_scoresheet
+    game.scoresheet.display
+  end
+end
 
-#   end
+class Scoresheet
+  def update_score(result)
+    tally_basic_points
+    tally_bonus_points
+  end
 
-#   def final_score
+  def display
 
-#   end
+  end
 
-# end
+  private
 
-# class Frame
-#   def self.standard_play
+  def tally_basic_points
 
-#   end
+  end
 
-#   def self.play_last
-# end
+  def tally_bonus_points
+
+  end
+
+end
+
+class Frame
+  def self.standard_play
+
+  end
+
+  def self.play_last
+    
+  end
+end
