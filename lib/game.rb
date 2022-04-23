9caf93d670d2b9df06b13cf67adeadc1b25b09bc
Joe McNally <joe.t.mcnally@hotmail.com>
2022-04-23 14:19:29 +0100

    Created the score method which so far calculates the score of a gutter game and a simple game with no spares or strikes.

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,13 @@
 class Game
   def initialize(rolls)
-    
+    @rolls = rolls
+  end
+
+  def score
+    score = 0
+    @rolls.each do |frame|
+      score += frame.inject(0,:+)
+    end
+    score
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,23 @@
 require_relative '../lib/game'
 
 describe Game do
-  it "has a set of rolls as an argument when created" do
-    expect(Game).to receive(:new).with Array
-    Game.new([[10],[3,4],[5,3],[7,3],[9,1],[10],[9,1],[10],[9,1],[3,5]])
+  let(:game1) { Game.new([[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]) }
+  let(:game2) { Game.new([[3,6],[0,3],[0,8],[1,4],[0,0],[0,3],[0,0],[0,4],[7,0],[0,0]]) }
+
+  context "#initialize" do
+    it "has a set of rolls as an argument when created" do
+      expect(Game).to receive(:new).with Array
+      Game.new([[10],[3,4],[5,3],[7,3],[9,1],[10],[9,1],[10],[9,1],[3,5]])
+    end
+  end
+
+  context "#score" do
+    it "gives a score of 0 for a gutter game" do
+      expect(game1.score).to eq 0
+    end
+
+    it "calculates the score of a game with no strikes or spares" do
+      expect(game2.score).to eq 39
+    end
   end
 end
