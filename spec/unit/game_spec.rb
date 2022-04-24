0befb406e120349d48156258c783b9aa7c5ff34d
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-04-24 13:38:03 +0100

    /gutter/spare tested & working as expected

--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -11,5 +11,22 @@ describe Game do
     2.times { game.bowl(2) }
     expect(game.score).to eq 4
   end
+
+  it 'can score a gutter game' do
+    20.times { game.bowl(0) }
+    expect(game.score).to eq 0
+  end
+
+  it 'can score a game of all 2s' do
+    20.times { game.bowl(2) }
+    expect(game.score).to eq 40
+  end
+
+  it 'can score a spare, followed by a 2' do
+    2.times { game.bowl(5) }
+    game.bowl(2)
+    game.bowl(0)
+    expect(game.score).to eq 14
+  end
   
 end
