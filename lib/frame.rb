2d58f2d8450d875c3b253ae8b70f4669bf676c63
Conor Hope <c_hope@hotmail.co.uk>
2022-04-24 17:24:35 +0100

    Implemented Frame#strike? and Frame#spare? methods

--- lib/frame.rb
+++ lib/frame.rb
@@ -13,6 +13,18 @@ class Frame
   def reset_knocked_down_pins_log
     @knocked_down_pin_log = []
   end
-  
 
+  def spare?
+    if @knocked_down_pins_log.sum == 10 && @knocked_down_pins_log.length == 2
+      true
+    else false
+    end
+  end
+
+  def strike?
+    if @knocked_down_pins_log.sum == 10 && @knocked_down_pins_log.length == 1
+      true
+    else false
+    end
+  end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,6 +1,9 @@
 require_relative '../lib/frame'
 
 describe Frame do
+
+  let(:frame) { Frame.new }
+
   context '#knocked_down_pins' do
     it 'logs the number of pins knowed down' do
       subject.knocked_down_pins(5)
@@ -11,9 +14,35 @@ describe Frame do
 
   context '#reset_knocked_down_pins_log' do
     it 'resets the log of knocked down pins' do
-      frame = Frame.new
       frame.reset_knocked_down_pins_log
       expect(frame).to have_attributes(:knocked_down_pins_log => [])
+    end
+  end
+
+  context '#spare?' do
+    it 'returns true if user achieved a spare' do
+      frame.knocked_down_pins(6)
+      frame.knocked_down_pins(4)
+      expect(frame.spare?).to be_truthy
+    end
+
+    it 'returns false if user did not achieve a spare' do
+      frame.knocked_down_pins(6)
+      frame.knocked_down_pins(3)
+      expect(frame.spare?).to be_falsey
+    end
+  end
+
+  context '#strike?' do
+    it 'returns true if user acheived a strike' do
+      frame.knocked_down_pins(10)
+      expect(frame.strike?).to be_truthy
+    end
+
+    it 'returns false if user did not acheive a strike' do
+      frame.knocked_down_pins(5)
+      frame.knocked_down_pins(5)
+      expect(frame.strike?).to be_falsey
     end
   end
 end
