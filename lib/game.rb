dcf869c289b7c919f38eee45c82f2d5e98498857
Joe McNally <joe.t.mcnally@hotmail.com>
2022-04-24 12:23:42 +0100

    Finished refactoring the Game class with the use of private methods.

--- lib/game.rb
+++ lib/game.rb
@@ -6,21 +6,33 @@ class Game
   def score
     score = 0
     @rolls.each_with_index do |frame, i|
-      next if i > 9
-      frame_score = frame.inject(0,:+)
-      score += frame_score
-      if frame_score == 10
-        if frame.length == 1
-          if @rolls[i + 1].length == 1
-            score += @rolls[i + 1][0] + @rolls[i + 2][0]
-          else
-            score += @rolls[i + 1].inject(0,:+)
-          end
-        else
-          score += @rolls[i + 1][0]
-        end
-      end
+      score += frame_score(frame, i)
     end
     score
+  end
+
+  private
+
+  def frame_score(frame, index)
+    return 0 if index > 9                                    # don't count the bonus on their own
+    frame_score = frame.inject(0,:+)
+    frame_score += strike_or_spare(frame, index) if frame_score == 10 # if a strike or spare
+    frame_score
+  end
+
+  def strike_or_spare(frame, index)
+    if frame.length == 1                                    # if a strike
+      strike_score(index)
+    else                                                    # if a spare
+      @rolls[index + 1][0]
+    end
+  end
+
+  def strike_score(index)
+    if @rolls[index + 1].length == 1                         # if consecutive strikes
+      @rolls[index + 1][0] + @rolls[index + 2][0]
+    else                                                     # if just one strike
+      @rolls[index + 1].inject(0,:+)
+    end
   end
 end
