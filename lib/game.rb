093d4e32ed0102fb7cbfaaefc4a886e19eeed5ac
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-24 16:18:56 +0100

    implemented Game#play 10 frame game

--- lib/game.rb
+++ lib/game.rb
@@ -6,9 +6,17 @@ class Game
   def play_frame(frame = Frame.new)
     first_roll = frame.roll
     frame.update_score(:first_roll, first_roll)
-    second_roll = frame.roll
-    frame.update_score(:second_roll, second_roll) unless frame.strike?
+    unless frame.strike?
+      second_roll = frame.roll
+      frame.update_score(:second_roll, second_roll)
+    end
     frame.frame_score
+  end
+
+  def play(frame_class = Frame)
+    gamesheet = []
+    10.times { gamesheet << play_frame(frame_class.new) }
+    gamesheet
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,9 @@ require 'stringio'
 
 describe Game do
 
+  
   let(:frame) { double :frame }
+  let(:frame_class) { double :frame_class, new: frame }
 
   let(:game) { described_class.new }
 
@@ -20,6 +22,7 @@ describe Game do
       end
     end
 
+    # problem with this test. It doesn't actually test that the frame is closed after a single roll.
     describe 'when the first roll is a strike' do
       it 'ends the frame after one roll' do
         allow(frame).to receive(:roll).and_return(10)
@@ -28,6 +31,19 @@ describe Game do
         allow(frame).to receive(:frame_score).and_return( { first_roll: 10, second_roll: nil } )
         expect(game.play_frame(frame)).to eq( { first_roll: 10, second_roll: nil } )
       end
+    end
+  end
+
+  describe '#play' do
+    it 'plays through a full ten frames and returns a game scoresheet' do
+      allow(frame).to receive(:roll)
+      allow(frame).to receive(:update_score)
+      allow(frame).to receive(:strike?)
+      allow(frame).to receive(:update_score)
+      allow(frame).to receive(:frame_score).and_return(:frame_score)
+      gamesheet = []
+      10.times { gamesheet << game.play_frame(frame) }
+      expect(game.play(frame_class)).to eq gamesheet
     end
   end
 end
