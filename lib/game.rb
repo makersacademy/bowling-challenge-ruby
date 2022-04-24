eae7cf11bfcc7077d6ffd890040dd5b1bf39891c
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-24 12:40:32 +0100

    Game#play_frame fully implemented with one or two rolls depending on strike?

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,13 @@ require 'stringio'
 
 class Game
   def play_frame(frame = Frame.new)
+    frame_score = { first_roll: nil, second_roll: nil}
     first_roll = frame.roll
-    { first_roll: first_roll }
+    frame_score[:first_roll] = first_roll
+    unless frame_score[:first_roll] == 10
+      second_roll = frame.roll
+      frame_score[:second_roll] = second_roll
+    end
+    frame_score
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,11 +9,18 @@ describe Game do
 
   describe '#play_frame' do
 
-    it 'plays a frame and returns the frames basic score' do
-      allow(frame).to receive(:roll).and_return(6)
-      expect(game.play_frame(frame)).to eq( { first_roll: 6} )
+    describe 'when the first roll is not a strike' do
+      it "records first and second rolls and add them to the frame's basic score" do
+        allow(frame).to receive(:roll).and_return(6, 3)
+        expect(game.play_frame(frame)).to eq( { first_roll: 6, second_roll: 3 } )
+      end
     end
 
+    describe 'when the first roll is a strike' do
+      it 'ends the frame after one roll' do
+        allow(frame).to receive(:roll).and_return(10)
+        expect(game.play_frame(frame)).to eq( { first_roll: 10, second_roll: nil } )
+      end
+    end
   end
-
 end
