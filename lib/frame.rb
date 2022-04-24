5265c35d6cffd36c0e3fa43726b90b6d54937e66
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-24 10:28:59 +0100

    renamed standing pins in roll output hash to remaining pins

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,5 +1,5 @@
 def roll(standing_pins, knocked_down)
   score = knocked_down.to_i
-  standing_pins = standing_pins - score
-  { standing_pins: standing_pins, score: score }
+  remaining_pins = standing_pins - score
+  { remaining_pins: remaining_pins, score: score }
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -3,7 +3,7 @@ require './lib/frame'
 describe '#roll' do
   let(:user_input) { "6" }
   it 'reduces the number of standing pins by the user input of pins knocked down' do
-    expect(roll(10, user_input)[:standing_pins]).to eq 4
+    expect(roll(10, user_input)[:remaining_pins]).to eq 4
   end
 
   it 'outputs the score from the roll' do
