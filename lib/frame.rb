03a3a75d69dbabeaf6d244d9a0b6b7396eb75bf4
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-24 11:11:49 +0100

    Frame class created. frame#cleared? method checking remaining pins implemented.

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,6 +1,25 @@
-def roll(standing_pins, knocked_down)
-  raise "Cannot knock down more pins than are standing" if knocked_down > standing_pins
-  score = knocked_down.to_i
-  remaining_pins = standing_pins - score
-  { remaining_pins: remaining_pins, score: score }
+class Frame
+
+  attr_reader :pins_standing
+
+  def initialize
+    @pins_standing = 10
+  end
+
+
+  def roll(standing_pins, knocked_down)
+    raise "Cannot knock down more pins than are standing" if knocked_down > standing_pins
+    score = knocked_down.to_i
+    @pins_standing = standing_pins - score
+    { remaining_pins: @pins_standing, score: score }
+  end
+
+  def cleared?
+    if @pins_standing == 0
+      true
+    else
+      false
+    end
+  end
+
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,15 +1,34 @@
 require './lib/frame'
 
-describe '#roll' do
-  it 'reduces the number of standing pins by pins knocked down' do
-    expect(roll(10, 6)[:remaining_pins]).to eq 4
+describe Frame do
+
+  let(:frame) { described_class.new }
+
+  describe '#roll' do
+    it 'reduces the number of standing pins by pins knocked down' do
+      expect(frame.roll(10, 6)[:remaining_pins]).to eq 4
+    end
+
+    it 'outputs the score from the roll' do
+      expect(frame.roll(10, 6)[:score]).to eq 6
+    end
+
+    it 'raises an error if attempt to knock down more pins than are standing' do
+      expect { frame.roll(5, 7) }.to raise_error("Cannot knock down more pins than are standing")
+    end
   end
 
-  it 'outputs the score from the roll' do
-    expect(roll(10, 6)[:score]).to eq 6
+  describe '#cleared?' do 
+    
+    it 'returns true if there are no pins standing' do
+      frame.roll(10, 10)
+      expect(frame.cleared?).to be true
+    end
+
+    it 'returns false if there are still pins standing' do
+      frame.roll(10, 5)
+      expect(frame.cleared?).to be false
+    end
   end
 
-  it 'raises an error if attempt to knock down more pins than are standing' do
-    expect { roll(5, 7) }.to raise_error("Cannot knock down more pins than are standing")
-  end
 end
