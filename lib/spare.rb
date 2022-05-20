df0ddbf796ea8ca14f35ba23542d29564f85f0dd
Erlantz <erlantzram@gmail.com>
2022-05-20 23:21:08 +0100

    class Spare created and tested

---
+++ lib/spare.rb
@@ -0,0 +1,19 @@
+class Spare
+
+  def initialize
+    @score = 10
+    @roll_count = 0
+  end
+
+  def score
+    @score
+  end
+
+  def add(points)
+    fail 'Spare can only add 10 more points' unless points <= 10
+    fail 'Spare only counts next roll for bonus points' unless @roll_count < 1
+    @score += points
+    @roll_count += 1
+  end
+
+end
---
+++ spec/spare_spec.rb
@@ -0,0 +1,44 @@
+require 'spare'
+
+describe Spare do
+  
+  describe '#score' do
+    
+    it 'returns a score of 10 from the Spare when new' do
+      
+      expect(subject.score).to eq 10
+
+    end
+
+    it 'returns a score of 15 from the Spare when it has been added 5 as a bonus' do
+
+      subject.add(5)
+
+      expect(subject.score).to eq 15
+
+    end
+  end
+
+  describe '#add' do
+    
+    it 'adds 5 to @score when called with 5 as argument' do
+
+      expect { subject.add(5) }.to change { subject.score }.by(5)
+
+    end
+
+    it 'raise an error if tries to add more than 10' do
+
+      expect {subject.add(11) }.to raise_error 'Spare can only add 10 more points'
+
+    end
+
+    it 'raises an error if tries to add points twice (spare bonus only counts next roll)' do
+      
+      subject.add(3)
+
+      expect { subject.add(2) }.to raise_error 'Spare only counts next roll for bonus points'
+
+    end
+  end
+end
