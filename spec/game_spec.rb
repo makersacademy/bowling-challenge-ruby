31136c5ed3705acbbd6cf9854dafc73d6f45d9df
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-05-21 11:49:43 +0100

    test 'can roll a spare' passed

---
+++ spec/game_spec.rb
@@ -0,0 +1,30 @@
+require 'game'
+
+describe Game do
+  describe 'Bowling Game Behaviour' do
+
+    before do
+      @game = Game.new
+    end
+
+    it 'can roll gutter game' do
+      20.times{@game.roll(0)}
+      expect(@game.score).to eq(0)
+    end
+
+    it 'can roll once' do
+      20.times{@game.roll(1)}
+      expect(@game.score).to eq(20)
+    end
+
+    it 'can roll a spare' do
+      @game.roll(5)
+      @game.roll(5)
+      @game.roll(3)
+      17.times{@game.roll(0)}
+      expect(@game.score).to eq(16)
+    end
+
+
+  end
+end
