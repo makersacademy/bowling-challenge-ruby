73d60355e04f9d3cbe57785ad378ffffdf2393ae
Russell <russellmorbey@hotmail.co.uk>
2022-05-22 23:21:40 +0100

    Added Scorecard class and started to pass tests.

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,20 +1,23 @@
 class Scorecard
 
-  attr_reader :roll, :frame
+  attr_reader :roll, :frame, :total_score, :first_roll
 
   def initialize
     @roll = 1
     @frame = 1
-
+    @total_score = 0
+    @first_roll = 0
   end
 
   def roll_ball(pins)
-  
-
+    if roll == 1
+      @first_roll += pins
+      @roll + 1
+    else roll == 2
+      @total_score += (first_roll + pins)
+    end
+    puts @total_score
+    @roll = 1
+    @first_roll = 0
   end
-
-  def total_score
-
-  end
-
 end
