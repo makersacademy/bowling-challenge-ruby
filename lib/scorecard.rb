919d0ff3310816ec063b0845e0b28a59730f56c2
Russell <russellmorbey@hotmail.co.uk>
2022-05-23 10:35:17 +0100

    All Tests passed.

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,23 +1,54 @@
 class Scorecard
 
-  attr_reader :roll, :frame, :total_score, :first_roll
+  attr_reader :rolls
 
   def initialize
-    @roll = 1
-    @frame = 1
-    @total_score = 0
-    @first_roll = 0
+    @rolls = []
   end
 
   def roll_ball(pins)
-    if roll == 1
-      @first_roll += pins
-      @roll + 1
-    else roll == 2
-      @total_score += (first_roll + pins)
-    end
-    puts @total_score
-    @roll = 1
-    @first_roll = 0
+    rolls << pins
   end
+
+  def total_score
+    result = 0
+    rollIndex = 0
+
+    10.times do
+      if strike?(rollIndex)
+        result += strikeScore(rollIndex)
+        rollIndex += 1
+      elsif spare?(rollIndex)
+        result += spareScore(rollIndex)
+        rollIndex += 2
+      else
+        result += frameScore(rollIndex)
+        rollIndex += 2
+      end
+    end
+    result
+  end
+
+  private
+
+  def spare?(rollIndex)
+    @rolls[rollIndex] + @rolls[rollIndex + 1] == 10
+  end
+
+  def strike?(rollIndex)
+    @rolls[rollIndex] == 10
+  end
+
+  def spareScore(rollIndex)
+    10 + @rolls[rollIndex + 2]
+  end
+
+  def strikeScore(rollIndex)
+    10 + @rolls[rollIndex + 1] + @rolls[rollIndex + 2]
+  end
+
+  def frameScore(rollIndex)
+    @rolls[rollIndex] + @rolls[rollIndex + 1]
+  end
+
 end
