00380f65c80ec6e05b0d50f548d12d43c495bb15
Sam Button <sam_button_94@hotmail.co.uk>
2022-05-23 01:57:28 +0100

    added bonuses, spares and strikes

--- lib/game.rb
+++ lib/game.rb
@@ -2,13 +2,52 @@ class Game
 
   def initialize
     @score = 0
+    @roll_number = 0
+    @rolls = []
   end
 
-  def bowl(pins)
-    @score += pins
+  def regular_strike(pins)
+    @roll_number.even? && pins == 10 && @roll_number < 10
+  end
+
+  def first_roll?
+    @roll_number.even?
+  end
+
+  def bowl(pins, frame = Frame)
+    if regular_strike(pins)
+      @frame = frame.new
+     complete_frame(pins)
+      @roll_number += 2
+    elsif @roll_number == 20
+      @rolls.last.add_bonus(pins)
+    elsif first_roll?
+      @frame = frame.new
+      @frame.add_to_frame(pins)
+      @roll_number += 1
+    else
+      @frame.add_to_frame(pins)
+      @rolls << @frame
+      @roll_number += 1
+    end
+  end
+
+  def complete_frame(pins)
+    @frame.add_to_frame(pins)
+    @frame.add_to_frame(0)
+    @rolls << @frame
   end
 
   def score
+    @rolls.each_with_index do |frame, index|
+      if frame.strike?
+        @score += frame.total + @rolls[index + 1].total
+      elsif frame.spare?
+        @score += frame.total + @rolls[index + 1].first_roll
+      else
+        @score += frame.total
+      end
+    end
     @score
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,11 +9,30 @@ describe Game do
 
   end
 
-  it 'can enter a frame of 2 zeros' do
+  it 'can score a gutter game' do
 
-    2.times { game.bowl(0) }
-
+    20.times { game.bowl(0) }
+    
     expect(game.score).to eq 0
+    
+  end
+  
+  it 'can enter a full game of 4s' do
+    
+    20.times { game.bowl(4) }
+    expect(game.score).to eq 80
+  end
 
+  it 'can enter a spare frame, followed by a frame of 2' do
+    2.times { game.bowl(5) }
+    game.bowl(2)
+    game.bowl(0)
+    expect(game.score).to eq 14
+  end
+  
+  it 'can score a strike' do
+    game.bowl(10)
+    2.times { game.bowl(3) }
+    expect(game.score).to eq 22
   end
 end
