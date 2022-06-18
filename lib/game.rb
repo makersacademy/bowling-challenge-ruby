5ff4e6453e5d9d3776566c9df9992f1cf2e6e345
Joe McGarry <joewmcgarry@gmail.com>
2022-06-18 09:43:27 +0100

    Increases round by 1

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,13 @@
 class Game
-  attr_reader :current_round
+  attr_reader :current_round, :total_score
 
   def initialize
     @current_round = 1
+    @total_score = 0
   end
 
-  def total_score
-    8
+  def increase_score(score)
+    @current_round += 1
+    @total_score += score
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,14 +3,30 @@ require "game"
 describe Game do
   let(:game) { Game.new }
 
-  it "should show the current round as 1" do
-    expect(game.current_round).to eq 1
+  context "initially" do
+    it "should show the current round as 1" do
+      expect(game.current_round).to eq 1
+    end
+
+    it "should initialise with a total score of 0" do
+      expect(game.total_score).to eq 0
+    end
   end
 
   context "should calculate the total score" do
-    it "should return the total sum of 8" do
-      expect(game).to receive(:total_score).and_return(8)
-      expect(game.total_score).to eq 8
+    it "should return the total sum of 14" do
+      game.increase_score(1)
+      game.increase_score(3)
+      game.increase_score(4)
+      game.increase_score(6)
+      expect(game.total_score).to eq 14
+    end
+  end
+
+  context "frame management" do
+    it "should increase the frame by 1" do
+      game.increase_score(10)
+      expect(game.current_round).to eq 2
     end
   end
 end
