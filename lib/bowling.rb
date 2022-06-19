1bd04e0b574649d393b336fab8af7110392141a8
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-19 14:53:01 +0100

    Added methods to bowling class and first unit test has passed

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,2 +1,26 @@
 class Bowling 
+
+  def initialize
+    @frame = ""
+    @roll = ""
+    @pins = ""
+  end 
+
+  def frames(frame)
+    @frame = frame
+  end
+
+  def rolls(roll, pins)
+    @roll = roll
+    @pins = pins
+  end 
+
+  def frame_score
+    # @pins = pins
+  end 
+
+  def report
+    return "You just played frame: #{@frame} and roll: #{@roll}. You currently have #{@pins} points"
+  end
+
 end 
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,4 +1,19 @@
 require 'bowling'
 
 describe Bowling do 
-end
+
+  context "Bowler is currently playing frame 1" do 
+    subject(:game) { Bowling.new }
+    let(:frame) { '1' }
+    let(:roll) { '1' }
+    let(:pins) { '5' }
+
+    it "it returns the frame and the roll the bowler has just played and their current score" do 
+      game.frames(frame)
+      game.rolls(roll, pins)
+      game.frame_score
+      game.report
+      expect(game.report).to eq "You just played frame: 1 and roll: 1. You currently have 5 points"
+    end 
+  end 
+end 
