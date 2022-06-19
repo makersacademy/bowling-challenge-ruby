bc1690d9b90a3c547b9a22235f197a89d85ba477
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-19 18:18:09 +0100

    Added spare method, conditional for frame_score method and fix some rubocop offenses

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -5,6 +5,8 @@ class Bowling
     @roll = ""
     @pins = ""
     @pins_array = []
+    @current_frame = []
+    @score_array = []
   end 
 
   def frames(frame)
@@ -15,15 +17,30 @@ class Bowling
     @roll = roll
     @pins = pins # not too sure what tp use the variable for 
     @pins_array << pins.to_i
+    @current_frame = @pins_array.last(2)
+  end 
+
+  def spare
+    @spare = 1 
+    return "Well Done! You scored a spare!" if @current_frame.sum == 10
   end 
 
   def frame_score
-    p @pins_array
-    @total_pins = @pins_array.sum
+    if @spare == 1
+      frame_score = @current_frame[0] + 10
+    else 
+      frame_score = @current_frame.sum
+    end 
+    @score_array << frame_score 
+  end 
+
+  def total_score
+    @total_score = @score_array.sum
+    p @total_score
+    return "Your current score is #{@total_score} points"
   end 
 
   def report
-    return "You just played frame: #{@frame} and roll: #{@roll}. You currently have #{@total_pins} points."
+    return "You just played frame: #{@frame} and roll: #{@roll}."
   end
-
 end 
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -8,31 +8,49 @@ describe Bowling do
     let(:roll) { '1' }
     let(:pins) { '5' }
 
-    it "it returns the frame and the roll the bowler has just played and their current score" do 
+    it "it returns the frame and the roll the bowler has just played" do 
       game.frames(frame)
       game.rolls(roll, pins)
-      game.frame_score
       game.report
-      expect(game.report).to include "You just played frame: 1 and roll: 1. You currently have 5 points."
+      expect(game.report).to include "You just played frame: 1 and roll: 1."
     end 
   end 
 
   context "Bowler has finished playing frame 1 with no spares or strikes" do 
-    let(:frame) {'1'}
-    let(:roll_1) {'1'}
-    let(:pins_1) {'7'}
-    let(:roll_2) {'2'}
-    let(:pins_2) {'2'}
+    let(:frame) { '1' }
+    let(:roll_1) { '1' }
+    let(:pins_1) { '7' }
+    let(:roll_2) { '2' }
+    let(:pins_2) { '2' }
 
-    it "it returns the frame, the roll the bowler has just played and their current total score" do 
+    it "it returns the bowler's current score, the frame and the roll the bowler just played" do 
       game.frames(frame)
       game.rolls(roll_1, pins_1)
       game.rolls(roll_2, pins_2)
       game.frame_score
+      game.total_score
       game.report
-      expect(game.report).to eq "You just played frame: 1 and roll: 2. You currently have 9 points."
-    end 
+      expect(game.total_score).to eq "Your current score is 9 points"
+      expect(game.report).to eq "You just played frame: 1 and roll: 2."
+    end
   end 
 
+  context "Bowler has finished playing frame 1 and scored a spare" do 
+    let(:frame_1) { '1' }
+    let(:roll_1) { '1' }
+    let(:pins_1) { '8' } 
+    let(:roll_2) { '2' }
+    let(:pins_2) { '2' }
 
+    it " it return a well done message and the bowler's current score" do 
+      game.frames(frame_1)
+      game.rolls(roll_1, pins_1)
+      game.rolls(roll_2, pins_2)
+      game.spare
+      game.frame_score
+      game.total_score
+      expect(game.spare).to eq "Well Done! You scored a spare!"
+      expect(game.total_score).to eq "Your current score is 18 points" # (10+8)
+    end 
+  end 
 end 
