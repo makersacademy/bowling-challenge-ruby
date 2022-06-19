939b36f928a0336d537e10896018932cd0ebacd7
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-06-19 14:39:26 +0100

    refactored into a class to take advantage of private methods and to reduce arguments on methods

--- lib/score_calc.rb
+++ lib/score_calc.rb
@@ -1,12 +1,30 @@
-def score_calc(all_frames)
-  score = []
-  all_frames.each_with_index do |frame, index| 
-    # sum each frame
-    score <<frame.sum
-    # if a spare, also push the first index of the next frame
-    if frame.length == 2 && frame.sum == 10
-      score << all_frames[index + 1][0]
-    end
+class BowlingScore
+  attr_reader :all_frames
+
+  def initialize(all_frames)
+    @all_frames = all_frames
   end
-  score.sum
+
+  def result
+    score = []
+    all_frames.each_with_index do |frame, index| 
+      # add calculate score without bonuses
+      score << frame.sum
+      # apply logic for spare
+      if spare?(frame)
+        score << handle_spare(index)
+      end
+    end
+    score.sum
+  end
+
+  private
+
+  def spare?(frame)
+    frame.length == 2 && frame.sum == 10
+  end
+
+  def handle_spare(index)
+    all_frames[index + 1][0]
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,16 +1,18 @@
-describe ".score calculator" do
-  it "returns 0 if Gutter Game" do
-    game = score_calc([[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]])
-    expect(game).to eq 0
-  end
+describe BowlingScore do
+  describe ".result" do
+    it "returns 0 if Gutter Game" do
+      game = BowlingScore.new([[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]])
+      expect(game.result).to eq 0
+    end
 
-  it "returns correct score for game with no strike or spares" do
-    game = score_calc([[1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1]])
-    expect(game).to eq 20
-  end
+    it "returns correct score for game with no strike or spares" do
+      game = BowlingScore.new([[1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1]])
+      expect(game.result).to eq 20
+    end
 
-  it "returns correct score for game with one spare" do
-    game = score_calc([[1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [5,5], [1,1]])
-    expect(game).to eq 29
+    it "returns correct score for game with one spare" do
+      game = BowlingScore.new([[1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [5,5], [1,1]])
+      expect(game.result).to eq 29
+    end
   end
 end
