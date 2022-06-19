049c769a5cf2a91c48a32a8b21f6e042f85b0e95
Amir Khan <ak2022dev@gmail.com>
2022-06-19 20:32:12 +0100

    Also tested to correctly score a game including a spare followed immediately by a strike. All tests passed

--- spec/bowling_score_manager_spec.rb
+++ spec/bowling_score_manager_spec.rb
@@ -23,5 +23,8 @@ RSpec.describe BowlingScoreManager do
     it "Successfully scores a game including a strike followed immediately by a spare" do
       expect(BowlingScoreManager.score( [0,0, 1,1, 10, 7,3, 4,4, 4,5, 4,3, 3,2, 2,1, 0,0] )).to eq ([0,0, 1,1, 10, 7,3, 4,4, 4,5, 4,3, 3,2, 2,1, 0,0].sum + 7 + 3 + 4)
     end
+    it "Successfully scores a game including a spare followed immediately by a strike" do
+      expect(BowlingScoreManager.score( [0,0, 1,1, 4,6, 10, 4,4, 4,5, 4,3, 3,2, 2,1, 0,0] )).to eq ([0,0, 1,1, 4,6, 10, 4,4, 4,5, 4,3, 3,2, 2,1, 0,0].sum + 10 + 8)
+    end
   end
 end
