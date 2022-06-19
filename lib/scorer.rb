8fad85d4e039d016c09396191ee077c2a8b7e2d6
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-06-20 00:15:29 +0100

    x2 strikes fails test

--- lib/scorer.rb
+++ lib/scorer.rb
@@ -2,7 +2,9 @@ def scorer(array)
   score = 0
   turn = 1
   array.each do |frame|
-    if frame[1] == "/"
+    if frame[0] == "X"
+      score += (10 + array[turn][0] +  array[turn][1]) 
+    elsif frame[1] == "/"
       score += 10 + array[turn][0]
     else
       score += (frame[0] + frame[1])
--- spec/scorer_spec.rb
+++ spec/scorer_spec.rb
@@ -28,4 +28,26 @@ describe "when given a score" do
     expect(scorer([[1 , 7], [2, 5], [4, "/"], [3, 4]])).to eq 35
   end
 
+  it "returns the score for x3 frame with a 2 spares in a row" do
+  
+    expect(scorer([[2, "/"], [4, "/"], [3, 4]])).to eq 34
+  end
+
+  it "returns the score for x3 frame with a 3 spares in a row" do
+  
+    expect(scorer([[2, "/"], [4, "/"], [3, "/"], [4, 5]])).to eq 50
+  end
+
+  it "returns the score for x2 frame with a strike on frame 1" do
+  
+    expect(scorer([["X",""], [2, 5]])).to eq 24
+  end
+
+  it "returns the score for x3 frame with 2 strikes in a row" do
+  
+    expect(scorer([["X",""], ["X",""], [2, 5]])).to eq 24
+  end
+
+
+
 end
