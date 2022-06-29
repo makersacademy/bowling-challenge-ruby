8d7faee0b4b73649e4a73a8c93a45c2a43e2d605
Amir Khan <ak2022dev@gmail.com>
2022-06-29 06:54:57 +0100

    Refactor to get_grand_total in preparation to break into smaller methods

--- lib/bowling_score_manager.rb
+++ lib/bowling_score_manager.rb
@@ -18,10 +18,11 @@ class BowlingScoreManager
     @frames = self.setup_frames();
     # Populate @frames in simple case and score
     # by simply adding the rolls
-    self.score_frames(rollsArray);
+    self.score_frames( rollsArray );
   end
     
-=begin    
+    
+  def self.score_frames( rollsArray )
     frame_num = 1
     on_roll = 1
     for roll_from_start in 0...rollsArray.size
@@ -103,106 +104,8 @@ class BowlingScoreManager
           on_roll = 1
       end
     end
-    grand_total = 0
-    for i in 1..10
-#      Previous code when sub-totals were not kept in frame                                                                       
-#      grand_total += (@frames[i].roll1 + @frames[i].roll2)
-      grand_total += @frames[i].total
-    end
-    return grand_total
-  end
-=end
-
+    return self.get_grand_total()
     
-  def self.score_frames(rollsArray)
-    frame_num = 1
-    on_roll = 1
-    for roll_from_start in 0...rollsArray.size
-#      previously used i for the loop index         
-#      p "in for loop i is #{i}"
-      if frame_num == 10
-        # Handle remaining sub-array separately
-        handle_frame_10( rollsArray.drop(roll_from_start) )
-        # Exit the loop completely as work now done
-        break
-      end
-      # Remainder to be put in else after frame_num == 10 handled?
-      if on_roll == 1
-        # Enter value from rollsArray as roll1
-        # into appropriate frame
-#binding.irb
-          (@frames[frame_num]).roll1 = rollsArray[roll_from_start]
-          # Deal with possible spare from previous frame
-          if ((frame_num > 1) && @frames[frame_num-1].status == :spare)
-            # Add this roll to previous frame and mark it completed
-            @frames[frame_num-1].total += (@frames[frame_num]).roll1
-            @frames[frame_num-1].completed = true
-          end
-          # Deal with possible strike from previous frame
-          if ((frame_num > 1) && @frames[frame_num-1].status == :strike)
-            # Add this roll to previous frame but don't yet mark completed
-            # because two rolls need to be added to a strike
-            @frames[frame_num-1].total += (@frames[frame_num]).roll1
-            # Deal with possible strike from two frames ago
-            if ((frame_num > 2) && @frames[frame_num-2].status == :strike)
-              # Add this roll to strike from two frames ago
-              # and mark it complete as both of its following rolls
-              # have now been added
-              @frames[frame_num-2].total += (@frames[frame_num]).roll1
-              @frames[frame_num-1].completed = true
-            end
-          end
-          # Mark possible strike in this frame
-          if (@frames[frame_num]).roll1 == 10
-            @frames[frame_num].status = :strike
-            @frames[frame_num].roll2 = 0
-            @frames[frame_num].total = 10
-            @frames[frame_num].completed = false
-            frame_num += 1
-            # Go to next roll, still considering on_roll to be 1
-            next
-          end
-          # Switch to roll2 for next iteration
-          on_roll = 2
-          next
-      else
-        # Enter value from rollsArray as roll2
-        # into appropriate frame
-#binding.irb
-          (@frames[frame_num]).roll2 = rollsArray[roll_from_start]
-          # Deal with possible strike from previous frame
-          if ((frame_num > 1) && @frames[frame_num-1].status == :strike)
-            # Add this roll to previous frame and then mark completed
-            # because this is second of two rolls need to be added to a strike
-            @frames[frame_num-1].total += (@frames[frame_num]).roll2
-            @frames[frame_num-1].completed = true
-          end          
-          # Mark possible spare in this frame
-          if ((@frames[frame_num].roll1) + (@frames[frame_num].roll2) == 10)
-            @frames[frame_num].status = :spare
-            @frames[frame_num].total = 10
-            @frames[frame_num].completed = false
-            frame_num += 1
-            # Go to next roll as below is for normal frames
-            on_roll = 1
-            next
-          end
-          # For normal status do below but will need to amend for spare and strike
-          @frames[frame_num].total = @frames[frame_num].roll1 + @frames[frame_num].roll2
-          @frames[frame_num].completed = true
-          # frame_num is usually incremented after every second
-          # roll because two rolls per frame
-          frame_num += 1
-          on_roll = 1
-      end
-    end
-    grand_total = 0
-    for i in 1..10
-#      Previous code when sub-totals were not kept in frame                                                                       
-#      grand_total += (@frames[i].roll1 + @frames[i].roll2)
-      grand_total += @frames[i].total
-    end
-    return grand_total
   end
    
         
@@ -298,5 +201,14 @@ class BowlingScoreManager
     end
   end
   
-    
+  
+  def self.get_grand_total
+    grand_total = 0
+    for i in 1..10
+      grand_total += @frames[i].total
+    end
+    return grand_total
+  end
+  
+  
 end
