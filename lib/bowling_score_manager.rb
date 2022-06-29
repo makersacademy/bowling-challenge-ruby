2d6045d05dad4d3cb610ded24f55d6f6cecef6d6
Amir Khan <ak2022dev@gmail.com>
2022-06-29 10:00:44 +0100

    Refactor manage_frame_roll1 method

--- lib/bowling_score_manager.rb
+++ lib/bowling_score_manager.rb
@@ -90,35 +90,35 @@ class BowlingScoreManager
   end
   
 
-  def self.manage_frame_roll1( rollValue, frames_array, frame_num );
+  def self.manage_frame_roll1( rollValue, frames, frame_num );
     # Record most recent roll in current frame
-    (frames_array[frame_num]).roll1 = rollValue
+    (frames[frame_num]).roll1 = rollValue
     # Deal with possible spare from previous frame
-    if ((frame_num > 1) && frames_array[frame_num-1].status == :spare)
+    if ((frame_num > 1) && frames[frame_num-1].status == :spare)
       # Add this roll to previous frame and mark it completed
-      frames_array[frame_num-1].total += (frames_array[frame_num]).roll1
-      frames_array[frame_num-1].completed = true
+      frames[frame_num-1].total += (frames[frame_num]).roll1
+      frames[frame_num-1].completed = true
     end
     # Deal with possible strike from previous frame
-    if ((frame_num > 1) && frames_array[frame_num-1].status == :strike)
+    if ((frame_num > 1) && frames[frame_num-1].status == :strike)
       # Add this roll to previous frame but don't yet mark completed
       # because two rolls need to be added to a strike
-      frames_array[frame_num-1].total += (frames_array[frame_num]).roll1
+      frames[frame_num-1].total += (frames[frame_num]).roll1
       # Deal with possible strike from two frames ago
-      if ((frame_num > 2) && frames_array[frame_num-2].status == :strike)
+      if ((frame_num > 2) && frames[frame_num-2].status == :strike)
         # Add this roll to strike from two frames ago
         # and mark it complete as both of its following rolls
         # have now been added
-        frames_array[frame_num-2].total += (frames_array[frame_num]).roll1
-        frames_array[frame_num-1].completed = true
+        frames[frame_num-2].total += (frames[frame_num]).roll1
+        frames[frame_num-1].completed = true
       end
     end
     # Mark possible strike in this frame
-    if (frames_array[frame_num]).roll1 == 10
-      frames_array[frame_num].status = :strike
-      frames_array[frame_num].roll2 = 0
-      frames_array[frame_num].total = 10
-      frames_array[frame_num].completed = false
+    if (frames[frame_num]).roll1 == 10
+      frames[frame_num].status = :strike
+      frames[frame_num].roll2 = 0
+      frames[frame_num].total = 10
+      frames[frame_num].completed = false
       # Need to check for this condition on return and update frame number there
       # frame_num += 1
       # Go to next roll, still considering on_roll to be 1
