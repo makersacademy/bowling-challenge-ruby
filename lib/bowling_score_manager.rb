c72c82e77b5236e42fecbc64274c873b0947025a
Amir Khan <ak2022dev@gmail.com>
2022-06-29 19:12:54 +0100

    Removed lots of commented code now replaced by refactored methods

--- lib/bowling_score_manager.rb
+++ lib/bowling_score_manager.rb
@@ -73,43 +73,10 @@ class BowlingScoreManager
     (frames[frame_num]).roll1 = rollValue
     # Deal with possible spare from previous frame
     manage_poss_prev_spare( rollValue, frames, frame_num );
-=begin
-    if ((frame_num > 1) && frames[frame_num-1].status == :spare)
-      # Add this roll to previous frame and mark it completed
-      frames[frame_num-1].total += (frames[frame_num]).roll1
-      frames[frame_num-1].completed = true
-    end
-=end
     # Deal with possible strike from previous frame
     manage_poss_prev_strike( frames, frame_num )
-=begin
-    if ((frame_num > 1) && frames[frame_num-1].status == :strike)
-      # Add this roll to previous frame but don't yet mark completed
-      # because two rolls need to be added to a strike
-      frames[frame_num-1].total += (frames[frame_num]).roll1
-      # Deal with possible strike from two frames ago
-      if ((frame_num > 2) && frames[frame_num-2].status == :strike)
-        # Add this roll to strike from two frames ago
-        # and mark it complete as both of its following rolls
-        # have now been added
-        frames[frame_num-2].total += (frames[frame_num]).roll1
-        frames[frame_num-1].completed = true
-      end
-    end
-=end
     # Mark possible strike in this frame
     manage_poss_strike_this_frame( frames, frame_num )
-=begin    
-    if (frames[frame_num]).roll1 == 10
-      frames[frame_num].status = :strike
-      frames[frame_num].roll2 = 0
-      frames[frame_num].total = 10
-      frames[frame_num].completed = false
-      # Need to check for this condition on return and update frame number there
-      # frame_num += 1
-      # Go to next roll, still considering on_roll to be 1
-    end
-=end
   end
 
   
@@ -201,30 +168,8 @@ class BowlingScoreManager
         (frames[frame_num]).roll1 = last_rolls[rolls_at_end]
         # Deal with possible spare from previous frame
         self.manage_poss_prev_spare( frames[frame_num].roll1, frames, frame_num )
-=begin
-        if ((frame_num > 1) && frames[frame_num-1].status == :spare)
-          # Add this roll to previous frame and mark it completed
-          frames[frame_num-1].total += (frames[frame_num]).roll1
-          frames[frame_num-1].completed = true
-        end
-=end
         # Deal with possible strike from previous frame
         manage_poss_prev_strike( frames, frame_num )
-=begin
-        if ((frame_num > 1) && frames[frame_num-1].status == :strike)
-          # Add this roll to previous frame but don't yet mark completed
-          # because two rolls need to be added to a strike
-          frames[frame_num-1].total += (frames[frame_num]).roll1
-          # Deal with possible strike from two frames ago
-          if ((frame_num > 2) && frames[frame_num-2].status == :strike)
-            # Add this roll to strike from two frames ago
-            # and mark it complete as both of its following rolls
-            # have now been added
-            frames[frame_num-2].total += (frames[frame_num]).roll1
-            frames[frame_num-1].completed = true
-          end
-        end
-=end
         # Mark possible strike in this frame
         manage_poss_strike_this_frame( frames, frame_num )
         if (frames[frame_num]).roll1 == 10
@@ -233,19 +178,6 @@ class BowlingScoreManager
           on_roll = 2
           next
         end
-=begin        
-        if (frames[frame_num]).roll1 == 10
-          frames[frame_num].status = :strike
-          frames[frame_num].roll2 = 0
-          frames[frame_num].total = 10
-          frames[frame_num].completed = false
-          # We don't increase frame number in frame 10
-          # frame_num += 1
-          # Go to next roll, moving to roll2
-          on_roll = 2
-          next
-        end
-=end
         # Switch to roll2 for next iteration
         on_roll = 2
         next
