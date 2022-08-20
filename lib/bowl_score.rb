3a16157ab58992c3569b6302c65b3b5c0f43369b
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-20 13:06:46 +0100

    refactor and comments added

--- lib/bowl_score.rb
+++ lib/bowl_score.rb
@@ -1,6 +1,7 @@
 
+# rolls must have between 11 and 21 balls and all numbers must be between 0 and 10
 def rolls_valid?(rolls)
   return false if rolls.length < 11 || rolls.length > 21
-  rolls.each { |roll| return false if roll > 10 || roll < 0 }
+  return false if rolls.any? { |roll| roll > 10 || roll < 0 }
   true
 end
