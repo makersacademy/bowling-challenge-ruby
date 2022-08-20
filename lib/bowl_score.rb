694e69019f1207b52c56c2f27fa7bb35e380fabb
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-20 13:02:07 +0100

    5 rolls validation tests completed

--- lib/bowl_score.rb
+++ lib/bowl_score.rb
@@ -1,5 +1,6 @@
 
 def rolls_valid?(rolls)
-  rolls.each { |roll| return false if roll > 10 }
+  return false if rolls.length < 11 || rolls.length > 21
+  rolls.each { |roll| return false if roll > 10 || roll < 0 }
   true
 end
--- spec/bowl_score_spec.rb
+++ spec/bowl_score_spec.rb
@@ -1,15 +1,34 @@
 require 'bowl_score'
 
 RSpec.describe "bowl_score" do
-    context "checks numbers are between 0 and 10"do
-      it "returns true with [3, 5, 6, 4, 4, 8]" do
-        result = rolls_valid?([3, 5, 6, 4, 4, 8])
-        expect(result).to eq(true)
-      end
-
-      it "returns false with [11, 4, 6, 3, 6]" do
-        result = rolls_valid?([11, 4, 6, 3, 6])
-        expect(result).to eq(false)
-      end
+  context "checks rolls has between 11 and 21 balls" do
+    it "returns false for 6 balls" do
+      result = rolls_valid?([3, 5, 6, 4, 4, 8])
+      expect(result).to eq(false)
     end
+
+    it "returns false for 22 balls" do
+      rolls = []
+      22.times { rolls << 4 }
+      result = rolls_valid?(rolls)
+      expect(result).to eq(false)
+    end
+  end
+
+  context "checks numbers are between 0 and 10"do
+    it "returns true with [3, 5, 6, 4, 4, 8, 4, 5, 6, 3, 6]" do
+      result = rolls_valid?([3, 5, 6, 4, 4, 8, 4, 5, 6, 3, 6])
+      expect(result).to eq(true)
+    end
+
+    it "returns false with [11, 5, 6, 4, 4, 8, 4, 5, 6, 3, 6]" do
+      result = rolls_valid?([11, 5, 6, 4, 4, 8, 4, 5, 6, 3, 6])
+      expect(result).to eq(false)
+    end
+
+    it "returns false with [3, -4, 6, 4, 4, 8, 4, 5, 6, 3, 6]" do
+      result = rolls_valid?([3, -4, 6, 4, 4, 8, 4, 5, 6, 3, 6])
+      expect(result).to eq(false)
+    end
+  end
 end
