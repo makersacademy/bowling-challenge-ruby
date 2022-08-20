16a83d708bd4a22fcdfc4facc22277d00f5bf3f2
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-20 19:48:48 +0100

    refactor code

--- lib/bowl_score.rb
+++ lib/bowl_score.rb
@@ -3,27 +3,20 @@ def score(rolls)
   fail "Invalid rolls" unless rolls_valid?(rolls)
   game = to_frames(rolls)
 
-  bonus = game.map.each_with_index do |frame, ind|
-    # add in the next roll if this frame adds up to 10 and is not last frame
-    (frame.sum == 10 && ind < 9 ? game[ind + 1][0] : 0) +
+  bonus = (0..8).map do |ind|
+    # add in next roll if frame adds up to 10
+    (game[ind].sum == 10 ? game[ind + 1][0] : 0 ) +
+    # add second roll if strike
+    (game[ind][0] == 10 ? game[(ind + 1)..-1].flatten[1] : 0 )
+  end
 
-    # add second roll if strike and not last frame
-    (frame[0] == 10 && ind < 9 ? game[(ind + 1)..-1].flatten[1] : 0)
-  end.sum
-
-  rolls.sum + bonus
+  game.flatten.sum + bonus.sum
 end
 
-# convert rolls into frames - up to 3 rolls allowed in 10th frame
+# convert rolls into 10 frames - up to 3 rolls allowed in 10th frame
 def to_frames(rolls)
-  game = []
-  frame_num = 0
-  rolls.each do |roll|
-    game[frame_num] == nil ? game << [roll] : game[frame_num] << roll
-    frame_num += 1 if game[frame_num].length == 2 || game[frame_num].sum == 10
-    frame_num = 9 if frame_num > 9
-  end
-  game
+  result = (0..8).map { |frame| rolls[0] == 10 ? rolls.shift(1) : rolls.shift(2) }
+  result << rolls.shift(rolls.length) 
 end
 
 # rolls must have between 11 and 21 balls and all numbers must be between 0 and 10
