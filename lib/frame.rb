c32c8be45f27ef959ec42d4ac5bc56dd10ee9889
naomischlosser <naomi_schlosser@hotmail.com>
2022-08-21 08:27:12 +0100

    Refactoring of frame.rb

--- lib/frame.rb
+++ lib/frame.rb
@@ -2,17 +2,16 @@ class Frame
   def initialize(roll_1, roll_2)
     @roll_1 = roll_1
     @roll_2 = roll_2
-    @rolls = [roll_1, roll_2]
   end
 
-  attr_accessor :roll_1, :roll_2, :rolls
+  attr_accessor :roll_1, :roll_2
 
   def sum
-    return @rolls.sum
+    return @roll_1 + @roll_2
   end
 
   def spare?
-    return @roll_1 != 10 && @rolls.sum == 10
+    return @roll_1 != 10 && sum == 10
   end
 
   def strike?
