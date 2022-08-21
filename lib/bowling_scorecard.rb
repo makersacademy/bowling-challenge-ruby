74d16c01e436dbb29dd2e30fedef7d53aca3023d
Zac <zac@zacmoss.co.uk>
2022-08-21 17:36:24 +0100

    written test for values over 10

--- lib/bowling_scorecard.rb
+++ lib/bowling_scorecard.rb
@@ -3,9 +3,11 @@ require_relative 'frame'
 class BowlingScorecard
   def initialize(io = Kernel)
     @io = io
+    @frames = []
   end
-  
-  def roll(pinfall, frame)
+
+  def roll(frame)
+    pinfall = pinfall_check
     frame.rolls << pinfall
     if pinfall == 10
       frame.bonus_rolls += 2
@@ -16,5 +18,23 @@ class BowlingScorecard
 
   def frame_loop
     current_frame = Frame.new
+    roll_count = 1
+    until roll_count == 3
+      @io.puts "Enter roll #{roll_count} pinfall:"
+      roll(current_frame)
+      roll_count += 1
+    end
+    @frames << current_frame
+  end
+
+  private
+
+  def pinfall_check
+    loop do
+      value = @io.gets.to_i
+      return value unless value > 10
+
+      @io.puts 'Please enter a value of 10 or less'
+    end
   end
 end
--- spec/bowling_scorecard_spec.rb
+++ spec/bowling_scorecard_spec.rb
@@ -1,55 +1,65 @@
 require 'bowling_scorecard'
 require 'frame'
 
-describe BowlingScorecard do
+RSpec.describe BowlingScorecard do
+
   context 'roll method' do
     before(:each) do
-      @bowling_scorecard = BowlingScorecard.new
       @frame = Frame.new
+      @io = double :io
+      @bowling_scorecard = BowlingScorecard.new(@io)
     end
 
     it 'adds roll to the current Frame' do
-      @bowling_scorecard.roll(5, @frame)
+      expect(@io).to receive(:gets).and_return('5')
+      @bowling_scorecard.roll(@frame)
       expect(@frame.rolls).to eq [5]
-      @bowling_scorecard.roll(4, @frame)
+      expect(@io).to receive(:gets).and_return('4')
+      @bowling_scorecard.roll(@frame)
       expect(@frame.rolls).to eq [5, 4]
     end
 
     it 'adds 2 to frame bonus rolls if rolling a strike' do
-      @bowling_scorecard.roll(10, @frame)
+      expect(@io).to receive(:gets).and_return('10')
+      @bowling_scorecard.roll(@frame)
       expect(@frame.bonus_rolls).to eq 2
     end
 
     it 'adds 1 to frame bonus rolls if rolling a spare' do
-      @bowling_scorecard.roll(5, @frame)
-      @bowling_scorecard.roll(5, @frame)
+      expect(@io).to receive(:gets).and_return('5')
+      @bowling_scorecard.roll(@frame)
+      expect(@io).to receive(:gets).and_return('5')
+      @bowling_scorecard.roll(@frame)
       expect(@frame.bonus_rolls).to eq 1
     end
-
-    # it "decreases the previous frame's bonus_rolls in the frame array by 1 with each new roll" do
-    #   @frame.bonus_rolls = 2
-    #   new_frame = Frame.new
-    #   @bowling_scorecard.roll(5, new_frame)
-    #   expect(@frame.bonus_rolls).to eq 1
-    #   @bowling_scorecard.roll(5, new_frame)
-    #   expect(@frame.bonus_rolls).to eq 0
-    # end
   end
 
   context 'frame_loop method' do
-    before(:each)do
-        @io = double :io
-        @bowling_scorecard = BowlingScorecard.new(@io)
+    before(:each) do
+      @io = double :io
+      @bowling_scorecard = BowlingScorecard.new(@io)
     end
 
-    it 'asks for scores and then pushes a finished Frame to the frame array' do
-      expect(@io).to receive(:puts).with("Enter roll 1 pinfall:")
-      expect(@io).to receive(:gets).and_return("5")
-      expect(@io).to receive(:puts).with("Enter roll 2 pinfall:")
-      expect(@io).to receive(:gets).and_return("4")
+    it 'asks for pinfall and then pushes a finished Frame to the frame array' do
+      expect(@io).to receive(:puts).with('Enter roll 1 pinfall:')
+      expect(@io).to receive(:gets).and_return('5')
+      expect(@io).to receive(:puts).with('Enter roll 2 pinfall:')
+      expect(@io).to receive(:gets).and_return('4')
       result = @bowling_scorecard.frame_loop
       expect(result.length).to eq 1
       expect(result.first.rolls).to eq [5, 4]
+    end
+
+    it 'asks for the pinfall again if the value of the roll is more than ten' do
+      expect(@io).to receive(:puts).with('Enter roll 1 pinfall:')
+      expect(@io).to receive(:gets).and_return('11')
+      expect(@io).to receive(:puts).with('Please enter a value of 10 or less')
+      expect(@io).to receive(:gets).and_return('12')
+      expect(@io).to receive(:puts).with('Please enter a value of 10 or less')
+      expect(@io).to receive(:gets).and_return('5')
+      expect(@io).to receive(:puts).with('Enter roll 2 pinfall:')
+      expect(@io).to receive(:gets).and_return('5')
+      @bowling_scorecard.frame_loop
     end
   end
 end
