0b33682934b2bf6f0f01f20a359be25fbb3524d6
naomischlosser <naomi_schlosser@hotmail.com>
2022-08-21 15:49:50 +0100

    TDD scorecard tests for total score

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -9,7 +9,25 @@ class Scorecard
     @frames << frame
   end
 
-  def calculate_score
-    return 0
+  def calculate_frame(number)
+    if @frames[number - 1].strike?
+      if @frames[number].strike?
+        return 20 + @frames[number + 1].roll_1
+      end
+
+      return 10 + @frames[number].sum
+    elsif @frames[number - 1].spare?
+      return 10 + @frames[number].roll_1
+    else
+      return @frames[number - 1].sum
+    end
+  end
+
+  def calculate_total_score
+    score = (1..10).map do |number|
+      calculate_frame(number)
+    end
+
+    return score.sum
   end
 end
