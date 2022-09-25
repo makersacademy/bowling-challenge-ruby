b127f8b03388fd76bed3b56d0c7efcb90c38d6f1
Jovi <kwong.jovi@gmail.com>
2022-09-25 19:33:32 +0100

    refactor(Frame) Removed all methods, using attr_accessor only. Tests updated and passing.

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,4 +1,5 @@
 class Frame
+  # throw_three is for tenth frame
   def initialize
     @score = 0
     @throw_one = 0
@@ -6,22 +7,5 @@ class Frame
     @throw_three = 0
   end
 
-  attr_reader :score, :throw_one, :throw_two, :throw_three
-
-  def update_score(points)
-    @score += points
-  end
-
-  def update_throw_one(pins_knocked)
-    @throw_one += pins_knocked
-  end
-
-  def update_throw_two(pins_knocked)
-    @throw_two += pins_knocked
-  end
-
-  # For tenth frame if a third throw is allowed
-  def update_throw_three(pins_knocked)
-    @throw_three += pins_knocked
-  end
+  attr_accessor :score, :throw_one, :throw_two, :throw_three
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -11,32 +11,35 @@ RSpec.describe Frame do
     end
   end
 
-  context "#update_score" do
-    it "updates @score" do
+  context "@score" do
+    it "update @score" do
       frame = Frame.new
-      frame.update_score(5)
+      frame.score = (5)
       expect(frame.score).to eq (5)
     end
   end
 
-  context "#udpate_throw_one" do
+  context "@throw_one" do
     it "updates @throw_one" do
       frame = Frame.new
-      expect(frame.update_throw_one(5)).to eq (5)
+      frame.throw_one = 5
+      expect(frame.throw_one).to eq (5)
     end
   end
 
-  context "#udpate_throw_two" do
+  context "@throw_two" do
     it "updates @throw_two" do
       frame = Frame.new
-      expect(frame.update_throw_two(5)).to eq (5)
+      frame.throw_two = 5
+      expect(frame.throw_two).to eq (5)
     end
   end
 
-  context "#udpate_throw_three" do
+  context "@throw_three" do
     it "updates @throw_three" do
       frame = Frame.new
-      expect(frame.update_throw_three(5)).to eq (5)
+      frame.throw_three = 5
+      expect(frame.throw_three).to eq (5)
     end
   end  
 end
