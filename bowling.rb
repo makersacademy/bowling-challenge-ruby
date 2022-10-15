887c8faae614deeef50235d989a5593f14c066a9
Em Harrington <emilyharrington33@gmail.com>
2022-10-15 18:03:06 +0100

    completed

--- bowling.rb
+++ bowling.rb
@@ -1,16 +1,49 @@
 class Bowling
     def initialize
-        
+        @rolls = []
     end
 
-    def roll(turn)
-        if turn == 0
-            return "Gutter Game!"
-        elsif turn == 300
-            return "Perfect Game!"
+    def roll(pins)
+        @rolls << pins
+    end
+
+    def score
+        result = 0
+        rollIndex = 0
+        10.times do
+            if strike?(rollIndex)
+                result += strikeScore(rollIndex)
+                rollIndex += 1
+            elsif spare?(rollIndex)
+                result += spareScore(rollIndex)
+                rollIndex += 2
+            else
+                result += frameScore(rollIndex)
+                rollIndex += 2
+            end
         end
+        result
     end
 
-    #def total_score
-    #end
+    private
+
+    def spare?(rollIndex)
+        @rolls[rollIndex] + @rolls[rollIndex + 1] == 10
+    end
+
+    def spareScore(rollIndex)
+        @rolls[rollIndex] + @rolls[rollIndex + 1] + @rolls[rollIndex + 2]
+    end
+
+    def frameScore(rollIndex)
+        @rolls[rollIndex] + @rolls[rollIndex + 1]
+    end
+
+    def strike?(rollIndex)
+        @rolls[rollIndex] == 10
+    end
+
+    def strikeScore(rollIndex)
+        @rolls[rollIndex] + @rolls[rollIndex + 1] + @rolls[rollIndex + 2]
+    end
 end
