bc73f877a65a0154032cae3641cc89830cdaa611
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-15 11:23:22 +0100

    Add checks to prevent 11th frame

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -6,6 +6,8 @@ class Bowling
   attr_accessor :score_card
 
   def frame
+    if @score_card.length >= 9 then return 10 end
+
     @score_card.length + 1 
   end
 
@@ -15,14 +17,16 @@ class Bowling
   end
 
   def add_score(first = 0, second = 0)
+    if @score_card.length == 10 && @score_card.last.sum == 10
+      @score_card.last.push(first)
+    elsif @score_card.length == 10
+      puts 'Game Over'
+    else
     @score_card << [first, second]
+    end
   end
 
   def check_strike(arr)
     arr[0] == 10
   end
 end
-
-bowling = Bowling.new
-
-puts bowling.frame
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Bowling do
   describe '#frame' do
     it 'returns the correct frame' do
       bowling = Bowling.new
+      expect(bowling.frame).to eq(1)
       bowling.add_score(2, 4)
       expect(bowling.frame).to eq(2)
       bowling.add_score(2, 5)
@@ -11,6 +12,15 @@ RSpec.describe Bowling do
       bowling.add_score(7, 2)
       bowling.add_score(1, 5)
       expect(bowling.frame).to eq(5)
+      bowling.add_score(7, 2)
+      bowling.add_score(1, 5)
+      bowling.add_score(7, 2)
+      bowling.add_score(1, 5)
+      bowling.add_score(7, 2)
+      expect(bowling.frame).to eq(10)
+      bowling.add_score(1, 5) # The game goes up to 10 frames so there shouldn't be an 11th
+      p bowling.score_card
+      expect(bowling.frame).to eq(10)
     end
   end
 
@@ -41,6 +51,42 @@ RSpec.describe Bowling do
       bowling.add_score(7, 2)
       bowling.add_score(1, 6)
       expect(bowling.score_card).to eq([[1,2], [3,4], [5,5], [7,2], [1,6]])
+      #expect(bowling.score).to eq 36
+    end
+
+    it 'pushes the score onto the last element of the array if the score
+    card is 10 long and 10th frame is a spare' do
+      bowling = Bowling.new
+      bowling.add_score(0, 10)
+      bowling.add_score(10, 0)
+      bowling.add_score(3, 3)
+      bowling.add_score(2, 8)
+      bowling.add_score(3, 3)
+      bowling.add_score(4, 3)
+      bowling.add_score(3, 3)
+      bowling.add_score(3, 7)
+      bowling.add_score(3, 3)
+      bowling.add_score(5, 5) # 10th frame is a spare
+      bowling.add_score(6)
+      expect(bowling.score_card.length).to eq 10
+      expect(bowling.score_card.last).to eq [5, 5, 6]
+    end
+
+    it 'Rejects the last score if 10th frame is not a spare' do
+      bowling = Bowling.new
+      bowling.add_score(0, 10)
+      bowling.add_score(10, 0)
+      bowling.add_score(3, 3)
+      bowling.add_score(2, 8)
+      bowling.add_score(3, 3)
+      bowling.add_score(4, 3)
+      bowling.add_score(3, 3)
+      bowling.add_score(3, 7)
+      bowling.add_score(3, 3)
+      bowling.add_score(5, 4) # 10th frame is a spare
+      bowling.add_score(6)
+      expect(bowling.score_card.last).to eq [5, 4]
+      expect(bowling.score_card.length).to eq 10
     end
   end
 end
