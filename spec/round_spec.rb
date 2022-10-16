8cd5a96c774c7104ea3f369a90d38b468efa21d8
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-16 14:07:00 +0100

    Add test to check if add_pins sets spare to true

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -17,14 +17,23 @@ describe Round do
 
           expect(@current_round.round_pins).to eq([1, 4])
       end
-    end
+    
 
-  describe "#strike" do
-    it "sets strike to true" do
-      pins_knocked = 10
-      @current_round.add_pins(pins_knocked)
-  
-      expect(@current_round.strike).to eq(true)
-    end
+      it "sets strike to true" do
+        pins_knocked = 10
+        @current_round.add_pins(pins_knocked)
+    
+        expect(@current_round.strike).to eq(true)
+      end
+
+      it "sets spare to true" do
+        pins_knocked = 3
+        @current_round.add_pins(pins_knocked)
+        pins_knocked = 7
+        @current_round.add_pins(pins_knocked)
+
+        expect(@current_round.spare).to eq(true)
+        expect(@current_round.strike).to eq(false)
+      end
   end
 end
