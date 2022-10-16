818f9a202dffbae14c2d1e93be05b856bcc18f48
JoeOsborne77 <joeosborne77@gmail.com>
2022-10-16 20:37:23 +0100

    strike function working, working on spare

--- app.rb
+++ app.rb
@@ -1,52 +1,38 @@
 class Application
-  def initialize(scorecard, round)
-    @scorecard = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    @round = 1 
+  def initialize(score)
+    @scorecard = []
   end
+  
+  def play
+    round = 1 
+    10.times do
+      puts "Please enter the score from your first roll from round #{round}"  
+      @roll_1 = gets.chomp.to_i
+        if @roll_1 < 10
+          @scorecard << @roll_1
+      puts "Please enter the score from your second roll from round #{round}"  
+      @roll_2 = gets.chomp.to_i
+        @scorecard << @roll_2
+        round += 1 
 
-  def play_1
-    while @round <= 10 do
-    if @round == 10 
-      puts "Thanks for playing. Your total score is #{@scorecard.sum}"
-    end
-    puts "Please enter the score from your first roll from @round #{@round}"  
-    @roll_1 = gets.chomp.to_i
-      if @roll_1 < 10
-        @scorecard << @roll_1
-        play_2
-      else 
-        @scorecard << @roll_1
-        @scorecard << 0
-        @round += 1
-        x = @scorecard.each_index.select { |ind| @scorecard[ind] == 10 }
-        x.each { |index| @scorecard.insert(index, @scorecard[index+1] + @scorecard[index+2])}
-      end
-    end
-  end
-          
-  def play_2
-    puts "Please enter the score from your second roll from @round #{@round}"  
-    @roll_2 = gets.chomp.to_i
-    @round += 1 
-    if @roll_1 + @roll_2 == 10
-      @scorecard << @roll_2
-      spare
-      puts "Tally so far is #{@scorecard}"  
-    end
-    play_1
-  end
+        puts "Total so far is #{@scorecard}"
 
-  # def spare
-  #   a = @scorecard.count(10)
-  #   @scorecard.delete(10)
-  #   @scorecard.each_slice(2).to_a
-  #   @scorecard.each { |spare| if spare.sum == 10
-  #           @scorecard.insert(0, spare+1].sum)
-  #           end
-  #         }
-  #   @scorecard.insert(0, a * 10) 
-  # end
+        elsif @roll_1 == 10
+          @scorecard << @roll_1        
+          round += 1 
+        end
+    end
+
+    x = @scorecard.each_index.select { |ind| @scorecard[ind] == 10 }
+    x.each { |ind| @scorecard.insert(ind, @scorecard[ind+1] + @scorecard[ind+2])}
+
+   
+    puts @scorecard.sum
+  end 
 end
+
+
+
        
-app = Application.new(@scorecard, @round)
-app.play_1
+app = Application.new(@score)
+app.play
---
+++ application.rb
@@ -0,0 +1,57 @@
+class Application
+  def initialize(scorecard, round)
+    @scorecard = []
+    @round = 1 
+  end
+
+  def first_throw
+    while @round <= 11 do
+    if @round == 11
+      strike_calculator
+      puts "Thanks for playing. Your total score is #{@scorecard.sum}"
+      break
+    end
+    
+    puts "Please enter the score from your first roll from @round #{@round}"  
+    @roll_1 = gets.chomp.to_i
+      if @roll_1 < 10
+        @scorecard << @roll_1
+        second_throw
+      else
+        @scorecard << @roll_1        
+        round += 1 
+      end
+    end
+  end
+
+  def strike_calculator
+    x = @scorecard.each_index.select { |ind| @scorecard[ind] == 10 }
+    x.each { |ind| @scorecard.insert(ind, @scorecard[ind+1] + @scorecard[ind+2])}
+  end
+          
+  def second_throw
+    puts "Please enter the score from your second roll from @round #{@round}"  
+    @roll_2 = gets.chomp.to_i
+    @round += 1 
+    if @roll_1 + @roll_2 == 10
+      @scorecard << @roll_2
+      spare
+      puts "Tally so far is #{@scorecard}"  
+    end
+    first_throw
+  end
+
+  # def spare
+  #   a = @scorecard.count(10)
+  #   @scorecard.delete(10)
+  #   @scorecard.each_slice(2).to_a
+  #   @scorecard.each { |spare| if spare.sum == 10
+  #           @scorecard.insert(0, spare+1].sum)
+  #           end
+  #         }
+  #   @scorecard.insert(0, a * 10) 
+  # end
+end
+       
+app = Application.new(@scorecard, @round)
+app.first_throw
