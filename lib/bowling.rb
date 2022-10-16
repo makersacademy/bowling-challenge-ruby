407036ae9de72f57f19b663eab763ce7b5cd6a1e
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-16 20:56:30 +0100

    Add #check_strike/spare/bonus_score

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -29,4 +29,26 @@ class Bowling
   def check_strike(arr)
     arr[0] == 10
   end
+
+  def check_spare(arr)
+    arr[0] + arr[1] == 10 && arr[0] != 10
+  end
+
+  def bonus_score
+    bonus_score = 0
+    iterator = 0 
+    while iterator < @score_card.length do
+      if check_strike(@score_card[iterator])
+        # strikes need to add the score of the next two frames to the current frame
+        # To simulate, the next two frames get doubled score in the form of
+        # bonus score.
+        bonus_score += @score_card[iterator + 1].sum
+        bonus_score += @score_card[iterator + 2].sum
+      elsif check_spare(@score_card[iterator])
+        bonus_score += @score_card[iterator + 1].sum
+      end
+      iterator == 1
+    end
+    bonus_score
+  end
 end
