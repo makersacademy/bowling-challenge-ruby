bdb5746d34a80ded118840c7c68b7ed2fbc74d79
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-10-16 16:55:33 +0100

    Basic UI finished

--- controller/ten_pin_con.rb
+++ controller/ten_pin_con.rb
@@ -5,36 +5,77 @@ class TenPinCon
 
     end
 
+    def input_get
+        puts "Enter Roll 0-10, please only type numbers 0-10" 
+        input = gets.chomp
+        if ("0".."10").include?(input)
+            # return input
+            @input = input.to_i
+            return @input
+        else
+            input_get
+        end
+    end
+
+    def input_get_2
+        puts "next input number cannot be more than No of pins left: #{@pins_left}"
+        input_get
+        input_get_2 if @input > @pins_left
+    end 
+
+    def strike
+        puts 'Strike!'
+        @score.add_roll(10)
+        @score.add_roll(0)
+    end
+
+    def non_strike
+        @score.add_roll(@input)
+        @pins_left = (10 - @input)
+        input_get_2
+        puts "Spare!" if @input == @pins_left
+        @score.add_roll(@input)
+    end
+
     def run
         @score = TenPinScore.new
         @score.format_arrays
         roll_counter = 0 
         puts 'New Game'
         9.times do 
-            puts 'Enter Roll 0-10'
-            loop do
-                puts "Please only type numbers 0-10" 
-                @input = gets.chomp
-                break if (0..10).include?(@input.to_i)
-                
-            end
-            if @input.to_i == 10
-                puts 'Strike!'
-                @score.add_roll(10)
-                @score.add_roll(0)
+            input_get
+            if @input == 10
+                strike
             else
-                @score.add_roll(@input.to_i)
-                loop do 
-                    @pins_left = (10 - @input.to_i)
-                    @input = gets.chomp
-                    break if (0..10).include?(@input.to_i) && @input.to_i <= @pins_left
-                    puts "Please only type numbers 0-10 number cannot be more than No of pins #{@pins_left}"
-                end
-                puts "Spare!" if @input.to_i == @pins_left
-                @score.add_roll(@input.to_i)
+                @score.add_roll(@input)
+                @pins_left = (10 - @input)
+                input_get_2
+                puts "Spare!" if @input == @pins_left
+                @score.add_roll(@input)
             end
         end
+        input_get
+        if @input == 10
+            puts 'Strike! Extra 2 rolls!'
+            @score.add_roll(10)
+            input_get 
+            @score.add_roll(@input)
+            input_get 
+            @score.add_roll(@input)
+            puts "Game over"
+        else
+            @score.add_roll(@input)
+            @pins_left = (10 - @input)
+            input_get_2
+            puts "Spare!" if @input == @pins_left
+            @score.add_roll(@input)            
+            puts "Extra roll"
+            @score.add_roll(@input)
+            puts "Game over"
+        end
+        print @score.total_score 
     end
+    
 end
 
 new_game = TenPinCon.new
