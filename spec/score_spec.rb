d6e1861de32a825f987a1f0874d9ed124cf5bcd5
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-16 17:13:00 +0100

    Add another passing test for calculating scores when user enters two strikes on a row

--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -14,13 +14,36 @@ describe PlayerGameScore do
       expect(@game_score.round_scores).to eq([7])
     end
 
-    it "duplicates adds the next frame's points to the total points of a frame when strike" do
+    it "adds the next frame's points to the total points of a frame when strike" do
       fake_round1 = double(fake_round1, :round_pins => [10], strike: true)
       @game_score.add_round(fake_round1)
 
       fake_round2 = double(fake_round2, :round_pins => [2, 5], strike: false)
       @game_score.add_round(fake_round2)
 
+      expect(@game_score.round_scores).to eq([17, 7])
+    end
+
+    it "calculates points correctly when user enters two strikes on a row" do
+      fake_round1 = double(fake_round1, :round_pins => [10], strike: true)
+      @game_score.add_round(fake_round1)
+
+      fake_round2 = double(fake_round2, :round_pins => [10], strike: true)
+      @game_score.add_round(fake_round2)
+
+      fake_round2 = double(fake_round2, :round_pins => [2, 5], strike: false)
+      @game_score.add_round(fake_round2)
+
+      expect(@game_score.round_scores).to eq([20, 17, 7])
+    end
+
+    xit "adds the next frame's first roll points when spare" do
+      fake_round1 = double(fake_round1, :round_pins => [10], strike: true)
+      @game_score.add_round(fake_round1)
+  
+      fake_round2 = double(fake_round2, :round_pins => [2, 5], strike: false)
+      @game_score.add_round(fake_round2)
+  
       expect(@game_score.round_scores).to eq([17, 7])
     end
   end
