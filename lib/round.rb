34601676eab26600760e2309ee719bc84995463e
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-16 14:17:04 +0100

    Modify add_pins method to detect if spare and set variable to true

--- lib/round.rb
+++ lib/round.rb
@@ -5,13 +5,18 @@ class Round
     @round_pins = []
     @strike = false
     @spare = false
+    @counter = 1
   end
 
   def add_pins(pins_knocked)
     @round_pins << pins_knocked
     @strike = true if @round_pins[0] == 10
+    if @counter == 2
+      if (@round_pins[0] + @round_pins[1]) == 10
+        @spare = true 
+      end
+      @counter = 0
+    end
+    @counter += 1
   end
-
-
-  
 end
