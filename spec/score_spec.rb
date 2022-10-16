0aa6456f712df775711784010becc12a72061ba2
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-16 15:54:50 +0100

    Add second test for add_round to add next round's scores to current round when strike

--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -1,16 +1,27 @@
 require_relative "../lib/score"
 
-describe Score do
+describe PlayerGameScore do
   before (:each) do
     @game_score = PlayerGameScore.new
   end
 
   describe "#add_round" do
-    it "adds the current round pins to the round_scores array" do
+    it "adds the current frame's pins to the round_scores array" do
       fake_round = double(fake_round, :round_pins => [2, 5])
+    
       @game_score.add_round(fake_round)
 
-      expect(@game_score.round_scores).to eq([2, 5])
+      expect(@game_score.round_scores).to eq([7])
+    end
+
+    it "duplicates the next frame's points when strike" do
+      fake_round1 = double(fake_round, :round_pins => [10])
+      @game_score.add_round(fake_round1)
+
+      fake_round2 = double(fake_round, :round_pins => [2, 5])
+      @game_score.add_round(fake_round2)
+
+      expect(@game_score.round_scores).to eq([17, 7])
     end
   end
 end
