81e3ebbbd30ff02b17201a7fd4a292b6585a4967
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-16 17:33:39 +0100

    Add passing test to check if add_round calculates points correctly when two spares on a row

--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -46,5 +46,18 @@ describe PlayerGameScore do
   
       expect(@game_score.round_scores).to eq([12, 7])
     end
+
+    it "add the right number of points when two spares on a row" do
+      fake_round1 = double(fake_round1, :round_pins => [4, 6], strike: false, spare: true)
+      @game_score.add_round(fake_round1)
+
+      fake_round2 = double(fake_round2, :round_pins => [3, 7], strike: false, spare: true)
+      @game_score.add_round(fake_round2)
+
+      fake_round3 = double(fake_round3, :round_pins => [1, 3], strike: false, spare: false)
+      @game_score.add_round(fake_round3)
+
+      expect(@game_score.round_scores).to eq([13, 11, 4])
+    end
   end
 end
