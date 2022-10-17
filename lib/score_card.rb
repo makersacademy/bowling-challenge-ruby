e9f97e1a31eef0c26be5580d5ad5ab4ddf26afdf
Na-Rae Kim <narae1096@gmail.com>
2022-10-17 09:42:50 +0100

    calculate spare score

--- lib/score_card.rb
+++ lib/score_card.rb
@@ -13,21 +13,25 @@ class ScoreCard
 
   def total_score
     score = 0
-    @frames.each do |frame|
+    @frames.each_with_index do |frame, i|
+      if frame.spare? && frame != last_bonus_game
+        score += bonus_score_spare(frame, i)
+      elsif !frame.spare? && frame != last_bonus_game
       score += frame.score
+      end
     end
     return score
   end
 
-  def last_bonus_game?
+  def last_bonus_game
     if @frames.size == 11
       @frames.last
     end
   end
+  
+  private
 
-  def bonus_score_strike
-  end
-
-  def bonus_score_spare
+  def bonus_score_spare(frame, i)
+    frame.score + @frames[i + 1].first_roll
   end
 end
