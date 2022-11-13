457c0a1e52d233041afb30a88236028bdb5dd1bd
adonl <adnlwly@gmail.com>
2022-11-13 17:26:05 +0000

    add collect scores method

--- lib/bowling_scoring.rb
+++ lib/bowling_scoring.rb
@@ -2,6 +2,43 @@ class BowlingScoring
 
   def initialize
     @score = []
+    @scorecard = []
+    @frames = 0
+  end
+
+  def collect_scores(terminal)
+    @terminal = terminal
+  # First 9 frames
+    9.times do
+      @terminal.puts "Frame #{@frames + 1}: How many pins did your 1st bowl knock down?"
+      first_bowl = @terminal.gets.chomp.to_i
+       if first_bowl == 10
+        @scorecard << [first_bowl, 0]
+       else
+        @terminal.puts "Frame #{@frames + 1}: How many pins did your 2nd bowl knock down?"
+        second_bowl = @terminal.gets.chomp.to_i
+        @scorecard << [first_bowl, second_bowl]
+       end
+      @frames += 1
+    end
+
+  # Final (10th) frame
+    @terminal.puts "Last frame! How many pins did your 1st bowl knock down?"
+    first_bowl = @terminal.gets.chomp.to_i
+    @terminal.puts "Last frame! How many pins did your 2nd bowl knock down?"
+    second_bowl = @terminal.gets.chomp.to_i
+    if first_bowl == 10 || first_bowl + second_bowl == 10
+      @terminal.puts "Last frame! How many pins did your bonus bowl knock down?"
+      bonus_bowl = @terminal.gets.chomp.to_i
+      @scorecard << [first_bowl, second_bowl, bonus_bowl]
+      @frames += 1
+    else
+      @scorecard << [first_bowl, second_bowl]
+      @frames += 1
+    end
+
+    @terminal.puts "Your scorecard: #{@scorecard}" 
+    @terminal.puts "Your final score: #{calculate(@scorecard)}" 
   end
 
   def calculate(frames)
--- spec/bowling_scoring_spec.rb
+++ spec/bowling_scoring_spec.rb
@@ -67,6 +67,51 @@ describe BowlingScoring do
       result = scorecard.calculate([[10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10]])
       expect(result).to eq(300)
     end
+  end
 
+  context 'when collecting score user input' do
+    it 'collects scores for 10 frames of bowls' do
+      terminal = double(:terminal)
+      expect(terminal).to receive(:puts).with("Frame 1: How many pins did your 1st bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("2").ordered
+      expect(terminal).to receive(:puts).with("Frame 1: How many pins did your 2nd bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("8").ordered
+      expect(terminal).to receive(:puts).with("Frame 2: How many pins did your 1st bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("10").ordered
+      expect(terminal).to receive(:puts).with("Frame 3: How many pins did your 1st bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("10").ordered
+      expect(terminal).to receive(:puts).with("Frame 4: How many pins did your 1st bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("3").ordered
+      expect(terminal).to receive(:puts).with("Frame 4: How many pins did your 2nd bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("7").ordered
+      expect(terminal).to receive(:puts).with("Frame 5: How many pins did your 1st bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("5").ordered
+      expect(terminal).to receive(:puts).with("Frame 5: How many pins did your 2nd bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("5").ordered
+      expect(terminal).to receive(:puts).with("Frame 6: How many pins did your 1st bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("2").ordered
+      expect(terminal).to receive(:puts).with("Frame 6: How many pins did your 2nd bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("8").ordered
+      expect(terminal).to receive(:puts).with("Frame 7: How many pins did your 1st bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("8").ordered
+      expect(terminal).to receive(:puts).with("Frame 7: How many pins did your 2nd bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("1").ordered
+      expect(terminal).to receive(:puts).with("Frame 8: How many pins did your 1st bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("6").ordered
+      expect(terminal).to receive(:puts).with("Frame 8: How many pins did your 2nd bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("2").ordered
+      expect(terminal).to receive(:puts).with("Frame 9: How many pins did your 1st bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("10").ordered
+      expect(terminal).to receive(:puts).with("Last frame! How many pins did your 1st bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("5").ordered
+      expect(terminal).to receive(:puts).with("Last frame! How many pins did your 2nd bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("5").ordered
+      expect(terminal).to receive(:puts).with("Last frame! How many pins did your bonus bowl knock down?").ordered
+      expect(terminal).to receive(:gets).and_return("10").ordered
+      expect(terminal).to receive(:puts).with("Your scorecard: [[2, 8], [10, 0], [10, 0], [3, 7], [5, 5], [2, 8], [8, 1], [6, 2], [10, 0], [5, 5, 10]]").ordered
+      expect(terminal).to receive(:puts).with("Your final score: 172").ordered
+      new_game = BowlingScoring.new
+      new_game.collect_scores(terminal)
+    end
   end
 end
