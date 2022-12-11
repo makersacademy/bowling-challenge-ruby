17bd11cb16044fa8d98d6a2ecc1f35a3c73af4eb
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-11 12:54:33 +0000

    Keeps track of scores and checks for valid input

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,13 +1,26 @@
 class Frame
   def initialize
     @score = 0
+    @rolls = 0
   end
 
   def add_roll(roll)
-    @score += roll
+    raise "Only two rolls can be made per frame" if @rolls >= 2
+    raise ArgumentError.new "A roll must be an integer" unless integer_roll?(roll)
+    raise ArgumentError.new "A roll must be between 0 and 10" if roll < 0 || roll > 10
+    raise "Both rolls cannot add up to more than 10" if @score + roll > 10
+    
+    @score += roll.to_i
+    @rolls += 1
   end
 
   def score
     return @score
+  end
+
+  private
+
+  def integer_roll?(roll)
+    return roll.is_a?(Integer) || roll % 1 == 0.0
   end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -7,8 +7,30 @@ RSpec.describe Frame do
       frame.add_roll(5)
       expect(frame.score).to eq 5
     end
-  end
 
+    it "fails unless the roll is an integer" do
+      frame = Frame.new
+
+      error_message = "A roll must be an integer"
+      expect { frame.add_roll(1.5) }.to raise_error ArgumentError, error_message
+      expect { frame.add_roll("Hello world") }.to raise_error ArgumentError, error_message
+      
+      expect { frame.add_roll(1) }.to_not raise_error
+      expect(frame.score).to be 1
+
+      expect { frame.add_roll(1.0) }.to_not raise_error
+      expect(frame.score).to be 2
+    end
+    
+    it "fails unless roll is between 0 and 10" do
+      frame = Frame.new
+
+      error_message = "A roll must be between 0 and 10"
+      expect { frame.add_roll(-1) }.to raise_error ArgumentError, error_message
+      expect { frame.add_roll(11) }.to raise_error ArgumentError, error_message
+    end
+  end
+  
   context "Adds 2 rolls" do
     it "score updates when adding a roll of 5 and then 4" do
       frame = Frame.new
@@ -18,6 +40,49 @@ RSpec.describe Frame do
       
       frame.add_roll(4)
       expect(frame.score).to eq 9
+    end
+
+    it "fails if the second roll is not between 0 and 10" do
+      frame = Frame.new
+
+      frame.add_roll(0)
+      expect(frame.score).to eq 0
+
+      error_message = "A roll must be between 0 and 10"
+      expect { frame.add_roll(-1) }.to raise_error ArgumentError, error_message
+
+      frame = Frame.new
+
+      frame.add_roll(0)
+      expect(frame.score).to eq 0
+
+      error_message = "A roll must be between 0 and 10"
+      expect { frame.add_roll(-1) }.to raise_error ArgumentError, error_message
+    end
+
+    it "fails if both rolls add up to more than 10" do
+      frame = Frame.new
+
+      frame.add_roll(5)
+      expect(frame.score).to eq 5
+
+      error_message = "Both rolls cannot add up to more than 10"
+      expect { frame.add_roll(7) }.to raise_error error_message
+    end
+  end
+  
+  context "Adds 3 rolls" do
+    it "raises error since only 2 rolls can be added per frame" do
+      frame = Frame.new
+      
+      frame.add_roll(5)
+      expect(frame.score).to eq 5
+      
+      frame.add_roll(4)
+      expect(frame.score).to eq 9
+      
+      error_message = "Only two rolls can be made per frame"
+      expect { frame.add_roll(1) }.to raise_error error_message
     end
   end
 end
