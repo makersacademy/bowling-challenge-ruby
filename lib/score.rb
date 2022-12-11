91bf6f71da2f74f4a788c2cbf54e7072da02f875
Cecily Rowland <cec1rowland@gmail.com>
2022-12-11 14:34:14 +0000

    Made bonus_score and sum_score private

--- lib/score.rb
+++ lib/score.rb
@@ -6,6 +6,12 @@ class Score
     @frames = frames
   end 
 
+  def total_score
+    sum_score + bonus_score
+  end
+
+  private
+
   def bonus_score
     bonus_score = 0
     @frames.each_with_index do |frame, index|
@@ -21,8 +27,4 @@ class Score
   def sum_score
     @frames.sum { |frame| frame.frame_score }
   end 
-
-  def total_score
-    sum_score + bonus_score
-  end
 end 
