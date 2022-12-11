609476ef48857d06693c7d83c3d58734427ee29e
Marina Papapetrou <contact@marinapapap.com>
2022-12-11 11:51:57 +0000

    returns score for a game with no strikes or spares

--- spec/game_integration_spec.rb
+++ spec/game_integration_spec.rb
@@ -58,4 +58,60 @@ RSpec.describe "it runs the game" do
 
     expect(game.total_score).to eq 0
   end
+
+  it "returns the score for a gutter game" do
+    frame_1 = Frame.new
+    frame_1.add(2)
+    frame_1.add(1)
+
+    frame_2 = Frame.new
+    frame_2.add(3)
+    frame_2.add(2)
+
+    frame_3 = Frame.new
+    frame_3.add(4)
+    frame_3.add(1)
+
+    frame_4 = Frame.new
+    frame_4.add(2)
+    frame_4.add(4)
+
+    frame_5 = Frame.new
+    frame_5.add(6)
+    frame_5.add(1)
+
+    frame_6 = Frame.new
+    frame_6.add(4)
+    frame_6.add(3)
+
+    frame_7 = Frame.new
+    frame_7.add(1)
+    frame_7.add(1)
+
+    frame_8 = Frame.new
+    frame_8.add(0)
+    frame_8.add(3)
+
+    frame_9 = Frame.new
+    frame_9.add(1)
+    frame_9.add(0)
+
+    frame_10 = Frame.new
+    frame_10.add(5)
+    frame_10.add(3)
+
+    game = Game.new
+    game.add_frame(frame_1)
+    game.add_frame(frame_2)
+    game.add_frame(frame_3)
+    game.add_frame(frame_4)
+    game.add_frame(frame_5)
+    game.add_frame(frame_6)
+    game.add_frame(frame_7)
+    game.add_frame(frame_8)
+    game.add_frame(frame_9)
+    game.add_frame(frame_10)
+
+    expect(game.total_score).to eq 47
+  end
 end
