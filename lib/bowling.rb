f57343dc6f9c974f878d37a2cb6871e486404a93
JC Morgan <jcmorgan@mastudemaxzq6l4.home>
2022-12-11 23:16:52 +0000

    Bowling + Spec (Ruby)

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,31 +1,47 @@
 class BowlingScore
 
   def initialize(sheet) # where sheet is an array of scores
-    @score_sheet = sheet
-    @fail = false
+    @score_sheet = sheet.each_slice(2).to_a
+    @total_score = 0
   end
 
-  def fail_check
+  def special_check
+    return "Total Score: 0 (Gutter Game)" if @total_score == 0
+    return "Total Score: 300 (Perfect Game!)" if @total_score == 300
+  end
 
-    @score_sheet.each do |score|
-      if score != score.to_i || score > 10 || score < 0
-        @fail = true
+  def get_total
+    frame = 0
+    while frame <= 9 # frame will represent the index of the first 10 frames (0-9)
+
+      score_one = (@score_sheet[frame][0])  # stores the scores of each frame
+      score_two = (@score_sheet[frame][1])
+
+      next_frame = frame + 1
+
+      if score_one == 10  # (if strike) add ten + next two scores
+        @total_score += 10 + @score_sheet[next_frame][0]
+
+        if @score_sheet[next_frame][1] == "-"
+          @total_score += @score_sheet[next_frame+1][0]
+        else
+          @total_score += @score_sheet[next_frame][1]
+        end
+
+      elsif score_one + score_two == 10 # (if spare) add ten + next score
+        @total_score += 10 + @score_sheet[next_frame][0]
+      else
+        @total_score += score_one + score_two  # (else) adds scores as normal
       end
+
+      frame += 1  # on to the next frame
     end
-
-    fail "Score Sheet Provided is Not Readable." if @fail == true || @score_sheet.length > 21 || @score_sheet.length < 11
-
-  end
-
-  def quick_check
-    return "Total Score: 0 (Gutter Game)" if @score_sheet.sum == 0
   end
 
   def final_score
-    fail_check
-    return quick_check unless nil
-
-    #return "Total Score: 300 (Perfect Game!)"
+    get_total
+    return special_check unless special_check == nil
+    return "Total Score: #{@total_score}"
   end
 
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,18 +2,8 @@ require 'bowling'
 
 describe BowlingScore do
 
-  it "returns error if score sheet is not formatted correctly/ empty" do
-    score_sheet = []
-    score = BowlingScore.new(score_sheet)
-    expect { score.final_score }.to raise_error "Score Sheet Provided is Not Readable."
-
-    score_sheet = ["hi", 0, -12, 0, 0, 0, 0, 0, 0, 0, 0]
-    score = BowlingScore.new(score_sheet)
-    expect { score.final_score }.to raise_error "Score Sheet Provided is Not Readable."
-  end
-
   it "returns perfect game when score is 300" do
-    score_sheet = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+    score_sheet = [10, '-', 10, '-', 10, '-', 10, '-', 10, '-', 10, '-', 10, '-', 10, '-', 10, '-', 10, '-', 10, 10]
     score = BowlingScore.new(score_sheet)
     expect(score.final_score).to eq "Total Score: 300 (Perfect Game!)"
   end
@@ -31,9 +21,15 @@ describe BowlingScore do
   end
 
   it "returns the correct score for a game with some strikes and spares" do
-    score_sheet = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6]
+    score_sheet = [1, 4, 4, 5, 6, 4, 5, 5, 10, '-', 0, 1, 7, 3, 6, 4, 10, '-', 2, 8, 6]
     score = BowlingScore.new(score_sheet)
     expect(score.final_score).to eq "Total Score: 133"
+  end
+
+  it "returns the correct score for a game with all spares" do
+    score_sheet = [1,9,2,8,4,6,3,7,8,2,5,5,6,4,9,1,3,7,5,5,4]
+    score = BowlingScore.new(score_sheet)
+    expect(score.final_score).to eq "Total Score: 149"
   end
 
 end
