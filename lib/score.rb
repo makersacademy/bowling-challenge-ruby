32312e0f307c7a0fdf7568e68747e72c4fa6ed3a
Cecily Rowland <cec1rowland@gmail.com>
2022-12-11 14:31:19 +0000

    Wrote bonus_score, sum_score and total_score methods for Score class and successfully tested

--- lib/score.rb
+++ lib/score.rb
@@ -1,7 +1,28 @@
+require_relative './frame'
+
 class Score 
 
-  def total_score
+  def initialize(frames)
+    @frames = frames
+  end 
 
+  def bonus_score
+    bonus_score = 0
+    @frames.each_with_index do |frame, index|
+      if frame.first_roll == 10 
+        bonus_score =+ @frames[index+1].frame_score
+      elsif frame.first_roll + frame.second_roll == 10
+        bonus_score =+ @frames[index+1].first_roll
+      end
+    end
+    bonus_score
+  end 
+
+  def sum_score
+    @frames.sum { |frame| frame.frame_score }
+  end 
+
+  def total_score
+    sum_score + bonus_score
   end
-  
 end 
