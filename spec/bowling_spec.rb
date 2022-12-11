c3dc02e3187af516617dc4ad8a8c5a11144d9484
Magdaline <mamoakohene@gmail.com>
2022-12-11 21:22:21 +0000

    added tests for 10th frame - fail

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,17 +2,16 @@ require 'bowling'
 
 describe Scorecard do
 
-  context 'points' do
+  xcontext 'points' do
     it 'shows zero points overall if player has not scored any points' do
+      #gutter game
       io = double :io
 
-      #submit 0 scores only
       20.times {expect(io).to receive(:gets).and_return("0")}
 
       result = Scorecard.new(io)
-      result.run
 
-      expect(result.grand_total).to eq 0
+      expect(result.run).to eq 0
     end
 
     it 'shows the grand total if player has scored points (without bonuses)' do
@@ -22,14 +21,13 @@ describe Scorecard do
       expect(io).to receive(:gets).and_return("5")}
 
       result = Scorecard.new(io)
-      result.run
 
-      expect(result.grand_total).to eq 90
+      expect(result.run).to eq 90
     end
   end
 
 
-  context 'bonuses' do
+  xcontext 'bonuses' do
     it 'gives a bonus where player scores a strike' do
       io = double :io
 
@@ -37,9 +35,8 @@ describe Scorecard do
       2.times {expect(io).to receive(:gets).and_return("0")}
 
       result = Scorecard.new(io)
-      result.run
 
-      expect(result.grand_total).to eq 240
+      expect(result.run).to eq 240
     end
 
     it 'gives a bonus where player scores a spare' do
@@ -49,10 +46,54 @@ describe Scorecard do
       expect(io).to receive(:gets).and_return("0")
 
       result = Scorecard.new(io)
-      result.run
 
-      expect(result.grand_total).to eq 140
+      expect(result.run).to eq 140
     end
   end
 
+
+  context '10th frame' do
+    it 'allows player to win extra turns if they get a strike' do
+      #perfect game
+      io = double :io
+
+      10.times {expect(io).to receive(:gets).and_return("10")}
+      expect(io).to receive(:puts).with("Bonus round! Enter your score:")
+      expect(io).to receive(:gets).and_return("10")
+      expect(io).to receive(:puts).with("Wow, extra bonus! Enter your score:")
+      expect(io).to receive(:gets).and_return("10")
+
+      result = Scorecard.new(io)
+
+      expect(result.run).to eq 300
+    end
+
+    xit 'allows player to win extra turns if they get a strike' do
+      io = double :io
+
+      18.times {expect(io).to receive(:gets).and_return("0")}
+      expect(io).to receive(:gets).and_return("10")
+      expect(io).to receive(:puts).with("Bonus round! Enter your score:")
+      expect(io).to receive(:gets).and_return("10")
+      expect(io).to receive(:puts).with("Wow, extra bonus! Enter your score:")
+      expect(io).to receive(:gets).and_return("10")
+
+      result = Scorecard.new(io)
+
+      expect(result.run).to eq 30
+    end
+
+    xit 'allows player to win extra turns if they get a spare' do
+      io = double :io
+
+      20.times {expect(io).to receive(:gets).and_return("5")}
+      expect(io).to receive(:puts).with("Bonus round! Enter your score:")
+      expect(io).to receive(:gets).and_return("10")
+
+      result = Scorecard.new(io)
+
+      expect(result.run).to eq 155
+    end
+
+  end
 end
