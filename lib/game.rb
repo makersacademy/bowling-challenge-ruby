9cf5bf3e7309fa32eb0c1c25fc4affd26b2e626e
Marina Papapetrou <contact@marinapapap.com>
2022-12-11 13:17:24 +0000

    it returns the score for a game with 2 strikes not in the 10th frame

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,7 @@ class Game
 
   def initialize
     @frames = []
+    @strike = false
   end
 
   def add_frame(frame)
@@ -9,8 +10,29 @@ class Game
   end
 
   def total_score
-    @frames.map do |frame|
-      frame.scores
-    end.flatten.sum
+    new_array = []
+
+    @frames.each do |frame|
+    
+      if @strike == true
+        new_array << frame.scores + frame.scores
+        @strike = false
+      else
+        new_array << frame.scores
+      end
+
+      if frame.strike?
+        @strike = true
+      end
+
+    end
+
+    new_array.flatten.sum
+  end
+
+  private
+
+  def strike_check
+
   end
 end
