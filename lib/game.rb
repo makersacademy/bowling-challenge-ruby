aab8afee07d8620176f155247b24fc603af02bbc
Eva Pradhan <ejpradhan@yahoo.com>
2022-12-12 01:25:02 +0000

    testing for frames with strikes/spare added

--- lib/game.rb
+++ lib/game.rb
@@ -8,9 +8,22 @@ class Game
   end
 
   def calculate_current_score
+    score = 0
+    spare, strike, consecutive_strike = false
+    @frames.each { |frame|
+    score += frame.first_roll if spare
+    score += (frame.first_roll + frame.second_roll) if strike
+    score += (frame.first_roll) if consecutive_strike
+    score += (frame.first_roll + frame.second_roll)
+    consecutive_strike = strike && frame.strike
+    spare, strike = frame.spare, frame.strike
+    }
+    return score
   end
 
   def add_frame(first_roll, second_roll)
     @frames << Frame.new(first_roll, second_roll) if @frames.length < 10
   end
 end
+
+  
--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -20,21 +20,56 @@ RSpec.describe Game do
   end
 
   describe "#calculate_current_score" do
-    context "calculates the user's score so far when" do
-      it "one frame has been played" do
+    context "calculates the player's score when" do
+      it "one frame  played with no strikes or spares" do
         game = Game.new
         game.add_frame(1,1)
         expect(game.calculate_current_score).to eq 2
       end
-
-      it "two frames have been played" do
+  
+      it "two frames played with no strikes or spares" do
         game = Game.new
         game.add_frame(1,1)
         game.add_frame(1,1)
         expect(game.calculate_current_score).to eq 4
       end
+  
+      it "two frames  added with the first being a spare" do
+        game = Game.new
+        game.add_frame(9,1)
+        game.add_frame(1,1)
+        expect(game.calculate_current_score).to eq 13
+      end
+  
+      it "two frames added with the first being a strike" do
+        game = Game.new
+        game.add_frame(10,0)
+        game.add_frame(1,1)
+        expect(game.calculate_current_score).to eq 14
+      end
+  
+      it "three frames added with the first and second frames being strikes" do
+        game = Game.new
+        game.add_frame(10,0)
+        game.add_frame(10,0)
+        game.add_frame(1,1)
+        expect(game.calculate_current_score).to eq 35
+      end
+  
+      it "three frames with first frame being strike and second being spare" do
+        game = Game.new
+        game.add_frame(10,0)
+        game.add_frame(9,1)
+        game.add_frame(1,1)
+        expect(game.calculate_current_score).to eq 33
+      end
     end
   end
-
-
 end
+
+
+
+
+
+
+
