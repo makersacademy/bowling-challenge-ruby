2ce8c2bfb2ca22ba7bb27d7659a99fd781a45168
mantaslv <mantasvolkas@gmail.com>
2023-01-21 17:45:03 +0000

    tests tidied

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,38 +1,32 @@
 require 'game'
 
 describe Game do
+    let(:game) {Game.new}
+
     context 'player hits zero pins in every frame' do
         it 'after one frame shows total points as zero' do
-            game = Game.new
             game.add_frame([0, 0])
-            score = game.calculate_score
-            expect(score).to eq 0
+            expect(game.calculate_score).to eq 0
             expect(game.frames_played).to eq 1
         end
 
         it 'after 9 frames shows total points as zero' do
-            game = Game.new
             9.times { game.add_frame([0, 0]) }
-            score = game.calculate_score
-            expect(score).to eq 0
+            expect(game.calculate_score).to eq 0
             expect(game.frames_played).to eq 9
         end
         
         it 'after 10 frames shows total points as zero' do
-            game = Game.new
             10.times { game.add_frame([0, 0]) }
-            score = game.calculate_score
-            expect(score).to eq 0
+            expect(game.calculate_score).to eq 0
             expect(game.frames_played).to eq 10
         end
 
         it 'when trying to enter 11th frame shows a message' do
-            game = Game.new
             10.times { game.add_frame([0, 0]) }
             status = game.add_frame([0, 0])
             expect(status).to eq 'START NEW GAME'
-            score = game.calculate_score
-            expect(score).to eq 0
+            expect(game.calculate_score).to eq 0
             expect(game.frames_played).to eq 10
         end
     end
