065a5269fe801bdf0d9c792e7c6eb212f376eb76
mantaslv <mantasvolkas@gmail.com>
2023-01-21 18:41:25 +0000

    tests passed for one frame with no spares or strikes

--- lib/game.rb
+++ lib/game.rb
@@ -2,19 +2,24 @@ require 'frame'
 
 class Game
     def initialize
-        @score_card = []
+        @frames = []
         @frames_played = 0
     end
 
     def add_frame(frame)
         return 'START NEW GAME' if @frames_played == 10
-        @score_card << frame
+        @frames << frame
         @frames_played += 1
         return nil
     end
 
     def calculate_score
-        return 0
+        total = 0
+        @frames.each do |frame|
+            sum = frame.roll1 + frame.roll2
+            total += sum
+        end
+        return total
     end
 
     def frames_played
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -23,17 +23,34 @@ describe Game do
         end
 
         it 'after 10 frames shows total points as zero' do
-            10.times { game.add_frame([0, 0]) }
+            10.times { game.add_frame(frame) }
             expect(game.calculate_score).to eq 0
             expect(game.frames_played).to eq 10
         end
 
         it 'when trying to enter 11th frame shows a message' do
-            10.times { game.add_frame([0, 0]) }
-            status = game.add_frame([0, 0])
+            10.times { game.add_frame(frame) }
+            status = game.add_frame(frame)
             expect(status).to eq 'START NEW GAME'
             expect(game.calculate_score).to eq 0
             expect(game.frames_played).to eq 10
+        end
+    end
+
+    context 'player hits some pins without strike or spare for one' do
+        it 'returns correct score after one frame and gutter roll' do
+            frame.roll1 = 1
+            frame.roll2 = 0
+            p frame
+            game.add_frame(frame)
+            expect(game.calculate_score).to eq 1
+        end
+
+        it 'returns correct score after one frame and no gutter rolls' do
+            frame.roll1 = 7
+            frame.roll2 = 2
+            game.add_frame(frame)
+            expect(game.calculate_score).to eq 9
         end
     end
 end
