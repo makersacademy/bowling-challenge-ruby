ed2feaf4df8f2bb973b5b2af1c26f831b9217685
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-01-21 12:04:57 +0000

    check spare, strike and 2 rolls tested

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,4 +1,4 @@
-class Game
+class Bowling
   def initialize(io)
     @io = io
     @player_names = []
@@ -27,5 +27,19 @@ class Game
     end
     return @scorecard
   end
+
+  def check_spare(num1, num2)
+    num1 + num2 == 10 ? true : false
+  end
+
+  def check_strike(num)
+    num == 10 ? true : false
+  end
+
+  def check_rolls(hash, player_name)
+    hash[player_name].length % 2 == 0 ? true : false
+  end
+
+
 end
 
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,9 +1,9 @@
 require_relative "../lib/bowling"
 
-RSpec.describe Game do
+RSpec.describe Bowling do
   it "Greets the user, asks for the names of players and reads them back" do
     io = double :io
-    game1 = Game.new(io)
+    game1 = Bowling.new(io)
     allow(io).to receive(:puts).with("Welcome to Bowling")
     allow(io).to receive(:puts).with("Please enter the names of the players or type S to stop")
     allow(io).to receive(:puts).with("Player 1's name:")
@@ -15,7 +15,7 @@ RSpec.describe Game do
 
   it "takes the names of the players and sets up a scorecard for each" do
     io = double :io
-    game1 = Game.new(io)
+    game1 = Bowling.new(io)
 
     allow(io).to receive(:puts).with("Welcome to Bowling")
     allow(io).to receive(:puts).with("Please enter the names of the players or type S to stop")
@@ -26,5 +26,37 @@ RSpec.describe Game do
     game1.greeter
 
     expect(game1.scorecard_setup).to eq([{'Adam' => []}, {'Dave'=> []}, {'Bill'=> []}])
+  end
+
+  it "checks if the player bowled a spare" do
+    io = double :io
+    game1 = Bowling.new(io)
+    expect(game1.check_spare(2,8)).to eq true
+  end
+
+  it "checks if the player did not bowl a spare" do
+    io = double :io
+    game1 = Bowling.new(io)
+    expect(game1.check_spare(2,3)).to eq false
+  end
+
+  it "checks if the player bowled a strike" do
+    io = double :io
+    game1 = Bowling.new(io)
+    expect(game1.check_strike(10)).to eq true
+  end
+
+  it "checks if the player did not bowl a strike" do
+    io = double :io
+    game1 = Bowling.new(io)
+    expect(game1.check_strike(9)).to eq false
+  end
+
+  it "checks if the player has had two rolls" do
+    io = double :io
+    game1 = Bowling.new(io)
+    scorecard = {'Adam'=>[2,3]}
+    player_name = 'Adam'
+    expect(game1.check_rolls(scorecard, player_name)).to eq true
   end
 end
