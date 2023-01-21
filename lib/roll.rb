5cd5aa65c7d91c8909cd103c6bbf74f5058c02aa
Murat <murat@sertay.tech>
2023-01-21 17:18:07 +0000

    Finished unit tests and code for Roll class

--- lib/roll.rb
+++ lib/roll.rb
@@ -1,23 +1,42 @@
 # frozen_string_literal: true
 
 class Roll
-  attr_accessor :roll_count, :pins_hit, :hit_type
-  def initialize(roll_count,pins_hit)
-    if roll_count > 1
-      raise("You can only roll twice a frame")
-
+  attr_accessor  :pins_hit
+  attr_reader :roll_count, :hit_type
+  def initialize
+    @pins_hit = 0
+    @roll_count = 0
+  end
+  def roll_score(pins_hit)
+    roll_count_check
+    @roll_count = @roll_count + 1
+    @pins_hit = @pins_hit + pins_hit
+    @hit_type = self.hit_type_calculator
+    if @hit_type == "Strike"
+      print "You got a Strike you now move onto your next Frame"
     end
-    @roll_count = roll_count + 1
-    @pins_hit = pins_hit
-    # @hit_type = self.hit_type_calculator
   end
 
-  def next_roll
-
-  end
   private
+
+  def roll_count_check
+    if @roll_count > 1
+      raise("You can only roll twice a frame")
+    end
+  end
   def hit_type_calculator
 
+    case
+    when @pins_hit == 0
+      return "Gutter"
+    when @pins_hit < 10
+      return "Base"
+    when (@pins_hit == 10) && (@roll_count == 1)
+      return "Strike"
+    else
+      return "Spare"
+
+    end
 
   end
 end
--- spec/classes/roll_spec.rb
+++ spec/classes/roll_spec.rb
@@ -4,18 +4,73 @@ require 'rspec'
 
 describe 'Roll' do
   context 'when Roll is initialised' do
-    roll_count = 0
-    pins_hit = 5
-    roll1 = Roll.new(roll_count,pins_hit)
+    roll1 = Roll.new
     it 'should add 1 to the roll count' do
-      expect(roll1.roll_count).to eq 1
+      expect(roll1.roll_count).to eq 0
     end
-    it 'should return pins_hit as 5' do
-      expect(roll1.pins_hit).to eq 5
+    it 'should return pins_hit as 0' do
+      expect(roll1.pins_hit).to eq 0
+    end
+  end
+  context "when roll is being called multiple times" do
+    it 'should raise error when roll_count > 1 is passed in' do
+    roll = Roll.new
+      roll.roll_score(4)
+      roll.roll_score(3)
+      expect{roll.roll_score(3)}.to raise_error(RuntimeError)
+    end
+    it 'next roll should return 9 for pins_hit' do
+      roll = Roll.new
+      nine_pins_hit = roll.roll_score(9)
+      roll_count = roll.roll_count
+      expect(nine_pins_hit).to eq 9
+      expect(roll_count).to eq 1
     end
 
-    it 'should raise error when roll_count > 1 is passed in' do
-      expect{Roll.new(2,pins_hit)}.to raise_error('You can only roll twice a frame')
+    it 'roll_score should return 9 for pins_hit' do
+      roll = Roll.new
+      roll.roll_score(9)
+      roll_count = roll.roll_count
+      expect(roll.pins_hit).to eq 9
+      expect(roll_count).to eq 1
     end
+    it 'roll_score should return 10 for pins_hit & "Strike" for hit_type' do
+      roll = Roll.new
+      roll.roll_score(10)
+      expect(roll.hit_type).to eq "Strike"
+      expect(roll.pins_hit).to eq 10
+    end
+    it 'roll_score should return 10 for pins_hit & "Spare" for hit_type' do
+      roll = Roll.new
+      roll.roll_score(5)
+      roll.roll_score(5)
+      expect(roll.hit_type).to eq "Spare"
+      expect(roll.pins_hit).to eq 10
+    end
+    it 'roll_score should return 10 for pins_hit & "Spare" for hit_type' do
+      roll = Roll.new
+      roll.roll_score(10)
+      expect(roll.hit_type).to eq "Strike"
+      expect(roll.pins_hit).to eq 10
+    end
+    it "should test every combination of rolls" do
+    [10,9,8,7,6,5,5,4,3,2,1,0].map.with_index do |n,index|
+      roll = Roll.new
+      roll.roll_score(n)
+      roll.roll_score(index)
+      case
+      when @pins_hit == 0
+        expect(roll.hit_type).to eq "Gutter"
+      when roll.pins_hit < 10
+        expect(roll.hit_type).to eq  "Base"
+      when (@pins_hit = 10) && (@roll_count == 1)
+        expect(roll.hit_type).to eq  "Strike"
+      else
+        expect(roll.hit_type).to eq  "Spare"
+      end
+
+    end
+    end
+
   end
 end
