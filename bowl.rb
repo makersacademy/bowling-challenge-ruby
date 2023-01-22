83fe153f801022510d1250346863ef3f2d6280c8
Oana <oana.demian@gmail.com>
2023-01-22 14:17:07 +0000

    bowl.rb added test for one strike  game score, needs refactoring

--- bowl.rb
+++ bowl.rb
@@ -7,16 +7,26 @@ class Bowl
 
   def score(frames)
     frames.map.with_index do |frame, index|
-      if (index <= frames.length - 2) && spare?(frame)
-       balls(frame) + frames.fetch(index+1)[0]
+      if index <= frames.length - 2 && strike?(frame)
+        if frames.fetch(index + 1)[1].nil? 
+          frame[0] + frames.fetch(index + 1)[0] + frames.fetch(index + 2)[0]        
+        else 
+          frame[0] + frames.fetch(index + 1)[0] + frames.fetch(index + 1)[1]
+        end
+      elsif index <= frames.length - 2 && spare?(frame)
+        balls(frame) + frames.fetch(index+1)[0]
       else
-      balls(frame)
+        balls(frame)
       end
     end.sum
   end
 
   def spare?(frame)
     frame[0] + frame[1] == 10 
+  end
+
+  def strike?(frame)
+    frame[0] == 10
   end
 
   def balls(frame)
--- spec/bowl_spec.rb
+++ spec/bowl_spec.rb
@@ -18,5 +18,12 @@ RSpec.describe Bowl do
     bowl = Bowl.new
     expect(bowl.score(arr)).to eq 16
   end
+
+
+  it 'gives a score of 24 case one strike - a strike, 3 and 4 pins then 0 for the next 16 rolls' do
+    arr = [[10], [3,4], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]
+    bowl = Bowl.new
+    expect(bowl.score(arr)).to eq 24
+  end
   
 end
