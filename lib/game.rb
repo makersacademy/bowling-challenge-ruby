8d9717d9f1d39448a7a26eb1d74b0dbed81ee470
charlie lambert <char.lam@ntlworld.com>
2023-01-22 23:38:52 +0000

    All logic done, tests passing

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,12 @@
 class Game
-    attr_accessor :total_score, :roll_number, :frame, :board, :bonus
+    attr_accessor :total_score, :roll_number, :frame, :board, :bonus, :finished
     def initialize
+        @finsihed = false
         @total_score = 0
         @roll_number = 1
         @frame = 1
         @board = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0,0]]
-        @bonus = [0,0,0,0,0,0,0,0,0,0]
+        @bonus = [0,0,0,0,0,0,0,0,0]
     end
     def add_base_score
         temp = @board.flatten()
@@ -14,9 +15,9 @@ class Game
 
     def add_bonus_score
         @board.each_with_index do |ind_frame, index|
-            if ind_frame.sum == 10 && ind_frame[1] != 0
+            if ind_frame.sum == 10 && ind_frame[1] != 0 && index !=9
                 @bonus[index] = @board[index + 1][0]
-            elsif ind_frame[0] == 10
+            elsif ind_frame[0] == 10 && index != 9
                 case @board[index + 1][0]
                 when 10
                     @bonus[index] = @board[index + 1][0] + @board[index + 2][0]
@@ -28,27 +29,32 @@ class Game
         @total_score += @bonus.sum
     end
     def roll(number) 
-        @board[@frame -1][@roll_number - 1] = number
         case 
         when @frame < 10
+            @board[@frame -1][@roll_number - 1] = number
             if @roll_number == 1 && number == 10
                 @frame += 1
             elsif @roll_number == 1
-                @roll_number = 2
+                @roll_number = 2 
             else
                 @frame +=1
                 @roll_number = 1
             end
         when @frame = 10
-
+            if @roll_number > 3
+                finish
+            elsif @roll_number > 2 && @board[@frame -1].sum < 10
+                finish
+            else
+                @board[@frame -1][@roll_number - 1] = number
+                @roll_number += 1
+            end
         end
         add_base_score
         add_bonus_score
-        p board
-        p bonus
     end
 
-    def end
-
+    def finish
+        @finished = true
     end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,7 +2,7 @@ require 'game'
 
 RSpec.describe Game do
     context 'initilaize game' do
-        xit "should start game with 0 score" do
+        it "should start game with 0 score" do
             game = Game.new
             expect(game.total_score).to eq 0
             expect(game.roll_number).to eq 1
@@ -10,7 +10,7 @@ RSpec.describe Game do
         end
     end
     context 'roll a frame' do
-        xit "should roll a 4 and add roll score to total score, add to the roll number and keep the same frame" do
+        it "should roll a 4 and add roll score to total score, add to the roll number and keep the same frame" do
             game = Game.new
             game.roll(4)
             expect(game.frame).to eq 1
@@ -18,7 +18,7 @@ RSpec.describe Game do
             expect(game.total_score).to eq 4
         end
 
-        xit "should roll a 10 and add to total score, skip over to the next frame" do
+        it "should roll a 10 and add to total score, skip over to the next frame" do
             game = Game.new
             game.roll(10)
             expect(game.frame).to eq 2
@@ -26,7 +26,7 @@ RSpec.describe Game do
             expect(game.total_score).to eq 10
         end
 
-        xit "should return 10 after two rolls with a frame change" do
+        it "should return 10 after two rolls with a frame change" do
             game = Game.new
             game.roll(6)
             expect(game.frame).to eq 1
@@ -40,7 +40,7 @@ RSpec.describe Game do
     end
 
     context 'roll two frames with some bonuses' do
-        xit "should return 20 for score due to spare bonus, the frame should change and the 3rd roll added to the 1st index of the board" do
+        it "should return 20 for score due to spare bonus, the frame should change and the 3rd roll added to the 1st index of the board" do
             game = Game.new
             game.roll(7)
             expect(game.frame).to eq 1
@@ -56,7 +56,7 @@ RSpec.describe Game do
             expect(game.total_score).to eq 20
         end
 
-        xit "should return 28 with strike bonus after 3 rolls, frame should return 3 on a new frame with roll counter to 1" do
+        it "should return 28 with strike bonus after 3 rolls, frame should return 3 on a new frame with roll counter to 1" do
             game = Game.new
             game.roll(10)
             game.roll(5)
@@ -68,7 +68,7 @@ RSpec.describe Game do
     end
 
     context "throw three frames" do 
-        xit "should return score as 60 after 3 strikes and be on the 4th frame" do
+        it "should return score as 60 after 3 strikes and be on the 4th frame" do
             game = Game.new
             game.roll(10)
             game.roll(10)
@@ -78,7 +78,7 @@ RSpec.describe Game do
             expect(game.roll_number).to eq 1
         end
 
-        xit "should return score as 48 after 1 strike, a spare, a 9 and then a 0 and be on the 4th frame" do
+        it "should return score as 48 after 1 strike, a spare, a 9 and then a 0 and be on the 4th frame" do
             game = Game.new
             game.roll(10)
             game.roll(5)
@@ -92,7 +92,7 @@ RSpec.describe Game do
     end
 
     context "after 8 frames" do
-        xit "should return 90 with board below" do
+        it "should return 90 with board below" do
             game = Game.new
             game.board = [[7,3],[5,2],[10,0],[5,4],[6,0],[4,4],[8,2],[7,0],[0,0],[0,0,0]]
             game.roll_number = 2
@@ -101,6 +101,72 @@ RSpec.describe Game do
             expect(game.total_score).to eq 90
             expect(game.frame).to eq 9
             expect(game.roll_number).to eq 1
+        end
+    end
+
+    context "after a full game" do
+        it "should return 111 with no spare or strike at end" do
+            game = Game.new
+            game.board = [[7,3],[5,2],[10,0],[5,4],[6,0],[4,4],[8,2],[7,2],[9,1],[4,0,0]]
+            game.roll_number = 2
+            game.frame = 10
+            game.roll(3)
+            expect(game.total_score).to eq 111
+            expect(game.frame).to eq 10
+            expect(game.roll_number).to eq 3
+        end
+
+        it "should return 111 with a spare at end" do
+            game = Game.new
+            game.board = [[7,3],[5,2],[10,0],[5,4],[6,0],[4,4],[8,2],[7,2],[9,1],[1,9,0]]
+            game.roll_number = 3
+            game.frame = 10
+            game.roll(5)
+            expect(game.total_score).to eq 116
+            expect(game.frame).to eq 10
+            expect(game.roll_number).to eq 4
+        end
+
+        it "should return 140 with 3 strikes at the end" do
+            game = Game.new
+            game.board = [[7,3],[5,2],[10,0],[5,4],[6,0],[4,4],[8,2],[7,2],[9,1],[10,10,0]]
+            game.roll_number = 3
+            game.frame = 10
+            game.roll(10)
+            expect(game.total_score).to eq 140
+            expect(game.frame).to eq 10
+            expect(game.roll_number).to eq 4
+        end
+
+        it "should return 129 with a spare and 1 strike at the end" do
+            game = Game.new
+            game.board = [[7,3],[5,2],[10,0],[5,4],[6,0],[4,4],[8,2],[7,2],[9,1],[9,1,0]]
+            game.roll_number = 3
+            game.frame = 10
+            game.roll(10)
+            expect(game.total_score).to eq 129
+            expect(game.frame).to eq 10
+            expect(game.roll_number).to eq 4
+        end
+
+        it "should return 100 with all gutterballs at the end" do
+            game = Game.new
+            game.board = [[7,3],[5,2],[10,0],[5,4],[6,0],[4,4],[8,2],[7,2],[9,1],[0,0,0]]
+            game.roll_number = 2
+            game.frame = 10
+            game.roll(0)
+            expect(game.total_score).to eq 100
+            expect(game.frame).to eq 10
+            expect(game.roll_number).to eq 3
+        end
+
+        it "should return 100 with all gutterballs at the end" do
+            game = Game.new
+            game.board = [[7,3],[5,2],[10,0],[5,4],[6,0],[4,4],[8,2],[7,2],[9,1],[0,0,0]]
+            game.roll_number = 3
+            game.frame = 10
+            game.roll(5)
+            expect(game.finished).to eq true
         end
     end
 end
