137adc5c892aea640d630337dcf25c13b165be72
Oana <oana.demian@gmail.com>
2023-01-22 13:35:43 +0000

    bowl.rb added test for all ones  game score

--- spec/bowl_spec.rb
+++ spec/bowl_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Bowl do
     expect(bowl.score(arr)).to eq 0
   end
 
-
+  it 'returns a score of 20 for all ones' do
+    arr = Array.new(10, [1, 1])
+    bowl = Bowl.new
+    expect(bowl.score(arr)).to eq 20
+  end
+  
 end
