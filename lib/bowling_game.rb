7655154d5beab7604f5191b19304e8e8e2ad457b
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-01-23 09:30:50 +0000

    update of bowling challenge

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,37 +1,57 @@
 class BowlingGame
   def initialize
-    @frame = []
+    @player_rolls = []
   end
 
-  def roll(points)
-    @frame.push(points)
+  def roll(pins_knocked_down)
+    if pins_knocked_down > 10
+      raise "Invalid number, pins knocked down must be a number from 0 to 10"
+    end
+    @player_rolls.push(pins_knocked_down)
   end
 
   def score
-    result = 0
     roll_index = 0
+    scorecard = 0
 
     10.times do 
-      if roll_a_spare?(roll_index)
-        result += spare_score(roll_index)
+      if roll_a_strike?(roll_index)
+        scorecard += strike_score(roll_index)
+        roll_index += 1
+      elsif roll_a_spare?(roll_index)
+        scorecard += spare_score(roll_index)
+        roll_index += 2
       else
-        result += @frame[roll_index] + @frame[roll_index + 1 ]
+        scorecard  += score_of_frame(roll_index)
+        roll_index += 2
       end
-      roll_index += 2
     end
-    return result
+    return scorecard
   end
 
 
   private
 
+  def roll_a_strike?(roll_index)
+    @player_rolls[roll_index] == 10
+  end
+
+  def strike_score(roll_index)
+    @player_rolls[roll_index] + @player_rolls[roll_index + 1] + @player_rolls[roll_index + 2]
+  end
+
   def roll_a_spare?(roll_index)
-    @frame[roll_index] + @frame[roll_index + 1 ] == 10
+    @player_rolls[roll_index] + @player_rolls[roll_index + 1] == 10
   end
 
   def spare_score(roll_index)
-    @frame[roll_index] + @frame[roll_index + 1] + @frame[roll_index + 2]
+    @player_rolls[roll_index] + @player_rolls[roll_index + 1] + @player_rolls[roll_index + 2]
   end
+
+  def score_of_frame(roll_index)
+    @player_rolls[roll_index] + @player_rolls[roll_index + 1]
+  end
+
 end
 
 
