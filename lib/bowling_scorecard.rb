624c419b70bf5079dc371e16a91fa56922dbbaa5
Alicia Moses <aliciamoses.dev@gmail.com>
2023-01-29 12:49:01 +0000

    Passing test for allocating strikes to frames

--- lib/bowling_scorecard.rb
+++ lib/bowling_scorecard.rb
@@ -3,4 +3,12 @@ class BowlingScorecard
   def initialize(rolls)
     @rolls = rolls
   end
+  def set_frames
+    @rolls.map {
+      @rolls[0] == 10 ?
+      @rolls << @rolls.shift(1) : 
+      @rolls << @rolls.shift(2)
+    }
+  end
+
 end 
--- spec/bowling_scorecard_spec.rb
+++ spec/bowling_scorecard_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe "BowlingScorecard" do
     scorecard = BowlingScorecard.new([1,2,3,4])
     expect(scorecard).to be_an_instance_of(BowlingScorecard)
   end 
+  context "when an array contains a strike score of ten" do
+    it "adds the score to its own frame" do
+    scorecard = BowlingScorecard.new([10, 10, 4, 2, 10, 6, 2, 5, 5, 3, 2, 10, 4, 2, 10, 2, 3, 6, 1])
+    scorecard.set_frames
+    expect(scorecard.set_frames[0]).to include([[10],[10]])
+    end
+  end 
 end 
