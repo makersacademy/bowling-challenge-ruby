37f22a169d16d224c313ce559b9a23f2b9548db7
marie taylor <marietaylortailor@hotmail.com>
2023-02-18 15:39:43 +0000

    couple more simple game_repo rspec tests added

--- spec/game_repository_spec.rb
+++ spec/game_repository_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe GameRepository do
       expect(score.score_card).to eq(8)
     end
   end
+
+  it "displays score from 4 frames with results added to scorecard." do
+    score = GameRepository.new
+    score.add(8)
+    score.add(8) 
+    score.add(8) 
+    score.add(2)
+    expect(score.score_card).to eq(26)
+  end
+
+  it "displays a zero score when initial frame returns a score of zeros." do
+    score = GameRepository.new
+    score.add(0)
+    expect(score.score_card).to eq(0)
+  end
 end
