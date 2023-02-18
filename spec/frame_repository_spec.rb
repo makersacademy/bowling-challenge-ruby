95c9ce0394c13b353b7656f210654f5d8734ce96
marie taylor <marietaylortailor@hotmail.com>
2023-02-18 14:19:51 +0000

    Spec with additional rolls and running sum

--- spec/frame_repository_spec.rb
+++ spec/frame_repository_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe FrameRepository do
       expect(score.score_card).to eq(2)
     end
   end
+
+
+  context "bowling game in progress" do
+    it "displays score after 3 rolls." do
+      score = FrameRepository.new
+      score.add(2)
+      score.add(7)
+      score.add(4)
+      expect(score.score_card).to eq(13)
+    end
+  end
 end
