5ef910434925a8314ba8801fb071b9d401bda805
James Mcleish <james.mcleish@icloud.com>
2023-02-20 00:59:39 +0000

    made a bowling scorecard

---
+++ lib/bowling.rb
@@ -0,0 +1,74 @@
+class Bowling
+
+def initialize
+  @array = []
+  @sum = 0
+end
+
+def score_card
+  10.times do
+    $mini_array = []
+    puts "input the score of your first bowl"
+    bowl1 = gets.chomp
+    $mini_array << bowl1.to_i
+    if bowl1.to_i == 10
+      @array << [10,0]
+    else
+      puts "input the score of your second bowl"
+      bowl2 = gets.chomp.to_i
+      $mini_array << bowl2
+      @array << $mini_array
+    end
+  end
+  if @array[-1][0] == 10
+    puts "input put the score of your bowl"
+    bowl1 = gets.chomp
+    puts "input put the score of your bowl"
+    bowl2 = gets.chomp
+    @array << [bowl1.to_i, bowl2.to_i]
+  elsif @array[-1][0] + @array[-1][1] == 10
+    puts "input put the score of your bowl"
+    bowl1 = gets.chomp
+    @array << [bowl1.to_i, 0]
+  end
+  pmj @array
+  return score
+end
+
+def score
+  @array.each_with_index do |x,i|
+    if x[0].to_i == 10
+      @sum += 10
+      if @array[i+1][0] == 10
+        @sum += @array[i+1][0] + @array[i+2][0]
+      else
+        @sum += @array[i+1][0] + @array[i+1][1]
+      end
+    elsif (x[0] + x[1]) == 10
+      @sum += 10 + @array[i+1][0]
+    else
+      @sum += @array[i][0] + @array[i][1]
+    end
+    if i == 8
+      if @array[9][0] == 10
+        @sum += @array[9][0]
+        break
+      else
+        @sum +=  @array[9][0] + @array[9][1]
+        break
+      end
+    end
+  end
+  if @array[9][0] == 10
+    @sum += (@array[10][0] + @array[10][1])
+  elsif (@array[9][0] + @array[9][1]) == 10
+    @sum += @array[10][0]
+  end
+  p @sum
+  return @sum
+end
+
+end
+
+game = Bowling.new
+game.score_card
