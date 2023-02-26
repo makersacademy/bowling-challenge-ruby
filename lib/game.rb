576fefed93a68130927476fc0fe50db82e46dbd4
Hayley Dobbs <iamdobbs@icloud.com>
2023-02-26 19:33:26 +0000

    game.rb complete with all tests

--- lib/game.rb
+++ lib/game.rb
@@ -11,4 +11,36 @@ class Game
     @framecount = 0
   end  
 
+  def roll(pins)
+    current_frame = @frames[@framecount]
+    if @framecount < 9
+      current_frame.add_roll(pins)
+      if current_frame.frame_complete?(@framecount)
+        @framecount += 1
+      end 
+    else
+      if !current_frame.frame_complete?(@framecount)
+        current_frame.add_roll(pins)
+      end
+    end
+  end  
+
+  def total_score
+    totalscore = 0
+    @frames.each_with_index do |frame, index|
+      if index < 9 
+        if frame.strike?
+          totalscore += frame.score + @frames[index + 1].rolls[0] + @frames[index + 1].rolls[1] 
+        elsif frame.spare?
+          totalscore += frame.score + @frames[index + 1].rolls[0] 
+        else 
+          totalscore += frame.score
+        end
+      else
+        totalscore += frame.score
+      end
+    end
+    return totalscore
+  end     
+  
 end  
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,5 +5,61 @@ describe Game do
     game = Game.new
     expect(game.frames.length).to eq(10)
   end
+
+  it 'can add two rolls to same nested array in @frames array' do
+    game = Game.new
+    game.roll(5)
+    game.roll(4)
+    first_frame = game.frames[0]
+    expect(first_frame.rolls).to eq([5, 4])
+  end
+
+  it 'can add multiple rolls and assign to correct frame' do
+    game = Game.new
+    game.roll(5)
+    game.roll(4)
+    game.roll(6)
+    game.roll(7)
+    current_frame = game.frames[1]
+    expect(current_frame.rolls).to eq([6, 7])
+  end  
+
+  it 'can add rolls to correct frame and account for strike' do
+    game = Game.new
+    game.roll(5)
+    game.roll(4)
+    game.roll(10)
+    game.roll(6)
+    game.roll(7)
+    current_frame = game.frames[1]
+    expect(current_frame.rolls).to eq([10])
+  end  
+
+  context 'calculates bowling score' do
+    it 'returns totalscore of 133 based on scores from README example' do
+      game = Game.new
+      pins = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6]
+      pins.each do |pin|
+        game.roll(pin)
+      end
+      expect(game.total_score).to eq(133)
+    end  
+
+    it 'returns score 300 for perfect game' do
+      game = Game.new
+      game.roll(10)
+      game.roll(10)
+      game.roll(10)
+      game.roll(10)
+      game.roll(10)
+      game.roll(10)
+      game.roll(10)
+      game.roll(10)
+      game.roll(10)
+      game.roll(10)
+      game.roll(10)
+      game.roll(10)
+    end  
+  end 
   
 end    
