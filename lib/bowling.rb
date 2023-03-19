9530eefbacf63ae0d64e8c29acc8afe6bbdca929
Konrad <kdun.work@gmail.com>
2023-03-19 23:29:45 +0000

    Testing for simple bowling outcomes

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,4 +1,27 @@
 class Bowling
+  attr_reader :rolls
+  
   def initialize
+    @rolls = []
+  end
+
+  def roll(pins)
+    @rolls.push(pins)
+  end
+
+  def score
+    total_points = 0
+    frame_index = 0
+
+    (0..9).each do |single_frame|
+      total_points += sum_of_points_in_frame(frame_index)
+      frame_index += 2
+    end
+
+    return total_points
+  end
+
+  def sum_of_points_in_frame(frame_index)
+    return @rolls[frame_index] + @rolls[frame_index + 1]
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,7 +1,31 @@
-require 'bowling'
+require_relative '../lib/bowling'
 
-describe Bowling do
-  it 'initiates' do
-    repo = Bowling.new
+RSpec.describe Bowling do
+
+  let(:repo) { Bowling.new }
+
+  def roll_multiple(count, pins)
+    count.times { repo.roll(pins) }
+  end
+
+  context '#score' do
+    it 'can score a gutter game' do
+      roll_multiple(20, 0)
+      expect(repo.score).to eq(0)
+    end
+
+    it 'can score a game of all ones' do
+      roll_multiple(20, 1)
+      expect(repo.score).to eq(20)
+    end
+
+    it 'can score a game of mixed rolls without spares and strikes' do
+      roll_multiple(5, 1)
+      roll_multiple(5, 2)
+      roll_multiple(5, 3)
+      roll_multiple(5, 5)
+      expect(repo.score).to eq(55)
+    end
+
   end
 end
