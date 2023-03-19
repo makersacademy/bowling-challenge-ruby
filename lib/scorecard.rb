aa9e29f519ed60b5f6cd9637966c6d98fd011a9d
ChangWynn <huynhchang.one@gmail.com>
2023-03-19 19:54:40 +0000

    TDD frame 1 basic behaviour

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,6 +1,6 @@
 class ScoreCard
   attr_accessor :score_card
-  
+
   def initialize(io = Kernel)
     @io = io
     @score_card = _create_frames
@@ -8,6 +8,24 @@ class ScoreCard
 
   def run
 
+    @score_card.each_with_index do |frame, index|
+      _game_setup(frame, index)
+      
+      @rolls.each do |roll|
+    #--------------------------------------------
+        
+        input = @io.gets.chomp.to_i
+        frame[:rolls] << input
+
+        if roll == 1 && input == 10
+          frame[:status] = "STRIKE"
+          break
+        end
+        if roll == 2 && frame[:rolls].sum == 10
+          frame[:status] = "SPARE" 
+        end
+      end
+    end
   end
 
   private
@@ -19,5 +37,14 @@ class ScoreCard
       @score_card[index] = {number: nil, status: nil, rolls: [], bonus: []}
     end
     return @score_card
+  end
+
+  # ---------------------------------
+  # GAME LOGIC ENTRY POINT
+  # ---------------------------------
+  def _game_setup(frame, index)
+    @frame_number = index + 1
+    frame[:number] = @frame_number
+    @rolls = [1, 2]
   end
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -3,6 +3,7 @@ require "scorecard"
 describe ScoreCard do
 
   let(:kernel) { spy(double :Kernel) }
+  let(:score_card) { spy(double :ScoreCard) }
   let(:subject) { described_class.new(kernel) }
 
 
@@ -27,6 +28,67 @@ describe ScoreCard do
         expect(frame[:status]).to be_nil
         expect(frame[:rolls]).to be_empty
         expect(frame[:bonus]).to be_empty
+      end
+    end
+  end
+
+  describe ".run method" do
+    it "should give the index number incremented by one to the first hash" do
+      allow(kernel).to receive(:gets) { "5" }
+      allow(kernel).to receive(:gets) { "4" }
+      subject.run
+      result = subject.score_card[0]
+      expect(result[:number]).to eq 1
+    end
+
+    # context "valid inputs" do
+    #   it "should be between 0 and 10" do
+    #     expect(kernel).to receive(:gets).at_least(1).times { "11" }
+    #     result = subject.score_card[0]
+    #     expect(result[:rolls]).to eq []
+    #   end
+    # end
+
+
+    context "first frame" do
+      it "should prompt the user for two inputs constituting the two rolls" do
+        expect(kernel).to receive(:gets).at_least(2).times { "4" }
+        subject.run
+      end
+      it "should save both rolls into the :rolls key" do
+        expect(kernel).to receive(:gets) { "5" }
+        expect(kernel).to receive(:gets).at_least(1).times { "4" }
+        subject.run
+        result = subject.score_card[0]
+        expect(result[:rolls]).to eq [5, 4]
+      end
+      it "should set :status to SPARE if the total of the two rolls is equal to 10" do
+        expect(kernel).to receive(:gets) { "5" }
+        expect(kernel).to receive(:gets).at_least(1).times { "5" }
+        subject.run
+        result = subject.score_card[0]
+        expect(result[:status]).to eq "SPARE"
+        expect(result[:rolls]).to eq [5, 5]
+      end
+      it "should jump to the next frame after 2 rolls was played" do
+        expect(kernel).to receive(:gets) { "5" }
+        expect(kernel).to receive(:gets).at_least(1).times { "5" }
+        subject.run
+        result = subject.score_card[1]
+        expect(result[:number]).to eq 2
+      end
+      it "should set :status to STRIKE and jump to next frame if input is 10 on roll 1" do
+        expect(kernel).to receive(:gets).at_least(1).times { "10" }
+        subject.run
+        result = subject.score_card[0]
+        expect(result[:status]).to eq "STRIKE"
+        expect(result[:rolls]).to eq [10]
+      end
+      it "should jump to the next frame after a strike was rolled" do
+        expect(kernel).to receive(:gets).at_least(1).times { "10" }
+        subject.run
+        result = subject.score_card[1]
+        expect(subject.score_card[1][:number]).to eq 2
       end
     end
   end
