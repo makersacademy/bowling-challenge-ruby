202163bc2784dadc84c0cc1b4e333556a7fa9d47
Konrad <kdun.work@gmail.com>
2023-03-19 23:55:54 +0000

    Used 'subject' instead of repo object in the RSpec tests.

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,22 +1,20 @@
 require_relative '../lib/bowling'
 
 RSpec.describe Bowling do
-
-  let(:repo) { Bowling.new }
-
+  
   def roll_multiple(count, pins)
-    count.times { repo.roll(pins) }
+    count.times { subject.roll(pins) }
   end
 
   context '#score' do
     it 'can score a gutter game' do
       roll_multiple(20, 0)
-      expect(repo.score).to eq(0)
+      expect(subject.score).to eq(0)
     end
 
     it 'can score a game of all ones' do
       roll_multiple(20, 1)
-      expect(repo.score).to eq(20)
+      expect(subject.score).to eq(20)
     end
 
     it 'can score a game of mixed rolls without spares and strikes' do
@@ -24,29 +22,29 @@ RSpec.describe Bowling do
       roll_multiple(5, 2)
       roll_multiple(5, 3)
       roll_multiple(5, 4)
-      expect(repo.score).to eq(50)
+      expect(subject.score).to eq(50)
     end
 
     it 'can score a game with one spare' do
-      repo.roll(4)
-      repo.roll(6)
-      repo.roll(7)
+      subject.roll(4)
+      subject.roll(6)
+      subject.roll(7)
       roll_multiple(17, 0)
-      expect(repo.score).to eq(24)
+      expect(subject.score).to eq(24)
     end
 
     it 'can score a game with one strike' do
-      repo.roll(10)
-      repo.roll(6)
-      repo.roll(3)
-      repo.roll(2)
+      subject.roll(10)
+      subject.roll(6)
+      subject.roll(3)
+      subject.roll(2)
       roll_multiple(16, 0)
-      expect(repo.score).to eq(30)
+      expect(subject.score).to eq(30)
     end
 
     it 'can score a perfect game' do
       roll_multiple(12, 10)
-      expect(repo.score).to eq(300)
+      expect(subject.score).to eq(300)
     end
   end
 end
