6b04b01f9870c5836f7fd98c070b6c8c0887a745
Dilan Patel <patel_dilan@hotmail.com>
2023-03-20 07:58:57 +0000

    Tested a game of rolling all 'ones'

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,10 +1,17 @@
 class BowlingGame
 
-  def roll(pins)
+  def initialize
+    # Empty array that contains no 'rolls' when a new game is created
+    @rolls = []
+  end
+
+  def roll(pins) # Pins is the number of pins knocked down in a roll
+    # Pushes the roll (number of pins) into the rolls array
+    @rolls << pins
   end
 
   def score
-    return 0
+    return @rolls.sum
   end
 
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -11,14 +11,21 @@ RSpec.describe BowlingGame do
   end
 
   context "when a player rolls all zeros" do
-    
     it "scores a gutter game" do
       20.times do 
         @game.roll(0)
       end
       expect(@game.score).to eq 0
     end
+  end
 
+  context "when a player rolls all ones" do
+    it "scores a game of 20" do
+      20.times do
+        @game.roll(1)
+      end
+      expect(@game.score).to eq 20
+    end
   end
 
 end
