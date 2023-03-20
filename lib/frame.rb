e5aa23b6e9e77728c840e1732bacef2afcbdd2d7
Sarah <sarahdavies186@gmail.com>
2023-03-20 10:15:28 +0000

    refactoring code

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,44 +1,28 @@
-class Frame
-  attr_reader :rolls
+# class Frame
+# #   attr_reader :rolls
 
-  def initialize
-    @rolls = []
-  end
+# #   def initialize
+# #     @rolls = []
+# #   end
 
-  def add_roll(pins)
-    @rolls << pins
-  end
+# #   def add_roll(pins)
+# #     @rolls << pins
+# #   end
 
-  def score
-    @rolls.sum
-  end
+# #   def score
+# #     @rolls.sum
+# #   end
 
-  def strike?
-    @rolls.first == 10
-  end
+# #   # def strike?
+# #   #   @rolls.first == 10
+# #   # end
 
-  def spare?
-    @rolls.sum == 10 && @rolls.first != 10
-  end
+# #   # def spare?
+# #   #   @rolls.sum == 10 && @rolls.first != 10
+# #   # end
 
-  def strike_bonus(roll_index)
-    if @rolls.length > roll_index + 2
-      @rolls[roll_index + 1] + @rolls[roll_index + 2]
-    else
-      0
-    end
-  end
+# #   def complete?
+# #     @rolls.length == 2 || strike?
+# #   end
 
-  def spare_bonus(roll_index)
-    if @rolls.length > roll_index + 1
-      @rolls[roll_index + 1]
-    else
-      0
-    end
-  end
-
-  def complete?
-    @rolls.length == 2 || strike?
-  end
-
-end
+# end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,26 +1,26 @@
-require 'frame'
+# require 'frame'
 
-RSpec.describe Frame do 
-  it "adds the total roll score to the frame when 2 rolls" do 
-    subject.add_roll(4)
-    subject.add_roll(4)
-    expect(subject.score).to eq 8
-  end
+# RSpec.describe Frame do 
+#   xit "adds the total roll score to the frame when 2 rolls" do 
+#     subject.add_roll(4)
+#     subject.add_roll(4)
+#     expect(subject.score).to eq 8
+#   end
 
-  it "returns true if a strike" do 
-    subject.add_roll(10)
-    expect(subject.strike?).to eq true
-  end
+#   xit "returns true if a strike" do 
+#     subject.add_roll(10)
+#     expect(subject.strike?).to eq true
+#   end
 
-  it "returns true if a spare" do
-   subject.add_roll(6)
-   subject.add_roll(4)
-   expect(subject.spare?).to eq true
-  end
+#   xit "returns true if a spare" do
+#    subject.add_roll(6)
+#    subject.add_roll(4)
+#    expect(subject.spare?).to eq true
+#   end
 
-  it "returns true if game complete" do
-    subject.add_roll(10)
-    expect(subject.complete?).to eq true
-  end
-end
+#   xit "returns true if game complete" do
+#     subject.add_roll(10)
+#     expect(subject.complete?).to eq true
+#   end
+# end
 
