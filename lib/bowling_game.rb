fbb370a88b266eeb8a30ed19144dcb6bb33889d3
Dilan Patel <patel_dilan@hotmail.com>
2023-03-20 08:55:03 +0000

    Refactored to include 'spare?' and 'spare_score' private methods

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -12,11 +12,13 @@ class BowlingGame
 
   def score
     total_score = 0
+    # Index of the @rolls array
     roll_index = 0
 
+    # Loops through frame 10 times to cover a full game
     10.times do |frame|
-      if @rolls[roll_index] + @rolls[roll_index + 1] == 10
-        total_score += 10 + @rolls[roll_index + 2]
+      if spare?(roll_index)
+        total_score += spare_score(roll_index)
         roll_index += 2
       else
         total_score += @rolls[roll_index] + @rolls[roll_index + 1] 
@@ -26,5 +28,16 @@ class BowlingGame
 
     return total_score
   end
+
+  private
+  
+  def spare?(roll_index)
+    @rolls[roll_index] + @rolls[roll_index + 1] == 10
+  end
+
+  def spare_score(roll_index)
+    10 + @rolls[roll_index + 2]
+  end
+  
 
 end
