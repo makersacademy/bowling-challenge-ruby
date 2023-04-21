46aa4031ac5b83be0d0d54a34ebc0d5a55d21109
oana.pintoiu <oana.pintoiu@yahoo.com>
2023-04-21 21:14:59 +0100

    refactored class initialization in spec file

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,23 +1,20 @@
 class BowlingGame
-
-def initialize
-@rolls = []
-end 
-
+    
+  def initialize
+    @rolls = []
+  end
 
   def roll(pins)
     @rolls.push pins
   end
 
-
-
   def score
     result = 0
     rolls_count = 0
     20.times do
-        result += @rolls[rolls_count]
-        rolls_count =+ 1
+      result += @rolls[rolls_count]
+      rolls_count = + 1
+    end
+    result
   end
-  result
-end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,23 +1,28 @@
 require 'bowling_game'
 
 describe BowlingGame do
+
+    # reafctoring: to avoid initializing 'game' via BowlingGame.new, we can call it with @ via:
     
-    context 'creates and plays a bowling game with 1 player' do
-        it 'creates game' do
-            game = BowlingGame.new #initializes game/class
-        end
+    before do
+        @game = BowlingGame.new
     end
- 
-it 'rolls a gutter game, 20 rolls with no pins knocked over' do
+
+  context 'creates and plays a bowling game with 1 player' do
+    it 'creates game' do
+      @game # initializes game/class
+    end
+  end
+
+  it 'rolls a gutter game, 20 rolls with no pins knocked over' do
     game = BowlingGame.new
-    20.times{game.roll 0}
-    expect(game.score).to eq 0
-end
+    20.times { @game.roll 0 }
+    expect(@game.score).to eq 0
+  end
 
-it 'can roll and knock just 1 pin 20 times' do
-game = BowlingGame.new
-    20.times{game.roll 1}
-    expect(game.score).to eq 20
-end
-
+  it 'can roll and knock just 1 pin 20 times' do
+    game = BowlingGame.new
+    20.times { @game.roll 1 }
+    expect(@game.score).to eq 20
+  end
 end
