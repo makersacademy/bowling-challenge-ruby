a97fa5b39b273fa2581b03ffacb9a505c0936fa7
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-04-21 20:32:14 +0100

    Adding spec helper

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ source "https://rubygems.org"
 # gem "rails"
 
 gem "rspec", "~> 3.12"
+gem 'simplecov', require: false
---
+++ spec/bowling_spec.rb
@@ -0,0 +1,10 @@
+require './bowling_repo'
+require_relative 'spec_helper'
+
+
+RSpec.describe Scorecard do
+  context "basic game simulation" do
+    it "simulates a game without strike" do
+    end
+  end
+end
