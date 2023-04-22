ccdb3fd68ef357c71c1cc08abd3371189a00f4c5
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-04-22 09:41:25 +0100

    Added frame < 10 logic for the score addition

--- lib/bowling_repo.rb
+++ lib/bowling_repo.rb
@@ -3,40 +3,36 @@ require 'frame_repo'
 class Scorecard
   def initialize 
     @total_score = 0
+    @frame = 0
   end
 
-  def add_score(score)
-    @total_score += score
+  def add_score(roll_one, roll_two)
+    if @frame < 10
+      @total_score += roll_one + roll_two
+      @frame += 1
+    end
   end
 
   def total_score
-    return @total_score
-  end
-
-  def score_card
-    frame = 1
-    puts 'Please roll and I will output the scores so far'
-    while frame < 10
-      puts "Frame: #{frame}"
-      puts 'What is roll one?'
-      roll_one = gets.chomp.to_i
-      @total_score += roll_one
-      puts 'What is roll two?'
-      roll_two = gets.chomp.to_i
-      @total_score += roll_two
-      puts "Your score so far is: #{@total_score}"
-      frame += 1
-    end
-    final_score_comment(@total_score)
-  end
-
-  def final_score_comment(total_score)
-    if @total_score == 300
-      puts 'You got a perfect score!'
-    elsif @total_score == 0
-      puts 'Gutter Ball! Better luck next time.'
-    else
-      puts "Your final score is: #{total_score}"
-    end
+    @total_score
   end
 end
+
+#   def strike_or_spare
+#       if roll_one != 10 && (roll_one + roll_two == 10)
+#       total_score += frame -1
+#       spare_points == true
+#     elsif roll_one == 0
+#       strike_points == true
+#     end
+#   end
+
+#   def final_score_comment(total_score)
+#     if @total_score == 300
+#       return "Perfect score!"
+#     elsif @total_score == 0
+#       return "Gutter Ball!"
+#     else
+#       return "Final score is: #{total_score}"
+#     end
+# end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -3,12 +3,47 @@ require_relative 'spec_helper'
 
 
 RSpec.describe Scorecard do
-  context "basic game simulation" do
+  context "add_score method to total_score " do
     it "adds scores and returns total score" do
       scorecard = Scorecard.new
-      scorecard.add_score(1)
-      scorecard.add_score(2)
+      scorecard.add_score(1, 2)
       expect(scorecard.total_score).to eq(3)
+    end
+  end
+
+  context "score_card method" do
+    it "simulates a non strike/spare game" do
+      scorecard = Scorecard.new
+      scorecard.add_score(1, 2)
+      scorecard.add_score(2, 4)
+      scorecard.add_score(4, 2)
+      scorecard.add_score(1, 3)
+      scorecard.add_score(2, 3)
+      scorecard.add_score(6, 3)
+      scorecard.add_score(2, 3)
+      scorecard.add_score(3, 3)
+      scorecard.add_score(4, 3)
+      scorecard.add_score(5, 3)
+      expect(scorecard.total_score).to eq(59)
+    end
+  end
+
+  context "score_card method" do
+    it "doesn't add any scores after 10 frames" do
+      scorecard = Scorecard.new
+      scorecard.add_score(1, 2)
+      scorecard.add_score(2, 4)
+      scorecard.add_score(4, 2)
+      scorecard.add_score(1, 3)
+      scorecard.add_score(2, 3)
+      scorecard.add_score(6, 3)
+      scorecard.add_score(2, 3)
+      scorecard.add_score(3, 3)
+      scorecard.add_score(4, 3)
+      scorecard.add_score(5, 3)
+      scorecard.add_score(6, 3)
+      scorecard.add_score(6, 3)
+      expect(scorecard.total_score).to eq(59)
     end
   end
 end
