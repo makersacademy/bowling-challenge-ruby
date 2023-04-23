2f8a81cca1a1f02514031d2637e1a207add9a2f6
bookloverbabe <solmaz@hotmail.co.uk>
2023-04-23 18:11:03 +0100

    Created spare method

--- lib/score.rb
+++ lib/score.rb
@@ -1,6 +1,6 @@
 class Score
   # Create an attribute reader (read only) called count, so the user can only see their score count
-  attr_reader :count
+  attr_reader :count, :roll_one, :roll_two
 
     def initialize
       # Implement count as an array, then add score in array
@@ -8,11 +8,16 @@ class Score
     end
 
     def score_count(count)
+      # Sum (add together) all of the numbers in the count array and add in the new count with a count parameter
       @count.sum(count)
     end
 
-    # def spare
-    # end
+    def spare(spare_count)
+      # The score is ten plus the first roll of the next frame
+      # If both rolls = 10, score = 10 + FX + 1.R1
+      roll_one = 4
+      spare = 10 + roll_one
+    end
 
     # def strike
     # end
--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -13,7 +13,21 @@ RSpec.describe Score do
       score.score_count(3)
       expect(score.score_count(3)).to eq (3)
     end
+    it "first roll knocks down 5, second 3" do
+      score = Score.new
+      score.score_count(5)
+      score.score_count(3)
+      expect(score.score_count(8)).to eq (8)
+    end
   end
 
-  
+  context "players scores a spare" do
+    it "score is 10 plus first roll of next frame, which is 4" do
+      score = Score.new
+      roll_one = 4
+      score.score_count(10 + roll_one)
+      expect(score.spare(14)).to eq (14)
+    end
+  end
+
 end
