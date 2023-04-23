d8649fb939836999b1fce06dcad9b2cea3084bb6
Quddus Rahman <qrahman115@gmail.com>
2023-04-23 15:33:24 +0100

    initialise method working for game.rb, roll, frame and scoreboard classes created to allow this, starting on roll method

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,8 @@ require 'scorecard'
 require 'frame'
 
 class Game 
+  attr_reader :scorecard, :current_frame, :frames, :roll_count
+  
   def initialize
     @scorecard = Scorecard.new
     @current_frame = Frame.new
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Game do
     it 'creates a new scorecard, current frame, frames array, and roll count' do
       game = Game.new
 
-      expect(game.instance_variable_get(:@scorecard)).to be_a(Scorecard)
-      expect(game.instance_variable_get(:@current_frame)).to be_a(Frame)
-      expect(game.instance_variable_get(:@frames)).to eq([game.instance_variable_get(:@current_frame)])
-      expect(game.instance_variable_get(:@roll_count)).to eq(0)
+      expect(game.scorecard).to be_a(Scorecard)
+      expect(game.current_frame).to be_a(Frame)
+      expect(game.frames).to eq([game.current_frame])
+      expect(game.roll_count).to eq(0)
     end
   end
 end
