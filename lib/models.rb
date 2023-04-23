cb0b25ff23b556a43b5bfb7db4ce900352e174a0
Jon P <jon.pillay@gmail.com>
2023-04-23 17:49:22 +0100

    score calculations (current_score/calc_bonuses duo) passing all current tests, which I think give full coverage. Needs a README to explain design.

---
+++ lib/models.rb
@@ -0,0 +1,69 @@
+class Frame
+  attr_accessor :frame_num, :score, :str_rep, :message
+  def initialize(frame_num, score=[])
+    @frame_num = frame_num
+    @score = score
+    @str_rep = []
+    @message = ""
+  end
+
+  def get_score
+    return @score
+  end
+
+  def add_roll(roll_score)
+    @score.push(roll_score)
+  end
+
+  def str_rep(score)
+    if @score[0] = 10
+      @str_rep = ["", "X"]
+    elsif @score.sum == 10
+      @str_rep = [@score[0].to_s, "/"]
+    elsif
+      for i in @score
+        @str_rep.push(i.to_s)
+      end
+    end
+  end
+end
+
+class ScoreCard
+  attr_accessor :frames, :frame_bonuses
+  def initialize
+    @frames = []
+    @frame_bonuses = []
+  end
+
+  def add_frame(frame)
+    @frames.push(frame)
+  end
+
+  def current_score
+    frames_total = 0
+    @frames.each { |frame| frames_total += frame.score.sum }
+    self.calc_bonuses
+    return frames_total + @frame_bonuses.sum
+  end
+
+  def calc_bonuses
+    for i in 0...@frames.length
+      if @frames[i].score[0] == 10
+        if @frames[i+1] == nil and @frames[i].frame_num != 9
+          return nil
+        elsif @frames[i].frame_num == 9
+          @frame_bonuses.push(@frames[i+1].score[0..1].sum)
+          return nil
+        elsif @frames[i+1].score[0] == 10
+          @frame_bonuses.push(10+@frames[i+2].score[0])
+        else
+          @frame_bonuses.push(@frames[i+1].score.sum)
+        end
+      elsif @frames[i].score.sum == 10
+        frame_bonuses.push(@frames[i+1].score[0])
+      else
+        frame_bonuses.push(0)
+      end 
+    end  
+  end 
+end
---
+++ spec/models_spec.rb
@@ -0,0 +1,112 @@
+require 'models'
+
+RSpec.describe ScoreCard do
+  context "ScoreCard scores a perfect game" do
+    it "returns a score of 300" do
+      frame_1 = Frame.new(1, [10])
+      frame_2 = Frame.new(2, [10])  
+      frame_3 = Frame.new(3, [10])  
+      frame_4 = Frame.new(4, [10])  
+      frame_5 = Frame.new(5, [10])  
+      frame_6 = Frame.new(6, [10])  
+      frame_7 = Frame.new(7, [10])  
+      frame_8 = Frame.new(8, [10])  
+      frame_9 = Frame.new(9, [10])  
+      frame_10 = Frame.new(10, [10,10,10])
+      score_card = ScoreCard.new
+      score_card.add_frame(frame_1)
+      score_card.add_frame(frame_2)
+      score_card.add_frame(frame_3)
+      score_card.add_frame(frame_4)
+      score_card.add_frame(frame_5)
+      score_card.add_frame(frame_6)
+      score_card.add_frame(frame_7)
+      score_card.add_frame(frame_8)
+      score_card.add_frame(frame_9)
+      score_card.add_frame(frame_10)
+      result = score_card.current_score
+      expect(result).to eq 300
+    end
+  end
+  context "ScoreCard scores Maker's example" do
+    it "returns a score of 133" do
+      frame_1 = Frame.new(1, [1, 4])
+      frame_2 = Frame.new(2, [4, 5])  
+      frame_3 = Frame.new(3, [6, 4])  
+      frame_4 = Frame.new(4, [5, 5])  
+      frame_5 = Frame.new(5, [10])  
+      frame_6 = Frame.new(6, [0, 1])  
+      frame_7 = Frame.new(7, [7, 3])  
+      frame_8 = Frame.new(8, [6, 4])  
+      frame_9 = Frame.new(9, [10])  
+      frame_10 = Frame.new(10, [2, 8, 6])
+      score_card = ScoreCard.new
+      score_card.add_frame(frame_1)
+      score_card.add_frame(frame_2)
+      score_card.add_frame(frame_3)
+      score_card.add_frame(frame_4)
+      score_card.add_frame(frame_5)
+      score_card.add_frame(frame_6)
+      score_card.add_frame(frame_7)
+      score_card.add_frame(frame_8)
+      score_card.add_frame(frame_9)
+      score_card.add_frame(frame_10)
+      result = score_card.current_score
+      expect(result).to eq 133
+    end
+  end
+  context "ScoreCard scores SGactive example" do
+    it "returns a score of 187" do
+      frame_1 = Frame.new(1, [10])
+      frame_2 = Frame.new(2, [9, 1])  
+      frame_3 = Frame.new(3, [5, 5])  
+      frame_4 = Frame.new(4, [7, 2])  
+      frame_5 = Frame.new(5, [10])  
+      frame_6 = Frame.new(6, [10])  
+      frame_7 = Frame.new(7, [10])  
+      frame_8 = Frame.new(8, [9, 0])  
+      frame_9 = Frame.new(9, [8, 2])  
+      frame_10 = Frame.new(10, [9, 1, 10])
+      score_card = ScoreCard.new
+      score_card.add_frame(frame_1)
+      score_card.add_frame(frame_2)
+      score_card.add_frame(frame_3)
+      score_card.add_frame(frame_4)
+      score_card.add_frame(frame_5)
+      score_card.add_frame(frame_6)
+      score_card.add_frame(frame_7)
+      score_card.add_frame(frame_8)
+      score_card.add_frame(frame_9)
+      score_card.add_frame(frame_10)
+      result = score_card.current_score
+      expect(result).to eq 187
+    end
+  end
+  context "ScoreCard scores SGactive example with player not making last strike/spare (no bonus)" do
+    it "returns a score of 177" do
+      frame_1 = Frame.new(1, [10])
+      frame_2 = Frame.new(2, [9, 1])  
+      frame_3 = Frame.new(3, [5, 5])  
+      frame_4 = Frame.new(4, [7, 2])  
+      frame_5 = Frame.new(5, [10])  
+      frame_6 = Frame.new(6, [10])  
+      frame_7 = Frame.new(7, [10])  
+      frame_8 = Frame.new(8, [9, 0])  
+      frame_9 = Frame.new(9, [8, 1])  
+      frame_10 = Frame.new(10, [9, 1, 10])
+      score_card = ScoreCard.new
+      score_card.add_frame(frame_1)
+      score_card.add_frame(frame_2)
+      score_card.add_frame(frame_3)
+      score_card.add_frame(frame_4)
+      score_card.add_frame(frame_5)
+      score_card.add_frame(frame_6)
+      score_card.add_frame(frame_7)
+      score_card.add_frame(frame_8)
+      score_card.add_frame(frame_9)
+      score_card.add_frame(frame_10)
+      result = score_card.current_score
+      expect(result).to eq 177
+    end
+  end
+end
