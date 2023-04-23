11f973f3d4de84e6b08fcb724e992662c46f94de
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-04-23 11:24:41 +0100

    File name changes

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,36 +1,19 @@
+require 'frame'
+
 class Scorecard
-  def initialize 
+  def initialize
     @frames = [Frame.new]
     @current_frame = 1
     @total_score = 0
   end
 
-  def add_roll(pins)
-    current_frame.add_roll(pins)
-
-  def add_score(roll_one, roll_two, roll_three = 0)
-    if @frame <= 10
-      if @frame < 10
-        @total_score += roll_one + roll_two
-      else
-        if @frame == 10 && (roll_one == 10 || roll_one + roll_two == 10)
-          @total_score += roll_one + roll_two + roll_three
-        elsif @frame == 10 && (roll_one != 10 || roll_one + roll_two != 10)
-          @total_score += roll_one + roll_two
-        end
-      end
-      @frame += 1
-    end
+  def add_frame_points_to_total_score(frame)
+    @frames << frame
+    @total_score += @frames.last.frame_points
+    @current_frame += 1
   end
 
-  def total_score
-    if @total_score == 0
-      return "Gutter Ball!"
-    elsif
-      @total_score == 300
-      return "Perfect Score!"
-    else
-      return @total_score
-    end
+  def score_so_far
+    return @total_score
   end
 end
