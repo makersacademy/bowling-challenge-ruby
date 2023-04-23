eec90843721989205c510c862c30b5c3b309951c
Quddus Rahman <qrahman115@gmail.com>
2023-04-23 20:20:00 +0100

    refactored scoreboard so format is seperate

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -17,13 +17,16 @@ class Scorecard
     score = 0
     @frames.each_with_index do |frame, index|
       score += frame.total
-      scorecard_str += "FRAME #{index + 1}:"
-      frame.rolls.each_with_index do |roll, r_index|
-        scorecard_str += " ROLL #{r_index + 1}: #{roll.score}"
-      end
-      scorecard_str += " | TOTAL: #{frame.total}"
-      scorecard_str += " | SCORE: #{score}\n"
+      scorecard_str += format_frame(index, frame, score)
     end
     scorecard_str
+  end
+
+  private
+
+  def format_frame(index, frame, score)
+    frame_info = "FRAME #{index + 1}:"
+    rolls_info = frame.rolls.each_with_index.map { |roll, r_index| "ROLL #{r_index + 1}: #{roll.score}" }.join(' ')
+    "#{frame_info} #{rolls_info} | TOTAL: #{frame.total} | SCORE: #{score}\n"
   end
 end
