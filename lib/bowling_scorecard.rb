c65dcb99f94cfdfc81ed81080c04b294831e8415
awdem <wdemontmollin@gmail.com>
2023-05-20 14:30:54 +0100

    TDD implementation of BowlingScorecard#intitialize

--- lib/bowling_scorecard.rb
+++ lib/bowling_scorecard.rb
@@ -1,3 +1,8 @@
+# this class represents a scorecard for a bowling game
 class BowlingScorecard
+  attr_reader :frame_scores
 
+  def initialize
+    @frame_scores = []
+  end
 end
--- spec/bowling_scorecard_spec.rb
+++ spec/bowling_scorecard_spec.rb
@@ -1,5 +1,9 @@
 require 'bowling_scorecard'
 
 RSpec.describe BowlingScorecard do
-  
+  let(:scorecard) { BowlingScorecard.new }
+
+  it 'constructs' do
+    expect(scorecard.frame_scores).to eq []
+  end
 end
