eb3f6476d2910305e9c9addc0fdf57fa3f14efe8
awdem <wdemontmollin@gmail.com>
2023-05-20 13:32:59 +0100

    wrote initial integration test and created class and spec files

---
+++ spec/integration/scoring_integration_spec.rb
@@ -0,0 +1,31 @@
+require 'bowling_game'
+require 'bowling_scorecard'
+
+RSpec.describe "scoring integration" do
+  it 'calculates the score of a regular bowling game with no strikes or spare' do
+    game = BowlingGame.new
+
+    game.roll(3)
+    game.roll(4)
+    game.roll(1)
+    game.roll(7)
+    game.roll(0)
+    game.roll(3)
+    game.roll(2)
+    game.roll(6)
+    game.roll(8)
+    game.roll(0)
+    game.roll(1)
+    game.roll(0)
+    game.roll(0)
+    game.roll(0)
+    game.roll(1)
+    game.roll(5)
+    game.roll(7)
+    game.roll(2)
+    game.roll(4)
+    game.roll(1)
+    
+    expect(game.total_score).to eq 55
+  end
+end
