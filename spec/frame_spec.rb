2eb8f0a4c58cf3acf8b4d208179237a6cbee900e
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-05-21 15:11:31 +0100

    removed rolls method

--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Frame do
   context 'initially' do
     it 'constructs a frame' do
       frame = Frame.new([0, 0])
-      expect(frame.rolls).to eq [0, 0]
       expect(frame.total_score).to eq 0
     end
   end
