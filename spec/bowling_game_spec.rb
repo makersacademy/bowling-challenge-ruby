129c81b6f3bb1ca259b15de206f5d8e1f09d9430
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-05-21 20:59:56 +0100

    passed test for 5 strike game

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -144,5 +144,31 @@ RSpec.describe BowlingGame do
 
       expect(bowling_game.current_total_score).to eq 50
     end
+
+    it 'calculates total with 5 strikes' do
+      bowling_game = BowlingGame.new
+      frame1 = Frame.new([10])
+      frame2 = Frame.new([2, 2])
+      5.times do
+        bowling_game.add_frame(frame1)
+        bowling_game.add_frame(frame2)
+      end
+
+      expect(bowling_game.current_total_score).to eq 90
+    end
+
+    it 'calculates for all strike game' do
+      bowling_game = BowlingGame.new
+      frame1 = Frame.new([10])
+      last_frame = [10, 10, 10]
+      9.times do
+        bowling_game.add_frame(frame1)
+      end
+
+      bowling_game.add_frame(last_frame)
+
+
+      expect(bowling_game.current_total_score).to eq 90
+    end
   end
 end
