da9f6a4e7125bcdeb4cc65c7d38f88a788d94ef4
Sharmine <sharmine.ss@gmail.com>
2023-05-22 00:28:47 +0200

    spares working correctly, only strikes are not yet working

--- lib/scores.rb
+++ lib/scores.rb
@@ -4,33 +4,37 @@ class Scores
     @io = io
     @total_score = 0
     @scoreboard = []
-    @strike = false
-    @spare = false
+    @strike = 1
+    @spare = 1
+    @frame = 1
   end
 
   def roll
     @io.puts 'Enter score:'
     input = @io.gets.chomp.to_i
     @frame_score += input
+    @total_score += input * @spare
     return input
   end
 
   def frame
+    @frame += 1
     @frame_score = 0
     rolls = []
+
     roll_1 = self.roll
     rolls << roll_1
+    @spare = 1
+    
     if @frame_score < 10
       roll_2 = self.roll
       rolls << roll_2
-    else
-      
     end
-    @total_score += @frame_score
 
     score(rolls)
 
     @io.puts "Frame score: #{@frame_score}"
+    bonus(roll_1, roll_2)
   end
 
   def last_frame
@@ -46,20 +50,28 @@ class Scores
 
     if @frame_score >= 10
       roll_3 = self.roll
-
       rolls << roll_3
     end
 
     score(rolls)
 
-    @total_score += @frame_score
     @io.puts "Frame score: #{@frame_score}"
   end
 
   def score(rolls)
-    @frame = [rolls, @total_score]
-    @scoreboard << @frame
+    @scoreboard << rolls
     return @scoreboard
+  end
+
+  def bonus(roll_1, roll_2)
+    if roll_1 == 10
+      @strike = 2
+    elsif roll_1 + roll_2 == 10
+      @spare = 2
+    else
+      @strike = 1
+      @spare = 1
+    end
   end
 
   def total
@@ -74,4 +86,6 @@ class Scores
 end
 
 # game = Scores.new(Kernel)
+# game.frame
+# game.frame
 # game.run
