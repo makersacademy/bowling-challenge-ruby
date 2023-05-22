594969c640fd7bb3cd34964a7401833e97089304
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-05-22 09:58:57 +0100

    using helper method in test suite to simulate games

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -48,23 +48,16 @@ RSpec.describe BowlingGame do
     end
 
     it 'finds total score when 2 frames added' do
-      bowling_game = BowlingGame.new
-      frame1 = Frame.new([1, 2])
-      frame2 = Frame.new([3, 4])
-
-      bowling_game.add_frame(frame1)
-      bowling_game.add_frame(frame2)
+      bowling_game = simulate_game([1, 2], [3, 4])
 
       expect(bowling_game.total_up_to(1)).to eq 10
     end
 
     it 'finds total score when 10 frames added' do
-      bowling_game = BowlingGame.new
-      frame = Frame.new([2, 2])
-
-      10.times do
-        bowling_game.add_frame(frame)
-      end
+      bowling_game = simulate_game(
+        [2, 2], [2, 2], [2, 2], [2, 2], [2, 2],
+        [2, 2], [2, 2], [2, 2], [2, 2], [2, 2]
+      )
 
       expect(bowling_game.total_up_to(9)).to eq 40
     end
@@ -72,48 +65,31 @@ RSpec.describe BowlingGame do
 
   context 'when a game contains spares' do
     it 'does not add the spare frame before next frame played' do
-      bowling_game = BowlingGame.new
-      frame = Frame.new([2, 8])
+      bowling_game = simulate_game([2, 8])
       
-      bowling_game.add_frame(frame)
       expect(bowling_game.total_up_to(0)).to eq nil
     end
 
     it 'adds the spare with bonus when next frame played' do
-      bowling_game = BowlingGame.new
-      frame1 = Frame.new([2, 8])
-      frame2 = Frame.new([1, 1])
-      
-      bowling_game.add_frame(frame1)
-      bowling_game.add_frame(frame2)
+      bowling_game = simulate_game([2, 8], [1, 1])
 
       expect(bowling_game.total_up_to(1)).to eq 13
     end
 
     it 'finds total when spare in middle of game' do
-      bowling_game = BowlingGame.new
-      frame1 = Frame.new([2, 8])
-      frame2 = Frame.new([1, 1])
-      4.times do
-        bowling_game.add_frame(frame2)
-      end
-      bowling_game.add_frame(frame1)
-
-      expect(bowling_game.total_up_to(4)).to eq nil
-      bowling_game.add_frame(frame2)
+      bowling_game = simulate_game(
+        [1, 1], [1, 1], [1, 1], [1, 1], [2, 8], [1, 1]
+      )
 
       expect(bowling_game.total_up_to(4)).to eq 19
       expect(bowling_game.total_up_to(5)).to eq 21
     end
 
     it 'finds total score when game ends on a spare' do
-      bowling_game = BowlingGame.new
-      frame = Frame.new([2, 3])
-      last_frame = Frame.new([5, 5, 2])
-      9.times do
-        bowling_game.add_frame(frame)
-      end
-      bowling_game.add_frame(last_frame)
+      bowling_game = simulate_game(
+        [2, 3], [2, 3], [2, 3], [2, 3], [2, 3],
+        [2, 3], [2, 3], [2, 3], [2, 3], [5, 5, 2]
+      )
 
       expect(bowling_game.total_up_to(9)).to eq 57
     end
@@ -121,59 +97,40 @@ RSpec.describe BowlingGame do
 
   context 'when a game contains strikes' do
     it 'does not add a total when first and only frame is a strike' do
-      bowling_game = BowlingGame.new
-      frame = Frame.new([10])
-      bowling_game.add_frame(frame)
+      bowling_game = simulate_game([10])
 
       expect(bowling_game.total_up_to(0)).to eq nil
     end
 
     it 'calculates total after one strike and one simple frame' do
-      bowling_game = BowlingGame.new
-      frame1 = Frame.new([10])
-      frame2 = Frame.new([2, 2])
-      bowling_game.add_frame(frame1)
-      bowling_game.add_frame(frame2)
+      bowling_game = simulate_game([10], [2, 2])
 
       expect(bowling_game.total_up_to(1)).to eq 18
     end
 
     it 'calculates total when strike in middle of game' do
-      bowling_game = BowlingGame.new
-      frame1 = Frame.new([10])
-      frame2 = Frame.new([2, 2])
-      5.times do
-        bowling_game.add_frame(frame2)
-      end
-      bowling_game.add_frame(frame1)
-      4.times do
-        bowling_game.add_frame(frame2)
-      end
+      bowling_game = simulate_game(
+        [2, 2], [2, 2], [2, 2], [2, 2], [2, 2],
+        [10], [2, 2], [2, 2], [2, 2], [2, 2]
+      )
 
       expect(bowling_game.total_up_to(9)).to eq 50
     end
 
     it 'calculates total with 5 strikes' do
-      bowling_game = BowlingGame.new
-      frame1 = Frame.new([10])
-      frame2 = Frame.new([2, 2])
-      5.times do
-        bowling_game.add_frame(frame1)
-        bowling_game.add_frame(frame2)
-      end
+      bowling_game = simulate_game(
+        [10], [2, 2], [10], [2, 2], [10],
+        [2, 2], [10], [2, 2], [10], [2, 2]
+      )
 
       expect(bowling_game.total_up_to(9)).to eq 90
     end
 
     it 'calculates for all strike game' do
-      bowling_game = BowlingGame.new
-      frame1 = Frame.new([10])
-      last_frame = Frame.new([10, 10, 10])
-      9.times do
-        bowling_game.add_frame(frame1)
-      end
-
-      bowling_game.add_frame(last_frame)
+      bowling_game = simulate_game(
+        [10], [10], [10], [10], [10],
+        [10], [10], [10], [10], [10, 10, 10]
+      )
 
       expect(bowling_game.total_up_to(9)).to eq 300
     end
@@ -219,7 +176,6 @@ RSpec.describe BowlingGame do
       expect(bowling_game.total_up_to(7)).to eq 103
       expect(bowling_game.total_up_to(8)).to eq 123
       expect(bowling_game.total_up_to(9)).to eq 140
-
     end
   end
 end
