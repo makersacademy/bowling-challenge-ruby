3a9e336203216fd26e7499e1c5bb1c9ed1fceda1
wimdavies <william.johnston.davies@gmail.com>
2023-05-25 10:16:20 +0100

    test-drove basic zero return for Gutter Game

--- lib/score_card.rb
+++ lib/score_card.rb
@@ -5,7 +5,11 @@ class ScoreCard
     @frames = []
     # Initialize ten frames?
     # 10.times { @frames << Frame.new }
+    @game_score = 0
   end
 
   # Methods to interact with frames and rolls
+  def game_score
+    return @game_score
+  end
 end
--- spec/score_card_integration_spec.rb
+++ spec/score_card_integration_spec.rb
@@ -2,13 +2,10 @@ require 'frame'
 require 'score_card'
 
 RSpec.describe 'integration' do
-  xit 'returns game score of 0 for a Gutter Game' do
-    # score_card = ScoreCard.new
-
+  it 'returns game score of 0 for a Gutter Game' do
+    score_card = ScoreCard.new
     
-    # result = score_card.game_score
-
-    # expect(result).to eq(0)
+    expect(score_card.game_score).to eq(0)
   end
 
   xit 'returns game score of 300 for a Perfect Game' do
