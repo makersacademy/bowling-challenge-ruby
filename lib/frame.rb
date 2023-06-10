ab6d446f78819ac7f7bba4b0ceb63ab6a62f5156
Sean Peters <sean@snptrs.dev>
2023-06-10 15:03:06 +0100

    Add Frame.score and tests

--- lib/frame.rb
+++ lib/frame.rb
@@ -2,6 +2,8 @@ class Frame
   def initialize(frame_number)
     @frame_number = frame_number
     @ball_scores = [0, 0, 0]
+    @frame_score = 0
+    @bonus_score = 0
   end
 
   def add_ball_score(ball, score)
@@ -14,5 +16,9 @@ class Frame
 
   def frame_number
     return @frame_number
+  end
+
+  def score
+    return @ball_scores.sum + @bonus_score
   end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe Frame do
       expect(frame.get_ball_score(1)).to eq 5
     end
   end
+
+  context ".get_ball_score" do
+    it "returns the score for a given ball in the frame" do
+      frame = Frame.new(1)
+      frame.add_ball_score(2, 3)
+      expect(frame.get_ball_score(2)).to eq 3
+    end
+  end
+
+  context ".score" do
+    it "returns the score for the frame" do
+      frame = Frame.new(1)
+      frame.add_ball_score(1, 4)
+      frame.add_ball_score(2, 3)
+      expect(frame.score).to eq 7
+    end
+  end
 end
