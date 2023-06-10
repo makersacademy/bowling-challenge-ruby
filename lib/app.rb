466dfa5b5fa043164db00a60619ce80a4ccf73db
Sean Peters <sean@snptrs.dev>
2023-06-10 19:11:45 +0100

    Add Application.run

--- lib/app.rb
+++ lib/app.rb
@@ -1,17 +1,29 @@
 require_relative 'frame'
+require_relative 'gameplay'
 
 class Application
   def initialize
-    @frames = []
-    @current_frame = 1
-    @current_frame_ball = 1
+    @frames = {}
+    @gameplay = Gameplay.new
     create_frames
+  end
+
+  def run
+    loopy = true
+    while loopy do
+      @gameplay.score_prompt
+      @gameplay.next_ball
+      loopy = @gameplay.continue?
+    end
+
+    @gameplay.final_score(@frames)
+    return
   end
 
   def create_frames
     10.times do |i|
       frame = Frame.new(i + 1)
-      @frames.push(frame)
+      @frames[i + 1] = frame
     end
   end
 
@@ -19,3 +31,6 @@ class Application
     return @frames
   end
 end
+
+app = Application.new
+app.run
