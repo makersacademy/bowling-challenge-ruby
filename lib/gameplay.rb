bb6235ecf2cc8bef637e078e2f21a98a4235a783
Sean Peters <sean@snptrs.dev>
2023-06-11 14:42:17 +0100

    Add Gameplay.score_prompt, update .continue?

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -1,18 +1,39 @@
+require_relative 'scorecard'
+
 class Gameplay
   attr_accessor :current_frame, :current_ball
 
   def initialize
     @current_frame = 1
     @current_ball = 1
+    @scorecard = ScoreCard.new
   end
 
-  def prompt
-
+  def score_prompt(frames)
+    puts "Enter score for frame #{current_frame}, ball #{current_ball}:"
+    input = gets.chomp
+    input = input.to_i if input != "X" && input != "/"
+    frame = frames[@current_frame]
+    process_current_ball(frame, input)
+    
+  end
+  
+  def process_current_ball(frame, input)
+    if input == "X"
+      frame.strike(@current_ball)
+    elsif input == "/"
+      frame.spare
+    else
+      frame.add_ball_score(@current_ball, input)
+    # binding.irb
+    end
   end
 
-  def next_ball
+  def next_ball(frames)
+    @scorecard.update_pending_bonuses(frames, @current_frame)
+    
     if @current_frame < 10
-      if @current_ball == 1
+      if @current_ball == 1 && frames[@current_frame].strike? != true
         @current_ball = 2
       else
         @current_frame = @current_frame + 1
@@ -23,7 +44,23 @@ class Gameplay
     end
   end
 
-  def continue?
-    return true if @current_frame <= 10 && @current_ball <= 3
+  def continue?(frames)
+    frame = frames[@current_frame]
+    # binding.irb
+    if @current_frame == 10 && @current_ball == 4
+      return false
+    elsif @current_frame == 10 && frame.two_balls? == true && frame.spare? == false && frame.strike? == false
+      return false
+    else
+      return true
+    end
+  end
+
+  def final_score(frames)
+    score = 0
+    frames.values.each do |frame|
+      score = score + frame.total_frame_score
+    end
+    puts "\nYour final score is: #{score}"
   end
 end
